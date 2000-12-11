bea7c4b0e7d218db175e6c795ba1ffe82abfb2bc
wimg <wimg@69d48449-a624-0410-ae76-85ddf3b22ea5>
2000-12-11 12:55:42 +0000

    Fixed a problem with creditstats

--- creditstats.php
+++ creditstats.php
@@ -144,7 +144,7 @@ while ($row_clients = mysql_fetch_array($res_clients))
       <tr>
            <?
            if (isset($lowID) && strlen($lowID) > 0)
-             print "<td><a href=creditstats.php?lowID=$prevlowID><?print $strPrevious;?> 10</a></td>";
+             print "<td><a href=creditstats.php?lowID=$prevlowID>$strPrevious 10</a></td>";
            ?>
        <td align=right><a href=creditstats.php?prevlowID=<?print $lowID."&lowID=".$highID;?>><?print $strNext;?> 10</a></td></tr>
        <tr><td><br></td></tr>
