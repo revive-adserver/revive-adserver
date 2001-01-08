235388b51c2bbc4efa2150f2a72e20b7a42e2499
wimg <wimg@69d48449-a624-0410-ae76-85ddf3b22ea5>
2001-01-08 22:15:47 +0000

    Added some banneracl stuff

--- banneracl.inc.php
+++ banneracl.inc.php
@@ -21,7 +21,7 @@ function acltypeselect($default)
 	<SELECT name="acl_type">
 	<?
 	reset($acl_types);
-	while ( list($acl_type, $acl_name) = each($acl_types) )
+	while ( list($acl_type, $acl_name) = each($acl_types) ) 
 	{
 		?>
 		<OPTION value=<? printf("\"%s\" %s>", $acl_type, $acl_type == $default ? 'selected':''); print ("$acl_name\n"); ?>
@@ -39,7 +39,7 @@ function acladselect($default)
 	<SELECT name="acl_ad">
 	<?
 	reset($aclad_types);
-	while ( list($acl_ad, $acl_name) = each($aclad_types) )
+	while ( list($acl_ad, $acl_name) = each($aclad_types) ) 
 	{
 		?>
 		<OPTION value=<? printf("\"%s\" %s>", $acl_ad, $acl_ad == $default ? 'selected':''); print ("$acl_name\n"); ?>
@@ -50,64 +50,62 @@ function acladselect($default)
 	<?
 }
 
-function showaclrow($row, $total, $update)
+function showaclrow($row, $total, $update) 
 {
-	global $PHP_SELF;
+	global $PHP_SELF, $strSave, $strDelete, $strUp, $strDown;
 	?>
 	<TR>
-	<TD>
-	<FORM action="<?echo basename($PHP_SELF);?>" method="GET">
-	<INPUT type="hidden" name="bannerID" value="<? print $row['bannerID']; ?>">
-	<INPUT type="hidden" name="acl_order" value="<? print $row['acl_order']; ?>">
-	<INPUT type="hidden" name="update" value="<? print $update; ?>">
-	<TABLE width="100%">
-	<TR>
-	<TD>
-	<? acltypeselect(isset($row['acl_type']) ? $row['acl_type'] : ""); ?>
-	</TD>
-	<TD>
-	<? acladselect(isset($row['acl_ad']) ? $row['acl_ad']: ""); ?>
-	</TD>
-	<TD>
-	<INPUT type="text" size="25" name="acl_data" value="<? print isset($row['acl_data']) ? $row['acl_data'] : ""; ?>">
-	</TD>
-	<TD>
-	<?
-	if ($row['acl_order'] && $row['acl_order'] < $total)
-	{
-		?>
-		<INPUT type="submit" name="submit" value="UP">
+		<FORM action="<?echo basename($PHP_SELF);?>" method="GET">
+		<TD>
+			<INPUT type="hidden" name="bannerID" value="<? print $row['bannerID']; ?>">
+			<INPUT type="hidden" name="acl_order" value="<? print $row['acl_order']; ?>">
+			<INPUT type="hidden" name="update" value="<? print $update; ?>">
+			<?
+			acltypeselect(isset($row['acl_type']) ? $row['acl_type'] : ""); 
+			?>
+		</TD>
+		<TD>
 		<?
-	}
-	?>&nbsp;
-	</TD>
-	<TD>
-	<?
-	if ($row['acl_order'] < $total - 1)
-	{
+		acladselect(isset($row['acl_ad']) ? $row['acl_ad']: ""); 
 		?>
-		<INPUT type="submit" name="submit" value="DOWN">
-		<?
-	}
-	?>
-	</TD>
-	<TD>
-	<INPUT type="submit" name="submit" value="Save">
-	</TD>
-	<TD>
-	<?
-	if ($row['acl_order'] < $total)
-	{
+		</TD>
+		<TD>
+			<INPUT type="text" size="25" name="acl_data" value="<? print isset($row['acl_data']) ? $row['acl_data'] : ""; ?>">
+		</TD>
+		<TD>
+		<? 
+		if ($row['acl_order'] && $row['acl_order'] < $total) 
+		{
+			?>
+			<INPUT type="submit" name="submit" value="<?print $strUp?>">
+			<? 
+		} 
 		?>
-		<INPUT type="submit" name="submit" value="Delete">
+		</TD>
+		<TD>
 		<?
-	}
-	?>
-	</TD>
-	</TR>
-	</TABLE>
-	</FORM>
-	</TD>
+		if ($row['acl_order'] < $total - 1) 
+		{
+			?>
+			<INPUT type="submit" name="submit" value="<?print $strDown?>">
+			<?
+		}
+		?>
+		</TD>
+		<TD>
+			<INPUT type="submit" name="submit" value="<?print $strSave?>">
+		</TD>
+		<TD>
+		<?
+		if ($row['acl_order'] < $total) 
+		{
+			?>
+			<INPUT type="submit" name="submit" value="<?print $strDelete?>">
+			<?
+		} 
+		?>
+		</TD>
+		</FORM>
 	</TR>
 	<?
 }
--- banneracl.php
+++ banneracl.php
@@ -6,93 +6,94 @@ require("banneracl.inc.php");
 
 
 kc_auth_admin();
-page_header("$strBannerAdminAcl");
+page_header("$phpAds_name: $strBannerAdminAcl");
 
-if (!empty($bannerID))
+if (!empty($bannerID)) 
 {
 	$Session["bannerID"] = "$bannerID";
 }
 
 // If the form is being submitted, add a new record to banners
-if (isset($submit))
+if (isset($submit)) 
 {
-	if ($submit == "Delete")
+	if ($submit == $strDelete) 
 	{
-		if (!isset($acl_order))
-		{
-			php_die("hu?", "Where is my acl_order? I've lost my acl_order! Moooommmeee... I want my acl_order back!");
-		}
+		if (!isset($acl_order)) 
+			php_die("hu?", "Where is my acl_order? I've lost my acl_orde! Moooommmeee... I want my acl_order back!");
 		$res = mysql_db_query($phpAds_db, "
        			DELETE FROM $phpAds_tbl_acls WHERE
          		bannerID = $bannerID AND acl_order = $acl_order ") or
 			mysql_die();
+		// get banner-acl after the deleted one
 		$res = mysql_db_query($phpAds_db, "
 			SELECT * FROM $phpAds_tbl_acls WHERE
 			bannerID = $bannerID AND acl_order > $acl_order") or
 			mysql_die();
-		while ($row = mysql_fetch_array($res))
+	    // decrement every following acl
+		while ($row = mysql_fetch_array($res)) 
 		{
 			$old_order = $row['acl_order'];
 			$res1 = mysql_db_query($phpAds_db, "
 				UPDATE $phpAds_tbl_acls SET
 				acl_order = acl_order - 1 WHERE
 				acl_order = $old_order
-				AND bannerID = $bannerID
-				") or mysql_die();
+                AND bannerID = $bannerID
+                ") or mysql_die();
 		}
-		show_message("ACL was deleted");
+		show_message("$strACL $strDeleted");
 	}
-	if ($submit == 'Save')
+	if ($submit == $strSave) 
 	{
-		if ($update)
+		if ($update) 
 		{
 			$res = mysql_db_query($phpAds_db, "
 				UPDATE $phpAds_tbl_acls SET
 				acl_type = '$acl_type', acl_data = '$acl_data',
 				acl_ad = '$acl_ad' where bannerID = $bannerID 
 				AND acl_order = $acl_order") or mysql_die();
-			show_message("ACL was updated");
-		}
-		else
+			show_message("$strACL $strUpdated");
+		} 
+		else 
 		{
 			$res = mysql_db_query($phpAds_db, "
 				INSERT into $phpAds_tbl_acls SET
 				acl_order = $acl_order, bannerID = $bannerID,
 				acl_type = '$acl_type', acl_data = '$acl_data',
 				acl_ad = '$acl_ad'") or mysql_die();
-			show_message("ACL was saved");
-
+			show_message("$strACL $strSaved");
 		}
 	}
-	if ($submit == 'UP')
+	if ($submit == $strUp) 
 	{
 		if ($acl_order < 1) 
-			 php_die("oops",
-				"Can't move up first row");
-                $res = mysql_db_query($phpAds_db, "
-                        DELETE FROM $phpAds_tbl_acls WHERE
-                        bannerID = $bannerID AND acl_order = $acl_order ") or
-                        mysql_die();
+			 php_die("oops", $strNoMoveUp);
+        // delete current acl
+		$res = mysql_db_query($phpAds_db, "
+			DELETE FROM $phpAds_tbl_acls WHERE
+			bannerID = $bannerID AND acl_order = $acl_order ") or
+			mysql_die();		
+		// increment previous acl
 		$new_acl_order = $acl_order - 1;
 		$res = mysql_db_query($phpAds_db, "
 			UPDATE $phpAds_tbl_acls SET
 			acl_order = acl_order + 1 WHERE 
-			acl_order = $new_acl_order
+			acl_order = $new_acl_order 
 			AND bannerID = $bannerID
-			") or mysql_die();
-                $res = mysql_db_query($phpAds_db, "
-                        INSERT into $phpAds_tbl_acls SET
-                        acl_order = $new_acl_order, bannerID = $bannerID,
-                        acl_type = '$acl_type', acl_data = '$acl_data',
-                        acl_ad = '$acl_ad'") or mysql_die();
-		show_message("ACL was moved UP");
+			") or mysql_die();	 
+		// insert actual acl with decremented order
+		$res = mysql_db_query($phpAds_db, "
+			INSERT into $phpAds_tbl_acls SET
+			acl_order = $new_acl_order, bannerID = $bannerID,
+			acl_type = '$acl_type', acl_data = '$acl_data',
+			acl_ad = '$acl_ad'") or mysql_die();
+		show_message("$strACL $strMovedUp");
 	}
-	if ($submit == 'DOWN')
+	if ($submit == $strDown) 
 	{
-                $res = mysql_db_query($phpAds_db, "
-                        DELETE FROM $phpAds_tbl_acls WHERE
-                        bannerID = $bannerID AND acl_order = $acl_order ") or
-                        mysql_die();
+		$res = mysql_db_query($phpAds_db, "
+			DELETE FROM $phpAds_tbl_acls WHERE
+			bannerID = $bannerID AND acl_order = $acl_order ") or
+		mysql_die();
 		$new_acl_order = $acl_order + 1;
 		$res = mysql_db_query($phpAds_db, "
 			UPDATE $phpAds_tbl_acls SET
@@ -100,53 +101,58 @@ if (isset($submit))
 			acl_order = $new_acl_order
 			AND bannerID = $bannerID
 			") or mysql_die();
-                $res = mysql_db_query($phpAds_db, "
-                        INSERT into $phpAds_tbl_acls SET
-                        acl_order = $new_acl_order, bannerID = $bannerID,
-                        acl_type = '$acl_type', acl_data = '$acl_data',
-                        acl_ad = '$acl_ad'") or mysql_die();
-		show_message("ACL was moved UP");
+		$res = mysql_db_query($phpAds_db, "
+			INSERT into $phpAds_tbl_acls SET
+			acl_order = $new_acl_order, bannerID = $bannerID,
+			acl_type = '$acl_type', acl_data = '$acl_data',
+			acl_ad = '$acl_ad'") or mysql_die();
+		show_message("$strACL $strMovedDown");
 	}
 }
 // If we find an ID, means that we're in update mode  
-if (isset($bannerID))
-{
-	show_nav("1.3.3");
-	$res = mysql_db_query($phpAds_db, "
-          SELECT
-            *
-          FROM
-            $phpAds_tbl_acls
-          WHERE
-            bannerID = $bannerID ORDER by acl_order
-          ") or mysql_die();
-}
-else
+if (!isset($bannerID)) 
 {
 	php_die("hu?",
 	"Where is my ID? I've lost my ID! Moooommmeee... I want my ID back!");
 }
+
+show_nav("1.3.3");
+$res = mysql_db_query($phpAds_db, "
+	SELECT
+		*
+	FROM
+		$phpAds_tbl_acls
+	WHERE
+		bannerID = $bannerID ORDER by acl_order
+	") or mysql_die();
 ?>
 <TABLE>
 <?
-$data = array();
-while ($row = mysql_fetch_array($res))
+$count = mysql_num_rows($res);
+if ($count > 0)
 {
-	$data[$row['acl_order'].'a'] =  $row; // Trick for sorting
+	?>
+	<TR>
+		<TD COLSPAN="7"><BR><? echo $strACLExist; ?></TD>
+	</TR>
+	<?
+	while ($row = mysql_fetch_array($res))	// acl array already sorted by select
+	{
+		showaclrow($row, $count, 1);
+	}
 }
-ksort($data);
-$count = count($data);
-$i = 0;
-for (reset($data); $key = key($data); next($data))
-{
-	showaclrow($data[$key], $count, 1);
-	$i++;
-}
-$newdata['acl_order'] = $i;
+?>
+<TR>
+	<TD COLSPAN="7"><HR SIZE="1"><? echo $strACLAdd; ?></TD>
+</TR>
+<?
+$newdata['acl_order'] = $count;
 $newdata['bannerID'] = $bannerID;
 showaclrow($newdata, 0, 0);
 ?>
 </TABLE>
-<?
+<?	
+// show acl help file
+@require("banneracl.".$phpAds_language.".inc.php");	// suppress error if file not found
 page_footer();
 ?>
