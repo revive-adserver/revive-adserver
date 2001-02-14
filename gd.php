ec7ee37bccac2e3e7efb7058da726ddf059e1a1b
phord <phord@69d48449-a624-0410-ae76-85ddf3b22ea5>
2001-02-14 00:09:00 +0000

    Changes to support php 4.02+ image type detection

--- gd.php
+++ gd.php
@@ -1,14 +1,38 @@
 <?
 
-// Decide which image format to use
-if (function_exists("imagepng"))
-	$gdimageformat = "png";    
-elseif (function_exists("imagejpeg"))
-	$gdimageformat = "jpeg";    
-elseif (function_exists("imagegif"))
-	$gdimageformat = "gif";    
-else
-	$gdimageformat = "none";
+// Determine php version 
+$phpversion = ereg_replace ("([^0-9])", "", phpversion()); 
+$phpversion = $phpversion / pow (10, strlen($phpversion) - 1); 
+
+if ($phpversion >= 4.02) { 
+    // Use ImageTypes() to dermine image format 
+    if (ImageTypes() & IMG_PNG) 
+        $gdimageformat = "png"; 
+    
+    elseif (ImageTypes() & IMG_JPG) 
+        $gdimageformat = "jpeg"; 
+    
+    elseif (ImageTypes() & IMG_GIF) 
+        $gdimageformat = "gif"; 
+    
+    else 
+        $gdimageformat = "none"; 
+
+} else { 
+    // Use Function_Exists to determine image format 
+    if (function_exists("imagepng")) 
+        $gdimageformat = "png"; 
+    
+    elseif (function_exists("imagejpeg")) 
+        $gdimageformat = "jpeg"; 
+    
+    elseif (function_exists("imagegif")) 
+        $gdimageformat = "gif"; 
+    
+    else 
+        $gdimageformat = "none"; 
+} 
+
 
 function showimage(&$im)
 {
