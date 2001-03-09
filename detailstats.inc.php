5b1bd68c087428cc544e75d40871e888a65c3a7d
rakaz <rakaz@69d48449-a624-0410-ae76-85ddf3b22ea5>
2001-03-09 20:24:55 +0000

    Updated for the compact statistics

--- detailstats.inc.php
+++ detailstats.inc.php
@@ -2,22 +2,47 @@
 
 function stats($what, $totalTitle, $avgTitle)
 {
-	global $phpAds_db, $date_format, $phpAds_url_prefix,$pageid,$fncpageid;
-	$result = db_query("
-		SELECT
-			*,
-			count(*) as qnt,
-			DATE_FORMAT(t_stamp, '$date_format') as t_stamp_f
-		FROM
-			$what
-		WHERE
-			bannerID = $GLOBALS[bannerID]
-		GROUP BY
-			t_stamp_f  
-		ORDER BY
-			t_stamp DESC
-		LIMIT 7           
-		") or mysql_die();
+	global $phpAds_db, $date_format, $phpAds_url_prefix, $pageid, $fncpageid;
+	global $phpAds_compact_stats, $phpAds_tbl_adviews, $phpAds_tbl_adclicks, $phpAds_tbl_adstats;
+
+	if (!$phpAds_compact_stats)
+	{
+		if ($what == "views")
+			$table = $phpAds_tbl_adviews;
+		else
+			$table = $phpAds_tbl_adclicks;
+		
+		$stats_query = " SELECT
+							*,
+							count(*) as qnt,
+							DATE_FORMAT(t_stamp, '$date_format') as t_stamp_f
+				 		 FROM
+							$table
+						 WHERE
+							bannerID = $GLOBALS[bannerID]
+						 GROUP BY
+							t_stamp_f  
+						 ORDER BY
+							t_stamp DESC
+						 LIMIT 7          
+		";
+	}
+	else
+		$stats_query = " SELECT
+							*,
+							$what as qnt,
+							DATE_FORMAT(day, '$date_format') as t_stamp_f
+				 		 FROM
+							$phpAds_tbl_adstats
+						 WHERE
+							bannerID = $GLOBALS[bannerID]
+						 ORDER BY
+							day DESC
+						 LIMIT 7          
+		";
+
+	$result = db_query($stats_query) or mysql_die();
+
 	$max = 0;
 	$total = 0;
 	while ($row = mysql_fetch_array($result))
@@ -45,7 +70,9 @@ function stats($what, $totalTitle, $avgTitle)
 				<b><?echo $row["qnt"];?></b>
 			</td>
 			<td bgcolor="<?echo $bgcolor;?>">
+				<?	if (!$phpAds_compact_stats) { ?>
 				<a href="dailystats.php<?echo ($fncpageid)?>&day=<?echo urlencode($row["t_stamp_f"]);?>&bannerID=<?echo $GLOBALS["bannerID"];?>"><?echo $GLOBALS["strDailyStats"];?></a>
+				<? } ?>				
 			</td>
 		</tr>
 		<?
@@ -76,9 +103,9 @@ function stats($what, $totalTitle, $avgTitle)
 		<table width="100%" cellpadding=3 cellspacing=1 border=0>
 		<?
 			echo "<tr><td colspan=4 bgcolor='#FFFFFF'><b>$strViews:</b></td></tr>";
-			stats("$phpAds_tbl_adviews", $strTotalViews7Days, $strAvgViews7Days);
+			stats("views", $strTotalViews7Days, $strAvgViews7Days);
 			echo "<tr><td colspan=4 bgcolor='#FFFFFF'><b>$strClicks:</b></td></tr>";
-			stats("$phpAds_tbl_adclicks", $strTotalClicks7Days, $strAvgClicks7Days);
+			stats("clicks", $strTotalClicks7Days, $strAvgClicks7Days);
 		?>
 		</table>
 	</td></tr>
