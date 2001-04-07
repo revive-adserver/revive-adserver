dbfcfb4b52a6a7e6f679f9018c2e3478b6b236d8
rakaz <rakaz@69d48449-a624-0410-ae76-85ddf3b22ea5>
2001-04-07 14:33:20 +0000

    Replaced: New GUI

--- index.php
+++ index.php
@@ -1,37 +1,3 @@
 <?
-require ("config.php");
-
-
-phpAds_checkAccess(phpAds_Admin+phpAds_Client);
-
-
-if (phpAds_isUser(phpAds_Admin))
-{
-	Header("Location: $phpAds_url_prefix/admin.php");
-	exit;
-}
-
-if (phpAds_isUser(phpAds_Client))
-{
-	$clientID = $Session[clientID];
-	
-	page_header();
-	show_nav("2");
-	
-	$res_banners = db_query("
-		SELECT
-			banner,
-			bannerID,
-			width,
-			height,
-			format
-		FROM
-			$phpAds_tbl_banners
-		WHERE
-			clientID = $clientID
-		") or mysql_die();
-	
-	require("./stats.inc.php");
-	page_footer();
-}
+Header("Location: admin/index.php");
 ?>
