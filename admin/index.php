57e274a9ad796833f51fc85ecb18ab0595c21960
rakaz <rakaz@69d48449-a624-0410-ae76-85ddf3b22ea5>
2001-04-07 14:26:52 +0000

    New GUI

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,19 @@
 <?
-Header("Location: ../admin.php");
+require ("config.php");
+
+
+phpAds_checkAccess(phpAds_Admin+phpAds_Client);
+
+
+if (phpAds_isUser(phpAds_Admin))
+{
+	Header("Location: $phpAds_url_prefix/admin/admin.php");
+	exit;
+}
+
+if (phpAds_isUser(phpAds_Client))
+{
+	Header("Location: $phpAds_url_prefix/admin/stats-client.php");
+	exit;
+}
 ?>
