6c15a04cbb045a30fa04135cedc217e524640d13
rakaz <rakaz@69d48449-a624-0410-ae76-85ddf3b22ea5>
2001-04-14 19:36:03 +0000

    Bugfix: Empty strings in mail.php fixed Bugfix: Deactivation bug fixed in mail.php Added mailed reports in client language

--- mail.php
+++ mail.php
@@ -2,11 +2,23 @@
 
 require("config.inc.php");
 require("dblib.php");
-
-db_connect();
-
 require("nocache.inc.php");
 
+// Load language strings
+require("language/$phpAds_language.inc.php");
+
+
+if (!get_cfg_var ('safe_mode'))
+{
+	set_time_limit (300);
+	ignore_user_abort(1);
+}
+
+
+// Make database connection
+db_connect();
+
+// Send Query
 $res_clients = db_query("
 	SELECT
 		clientID,
@@ -15,11 +27,17 @@ $res_clients = db_query("
 		email,
 		views,
 		clicks,
-		to_days(expire)-to_days(curdate()) as days,
-		expire
+		expire,
+		UNIX_TIMESTAMP(expire) as expire_st,
+		activate,
+		UNIX_TIMESTAMP(activate) as activate_st,
+		active,
+		language
 	FROM
 		$phpAds_tbl_clients
 	") or die($strLogErrorClients);
+
+
 $i = 0;
 $logs = array();
 $clients = array();
@@ -33,25 +51,19 @@ return strlen($temp)-strlen($string);
 }
 
 
-while($row_clients = mysql_fetch_array($res_clients))
+while($client = mysql_fetch_array($res_clients))
 {
-	$clients[$i] = array();
-	$clients[$i]["clientID"] = $row_clients["clientID"];      
-	$clients[$i]["email"] = $row_clients["email"];
-	$clients[$i]["contact"] = $row_clients["contact"];
-	$clients[$i]["clientname"] = $row_clients["clientname"];      
-	$clients[$i]["views"] = $row_clients["views"];
-	$clients[$i]["clicks"] = $row_clients["clicks"];
-	if (substr_count2($row_clients["expire"],"0") == 8) {
-		$clients[$i]["days"] = 0 ;
-	} else {
-		$clients[$i]["days"] = $row_clients["days"];
-	}
-	$clients[$i]["active"] = false;
-
-	print "Processing ".$clients[$i]["clientname"]."...<BR>\n";
+	// Process this client
+	print "Processing ".$client["clientname"]."...<BR>\n";
 	flush();
     
+	// Load client language strings
+	if ($client["language"] != "")
+		include ("language/".$client["language"].".inc.php");
+	else
+		include ("language/$phpAds_language.inc.php");
+	
+	
 	// Fetch all banners belonging to client   
 	$res_banners = db_query("
 		SELECT
@@ -62,24 +74,22 @@ while($row_clients = mysql_fetch_array($res_clients))
 		FROM
 			$phpAds_tbl_banners
 		WHERE
-			clientID = $row_clients[clientID]
+			clientID = $client[clientID]
 		") or die($strLogErrorBanners);
     
-	$logs[$i] = "";
+	$log = "____________________________________________________________\n";
+	
 	while($row_banners = mysql_fetch_array($res_banners))
 	{
-		if($row_banners["active"] == "true")
-			$clients[$i]["active"] = true;
-       
-		$logs[$i] .= "BannerID: $row_banners[bannerID] [linked to: $row_banners[URL]]\n";
-
+		$log .= "BannerID: $row_banners[bannerID] [linked to: $row_banners[URL]]\n";
+		
 		print "<LI>Processing banner $row_banners[bannerID] [linked to: $row_banners[URL]]...<BR>\n";
 		flush();
-
+		
 		$adviews = db_total_views($row_banners["bannerID"]);
-        $clients[$i]["views_used"] = $adviews;
-		$logs[$i] .= " $strViews (total): $adviews\n";
-
+        $client["views_used"] = $adviews;
+		$log .= " $strViews (total): $adviews\n";
+		
 		// Fetch all adviews belonging to banner belonging to client, grouped by day
 		if ($phpAds_compact_stats)
             $res_adviews = db_query("
@@ -117,13 +127,13 @@ while($row_clients = mysql_fetch_array($res_clients))
     			") or die($strLogErrorViews);
                            
 		while($row_adviews = mysql_fetch_array($res_adviews))
-			$logs[$i] .= "  $row_adviews[t_stamp_f]: $row_adviews[qnt]\n";
+			$log .= "  $row_adviews[t_stamp_f]: $row_adviews[qnt]\n";
         
 		// Total adclicks
 		$adclicks = db_total_clicks($row_banners["bannerID"]);
-		$clients[$i]["clicks_used"] = $adclicks;
-        $logs[$i] .= " $strClicks (total) : $adclicks\n";                  
-
+		$client["clicks_used"] = $adclicks;
+        $log .= " $strClicks (total) : $adclicks\n";                  
+		
 		// Fetch all adclicks belonging to banner belonging to client, grouped by day
 		if ($phpAds_compact_stats)
             $res_adclicks = db_query("
@@ -159,49 +169,64 @@ while($row_clients = mysql_fetch_array($res_clients))
     			LIMIT 7
     			") or die("$strLogErrorClicks ".mysql_error());
 		while($row_adclicks = mysql_fetch_array($res_adclicks))
-			$logs[$i] .= "  $row_adclicks[t_stamp_f]: $row_adclicks[qnt]\n";
-
-		$logs[$i] .= "____________________________________________________________\n";
+			$log .= "  $row_adclicks[t_stamp_f]: $row_adclicks[qnt]\n";
+		
+		$log .= "____________________________________________________________\n";
 	}
-	$i++;
-}
-    
-for($i=0; $i<count($logs); $i++)
-{
-	if($clients[$i]["active"] == false)
-		continue;
-    
-	// Check if the client has remaining adviews
-	if ($clients[$i]["views"] = 0 or $clients[$i]["clicks"] = 0 or $clients[$i]["days"] < 0)
+	
+	
+	
+	$active = "true";
+	
+	if ($client["clicks"] == 0 || $client["views"] == 0)
+		$active = "false";
+	
+	if (time() < $client["activate_st"])
+		$active = "false";
+	
+	if (time() > $client["expire_st"] && $client["expire_st"] != 0)
+		$active = "false";
+	
+	if ($client["active"] != $active)
 	{
-		$client_name = $clients[$i]["clientname"];
-		unset ($client_ID);
-		print "Desactivate ".$client_name."\n\n";
-		$client_ID = $clients[$i]['clientID'];
-		$result = db_query("
-			UPDATE
-				$phpAds_tbl_banners
-			SET
-				active = 'false'
-			WHERE
-				clientID = $client_ID
-			") or mysql_die ("$strLogErrorDisactivate");
-			
-		if ( $email = $clients[$i]["email"] )
+		$client_name = $client["clientname"];
+		$client_ID 	 = $client['clientID'];
+		
+		print "Setting activation to $active $client_name <br><br>";
+		$activateresult = db_query("UPDATE $phpAds_tbl_clients SET active='$active' WHERE clientID=$client_ID") or mysql_die ("$strLogErrorDisactivate");
+		
+		if ($active == "false")
 		{
+			// Email deactivation warning
+			if ($email = $client["email"])
+			{
         		$strMailSubject2 =  $strMailSubjectDeleted.": ".$client_name;
-			eval("\$body = \"$strMailHeader\n$strMailNothingLeft\n\n$strMailFooter\";");
-			mail($clients[$i]["email"], $strMailSubject2, $body,$phpAds_admin_email_headers);
-			unset($strMailSubject2 ) ;
+				$body = "$strMailHeader\n";
+				
+				$body .= $strMailClientDeactivated;
+				if ($client['clicks'] == 0) 			$body .= ", $strNoMoreClicks";
+				if ($client['views'] == 0) 				$body .= ", $strNoMoreViews";
+				if (time() < $client["activate_st"])	$body .= ", $strBeforeActivate";
+				if (time() > $client["expire_st"] && $client["expire_st"] != 0)
+					$body .= ", $strAfterExpire";
+				
+				$body .= ". $strMailNothingLeft\n\n$strMailFooter";
+				mail ($client["email"], $strMailSubject2, $body,$phpAds_admin_email_headers);
+				unset ($strMailSubject2) ;
+			}
 		}
 	}
-	if ( $email = $clients[$i]["email"] )
+	
+	// E-mail Stats to active clients
+	if ($email = $client["email"] && $client["active"] == "true")
 	{
-		eval("\$body = \"$strMailHeader\n$strMailBannerStats\n\n\$logs[$i]\n$strMailFooter\";");
-		$strMailSubject1 =  $strMailSubject.": ".$clients[$i]["clientname"];
-		mail($clients[$i]["email"], $strMailSubject1, $body, $phpAds_admin_email_headers);
-		unset ($strMailSubject1 ) ;
-	}	
+		$strMailSubject1 =  $strMailSubject.": ".$client["clientname"];
+		$body = "$strMailHeader\n$strMailBannerStats\n\n$log\n$strMailFooter";
+		mail ($client["email"], $strMailSubject1, $body, $phpAds_admin_email_headers);
+		unset ($strMailSubject1) ;
+	}
+	
+	$i++;
 }
 
 echo "$strLogMailSent\n";  
