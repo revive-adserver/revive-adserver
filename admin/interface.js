ec804c69b632464a7312bdb281b3715716b76a0f
rakaz <rakaz@69d48449-a624-0410-ae76-85ddf3b22ea5>
2001-04-17 21:21:39 +0000

    External CSS and JavaScript file

---
+++ admin/interface.js
@@ -0,0 +1,75 @@
+// $Revision$
+
+/************************************************************************/
+/* phpAdsNew 2                                                          */
+/* ===========                                                          */
+/*                                                                      */
+/* Copyright (c) 2001 by the phpAdsNew developers                       */
+/* http://sourceforge.net/projects/phpadsnew                            */
+/*                                                                      */
+/* This program is free software. You can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation; either version 2 of the License.       */
+/************************************************************************/
+
+
+
+/*********************************************************/
+/* General functions                                     */
+/*********************************************************/
+
+function findObj(n, d) { 
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+  d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && document.getElementById) x=document.getElementById(n); return x;
+}
+
+
+
+/*********************************************************/
+/* Confirm form submit                                   */
+/*********************************************************/
+
+function confirm_submit(o, str)
+{
+	f = findObj(o);
+	if(confirm(str)) f.submit();
+}
+
+
+
+/*********************************************************/
+/* Disable checkbox                                      */
+/*********************************************************/
+
+function disable_checkbox(o)
+{
+	c = findObj(o);
+	if(c.checked = true) c.checked = false;
+}
+
+
+
+/*********************************************************/
+/* Disable checkbox                                      */
+/*********************************************************/
+
+function click_checkbox(oc,oe)
+{
+	e = findObj(oe);
+	c = findObj(oc);
+	
+	if (c.checked == true) 
+	{
+		e.value = "-";
+	} 
+	else 
+	{
+		e.value = "";
+		e.focus();
+	}
+
+}
+
