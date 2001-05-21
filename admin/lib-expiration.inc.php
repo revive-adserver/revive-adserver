4e88fbb2d192d969fb4e59893778bd9f3fa495eb
rakaz <rakaz@69d48449-a624-0410-ae76-85ddf3b22ea5>
2001-05-21 20:45:42 +0000

    Bugfix: rewrote days_left() function

--- admin/lib-expiration.inc.php
+++ admin/lib-expiration.inc.php
@@ -82,8 +82,6 @@ function adclicks_left ($clientID)
 /* Estimate time before expiration                       */
 /*********************************************************/
 /*                                                       */
-/* Copyright (c) 2001 by Martin Braun <martin@braun.cc>  */
-/*                                                       */
 /* this function calculates the estimated end of a       */
 /* clients credits in clicks or views based on used      */
 /* views and clicks, the time from the first to the last */
@@ -118,165 +116,215 @@ function days_left($clientID)
 	$description="";
 	$absolute=0;
 	
-	// get client record
+	
+	
+	// Get client record
 	$client_query="
 		SELECT
 			views,
-			clicks,                                     
-			DATE_FORMAT(expire, '".$date_format."') as expire,
+			clicks,
+			expire,
+			DATE_FORMAT(expire, '".$date_format."') as expire_f,
 			TO_DAYS(expire)-TO_DAYS(NOW()) as days_left
 		FROM
 			$phpAds_tbl_clients
 		WHERE 
 			clientID = ".$clientID;
 	$res_client = db_query($client_query) or mysql_die() ;
-	if ( mysql_num_rows($res_client)==1)
+	
+	
+	if (mysql_num_rows ($res_client) == 1)
 	{
 		$row_client = mysql_fetch_array($res_client);
-
-		$rawexpire = empty($row_client["expire"]) ? "0000-00-00" : $row_client["expire"];
-	
-		// handle things when client works with credit days
-		if ( !empty($row_client["expire"]) )
+		
+		// Check if the expiration date is set
+		if ($row_client['expire'] != '0000-00-00' && $row_client['expire'] != '')
 		{
-			$days_left = sprintf("%d",$row_client["days_left"]);
-			$estimated_end = $row_client["expire"];
-			$absolute = 1;
-  		}
-		else // client has (had) credits in views/clicks
+			$expiration[] = array (
+				"days_left" => round($row_client["days_left"]),
+				"date"	  	=> $row_client["expire_f"],
+				"absolute"  => true
+			);
+		}
+		
+		if ($row_client["views"] != -1)
 		{
-            // Note: only one of these queries will run.  It will not get the
-            // true total if some are in verbose stats format and some are in compact
-			
-            // get views
-            $view_query="
-				SELECT
-					count(*) as total_views,
-					MAX(TO_DAYS(v.t_stamp))-TO_DAYS(NOW()) as days_since_last_view,
-					TO_DAYS(NOW())-MIN(TO_DAYS(v.t_stamp)) as days_since_start
-				FROM
-					$phpAds_tbl_adviews AS v, 
-					$phpAds_tbl_banners AS b 
-				WHERE
-					b.clientID = $clientID 
-					AND
-					b.bannerID = v.bannerID";
-			
-			if ($phpAds_compact_stats) {
-            	$view_query="
-                	SELECT
-                    	SUM(views) as total_views,
-                        MAX(TO_DAYS(day))-TO_DAYS(NOW()) as days_since_last_view,
-                        TO_DAYS(NOW())-MIN(TO_DAYS(day)) as days_since_start
-                    FROM
-                    	$phpAds_tbl_adstats AS v
-                        LEFT JOIN $phpAds_tbl_banners AS b USING (bannerID)
-                    WHERE
-                    	b.clientID = $clientID";
+			if ($phpAds_compact_stats) 
+			{
+	           	$view_query="
+	               	SELECT
+	                   	SUM(views) as total_views,
+	                    MAX(TO_DAYS(day))-TO_DAYS(NOW()) as days_since_last_view,
+	                    TO_DAYS(NOW())-MIN(TO_DAYS(day)) as days_since_start
+	                FROM
+	                   	$phpAds_tbl_adstats AS v
+	                    LEFT JOIN $phpAds_tbl_banners AS b USING (bannerID)
+	                WHERE
+	                  	b.clientID = $clientID";
             }
+			else
+			{
+	            $view_query="
+					SELECT
+						count(*) as total_views,
+						MAX(TO_DAYS(v.t_stamp))-TO_DAYS(NOW()) as days_since_last_view,
+						TO_DAYS(NOW())-MIN(TO_DAYS(v.t_stamp)) as days_since_start
+					FROM
+						$phpAds_tbl_adviews AS v, 
+						$phpAds_tbl_banners AS b 
+					WHERE
+						b.clientID = $clientID 
+						AND
+						b.bannerID = v.bannerID";
+			}
 			
-			$res_views = db_query($view_query) or mysql_die() ;
-			if ( mysql_num_rows($res_views)==1 )
+			$res_views = db_query($view_query) or mysql_die();
+			if (mysql_num_rows ($res_views) == 1)
 			{
 				$row_views = mysql_fetch_array($res_views);
-				// calculate estimated end of views
-				if (empty($row_views["days_since_start"]) || $row_views["days_since_start"] == 0 || $row_views["days_since_start"] == null )
+				
+				if ($row_views["days_since_start"] == '' ||
+				    $row_views["days_since_start"] == 0  ||
+					$row_views["days_since_start"] == null)
 				{
-					// to avoid division by zero
-					$row_views["days_since_start"]=1;
+					$row_views["days_since_start"] = 1;
 				}
-				if ( !empty($row_views["total_views"]) && $row_views["total_views"] > 0 )
+				
+				if (!empty ($row_views["total_views"]) && $row_views["total_views"] > 0)
 				{
-					$days_left = sprintf("%d",$row_client["views"]/($row_views["total_views"]/$row_views["days_since_start"]));
-					// get clicks
-					$click_query="
-						SELECT
-							count(*) as total_clicks,
-							MAX(TO_DAYS(c.t_stamp))- TO_DAYS(NOW()) as days_since_last_click,
-							TO_DAYS(NOW())-MIN(TO_DAYS(c.t_stamp)) as days_since_start
-						FROM
-							$phpAds_tbl_adclicks AS c, 
-							$phpAds_tbl_banners AS b 
-						WHERE 
-							b.clientID = $clientID
-							AND
-							b.bannerID = c.bannerID";
+					$days_left = round ($row_client["views"] / ($row_views["total_views"] / $row_views["days_since_start"]));
 					
-                    if ($phpAds_compact_stats) 
-                    {
-                    	$view_query="
-                        	SELECT
-                            	SUM(clicks) as total_clicks,
-                                MAX(TO_DAYS(day))-TO_DAYS(NOW()) as days_since_last_click,
-                                TO_DAYS(NOW())-MIN(TO_DAYS(day)) as days_since_start
-                            FROM
-                            	$phpAds_tbl_adstats
-                                LEFT JOIN $phpAds_tbl_banners USING (bannerID)
-                            WHERE
-                            	clientID = $clientID AND
-                                clicks > 0";
-                    }
-					
-					$res_clicks = db_query($click_query) or mysql_die() ;
-					if ( mysql_num_rows($res_clicks)==1)
+					if ($row_client["views"] > 0)
 					{
-						$row_clicks = mysql_fetch_array($res_clicks);
-						// all credits finished already?
-						if ( $row_client["views"]==0 && $row_client["clicks"]==0  )
-						{
-							$days_left = $row_views["days_since_last_view"]>$row_clicks["days_since_last_click"]?$row_views["days_since_last_view"]:$row_clicks["days_since_last_click"];
-							$absolute = 1;
-						}
-						else
-						{
-							// calculate estimated end of clicks
-							if ( $row_clicks["total_clicks"] > 0 && $row_clicks["days_since_start"] > 0 )
-							{
-								$click_days_left = $row_client["clicks"]/($row_clicks["total_clicks"]/$row_clicks["days_since_start"]);
-								$days_left = sprintf("%d",max(strval($days_left),$click_days_left));
-							}
-						}
+						$estimated_end = strftime ($date_format, mktime (0, 0, 0, date("m"), date("d") + $days_left, date("Y")));
+						$expiration[] = array (
+							"days_left" => $days_left,
+							"date"	  	=> $estimated_end,
+							"absolute"  => false
+						);
 					}
-					$estimated_end = strftime($date_format,mktime(0,0,0,date("m"),date("d")+$days_left,date("Y")));
+					else
+					{
+						$estimated_end = strftime ($date_format, mktime (0, 0, 0, date("m"), date("d") - $row_views["days_since_last_view"], date("Y")));
+						$expiration[] = array (
+							"days_left" => 0 - $row_views["days_since_last_view"],
+							"date"	  	=> $estimated_end,
+							"absolute"  => true
+						);
+					}
+				}
+			}
+		}
+		
+		if ($row_client["clicks"] != -1)
+		{
+			if ($phpAds_compact_stats) 
+            {
+            	$click_query="
+                	SELECT
+                    	SUM(clicks) as total_clicks,
+                        MAX(TO_DAYS(day))-TO_DAYS(NOW()) as days_since_last_click,
+                        TO_DAYS(NOW())-MIN(TO_DAYS(day)) as days_since_start
+					FROM
+						$phpAds_tbl_adstats
+						LEFT JOIN $phpAds_tbl_banners USING (bannerID)
+					WHERE
+						clientID = $clientID AND
+						clicks > 0";
+			}
+			else
+			{
+				$click_query="
+					SELECT
+						count(*) as total_clicks,
+						MAX(TO_DAYS(c.t_stamp))- TO_DAYS(NOW()) as days_since_last_click,
+						TO_DAYS(NOW())-MIN(TO_DAYS(c.t_stamp)) as days_since_start
+					FROM
+						$phpAds_tbl_adclicks AS c, 
+						$phpAds_tbl_banners AS b 
+					WHERE 
+						b.clientID = $clientID AND
+						b.bannerID = c.bannerID";
+			}
+			
+			$res_clicks = db_query($click_query) or mysql_die();
+			if (mysql_num_rows($res_clicks) == 1)
+			{
+				$row_clicks = mysql_fetch_array($res_clicks);
+				
+				if ($row_clicks["days_since_start"] == '' ||
+				    $row_clicks["days_since_start"] == 0  ||
+					$row_clicks["days_since_start"] == null)
+				{
+					$row_clicks["days_since_start"] = 1;
+				}
+				
+				if (!empty ($row_clicks["total_clicks"]) && $row_clicks["total_clicks"] > 0)
+				{
+					$days_left = round($row_client["clicks"] / ($row_clicks["total_clicks"] / $row_clicks["days_since_start"]));
+					
+					if ($row_client["views"] > 0)
+					{
+						$estimated_end = strftime ($date_format, mktime (0, 0, 0, date("m"), date("d") + $days_left, date("Y")));
+						$expiration[] = array (
+							"days_left" => $days_left,
+							"date"	  	=> $estimated_end,
+							"absolute"  => false
+						);
+					}
+					else
+					{
+						$estimated_end = strftime ($date_format, mktime (0, 0, 0, date("m"), date("d") - $row_clicks["days_since_last_view"], date("Y")));
+						$expiration[] = array (
+							"days_left" => 0 - $row_clicks["days_since_last_view"],
+							"date"	  	=> $estimated_end,
+							"absolute"  => true
+						);
+					}
 				}
 			}
 		}
 	}
 	
-	// build return value
-	$ret_val = array();
-	$days_left = $days_left>0?$days_left:0;
-	if (my_substr_count($rawexpire,"0")==8)
+	
+	
+	
+	// Build Return value
+	
+	if (isset($expiration) && sizeof($expiration) > 0)
 	{
-		$ret_val[] = $strExpiration.": ".$strNoExpiration;
-		$ret_val[] = '';
-		$ret_val[] = '';
-	} else
-	{
-		if ( $absolute )
+		$sooner = & $expiration[0];
+		
+		for ($i=0;$i<sizeof($expiration);$i++)
 		{
-			$ret_val[] = $strExpiration.": ".$estimated_end." (".$strDaysLeft.": ".$days_left.")";
+			if ($expiration[$i]['days_left'] < $sooner['days_left'])
+				$sooner = & $expiration[$i];
+		}
+		
+		if ($sooner['days_left'] < 0) $sooner['days_left'] = 0;
+		
+		if ($sooner['absolute'])
+		{
+			$ret_val[] = $strExpiration.": ".$sooner['date']." (".$strDaysLeft.": ".$sooner['days_left'].")";
 		}
 		else
 		{
-			$ret_val[] = $strEstimated.": ".$estimated_end." (".$strDaysLeft.": ".$days_left.")";
+			$ret_val[] = $strEstimated.": ".$sooner['date']." (".$strDaysLeft.": ".$sooner['days_left'].")";
 		}
-		$ret_val[]=$estimated_end;
-		$ret_val[]=$days_left;
+		
+		$ret_val[]=$sooner['date'];
+		$ret_val[]=$sooner['days_left'];
 	}
+	else
+	{
+		// Unknown
+		$ret_val[] = $strExpiration.": ".$strNoExpiration;
+		$ret_val[] = '';
+		$ret_val[] = '';
+	}
+	
 	return $ret_val;
 }
 
-
-
-/*********************************************************/
-/* PHP3 replacement for substr_count()                   */
-/*********************************************************/
-
-function my_substr_count($string,$search) 
-{ 
-    $temp = str_replace($search,$search."a",$string); 
-    return strlen($temp)-strlen($string); 
-} 
 
 ?>
