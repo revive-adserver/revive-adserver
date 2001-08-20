dab9ac7984a5926d659096df171680b043d600d1
rakaz <rakaz@69d48449-a624-0410-ae76-85ddf3b22ea5>
2001-08-20 22:23:20 +0000

    Some changes to the translations

--- language/banneracl.norwegian.inc.php
+++ language/banneracl.norwegian.inc.php
@@ -28,7 +28,7 @@
 				</tr>
 				<tr>
 					<td bgcolor="#FFFFFF">Domene</td>
-					<td bgcolor="#FFFFFF">Domene  suffix (f.eks. .jp, .edu, eller google.com)</td>
+					<td bgcolor="#FFFFFF">Domene suffix (f.eks. .jp, .edu, eller google.com)</td>
 					<td bgcolor="#FFFFFF">Vis banner bare til spesifikt domene.</td>
 				</tr>
 				<tr>
@@ -51,14 +51,13 @@
 	<li>Ukedag (0-6), <? echo $strAllow; ?>, argument 0 (for Søndag)</li>
     <li>Ukedag (0-6), <? echo $strDeny; ?>, argument * (for hvilken som helst dag)</li>
 </ul>
-Legg merke til at den siste posten trenger ikke å være &quot;Ukedag&quot;. Hvilken som 
-helst <? echo $strDeny; ?> * ACL vil være nok til å hindre banner hvis en tilordnet 
-<? echo $strAllow; ?> ikke allerede er matchet.
+Legg merke til at den siste posten trenger ikke å være &quot;Ukedag&quot;. Hvilken som helst <? echo $strDeny; ?> *
+ACL vil være nok til å hindre banner hvis en tilordnet <? echo $strAllow; ?> ikke allerede er matchet.
 
 <p>For å vise et banner mellom kl 17 og 20:</p>
 <ul>
     <li>Tid, <? echo $strAllow; ?>, argument 17</li>  (17:00 - 17:59)
-    <li>Tid, <? echo $strAllow; ?>, argument 18</li>  (18:00 - 18:59)
+    <li>Tid, <? echo $strAllow; ?>, argument 18</li>  (18:00pm - 18:59)
 	<li>Tid, <? echo $strAllow; ?>, argument 19</li>  (19:00 - 19:59)
     <li>Tid, <? echo $strDeny; ?>, argument * (for hvilken som helst tid)</li>
 </ul>
