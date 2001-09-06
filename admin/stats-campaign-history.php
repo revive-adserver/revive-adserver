f79b0da96e45c0d339461f2d4205dd709e7ad6dd
rakaz <rakaz@69d48449-a624-0410-ae76-85ddf3b22ea5>
2001-09-06 21:51:41 +0000

    Moved script and two includes to two different scripts

--- admin/stats-campaign-history.php
+++ admin/stats-campaign-history.php
@@ -14,6 +14,99 @@
 
 
 
+// Include required files
+require ("config.php");
+require ("lib-statistics.inc.php");
+require ("lib-expiration.inc.php");
+require ("lib-gd.inc.php");
+
+
+// Security check
+phpAds_checkAccess(phpAds_Admin+phpAds_Client);
+
+
+
+/*********************************************************/
+/* HTML framework                                        */
+/*********************************************************/
+
+if (phpAds_isUser(phpAds_Client))
+{
+	if (phpAds_clientID() == phpAds_getParentID ($campaignID))
+	{
+		$extra = '';
+		
+		$res = db_query("
+		SELECT
+			*
+		FROM
+			$phpAds_tbl_clients
+		WHERE
+			parent = ".$Session["clientID"]."
+		") or mysql_die();
+		
+		while ($row = mysql_fetch_array($res))
+		{
+			if ($campaignID == $row['clientID'])
+				$extra .= "&nbsp;&nbsp;&nbsp;<img src='images/box-1.gif'>&nbsp;";
+			else
+				$extra .= "&nbsp;&nbsp;&nbsp;<img src='images/box-0.gif'>&nbsp;";
+			
+			$extra .= "<a href=stats-campaign.php?campaignID=".$row['clientID'].">".phpAds_buildClientName ($row['clientID'], $row['clientname'])."</a>";
+			$extra .= "<br>"; 
+		}
+		$extra .= "<img src='images/break.gif' height='1' width='160' vspace='4'><br>";
+		
+		phpAds_PageHeader("1.1", $extra);
+	}
+	else
+	{
+		phpAds_PageHeader("1");
+		php_die ($strAccessDenied, $strNotAdmin);	
+	}
+}
+
+if (phpAds_isUser(phpAds_Admin))
+{
+	$extra = '';
+	
+	$res = db_query("
+	SELECT
+		*
+	FROM
+		$phpAds_tbl_clients
+	WHERE
+		parent > 0
+	") or mysql_die();
+	
+	while ($row = mysql_fetch_array($res))
+	{
+		if ($campaignID == $row['clientID'])
+			$extra .= "&nbsp;&nbsp;&nbsp;<img src='images/box-1.gif'>&nbsp;";
+		else
+			$extra .= "&nbsp;&nbsp;&nbsp;<img src='images/box-0.gif'>&nbsp;";
+		
+		$extra .= "<a href=stats-campaign.php?section=$section&campaignID=".$row['clientID'].">".phpAds_buildClientName ($row['clientID'], $row['clientname'])."</a>";
+		$extra .= "<br>"; 
+	}
+	$extra .= "<img src='images/break.gif' height='1' width='160' vspace='4'><br>";
+	
+	$extra .= "<br><br><br><br><br>";
+	$extra .= "<b>$strShortcuts</b><br>";
+	$extra .= "<img src='images/break.gif' height='1' width='160' vspace='4'><br>";
+	$extra .= "<img src='images/icon-client.gif' align='absmiddle'>&nbsp;<a href=client-edit.php?clientID=".phpAds_getParentID ($campaignID).">$strModifyClient</a><br>";
+	$extra .= "<img src='images/break.gif' height='1' width='160' vspace='4'><br>";
+	$extra .= "<img src='images/icon-edit.gif' align='absmiddle'>&nbsp;<a href=campaign-edit.php?campaignID=$campaignID>$strModifyCampaign</a><br>";
+	$extra .= "<img src='images/break.gif' height='1' width='160' vspace='4'><br>";
+	$extra .= "<img src='images/icon-campaign.gif' align='absmiddle'>&nbsp;<a href=campaign-index.php?campaignID=$campaignID>$strBanners</a><br>";
+	$extra .= "<img src='images/break.gif' height='1' width='160' vspace='4'><br>";
+	
+	phpAds_PageHeader("2.1.3", $extra);
+	phpAds_ShowSections(array("2.1.2", "2.1.3", "2.1.4"));
+}
+
+
+
 /*********************************************************/
 /* Main code                                             */
 /*********************************************************/
@@ -205,11 +298,10 @@ if ($totalviews > 0 || $totalclicks > 0)
 }
 
 echo "<tr>";
-echo "<form action='stats-campaign.php'>";
+echo "<form action='stats-campaign-history.php'>";
 echo "<td height='35' colspan='4' align='right'>";
 	echo $strHistory.":&nbsp;&nbsp;";
 	echo "<input type='hidden' name='campaignID' value='$campaignID'>";
-	echo "<input type='hidden' name='section' value='$section'>";
 	echo "<select name='limit' onChange=\"this.form.submit();\">";
 	echo "<option value='7' ".($limit==7?'selected':'').">7 ".$strDays."</option>";
 	echo "<option value='14' ".($limit==14?'selected':'').">14 ".$strDays."</option>";
@@ -239,3 +331,12 @@ if (phpAds_isUser(phpAds_Admin))
 	
 	echo "</table>";
 }
+
+
+/*********************************************************/
+/* HTML framework                                        */
+/*********************************************************/
+
+phpAds_PageFooter();
+
+?>
