0cb3530cb36895e2513d93a86a97de579a8d37b1
rakaz <rakaz@69d48449-a624-0410-ae76-85ddf3b22ea5>
2001-09-09 14:35:07 +0000

    Added support for rebuilding the whole cache

--- admin/lib-zones.inc.php
+++ admin/lib-zones.inc.php
@@ -24,64 +24,81 @@ define ("phpAds_ZoneRaw", 2);
 /* Rebuild zonecache                                     */
 /*********************************************************/
 
-function phpAds_RebuildZoneCache ($zoneid)
+function phpAds_RebuildZoneCache ($zoneid = '')
 {
 	global $phpAds_tbl_zones, $phpAds_zone_cache;
 	
-	// Get zone
-	$zoneres = @db_query("SELECT * FROM $phpAds_tbl_zones WHERE zoneid='$zoneid' ");
-	
-	if (@mysql_num_rows($zoneres) > 0)
+	if ($zoneid == '')
 	{
-		$zone = mysql_fetch_array($zoneres);
+		$res_zones = db_query("
+			SELECT 
+				*
+			FROM 
+				$phpAds_tbl_zones
+			") or mysql_die();
 		
-		// Set what parameter to zone settings
-		if (isset($zone['what']) && $zone['what'] != '')
-			$what = $zone['what'];
+		while ($row_zones = mysql_fetch_array($res_zones))
+		{
+			phpAds_RebuildZoneCache ($row_zones['zoneid']);
+		}
+	}
+	else
+	{
+		// Get zone
+		$zoneres = @db_query("SELECT * FROM $phpAds_tbl_zones WHERE zoneid='$zoneid' ");
+		
+		if (@mysql_num_rows($zoneres) > 0)
+		{
+			$zone = mysql_fetch_array($zoneres);
+			
+			// Set what parameter to zone settings
+			if (isset($zone['what']) && $zone['what'] != '')
+				$what = $zone['what'];
+			else
+				$what = '';
+		}
 		else
 			$what = '';
-	}
-	else
-		$what = '';
-	
-	
-	if ($phpAds_zone_cache)
-	{
-		// Get banners
-		$select = phpAds_buildQuery ($what, 1, '');
-		$res    = @db_query($select);
 		
-		// Build array for further processing...
-		$rows = array();
-		$weightsum = 0;
-		while ($tmprow = @mysql_fetch_array($res))
+		
+		if ($phpAds_zone_cache)
 		{
-			// weight of 0 disables the banner
-			if ($tmprow['weight'])
+			// Get banners
+			$select = phpAds_buildQuery ($what, 1, '');
+			$res    = @db_query($select);
+			
+			// Build array for further processing...
+			$rows = array();
+			$weightsum = 0;
+			while ($tmprow = @mysql_fetch_array($res))
 			{
-				if ($tmprow['format'] == 'gif' ||
-					$tmprow['format'] == 'jpeg' ||
-					$tmprow['format'] == 'png' ||
-					$tmprow['format'] == 'swf')
+				// weight of 0 disables the banner
+				if ($tmprow['weight'])
 				{
-					$tmprow['banner'] = '';
+					if ($tmprow['format'] == 'gif' ||
+						$tmprow['format'] == 'jpeg' ||
+						$tmprow['format'] == 'png' ||
+						$tmprow['format'] == 'swf')
+					{
+						$tmprow['banner'] = '';
+					}
+					
+					$weightsum += ($tmprow['weight'] * $tmprow['clientweight']);
+					$rows[] = $tmprow; 
 				}
-				
-				$weightsum += ($tmprow['weight'] * $tmprow['clientweight']);
-				$rows[] = $tmprow; 
 			}
+			
+			$cachecontents = addslashes (serialize (array ($weightsum, $rows)));
+			$cachetimestamp = time();
+		}
+		else
+		{
+			$cachecontents = '';
+			$cachetimestamp = 0;
 		}
 		
-		$cachecontents = addslashes (serialize (array ($weightsum, $rows)));
-		$cachetimestamp = time();
+		@db_query("UPDATE $phpAds_tbl_zones SET cachecontents='$cachecontents', cachetimestamp=$cachetimestamp WHERE zoneid='$zoneid' ");
 	}
-	else
-	{
-		$cachecontents = '';
-		$cachetimestamp = 0;
-	}
-	
-	@db_query("UPDATE $phpAds_tbl_zones SET cachecontents='$cachecontents', cachetimestamp=$cachetimestamp WHERE zoneid='$zoneid' ");
 }
 
 
