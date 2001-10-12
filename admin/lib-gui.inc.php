2cde5e3ea7b6247d43be34e675f38641155a815f
rakaz <rakaz@69d48449-a624-0410-ae76-85ddf3b22ea5>
2001-10-12 18:39:48 +0000

    Prepare for Beta 6

--- admin/lib-gui.inc.php
+++ admin/lib-gui.inc.php
@@ -197,7 +197,7 @@ function phpAds_PageHeader($ID, $extra="")
 	else
 	{
 		echo "<td height='48' bgcolor='#000063' valign='bottom'>";
-		echo "&nbsp;&nbsp;&nbsp;&nbsp;<img src='images/logo.gif' width='163' height='34' alt='phpAdsNew 2 beta 5'>";
+		echo "&nbsp;&nbsp;&nbsp;&nbsp;<img src='images/logo.gif' width='163' height='34' alt='phpAdsNew 2 beta 6'>";
 	}
 ?>
 	</td>
