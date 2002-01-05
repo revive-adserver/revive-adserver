e8c837c1a32d8ad4cec0d29a9789c35001ffe4c1
rakaz <rakaz@69d48449-a624-0410-ae76-85ddf3b22ea5>
2002-01-05 17:38:06 +0000

    Made AdView compatible with split tables

--- adview.php
+++ adview.php
@@ -33,107 +33,81 @@ include ("lib-cache.inc.php");
 // Open a connection to the database
 phpAds_dbConnect();
 
-if (isset($clientID) && !isset($clientid))	$clientid = $clientID;
-if (isset($bannerID) && !isset($bannerid))	$bannerid = $bannerID;
 
-if (isset($bannerid) && !isset($what))
+if (isset($clientID) && !isset($clientid))	
+	$clientid = $clientID;
+
+if (!isset($clientid))
+	$clientid = 0;
+
+if (!isset($what))
+	$what = '';
+
+if (!isset($source))
+	$source = '';
+
+
+
+$row = phpAds_fetchBanner($what, $clientid, 0, $source, false);
+
+if (is_array($row) && isset($row['bannerid']))
 {
-	$res = phpAds_dbQuery("
-		SELECT
-			b.contenttype, i.contents
-		FROM
-			".$phpAds_config['tbl_banners']." AS b,
-			".$phpAds_config['tbl_images']." AS i
-		WHERE
-			b.bannerid = $bannerid AND
-			b.filename = i.filename AND
-			b.storagetype = 'sql'
-		");
+	// Log this impression
+	phpAds_prepareLog ($row["bannerid"], $row["clientid"], $row["zoneid"]);
 	
-	if(phpAds_dbNumRows($res) == 0)
+	
+	// Send P3P Headers
+	if ($phpAds_config['p3p_policies'])
 	{
-		if ($phpAds_config['default_banner_url'] != "")
-		{
-			Header("Location: ".$phpAds_config['default_banner_url']);
-		}
-	}
-	else
-	{
-		$row = phpAds_dbFetchArray($res);
+		$p3p_header = '';
 		
-		if ($row['contenttype'] == 'swf')
-		{
-			Header("Content-type: application/x-shockwave-flash; name=".microtime()."\n");
-			echo $row['contents'];
-		}
-		else
-		{
-			Header("Content-type: image/".$row['contenttype']."; name=".microtime());
-			echo $row['contents'];
-		}
+		if (isset($phpAds_config['p3p_policy_location']) && 
+		    $phpAds_config['p3p_policy_location'] != '')
+			$p3p_header .= " policyref=\"".$phpAds_config['p3p_policy_location']."\"";
+		
+		if ($phpAds_config['p3p_compact_policy'] != '')
+			$p3p_header .= " CP=\"".$phpAds_config['p3p_compact_policy']."\"";
+		
+		if ($p3p_header != '')
+			header ("P3P: $p3p_header");
 	}
-}
-else
-{
-	// Fetch a banner
-	
-	if (!isset($what))
-		$what = '';
-	
-	if (!isset($clientid))
-		$clientid = 0;
-	
-	if (!isset($source))
-		$source = '';
 	
 	
-	$row = phpAds_fetchBanner($what, $clientid, 0, $source, false);
+	// Send bannerid headers
+	$url = parse_url($phpAds_config['url_prefix']);
+	SetCookie("bannerNum", $row["bannerid"], 0, $url["path"]);
+	if(isset($n)) SetCookie("banID[$n]", $row["bannerid"], 0, $url["path"]);
 	
-	if (is_array($row))
+	
+	// Send zoneid headers
+	if ($row['zoneid'] != 0)
 	{
-		if(!empty($row["bannerid"]))
-		{
-			// Log this impression
-			phpAds_prepareLog ($row["bannerid"], $row["clientid"], $row["zoneid"]);
+		SetCookie("zoneNum", $row["zoneid"], 0, $url["path"]);
+		if(isset($n)) SetCookie("zoneID[$n]", $row["zoneid"], 0, $url["path"]);
+	}
+	
+	
+	switch ($row['storagetype'])
+	{
+		case 'url':
+			$row['imageurl'] = str_replace ('{timestamp}', time(), $row['imageurl']);
+			$row['url']      = str_replace ('{timestamp}', time(), $row['url']);
 			
-			// Send P3P Headers
-			if ($phpAds_config['p3p_policies'])
+			// Determine cachebuster
+			if (eregi ('\{random(:([1-9])){0,1}\}', $row['filename'], $matches))
 			{
-				$p3p_header = '';
+				if ($matches[1] == "")
+					$randomdigits = 8;
+				else
+					$randomdigits = $matches[2];
 				
-				if (isset($phpAds_config['p3p_policy_location']) && 
-				    $phpAds_config['p3p_policy_location'] != '')
-					$p3p_header .= " policyref=\"".$phpAds_config['p3p_policy_location']."\"";
-				
-				if ($phpAds_config['p3p_compact_policy'] != '')
-					$p3p_header .= " CP=\"".$phpAds_config['p3p_compact_policy']."\"";
-				
-				if ($p3p_header != '')
-					header ("P3P: $p3p_header");
+				$randomnumber = sprintf ('%0'.$randomdigits.'d', mt_rand (0, pow (10, $randomdigits) - 1));
+				$row['imageurl'] = str_replace ($matches[0], $randomnumber, $row['imageurl']);
 			}
 			
-			$url = parse_url($phpAds_config['url_prefix']);
-			SetCookie("bannerNum", $row["bannerid"], 0, $url["path"]);
-			if(isset($n)) SetCookie("banID[$n]", $row["bannerid"], 0, $url["path"]);
-			
-			if ($row['zoneid'] != 0)
+			if (eregi ('\{random(:([1-9])){0,1}\}', $row['url'], $matches))
 			{
-				SetCookie("zoneNum", $row["zoneid"], 0, $url["path"]);
-				if(isset($n)) SetCookie("zoneID[$n]", $row["zoneid"], 0, $url["path"]);
-			}
-			
-			
-			if ($row["format"] == "url")
-			{
-				// URL
-				
-				// Replace standard variables
-				$row['banner'] = str_replace ('{timestamp}', time(), $row['banner']);
-				$row['url']    = str_replace ('{timestamp}', time(), $row['url']);
-				
-				
-				// Determine cachebuster
-				if (eregi ('\{random(:([1-9])){0,1}\}', $row['banner'], $matches))
+				if (!isset($randomnumber) || $randomnumber == '')
 				{
 					if ($matches[1] == "")
 						$randomdigits = 8;
@@ -141,97 +115,72 @@ else
 						$randomdigits = $matches[2];
 					
 					$randomnumber = sprintf ('%0'.$randomdigits.'d', mt_rand (0, pow (10, $randomdigits) - 1));
-					$row['banner'] = str_replace ($matches[0], $randomnumber, $row['banner']);
 				}
 				
-				if (eregi ('\{random(:([1-9])){0,1}\}', $row['url'], $matches))
-				{
-					if (!isset($randomnumber) || $randomnumber == '')
-					{
-						if ($matches[1] == "")
-							$randomdigits = 8;
-						else
-							$randomdigits = $matches[2];
-						
-						$randomnumber = sprintf ('%0'.$randomdigits.'d', mt_rand (0, pow (10, $randomdigits) - 1));
-					}
-					
-					$row['url'] = str_replace ($matches[0], $randomnumber, $row['url']);
-				}
-				
-				// Store destination URL
-				SetCookie("destNum", $row['url'], 0, $url["path"]);
-				if(isset($n)) SetCookie("destID[$n]", $row['url'], 0, $url["path"]);
-				
-				// Redirect to the banner
-				Header("Location: ".$row['banner']);
+				$row['url'] = str_replace ($matches[0], $randomnumber, $row['url']);
 			}
-			elseif ($row["format"] == "web")
+			
+			// Store destination URL
+			SetCookie("destNum", $row['url'], 0, $url["path"]);
+			if(isset($n)) SetCookie("destID[$n]", $row['url'], 0, $url["path"]);
+			
+			// Redirect to the banner
+			header ("Location: ".$row['imageurl']);
+			break;
+		
+		
+		case 'web':
+			SetCookie("destNum", $row['url'], 0, $url["path"]);
+			if(isset($n)) SetCookie("destID[$n]", $row['url'], 0, $url["path"]);
+			
+			// Redirect to the banner
+			header ("Location: ".$row['imageurl']);
+			break;
+		
+		
+		case 'sql':
+			SetCookie("destNum", $row['url'], 0, $url["path"]);
+			if(isset($n)) SetCookie("destID[$n]", $row['url'], 0, $url["path"]);
+			
+			// Load the banner from the database
+			$res = phpAds_dbQuery("
+				SELECT
+					contents
+				FROM
+					".$phpAds_config['tbl_images']."
+				WHERE
+					filename = '".$row['filename']."'
+			");
+			
+			if ($image = phpAds_dbFetchArray($res))
 			{
-				// WEB
-				
-				// Store destination URL
-				SetCookie("destNum", $row['url'], 0, $url["path"]);
-				if(isset($n)) SetCookie("destID[$n]", $row['url'], 0, $url["path"]);
-				
-				// Redirect to the banner
-				Header("Location: ".$row['banner']);
+				header ('Content-type: image/'.$row['contenttype'].'; name='.md5(microtime()).'.'.$row['contenttype']);
+				echo $image['contents'];
 			}
-			else
-			{
-				// SQL
-				
-				// Store destination URL
-				SetCookie("destNum", $row['url'], 0, $url["path"]);
-				if(isset($n)) SetCookie("destID[$n]", $row['url'], 0, $url["path"]);
-				
-				// Load the banner from the database
-				$res = phpAds_dbQuery("
-					SELECT
-						b.contenttype, i.contents
-					FROM
-						".$phpAds_config['tbl_banners']." AS b,
-						".$phpAds_config['tbl_images']." AS i
-					WHERE
-						b.bannerid = ".$row['bannerid']." AND
-						b.filename = i.filename AND
-						b.storagetype = 'sql'
-				");
-				
-				if ($row = phpAds_dbFetchArray($res))
-				{
-					Header("Content-type: image/".$row['contenttype']."; name=".microtime());
-					echo $row['contents'];
-				}
-			}
-		}
-		else
-		{
-			Header( "Content-type: image/$row[format]");
-			echo $row["banner"];
-		}
+			
+			break;
 	}
-	else
+}
+else
+{
+	if ($phpAds_config['p3p_policies'])
 	{
-		if ($phpAds_config['p3p_policies'])
-		{
-			$p3p_header = '';
-			
-			if ($phpAds_config['p3p_policy_location'] != '')
-				$p3p_header .= " policyref=\"".$phpAds_config['p3p_policy_location']."\"";
-			
-			if ($phpAds_config['p3p_compact_policy'] != '')
-				$p3p_header .= " CP=\"".$phpAds_config['p3p_compact_policy']."\"";
-			
-			if ($p3p_header != '')
-				header ("P3P: $p3p_header");
-		}
+		$p3p_header = '';
 		
-		SetCookie("bannerNum", "DEFAULT", 0, $url["path"]);
-		if(isset($n)) SetCookie("banID[$n]", "DEFAULT", 0, $url["path"]);
+		if ($phpAds_config['p3p_policy_location'] != '')
+			$p3p_header .= " policyref=\"".$phpAds_config['p3p_policy_location']."\"";
 		
-		Header ("Location: ".$phpAds_config['default_banner_url']);
+		if ($phpAds_config['p3p_compact_policy'] != '')
+			$p3p_header .= " CP=\"".$phpAds_config['p3p_compact_policy']."\"";
+		
+		if ($p3p_header != '')
+			header ("P3P: $p3p_header");
 	}
+	
+	SetCookie("bannerNum", "DEFAULT", 0, $url["path"]);
+	if(isset($n)) SetCookie("banID[$n]", "DEFAULT", 0, $url["path"]);
+	
+	Header ("Location: ".$phpAds_config['default_banner_url']);
 }
 
 phpAds_dbClose();
