50fd0ce7baa43d579868652884f14de6b282aa5b
rakaz <rakaz@69d48449-a624-0410-ae76-85ddf3b22ea5>
2002-03-03 20:11:53 +0000

    Added rulers to the preview

--- admin/banner-htmlpreview.php
+++ admin/banner-htmlpreview.php
@@ -44,14 +44,15 @@ if ($res)
 	echo "<body marginheight='0' marginwidth='0' leftmargin='0' topmargin='0' bgcolor='#EFEFEF'>";
 	echo "<table cellpadding='0' cellspacing='0' border='0'>";
 	echo "<tr height='32'><td width='32'><img src='images/cropmark-tl.gif' width='32' height='32'></td>";
-	echo "<td>&nbsp;</td><td width='32'><img src='images/cropmark-tr.gif' width='32' height='32'></td></tr>";
-	echo "<tr height='".$row['height']."'><td width='32'>&nbsp;</td><td bgcolor='#FFFFFF' width='".$row['width']."'>";
+	echo "<td background='images/ruler-top.gif'>&nbsp;</td><td width='32'><img src='images/cropmark-tr.gif' width='32' height='32'></td></tr>";
+	echo "<tr height='".$row['height']."'><td width='32' background='images/ruler-left.gif'>&nbsp;</td><td bgcolor='#FFFFFF' width='".$row['width']."'>";
 	
 	if ($row['contenttype'] == 'html')
 	{
 		$htmlcode = $row['htmlcache'];
 		$htmlcode = str_replace ('{bannerid}', $bannerid, $htmlcode);
 		$htmlcode = str_replace ('{zoneid}', '', $htmlcode);
+		$htmlcode = str_replace ('{source}', '', $htmlcode);
 		$htmlcode = str_replace ('{target}', $target, $htmlcode);
 		$htmlcode = str_replace ('[bannertext]', '', $htmlcode);
 		$htmlcode = str_replace ('[/bannertext]', '', $htmlcode);
@@ -59,7 +60,7 @@ if ($res)
 		echo $htmlcode;
 	}
 	else
-		echo phpAds_buildBannerCode ($row['bannerid']);
+		echo phpAds_buildBannerCode ($row['bannerid'], true);
 	
 	echo "</td><td width='32'>&nbsp;</td></tr>";
 	echo "<tr height='32'><td width='32'><img src='images/cropmark-bl.gif' width='32' height='32'></td>";
