4972a4b68ee444cc966137d684afd144c55a6a29
rakaz <rakaz@69d48449-a624-0410-ae76-85ddf3b22ea5>
2002-03-06 21:21:55 +0000

    Added extra priority info

--- admin/admin-priority.php
+++ admin/admin-priority.php
@@ -18,6 +18,7 @@
 require ("config.php");
 require ("lib-statistics.inc.php");
 require ("lib-zones.inc.php");
+require ("../lib-priority.inc.php");
 
 
 // Security check
@@ -116,11 +117,6 @@ function phpAds_showBanners ()
 		
 		// Footer
 		echo "<tr height='1'><td colspan='5' bgcolor='#888888'><img src='images/break.gif' height='1' width='100%'></td></tr>";
-		
-		echo "<tr height='25'><td colspan='3' height='25'>";
-		echo "<img src='images/".$phpAds_TextDirection."/icon-undo.gif' border='0' align='absmiddle'>&nbsp;<a href='admin-priority-calculate.php'>$strRecalculatePriority</a>&nbsp;&nbsp;";
-		echo "</td></tr>";
-		
 		echo "</table>";
 	}
 }
@@ -133,10 +129,114 @@ function phpAds_showBanners ()
 /* Main code                                             */
 /*********************************************************/
 
+
+echo "<br>";
+
+// Get the number of days running
+if ($phpAds_config['compact_stats'])
+{
+	$res = phpAds_dbQuery("SELECT UNIX_TIMESTAMP(MIN(day)) AS days_running FROM ".$phpAds_config['tbl_adstats']." WHERE day > 0 AND hour > 0");
+	$days_running = phpAds_dbResult($res, 0, 'days_running');
+}
+else
+{
+	$res = phpAds_dbQuery("SELECT UNIX_TIMESTAMP(MIN(t_stamp)) AS days_running FROM ".$phpAds_config['tbl_adviews']);
+	$days_running = phpAds_dbResult($res, 0, 'days_running');
+}
+
+if ($days_running > 0)
+{
+	$days_running = mktime (0, 0, 0, date('m'), date('d'), date('Y')) - $days_running;
+	$days_running = round ($days_running / (60 * 60 * 24)) - 1;
+}
+else
+	$days_running = 0;
+	
+	
+if ($days_running >= 2)
+{
+	echo str_replace ("{days}", $days_running, $strPriorityDaysRunning);
+	
+	if ($days_running >= 8)
+		echo $strPriorityBasedLastWeek;
+	
+	if ($days_running >= 2 && $days_running < 8)
+		echo $strPriorityBasedLastDays;
+	
+	if ($days_running == 1)
+		echo $strPriorityBasedYesterday;
+}
+else
+	echo $strPriorityNoData;
+
+
+$banners   = phpAds_PriorityPrepareBanners();
+$campaigns = phpAds_PriorityPrepareCampaigns();
+$profile   = array();
+
+list($profile, $profile_correction_executed) = phpAds_PriorityPredictProfile($campaigns, $banners, $profile);
+
+$estimated_hits = 0;
+for ($p=0; $p<24; $p++)
+{
+	$estimated_hits += $profile[$p];
+}
+
+if ($estimated_hits > $campaigns_target)
+	echo $strPriorityEnoughAdViews;
+else
+	echo $strPriorityNotEnoughAdViews;
+
+
+// Show recalculate button
+echo "<br><br>";
+phpAds_ShowBreak();
+echo "<img src='images/".$phpAds_TextDirection."/icon-undo.gif' border='0' align='absmiddle'>&nbsp;<a href='admin-priority-calculate.php'>$strRecalculatePriority</a>&nbsp;&nbsp;";
+phpAds_ShowBreak();
+
+
+// Show banners
+echo "<br><br>";
+phpAds_showBanners();
 echo "<br><br>";
 
-phpAds_showBanners();
 
+// Extra campaign info
+$res = phpAds_dbQuery("SELECT COUNT(*) AS count, SUM(target) AS sum_target FROM ".$phpAds_config['tbl_clients']." WHERE parent > 0 AND target > 0");
+$campaigns_count = phpAds_dbResult($res, 0, 'count');
+$campaigns_target = phpAds_dbResult($res, 0, 'sum_target');
+
+$res = phpAds_dbQuery("SELECT COUNT(*) AS campaigns FROM ".$phpAds_config['tbl_clients']." WHERE parent > 0 AND weight > 0");
+$campaigns_weight = phpAds_dbResult($res, 0, 'campaigns');
+
+
+echo "<br><br>";
+echo "<table width='100%' border='0' align='center' cellspacing='0' cellpadding='0'>";
+	echo "<tr height='25'>";
+	echo "<td height='25'>&nbsp;&nbsp;<b>".$strOverall."</b></td>";
+	echo "<td height='25'>&nbsp;</td>";
+	echo "</tr>";
+	
+echo "<tr height='1'><td colspan='2' bgcolor='#888888'><img src='images/break.gif' height='1' width='100%'></td></tr>";
+	echo "<tr height='25'>";
+	echo "<td height='25'>&nbsp;&nbsp;".$strHighPriorityCampaigns.": <b>".$campaigns_count."</b></td>";
+	echo "<td height='25'>".$strAdViewsAssigned.": <b>".$campaigns_target."</b></td>";
+	echo "</tr>";
+	
+echo "<tr height='1'><td colspan='2' bgcolor='#888888'><img src='images/break-l.gif' height='1' width='100%'></td></tr>";
+	echo "<tr height='25'>";
+	echo "<td height='25'>&nbsp;&nbsp;".$strLowPriorityCampaigns.": <b>".$campaigns_weight."</b></td>";
+	echo "<td height='25'>&nbsp;</td>";
+	echo "</tr>";
+	
+echo "<tr height='1'><td colspan='2' bgcolor='#888888'><img src='images/break-l.gif' height='1' width='100%'></td></tr>";
+	echo "<tr height='25'>";
+	echo "<td height='25'>&nbsp;&nbsp;".$strPredictedAdViews.": <b>".$estimated_hits."</b></td>";
+	echo "<td height='25'>&nbsp;</td>";
+	echo "</tr>";
+	
+echo "<tr height='1'><td colspan='2' bgcolor='#888888'><img src='images/break.gif' height='1' width='100%'></td></tr>";
+echo "</table>";
 echo "<br><br>";
 
 
