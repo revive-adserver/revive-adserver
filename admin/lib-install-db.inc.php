0947468cd882aed524ca1f7e37da521c439afc91
rakaz <rakaz@69d48449-a624-0410-ae76-85ddf3b22ea5>
2002-03-19 22:14:11 +0000

    Bugfix: do not 'requote' status text during upgrade

--- admin/lib-install-db.inc.php
+++ admin/lib-install-db.inc.php
@@ -501,7 +501,6 @@ function phpAds_upgradeSplitBanners ()
 		{
 			// Requote fields
 			$banners[$i]['alt'] 		= phpAds_htmlQuotes(stripslashes($banners[$i]['alt']));
-			$banners[$i]['status'] 		= phpAds_htmlQuotes(stripslashes($banners[$i]['status']));
 			$banners[$i]['bannertext'] 	= phpAds_htmlQuotes(stripslashes($banners[$i]['bannertext']));
 			
 			// Resplit keywords
