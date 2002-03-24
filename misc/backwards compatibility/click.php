3f86881a00b49f046e6134c6951e2c50a4d81ca5
rakaz <rakaz@69d48449-a624-0410-ae76-85ddf3b22ea5>
2002-03-24 02:30:54 +0000

    Added old invocation files for backwards compatibility

---
+++ misc/backwards compatibility/click.php
@@ -0,0 +1,21 @@
+<?php // $Revision$
+
+/************************************************************************/
+/* phpAdsNew 2                                                          */
+/* ===========                                                          */
+/*                                                                      */
+/* Copyright (c) 2001 by the phpAdsNew developers                       */
+/* http://sourceforge.net/projects/phpadsnew                            */
+/*                                                                      */
+/* This program is free software. You can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation; either version 2 of the License.       */
+/************************************************************************/
+
+
+
+// This file is included for backwards-compatibility.
+// Please use adclick.php instead.
+include ("adclick.php");
+
+?>
