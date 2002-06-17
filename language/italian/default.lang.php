2724c6cbbfd76c54292b6e1361267fd9fd1ad1f3
ciaccia <ciaccia@69d48449-a624-0410-ae76-85ddf3b22ea5>
2002-06-17 13:44:10 +0000

    Fixed date format

--- language/italian/default.lang.php
+++ language/italian/default.lang.php
@@ -25,7 +25,7 @@ $GLOBALS['phpAds_TextAlignLeft']			= "left";
 
 
 // Date & time configuration
-$GLOBALS['date_format']						= "%m/%d/%Y";
+$GLOBALS['date_format']						= "%d/%m/%Y";
 $GLOBALS['time_format']						= "%H:%i:%S";
 $GLOBALS['minute_format']					= "%H:%M";
 $GLOBALS['month_format']					= "%m/%Y";
