0762a5eb327ddf270214f863400e5879d0b1e977
ciaccia <ciaccia@69d48449-a624-0410-ae76-85ddf3b22ea5>
2002-07-23 05:36:31 +0000

    Changed header from phpPgAds to phpAdsNew

--- adlayer.php
+++ adlayer.php
@@ -1,11 +1,11 @@
-<?php // $Id$
+<?php // $Revision$
 
 /************************************************************************/
-/* phpPgAds                                                             */
-/* ========                                                             */
+/* phpAdsNew 2                                                          */
+/* ===========                                                          */
 /*                                                                      */
-/* Copyright (c) 2001-2002 by the phpPgAds developers                   */
-/* For more information visit: http://phppgads.sourceforge.net          */
+/* Copyright (c) 2000-2002 by the phpAdsNew developers                  */
+/* For more information visit: http://www.phpadsnew.com                 */
 /*                                                                      */
 /* This program is free software. You can redistribute it and/or modify */
 /* it under the terms of the GNU General Public License as published by */
