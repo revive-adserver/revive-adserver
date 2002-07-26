4172c9d1b31c32fad3be48c2055ff28e63a70437
rakaz <rakaz@69d48449-a624-0410-ae76-85ddf3b22ea5>
2002-07-26 17:07:56 +0000

    Added many new features

--- misc/layerstyles/cursor/invocation.inc.php
+++ misc/layerstyles/cursor/invocation.inc.php
@@ -25,6 +25,78 @@ define('phpAds_adLayerLoaded', true);
 
 function phpAds_placeLayerSettings ()
 {
+	global $stickyness, $offsetx, $offsety, $hide, $transparancy, $delay, $trail;
+	
+	if (!isset($trail)) $trail = '0';
+	if (!isset($stickyness)) $stickyness = 5;
+	if (!isset($offsetx)) $offsetx = 10;
+	if (!isset($offsety)) $offsety = 10;
+	if (!isset($hide)) $hide = '0';
+	if (!isset($transparancy)) $transparancy = 0;
+	if (!isset($delay)) $delay = 90;
+	
+	echo "<tr><td height='30' colspan='3'>&nbsp;</td></tr>";
+	echo "<tr height='1'><td colspan='3' bgcolor='#888888'><img src='images/break-el.gif' height='1' width='100%'></td></tr>";
+	echo "<tr><td height='10' colspan='3'>&nbsp;</td></tr>";
+	
+	echo "<tr><td width='30'>&nbsp;</td>";
+	echo "<td width='200'>".'Smooth movement'."</td><td width='370'>";
+	echo "<select name='trail' style='width:60px;' onChange='this.form.stickyness.disabled = this.selectedIndex ? true : false;'>";
+		echo "<option value='1'".($trail == '1' ? ' selected' : '').">".$GLOBALS['strYes']."</option>";
+		echo "<option value='0'".($trail == '0' ? ' selected' : '').">".$GLOBALS['strNo']."</option>";
+	echo "</select>";
+	echo "<tr><td width='30'><img src='images/spacer.gif' height='1' width='100%'></td>";
+	echo "<td colspan='2'><img src='images/break-l.gif' height='1' width='200' vspace='6'></td></tr>";
+	
+	echo "<tr><td width='30'>&nbsp;</td>";
+	echo "<td width='200'>".'Speed'."</td><td width='370'>";
+	echo "<select name='stickyness' style='width:60px;'".($trail == '0' ? ' disabled' : '').">";
+	for ($i=1;$i<=9;$i++)
+		echo "<option value='".$i."'".($stickyness == $i ? ' selected' : '').">".$i."</option>";
+	echo "</select>";
+	echo "<tr><td width='30'><img src='images/spacer.gif' height='1' width='100%'></td>";
+
+	echo "<tr><td height='30' colspan='3'>&nbsp;</td></tr>";
+	echo "<tr height='1'><td colspan='3' bgcolor='#888888'><img src='images/break-el.gif' height='1' width='100%'></td></tr>";
+	echo "<tr><td height='10' colspan='3'>&nbsp;</td></tr>";
+
+	echo "<tr><td width='30'>&nbsp;</td>";
+	echo "<td width='200'>".'Hide the banner when the cursor is not moving'."</td><td width='370'>";
+	echo "<select name='hide' style='width:60px;' onChange='this.form.transparancy.disabled = this.selectedIndex ? true : false; this.form.delay.disabled = this.selectedIndex ? true : false;'>";
+		echo "<option value='1'".($hide == '1' ? ' selected' : '').">".$GLOBALS['strYes']."</option>";
+		echo "<option value='0'".($hide == '0' ? ' selected' : '').">".$GLOBALS['strNo']."</option>";
+	echo "</select>";
+	echo "<tr><td width='30'><img src='images/spacer.gif' height='1' width='100%'></td>";
+	echo "<td colspan='2'><img src='images/break-l.gif' height='1' width='200' vspace='6'></td></tr>";
+	
+	echo "<tr><td width='30'>&nbsp;</td>";
+	echo "<td width='200'>".'Delay before banner is hidden'."</td><td width='370'>";
+		echo "<input class='flat' type='text' name='delay' size='' value='".$delay."' style='width:60px;'".($hide == '0' ? ' disabled' : '')."> ms</td></tr>";
+	echo "<tr><td width='30'><img src='images/spacer.gif' height='1' width='100%'></td>";
+	echo "<td colspan='2'><img src='images/break-l.gif' height='1' width='200' vspace='6'></td></tr>";
+	
+	echo "<tr><td width='30'>&nbsp;</td>";
+	echo "<td width='200'>".'Transparancy of the hidden banner'."</td><td width='370'>";
+	echo "<select name='transparancy' style='width:60px;'".($hide == '0' ? ' disabled' : '').">";
+	for ($i=0;$i<=9;$i++)
+		echo "<option value='".($i * 10)."'".($transparancy == ($i * 10) ? ' selected' : '').">".($i * 10)." %</option>";
+	echo "</select>";
+	echo "<tr><td width='30'><img src='images/spacer.gif' height='1' width='100%'></td>";
+
+	echo "<tr><td height='30' colspan='3'>&nbsp;</td></tr>";
+	echo "<tr height='1'><td colspan='3' bgcolor='#888888'><img src='images/break-el.gif' height='1' width='100%'></td></tr>";
+	echo "<tr><td height='10' colspan='3'>&nbsp;</td></tr>";
+
+	echo "<tr><td width='30'>&nbsp;</td>";
+	echo "<td width='200'>".$GLOBALS['strHShift']."</td><td width='370'>";
+		echo "<input class='flat' type='text' name='offsetx' size='' value='".$offsetx."' style='width:60px;'> ".$GLOBALS['strAbbrPixels']."</td></tr>";
+	echo "<tr><td width='30'><img src='images/spacer.gif' height='1' width='100%'></td>";
+	echo "<td colspan='2'><img src='images/break-l.gif' height='1' width='200' vspace='6'></td></tr>";
+	
+	echo "<tr><td width='30'>&nbsp;</td>";
+	echo "<td width='200'>".$GLOBALS['strVShift']."</td><td width='370'>";
+		echo "<input class='flat' type='text' name='offsety' size='' value='".$offsety."' style='width:60px;'> ".$GLOBALS['strAbbrPixels']."</td></tr>";
+	echo "<tr><td width='30'><img src='images/spacer.gif' height='1' width='100%'></td>";
 }
 
 
@@ -36,8 +108,22 @@ function phpAds_placeLayerSettings ()
 function phpAds_generateLayerCode ($parameters)
 {
 	global $phpAds_config;
+	global $stickyness, $offsetx, $offsety, $hide, $transparancy, $delay, $trail;
 	
 	$parameters[] = 'layerstyle=cursor';
+	$parameters[] = 'hide='.$hide;
+	$parameters[] = 'trail='.$trail;
+	$parameters[] = 'offsetx='.$offsetx;
+	$parameters[] = 'offsety='.$offsety;
+	
+	if ($trail == '1')
+		$parameters[] = 'stickyness='.$stickyness;
+	
+	if ($hide == '1')
+	{
+		$parameters[] = 'transparancy='.$transparancy;
+		$parameters[] = 'delay='.$delay;
+	}
 	
 	$buffer = "<script language='JavaScript' type='text/javascript' src='".$phpAds_config['url_prefix']."/adlayer.php";
 	if (sizeof($parameters) > 0)
@@ -60,7 +146,8 @@ function phpAds_getlayerShowVar ()
 		'clientid' => true,
 		'target' => true,
 		'source' => true,
-		'layerstyle' => true
+		'layerstyle' => true,
+		'layercustom' => true
 	);
 }
 
