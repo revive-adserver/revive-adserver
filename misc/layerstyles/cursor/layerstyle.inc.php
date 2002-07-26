13bdc6654d49cd73a27b18b853953c85b0abdba6
rakaz <rakaz@69d48449-a624-0410-ae76-85ddf3b22ea5>
2002-07-26 16:21:27 +0000

    Added many new features

--- misc/layerstyles/cursor/layerstyle.inc.php
+++ misc/layerstyles/cursor/layerstyle.inc.php
@@ -48,65 +48,167 @@ function phpAds_getLayerLimitations ()
 
 function phpAds_putLayerJS ($output, $uniqid)
 {
-?>
-var phpAds_ns6 = document.getElementById && !document.all;
-
-function phpAds_findObj(n, d) { 
-  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
-  d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
-  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
-  for(i=0;!x&&d.layers&&i>d.layers.length;i++) x=phpAds_findObj(n,d.layers[i].document);
-  if(!x && document.getElementById) x=document.getElementById(n); return x;
-}
-
-function phpAds_getMouseXPos(e)
-{
-	if (document.layers || phpAds_ns6)
-	    return parseInt(e.pageX+10);
-	else
-		return (parseInt(event.clientX+10) + parseInt(document.body.scrollLeft));
-}
-
-function phpAds_getMouseYPos(e)
-{
-	if (document.layers || phpAds_ns6)
-		return parseInt(e.pageY);
-	else
-		return (parseInt(event.clientY) + parseInt(document.body.scrollTop));
-}
-
-function phpAds_adlayers_place_<?php echo $uniqid; ?>(e)
-{
-	var c = phpAds_findObj('phpads_<?php echo $uniqid; ?>');
-
-	if (!c)
-		return false;
+	global $stickyness, $offsetx, $offsety, $hide, $transparancy, $delay;
 	
-	if (c.style)
-		c = c.style;
+	if (!isset($stickyness) || $stickyness == '') $stickyness = 5;
+	
+	if (!isset($offsetx) || $offsetx == '') $offsetx = 10;
+	if (!isset($offsety) || $offsety == '') $offsety = 10;
+	
+	if (!isset($hide) || $hide == '') $hide = 0;
+	if (!isset($transparancy) || $transparancy == '') $transparancy = 0;
+	if (!isset($delay) || $delay == '') $delay = 90;
+?>
 
-	var x = phpAds_getMouseXPos(e);
-	var y = phpAds_getMouseYPos(e);
 
-	if (document.all) {
-		c.pixelLeft = x;
-		c.pixelTop = y;
-	} else {
-		c.left = x;
-		c.top = y;
+
+var phpAds_ns4 = (document.layers) ? true : false;
+var phpAds_ie4 = (document.all) ? true : false;
+var phpAds_ns6 = ((document.getElementById) && (!phpAds_ie4)) ? true : false;
+
+var phpAds_<?php echo $uniqid; ?>_posX_old = 0;
+var phpAds_<?php echo $uniqid; ?>_posX_new = 0;
+var phpAds_<?php echo $uniqid; ?>_posY_old = 0;
+var phpAds_<?php echo $uniqid; ?>_posY_new = 0;
+var phpAds_<?php echo $uniqid; ?>_speedX = 0;
+var phpAds_<?php echo $uniqid; ?>_speedY = 0;
+var phpAds_<?php echo $uniqid; ?>_NoMove = 0;
+var phpAds_<?php echo $uniqid; ?>_transparancy = 0;
+
+
+if (phpAds_ie4 || phpAds_ns6) { document.onmousemove = phpAds_storePos_<?php echo $uniqid; ?>; }
+if (phpAds_ns4) { window.captureEvents(Event.MOUSEMOVE); onmousemove = phpAds_storePos_<?php echo $uniqid; ?>; }
+
+
+if (phpAds_ie4)
+	window.setInterval('phpAds_followMouse_<?php echo $uniqid; ?>()',1);
+else
+	window.setInterval('phpAds_followMouse_<?php echo $uniqid; ?>()',25);
+
+
+function phpAds_storePos_<?php echo $uniqid; ?>(e) {
+
+	if (phpAds_ie4)
+	{
+		phpAds_<?php echo $uniqid; ?>_posX_new = window.event.x;
+		phpAds_<?php echo $uniqid; ?>_posY_new = window.event.y + document.body.scrollTop;
+	}
+	else if (phpAds_ns4)
+	{
+		phpAds_<?php echo $uniqid; ?>_posX_new = e.pageX;
+		phpAds_<?php echo $uniqid; ?>_posY_new = e.pageY;
+	} 
+	else if (phpAds_ns6) 
+	{
+		phpAds_<?php echo $uniqid; ?>_posX_new = e.clientX;
+		phpAds_<?php echo $uniqid; ?>_posY_new = e.clientY;
+	}
+}
+
+
+function phpAds_setVisibility_<?php echo $uniqid; ?>(transparancy)
+{
+	if (phpAds_ie4 && document.all.phpads_<?php echo $uniqid; ?>.filters)
+	{
+		if (transparancy >= <?php echo $transparancy; ?>)
+		{
+			document.all.phpads_<?php echo $uniqid; ?>.filters.item("DXImageTransform.Microsoft.Alpha").opacity = transparancy;
+		}
+		else
+			transparancy = <?php echo $transparancy; ?>;
+	}
+	
+	if (transparancy > 0)
+	{	
+		if (phpAds_ie4)        	{ document.all.phpads_<?php echo $uniqid; ?>.style.visibility = 'visible'; }
+		else if (phpAds_ns4)   	{ document.layers['phpads_<?php echo $uniqid; ?>'].visibility = 'show'; }
+		else if (phpAds_ns6) 	{ var elm = document.getElementById('phpads_<?php echo $uniqid; ?>'); elm.style.visibility='visible'; }
+	}
+	else
+	{
+		if (phpAds_ie4)        	{ document.all.phpads_<?php echo $uniqid; ?>.style.visibility = 'hidden'; }
+		else if (phpAds_ns4)   	{ document.layers['phpads_<?php echo $uniqid; ?>'].visibility = 'hide'; }
+		else if (phpAds_ns6) 	{ var elm = document.getElementById('phpads_<?php echo $uniqid; ?>'); elm.style.visibility='hidden'; };
 	}
 
-	c.visibility = 'visible';
+	phpAds_<?php echo $uniqid; ?>_transparancy = transparancy;
+}
+
+function phpAds_setPos_<?php echo $uniqid; ?>(x, y)
+{
+	if (phpAds_ie4)
+	{
+		document.all.phpads_<?php echo $uniqid; ?>.style.left = x;
+		document.all.phpads_<?php echo $uniqid; ?>.style.top = y;
+	}
+ 	else if (phpAds_ns4)
+	{
+		document.phpads_<?php echo $uniqid; ?>.moveTo (x, y); 
+  	}
+  	else if (phpAds_ns6)
+	{
+  		var elm = document.getElementById('phpads_<?php echo $uniqid; ?>');
+  		elm.style.left = x;
+  		elm.style.top = y;
+  	}
 }
 
 
-if (document.layers)
+function phpAds_followMouse_<?php echo $uniqid; ?>() {
+	
+	if (Math.abs(phpAds_<?php echo $uniqid; ?>_posX_new - phpAds_<?php echo $uniqid; ?>_posX_old) < 3 && 
+		Math.abs(phpAds_<?php echo $uniqid; ?>_posY_new - phpAds_<?php echo $uniqid; ?>_posY_old) < 3) 
+	{
+		phpAds_<?php echo $uniqid; ?>_NoMove = phpAds_<?php echo $uniqid; ?>_NoMove + 1;
+	} 
+	else 
+	{
+		phpAds_<?php echo $uniqid; ?>_NoMove = 0;
+	}
+	
+	
+<?php 
+if ($hide == 1) 
 {
-	document.captureEvents(Event.MOUSEMOVE);
-	document.onMouseMove = phpAds_adlayers_place_<?php echo $uniqid; ?>;
+	?>
+	var transparancy = 100;
+	
+	if (phpAds_<?php echo $uniqid; ?>_NoMove > <?php echo $delay; ?>) 
+	{
+		// Cursor is still, hide banner
+		if (phpAds_<?php echo $uniqid; ?>_NoMove <= 10 + <?php echo $delay; ?>)
+			transparancy = 100 - ((phpAds_<?php echo $uniqid; ?>_NoMove - <?php echo $delay; ?>) * 10);
+		else
+			transparancy = 0;
+	}
+
+	if (transparancy != phpAds_<?php echo $uniqid; ?>_transparancy)
+		phpAds_setVisibility_<?php echo $uniqid; ?>(transparancy);	
+	
+	<?php
 }
 else
-	document.onmousemove = phpAds_adlayers_place_<?php echo $uniqid; ?>;
+{
+	?>
+	phpAds_setVisibility_<?php echo $uniqid; ?>(100);	
+	<?php
+}
+?>	
+	if (phpAds_<?php echo $uniqid; ?>_NoMove < <?php echo $delay; ?>)
+	{
+		// Calculate new position
+		phpAds_<?php echo $uniqid; ?>_speedX = phpAds_<?php echo $uniqid; ?>_speedX * (<?php echo $stickyness; ?> / 10) + (phpAds_<?php echo $uniqid; ?>_posX_new - phpAds_<?php echo $uniqid; ?>_posX_old) / 30;
+		phpAds_<?php echo $uniqid; ?>_speedY = phpAds_<?php echo $uniqid; ?>_speedY * (<?php echo $stickyness; ?> / 10) + (phpAds_<?php echo $uniqid; ?>_posY_new - phpAds_<?php echo $uniqid; ?>_posY_old) / 30;
+		phpAds_<?php echo $uniqid; ?>_posX_old = phpAds_<?php echo $uniqid; ?>_posX_old + phpAds_<?php echo $uniqid; ?>_speedX;
+		phpAds_<?php echo $uniqid; ?>_posY_old = phpAds_<?php echo $uniqid; ?>_posY_old + phpAds_<?php echo $uniqid; ?>_speedY;
+	
+		// Set position of banner
+		phpAds_setPos_<?php echo $uniqid; ?> (
+			phpAds_<?php echo $uniqid; ?>_posX_old + <?php echo $offsetx; ?>,
+			phpAds_<?php echo $uniqid; ?>_posY_old + <?php echo $offsety; ?>
+		);
+	}
+}
 
 <?php
 }
@@ -120,8 +222,8 @@ else
 function phpAds_getLayerHTML ($output, $uniqid)
 {
 	return '
-<div id="phpads_'.$uniqid.'" style="position:absolute; width:'.$output['width'].'px; height:'.$output['height'].'px; z-index:99; left: 0px; top: 0px; visibility: hidden"> 
-'.ereg_replace("</?a[^>]*>", '', $output['html']).'
+<div id="phpads_'.$uniqid.'" style="position:absolute; width:'.$output['width'].'px; height:'.$output['height'].'px; z-index:99; left: 0px; top: 0px; visibility: hidden; filter: progid:DXImageTransform.Microsoft.Alpha(opacity=100);"> 
+'.$output['html'].'
 </div>
 ';
 
