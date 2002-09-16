e773a492051944e051010535240e4ff984d195c6
rakaz <rakaz@69d48449-a624-0410-ae76-85ddf3b22ea5>
2002-09-16 19:32:50 +0000

    Prepare for release of new dev build

--- lib-dbconfig.inc.php
+++ lib-dbconfig.inc.php
@@ -18,8 +18,8 @@ define ('LIBDBCONFIG_INCLUDED', true);
 
 
 // Current phpAds version
-$phpAds_version = 200.112;
-$phpAds_version_readable = "2 dev 08092002";
+$phpAds_version = 200.115;
+$phpAds_version_readable = "2 dev 16092002";
 $phpAds_productname = "phpAdsNew";
 $phpAds_dbmsname = "MySQL";
 
