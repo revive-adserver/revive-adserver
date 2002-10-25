811b44702a54a745093e6dd4d80cb2335623cd81
rakaz <rakaz@69d48449-a624-0410-ae76-85ddf3b22ea5>
2002-10-25 21:05:54 +0000

    Added meta info about language

---
+++ language/spanish/index.lang.php
@@ -0,0 +1,22 @@
+<?php // $Revision$
+
+/************************************************************************/
+/* phpAdsNew 2                                                          */
+/* ===========                                                          */
+/*                                                                      */
+/* Copyright (c) 2000-2002 by the phpAdsNew developers                  */
+/* For more information visit: http://www.phpadsnew.com                 */
+/*                                                                      */
+/* This program is free software. You can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation; either version 2 of the License.       */
+/************************************************************************/
+
+
+
+// Meta information
+$translation_readable   = "Espa&ntilde;ol";
+$translation_maintainer	= "SeBoRaiD";
+$translation_contact	= "me2@seboraid.com.ar";
+
+?>
