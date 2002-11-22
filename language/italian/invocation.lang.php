4b83510af0c56c17a2ddc5e63790ef70e7acf183
ciaccia <ciaccia@69d48449-a624-0410-ae76-85ddf3b22ea5>
2002-11-22 14:11:22 +0000

    Updated italian language

--- language/italian/invocation.lang.php
+++ language/italian/invocation.lang.php
@@ -16,7 +16,7 @@
 
 // Invocation Types
 $GLOBALS['strInvocationRemote']			= "Invocazione remota";
-$GLOBALS['strInvocationJS']			= "Invocazione remota con Javascript";
+$GLOBALS['strInvocationJS']				= "Invocazione remota con Javascript";
 $GLOBALS['strInvocationIframes']		= "Invocazione remota con iframes";
 $GLOBALS['strInvocationXmlRpc']			= "Invocazione remota con XML-RPC";
 $GLOBALS['strInvocationCombined']		= "Invocazione remota Combinata";
@@ -30,8 +30,8 @@ $GLOBALS['strCopyToClipboard']			= "Copia negli appunti";
 
 
 // Measures
-$GLOBALS['strAbbrPixels']			= "px";
-$GLOBALS['strAbbrSeconds']			= "sec";
+$GLOBALS['strAbbrPixels']				= "px";
+$GLOBALS['strAbbrSeconds']				= "sec";
 
 
 // Common Invocation Parameters
@@ -40,28 +40,29 @@ $GLOBALS['strInvocationClientID']		= "Inserzionista o campagna";
 $GLOBALS['strInvocationTarget']			= "Frame destinazione";
 $GLOBALS['strInvocationSource']			= "Sorgente";
 $GLOBALS['strInvocationWithText']		= "Link testuale sotto il Banner";
-$GLOBALS['strInvocationDontShowAgain']		= "Non mostrare banner duplicati nella stessa pagina";
+$GLOBALS['strInvocationDontShowAgain']	= "Non mostrare banner duplicati nella stessa pagina";
+$GLOBALS['strInvocationDontShowAgainCampaign'] = "Non mostrare banner della stessa compagna nella pagina";
 $GLOBALS['strInvocationTemplate'] 		= "Assegna il banner a una variabile per utilizzarla in un template";
 
 
 // Iframe
 $GLOBALS['strIFrameRefreshAfter']		= "Aggiorna dopo";
 $GLOBALS['strIframeResizeToBanner']		= "Adatta dimensioni IFrame al Banner";
-$GLOBALS['strIframeMakeTransparent']		= "Sfondo IFrame trasparente";
-$GLOBALS['strIframeIncludeNetscape4']		= "Includi ilayer compatibile con Netscape 4";
+$GLOBALS['strIframeMakeTransparent']	= "Sfondo IFrame trasparente";
+$GLOBALS['strIframeIncludeNetscape4']	= "Includi ilayer compatibile con Netscape 4";
 
 
 // PopUp
-$GLOBALS['strPopUpStyle']			= "Tipo di Pop-up";
+$GLOBALS['strPopUpStyle']				= "Tipo di Pop-up";
 $GLOBALS['strPopUpStylePopUp']			= "Pop-up";
 $GLOBALS['strPopUpStylePopUnder']		= "Pop-under";
 $GLOBALS['strPopUpCreateInstance']		= "Apertura Pop-up";
 $GLOBALS['strPopUpImmediately']			= "Immediata";
-$GLOBALS['strPopUpOnClose']			= "Alla chiusura della pagina";
+$GLOBALS['strPopUpOnClose']				= "Alla chiusura della pagina";
 $GLOBALS['strPopUpAfterSec']			= "Dopo";
 $GLOBALS['strAutoCloseAfter']			= "Chiudi automaticamente dopo";
-$GLOBALS['strPopUpTop']				= "Positione Iniziale (alto)";
-$GLOBALS['strPopUpLeft']			= "Positione Iniziale (sinistra)";
+$GLOBALS['strPopUpTop']					= "Positione Iniziale (alto)";
+$GLOBALS['strPopUpLeft']				= "Positione Iniziale (sinistra)";
 
 
 // XML-RPC
@@ -69,47 +70,47 @@ $GLOBALS['strXmlRpcLanguage']			= "Linguaggio utilizzato";
 
 
 // AdLayer
-$GLOBALS['strAdLayerStyle']			= "Stile";
+$GLOBALS['strAdLayerStyle']				= "Stile";
 
-$GLOBALS['strAlignment']			= "Allinemento";
-$GLOBALS['strHAlignment']			= "Allinemento orizzontale";
-$GLOBALS['strLeft']				= "Sinistra";
-$GLOBALS['strCenter']				= "Centrato";
-$GLOBALS['strRight']				= "Destra";
+$GLOBALS['strAlignment']				= "Allinemento";
+$GLOBALS['strHAlignment']				= "Allinemento orizzontale";
+$GLOBALS['strLeft']						= "Sinistra";
+$GLOBALS['strCenter']					= "Centrato";
+$GLOBALS['strRight']					= "Destra";
 
-$GLOBALS['strVAlignment']			= "Allinemento verticale";
-$GLOBALS['strTop']				= "Alto";
-$GLOBALS['strMiddle']				= "Centrato";
-$GLOBALS['strBottom']				= "Basso";
+$GLOBALS['strVAlignment']				= "Allinemento verticale";
+$GLOBALS['strTop']						= "Alto";
+$GLOBALS['strMiddle']					= "Centrato";
+$GLOBALS['strBottom']					= "Basso";
 
 $GLOBALS['strAutoCollapseAfter']		= "Riduci automaticamente dopo";
-$GLOBALS['strCloseText']			= "Testo di chiusura";
-$GLOBALS['strClose']				= "[Chiudi]";
+$GLOBALS['strCloseText']				= "Testo di chiusura";
+$GLOBALS['strClose']					= "[Chiudi]";
 $GLOBALS['strBannerPadding']			= "Margini interni";
 
-$GLOBALS['strHShift']				= "Scostamento orizzontale";
-$GLOBALS['strVShift']				= "Scostamento verticale";
+$GLOBALS['strHShift']					= "Scostamento orizzontale";
+$GLOBALS['strVShift']					= "Scostamento verticale";
 
 $GLOBALS['strShowCloseButton']			= "Visualizza pulsante di chiusura";
 $GLOBALS['strBackgroundColor']			= "Colore di sfondo";
-$GLOBALS['strBorderColor']			= "Colore del bordo";
+$GLOBALS['strBorderColor']				= "Colore del bordo";
 
-$GLOBALS['strDirection']			= "Direzione";
-$GLOBALS['strLeftToRight']			= "Da sinistra a destra";
-$GLOBALS['strRightToLeft']			= "Da destra a sinistra";
-$GLOBALS['strLooping']				= "Ripetizioni";
-$GLOBALS['strAlwaysActive']			= "Sempre attivo";
-$GLOBALS['strSpeed']				= "Velocit&agrave;";
-$GLOBALS['strPause']				= "Pausa";
-$GLOBALS['strLimited']				= "Limitato";
-$GLOBALS['strLeftMargin']			= "Margine sinistro";
-$GLOBALS['strRightMargin']			= "Margine destro";
-$GLOBALS['strTransparentBackground']		= "Sfondo trasparente";
+$GLOBALS['strDirection']				= "Direzione";
+$GLOBALS['strLeftToRight']				= "Da sinistra a destra";
+$GLOBALS['strRightToLeft']				= "Da destra a sinistra";
+$GLOBALS['strLooping']					= "Ripetizioni";
+$GLOBALS['strAlwaysActive']				= "Sempre attivo";
+$GLOBALS['strSpeed']					= "Velocit&agrave;";
+$GLOBALS['strPause']					= "Pausa";
+$GLOBALS['strLimited']					= "Limitato";
+$GLOBALS['strLeftMargin']				= "Margine sinistro";
+$GLOBALS['strRightMargin']				= "Margine destro";
+$GLOBALS['strTransparentBackground']	= "Sfondo trasparente";
 
-$GLOBALS['strSmoothMovement']		= "Movimento fluido";
-$GLOBALS['strHideNotMoving']		= "Nascondi il banner quando il puntatore &egrave; fermo";
-$GLOBALS['strHideDelay']			= "Nascondi dopo";
-$GLOBALS['strHideTransparancy']		= "Trasparenza del banner nascosto";
+$GLOBALS['strSmoothMovement']			= "Movimento fluido";
+$GLOBALS['strHideNotMoving']			= "Nascondi il banner quando il puntatore &egrave; fermo";
+$GLOBALS['strHideDelay']				= "Nascondi dopo";
+$GLOBALS['strHideTransparancy']			= "Trasparenza del banner nascosto";
 
 
 $GLOBALS['strAdLayerStyleName']	= array(
