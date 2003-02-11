90ea1adbab910d49d95a9b859360621d33fe17f6
ciaccia <ciaccia@69d48449-a624-0410-ae76-85ddf3b22ea5>
2003-02-11 16:16:04 +0000

    Fixed mising closing paren

--- adview.php
+++ adview.php
@@ -66,7 +66,7 @@ if (!isset($n)) $n = 'default';
 
 // Remove referer, to be sure it doesn't cause problems with limitations
 if (isset($HTTP_SERVER_VARS['HTTP_REFERER'])) unset($HTTP_SERVER_VARS['HTTP_REFERER']);
-if (isset($HTTP_REFERER) unset($HTTP_REFERER);
+if (isset($HTTP_REFERER)) unset($HTTP_REFERER);
 
 
 if (phpAds_dbConnect())
