4e54a5743a66ad0f03e1ac0631cbc06350df894c
rakaz <rakaz@69d48449-a624-0410-ae76-85ddf3b22ea5>
2003-07-29 15:03:27 +0000

    Preparing for RC4.2 release

--- libraries/lib-dbconfig.inc.php
+++ libraries/lib-dbconfig.inc.php
@@ -18,8 +18,8 @@ define ('LIBDBCONFIG_INCLUDED', true);
 
 
 // Current phpAds version
-$phpAds_version = 200.172;
-$phpAds_version_readable = "2.0 RC 4.1";
+$phpAds_version = 200.175;
+$phpAds_version_readable = "2.0 RC 4.3";
 $phpAds_productname = "phpAdsNew";
 $phpAds_producturl = "www.phpadsnew.com";
 $phpAds_dbmsname = "MySQL";
