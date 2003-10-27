16517c5dbd840dd6e1a6bdb3a0a83541d3a21982
ciaccia <ciaccia@69d48449-a624-0410-ae76-85ddf3b22ea5>
2003-10-27 14:27:24 +0000

    Added german iso translations

---
+++ language/german/res-iso3166.inc.php
@@ -0,0 +1,277 @@
+<?php // $Revision: 2.2.2.2 $
+
+/************************************************************************/
+/* phpAdsNew 2                                                          */
+/* ===========                                                          */
+/*                                                                      */
+/* Copyright (c) 2000-2003 by the phpAdsNew developers                  */
+/* For more information visit: http://www.phpadsnew.com                 */
+/*                                                                      */
+/* This program is free software. You can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation; either version 2 of the License.       */
+/************************************************************************/
+
+
+// This is by no means a complete list of all iso3166 codes, but rather
+// an unofficial list used by most browsers. If you have corrections or
+// additions to this list, please send them to niels@creatype.nl
+
+// German
+// Corrections --> minichip@users.sourceforge.net
+
+$phpAds_ISO3166['AF'] = "Afghanistan";
+$phpAds_ISO3166['AL'] = "Albanien";
+$phpAds_ISO3166['DZ'] = "Algerien";
+$phpAds_ISO3166['AS'] = "Amerikanisch-Samoa";
+$phpAds_ISO3166['AD'] = "Andorra";
+$phpAds_ISO3166['AO'] = "Angola";
+$phpAds_ISO3166['AI'] = "Anguilla";
+$phpAds_ISO3166['AQ'] = "Antarktis"; 
+$phpAds_ISO3166['AG'] = "Antigua und Barbuda"; 
+$phpAds_ISO3166['AR'] = "Argentinien";
+$phpAds_ISO3166['AP'] = "ARIPO (Afrikanische Regionale ...)";
+$phpAds_ISO3166['AM'] = "Armenien";
+$phpAds_ISO3166['AW'] = "Aruba";
+$phpAds_ISO3166['AU'] = "Australien";
+$phpAds_ISO3166['AT'] = "Österreich";
+$phpAds_ISO3166['AZ'] = "Aserbaidschan";
+$phpAds_ISO3166['BS'] = "Bahamas";
+$phpAds_ISO3166['BH'] = "Bahrain";
+$phpAds_ISO3166['BD'] = "Bangladesch";
+$phpAds_ISO3166['BB'] = "Barbados";
+$phpAds_ISO3166['BY'] = "Weißrußland";
+$phpAds_ISO3166['BE'] = "Belgien";
+$phpAds_ISO3166['BZ'] = "Belize";
+$phpAds_ISO3166['BJ'] = "Benin"; 
+$phpAds_ISO3166['BM'] = "Bermuda";
+$phpAds_ISO3166['BT'] = "Bhutan";
+$phpAds_ISO3166['BO'] = "Bolivien";
+$phpAds_ISO3166['BA'] = "Bosnien und Herzegowina";
+$phpAds_ISO3166['BW'] = "Botsuana";
+$phpAds_ISO3166['BV'] = "Bouvetinsel"; 
+$phpAds_ISO3166['BR'] = "Brasilien";
+$phpAds_ISO3166['IO'] = "Britisches Territorium im Indischen Ozean"; 
+$phpAds_ISO3166['BN'] = "Brunei Darussalam";
+$phpAds_ISO3166['BG'] = "Bulgarien";
+$phpAds_ISO3166['BF'] = "Burkina Faso"; 
+$phpAds_ISO3166['BI'] = "Burundi";
+$phpAds_ISO3166['KH'] = "Kambodscha";
+$phpAds_ISO3166['CM'] = "Kamerun"; 
+$phpAds_ISO3166['CA'] = "Kanada";
+$phpAds_ISO3166['CV'] = "Kap Verde"; 
+$phpAds_ISO3166['KY'] = "Kaimaninseln";
+$phpAds_ISO3166['CF'] = "Zentralafrikanische Republik";
+$phpAds_ISO3166['TD'] = "Tschad"; 
+$phpAds_ISO3166['CL'] = "Chile";
+$phpAds_ISO3166['CN'] = "China";
+$phpAds_ISO3166['CX'] = "Weihnachtsinsel"; 
+$phpAds_ISO3166['CC'] = "Kokosinseln"; 
+$phpAds_ISO3166['CO'] = "Kolumbien";
+$phpAds_ISO3166['KM'] = "Komoren";
+$phpAds_ISO3166['CG'] = "Kongo"; 
+$phpAds_ISO3166['CD'] = "Kongo, Demokratische Republik"; 
+$phpAds_ISO3166['CK'] = "Cookinseln"; 
+$phpAds_ISO3166['CR'] = "Costa Rica";
+$phpAds_ISO3166['CI'] = "Elfenbeinküste"; 
+$phpAds_ISO3166['HR'] = "Kroatien";
+$phpAds_ISO3166['CU'] = "Kuba";
+$phpAds_ISO3166['CY'] = "Zypern";
+$phpAds_ISO3166['CZ'] = "Tschechische Republik";
+$phpAds_ISO3166['DK'] = "Dänemark";
+$phpAds_ISO3166['DJ'] = "Dschibuti";
+$phpAds_ISO3166['DM'] = "Dominica"; 
+$phpAds_ISO3166['DO'] = "Dominikanische Republik";
+$phpAds_ISO3166['TP'] = "Ost-Timor"; 
+$phpAds_ISO3166['EC'] = "Ecuador";
+$phpAds_ISO3166['EG'] = "Ägypten";
+$phpAds_ISO3166['SV'] = "El Salvador";
+$phpAds_ISO3166['GQ'] = "Äquatorialguinea"; 
+$phpAds_ISO3166['ER'] = "Eritrea";
+$phpAds_ISO3166['EE'] = "Estland";
+$phpAds_ISO3166['ET'] = "Äthiopien";
+$phpAds_ISO3166['EU'] = "Europa";
+$phpAds_ISO3166['FK'] = "Falklandinseln";
+$phpAds_ISO3166['FO'] = "Färöer"; 
+$phpAds_ISO3166['FJ'] = "Fidschi";
+$phpAds_ISO3166['FI'] = "Finnland";
+$phpAds_ISO3166['FR'] = "Frankreich";
+$phpAds_ISO3166['FX'] = "Frankreich, Metropolen"; 
+$phpAds_ISO3166['GF'] = "Französisch-Guayana"; 
+$phpAds_ISO3166['PF'] = "Französisch-Polynesien"; 
+$phpAds_ISO3166['TF'] = "Französische Süd- und Antarktisgebiete"; 
+$phpAds_ISO3166['GA'] = "Gabun"; 
+$phpAds_ISO3166['GM'] = "Gambia";
+$phpAds_ISO3166['GE'] = "Georgien";
+$phpAds_ISO3166['DE'] = "Deutschland";
+$phpAds_ISO3166['GH'] = "Ghana";
+$phpAds_ISO3166['GI'] = "Gibraltar";
+$phpAds_ISO3166['GR'] = "Griechenland";
+$phpAds_ISO3166['GL'] = "Grönland"; 
+$phpAds_ISO3166['GD'] = "Grenada"; 
+$phpAds_ISO3166['GP'] = "Guadeloupe"; 
+$phpAds_ISO3166['GU'] = "Guam"; 
+$phpAds_ISO3166['GT'] = "Guatemala";
+$phpAds_ISO3166['GN'] = "Guinea"; 
+$phpAds_ISO3166['GW'] = "Guinea-Bissau"; 
+$phpAds_ISO3166['GY'] = "Guyana";
+$phpAds_ISO3166['HT'] = "Haiti";
+$phpAds_ISO3166['HM'] = "Heard- und McDonaldinseln"; 
+$phpAds_ISO3166['VA'] = "Vatikanstadt"; 
+$phpAds_ISO3166['HN'] = "Honduras";
+$phpAds_ISO3166['HK'] = "Hong Kong";
+$phpAds_ISO3166['HU'] = "Ungarn";
+$phpAds_ISO3166['IS'] = "Island"; 
+$phpAds_ISO3166['IN'] = "Indien";
+$phpAds_ISO3166['ID'] = "Indonesien";
+$phpAds_ISO3166['IR'] = "Iran";
+$phpAds_ISO3166['IQ'] = "Irak";
+$phpAds_ISO3166['IE'] = "Irland";
+$phpAds_ISO3166['IL'] = "Israel";
+$phpAds_ISO3166['IT'] = "Italien";
+$phpAds_ISO3166['JM'] = "Jamaika";
+$phpAds_ISO3166['JP'] = "Japan";
+$phpAds_ISO3166['JO'] = "Jordanien";
+$phpAds_ISO3166['KZ'] = "Kasachstan";
+$phpAds_ISO3166['KE'] = "Kenia";
+$phpAds_ISO3166['KI'] = "Kiribati"; 
+$phpAds_ISO3166['KP'] = "Korea (Nord)"; 
+$phpAds_ISO3166['KR'] = "Korea (Süd)"; 
+$phpAds_ISO3166['KW'] = "Kuwait";
+$phpAds_ISO3166['KG'] = "Kirgisistan"; 
+$phpAds_ISO3166['LA'] = "Laos";
+$phpAds_ISO3166['LV'] = "Lettland";
+$phpAds_ISO3166['LB'] = "Libanon";
+$phpAds_ISO3166['LS'] = "Lesotho";
+$phpAds_ISO3166['LR'] = "Liberia";
+$phpAds_ISO3166['LY'] = "Libya";
+$phpAds_ISO3166['LI'] = "Liechtenstein"; 
+$phpAds_ISO3166['LT'] = "Litauen"; 
+$phpAds_ISO3166['LU'] = "Luxemburg";
+$phpAds_ISO3166['MO'] = "Macau";
+$phpAds_ISO3166['MK'] = "Mazedonien";
+$phpAds_ISO3166['MG'] = "Madagaskar"; 
+$phpAds_ISO3166['MW'] = "Malawi";
+$phpAds_ISO3166['MY'] = "Malaysia";
+$phpAds_ISO3166['MV'] = "Malediven";
+$phpAds_ISO3166['ML'] = "Mali"; 
+$phpAds_ISO3166['MT'] = "Malta";
+$phpAds_ISO3166['MH'] = "Marshallinseln"; 
+$phpAds_ISO3166['MQ'] = "Martinique"; 
+$phpAds_ISO3166['MR'] = "Mauretanien";
+$phpAds_ISO3166['MU'] = "Mauritius";
+$phpAds_ISO3166['YT'] = "Mayotte"; 
+$phpAds_ISO3166['MX'] = "Mexiko";
+$phpAds_ISO3166['FM'] = "Mikronesien"; 
+$phpAds_ISO3166['MD'] = "Moldavien";
+$phpAds_ISO3166['MC'] = "Monaco";
+$phpAds_ISO3166['MN'] = "Mongolei";
+$phpAds_ISO3166['MS'] = "Montserrat"; 
+$phpAds_ISO3166['MA'] = "Marokko";
+$phpAds_ISO3166['MZ'] = "Mosambik";
+$phpAds_ISO3166['MM'] = "Myanmar";
+$phpAds_ISO3166['NA'] = "Namibia";
+$phpAds_ISO3166['NR'] = "Nauru"; 
+$phpAds_ISO3166['NP'] = "Nepal";
+$phpAds_ISO3166['NL'] = "Niederlande";
+$phpAds_ISO3166['AN'] = "Niederländische Antillen";
+$phpAds_ISO3166['NC'] = "Neukaledonien"; 
+$phpAds_ISO3166['NZ'] = "Neuseeland";
+$phpAds_ISO3166['NI'] = "Nicaragua";
+$phpAds_ISO3166['NE'] = "Niger"; 
+$phpAds_ISO3166['NG'] = "Nigeria";
+$phpAds_ISO3166['NU'] = "Niue"; 
+$phpAds_ISO3166['NF'] = "Norfolkinsel"; 
+$phpAds_ISO3166['MP'] = "Nördliche Marianen"; 
+$phpAds_ISO3166['NO'] = "Norwegen";
+$phpAds_ISO3166['OM'] = "Oman";
+$phpAds_ISO3166['PK'] = "Pakistan";
+$phpAds_ISO3166['PW'] = "Palau"; 
+$phpAds_ISO3166['PA'] = "Panama";
+$phpAds_ISO3166['PG'] = "Papua-Neuguinea";
+$phpAds_ISO3166['PY'] = "Paraguay";
+$phpAds_ISO3166['PE'] = "Peru";
+$phpAds_ISO3166['PH'] = "Philippinen";
+$phpAds_ISO3166['PN'] = "Pitcairn"; 
+$phpAds_ISO3166['PL'] = "Polen";
+$phpAds_ISO3166['PS'] = "Palästinensische Gebiete, Besetzte";
+$phpAds_ISO3166['PT'] = "Portugal";
+$phpAds_ISO3166['PR'] = "Puerto Rico"; 
+$phpAds_ISO3166['QA'] = "Katar";
+$phpAds_ISO3166['RE'] = "Reunion"; 
+$phpAds_ISO3166['RO'] = "Rumänien";
+$phpAds_ISO3166['RU'] = "Rußlandssische Föderation";
+$phpAds_ISO3166['RW'] = "Ruanda";
+$phpAds_ISO3166['KN'] = "St. Kitts und Nevis"; 
+$phpAds_ISO3166['LC'] = "St. Lucia"; 
+$phpAds_ISO3166['VC'] = "St. Vincent und die Grenadinen"; 
+$phpAds_ISO3166['WS'] = "Samoa";
+$phpAds_ISO3166['SM'] = "San Marino";
+$phpAds_ISO3166['ST'] = "São Tomé und Príncipe"; 
+$phpAds_ISO3166['SA'] = "Saudi-Arabien";
+$phpAds_ISO3166['SN'] = "Senegal"; 
+$phpAds_ISO3166['SC'] = "Seychellen";
+$phpAds_ISO3166['SL'] = "Sierra Leone";
+$phpAds_ISO3166['SG'] = "Singapur";
+$phpAds_ISO3166['SK'] = "Slowakei";
+$phpAds_ISO3166['SI'] = "Slowenien";
+$phpAds_ISO3166['SO'] = "Somalia";
+$phpAds_ISO3166['SB'] = "Salomonen"; 
+$phpAds_ISO3166['ZA'] = "Südafrika";
+$phpAds_ISO3166['GS'] = "Südgeorgien und die Südlichen Sandwichinseln"; 
+$phpAds_ISO3166['ES'] = "Spanien";
+$phpAds_ISO3166['LK'] = "Sri Lanka";
+$phpAds_ISO3166['SH'] = "Ascension"; 
+$phpAds_ISO3166['PM'] = "St. Pierre und Miquelon"; 
+$phpAds_ISO3166['SD'] = "Sudan";
+$phpAds_ISO3166['SR'] = "Suriname";
+$phpAds_ISO3166['SJ'] = "Bäreninsel"; 
+$phpAds_ISO3166['SZ'] = "Swasiland";
+$phpAds_ISO3166['SE'] = "Schweden";
+$phpAds_ISO3166['CH'] = "Schweiz";
+$phpAds_ISO3166['SY'] = "Syrien";
+$phpAds_ISO3166['TW'] = "Taiwan";
+$phpAds_ISO3166['TJ'] = "Tadschikistan";
+$phpAds_ISO3166['TZ'] = "Tansania";
+$phpAds_ISO3166['TH'] = "Thailand";
+$phpAds_ISO3166['TG'] = "Togo"; 
+$phpAds_ISO3166['TK'] = "Tokelau"; 
+$phpAds_ISO3166['TO'] = "Tonga";
+$phpAds_ISO3166['TT'] = "Trinidad und Tobago";
+$phpAds_ISO3166['TN'] = "Tunesien";
+$phpAds_ISO3166['TR'] = "Türkei";
+$phpAds_ISO3166['TM'] = "Turkmenistan";
+$phpAds_ISO3166['TC'] = "Turks- und Caicosinseln"; 
+$phpAds_ISO3166['TV'] = "Tuvalu"; 
+$phpAds_ISO3166['UG'] = "Uganda";
+$phpAds_ISO3166['UA'] = "Ukraine";
+$phpAds_ISO3166['AE'] = "Vereinigte Arabische Emirate"; 
+$phpAds_ISO3166['GB'] = "Großbritannien"; 
+$phpAds_ISO3166['US'] = "Vereinigte Staaten";
+$phpAds_ISO3166['UM'] = "Amerikanisch-Ozeanien"; 
+$phpAds_ISO3166['UY'] = "Uruguay";
+$phpAds_ISO3166['UZ'] = "Usbekistan";
+$phpAds_ISO3166['VU'] = "Vanuatu";
+$phpAds_ISO3166['VE'] = "Venezuela";
+$phpAds_ISO3166['VN'] = "Vietnam";
+$phpAds_ISO3166['VG'] = "Jungferninseln (Britisch)"; 
+$phpAds_ISO3166['VI'] = "Jungferninseln (U.S.)"; 
+$phpAds_ISO3166['WF'] = "Wallis und Futuna"; 
+$phpAds_ISO3166['EH'] = "Westliche Sahara"; 
+$phpAds_ISO3166['YE'] = "Jemen";
+$phpAds_ISO3166['YU'] = "Jugoslawien";
+$phpAds_ISO3166['ZM'] = "Sambia";
+$phpAds_ISO3166['ZW'] = "Simbabwe";
+
+
+// MaxMind Geo-IP special codes
+$phpAds_ISO3166['A1'] = "Anonymer Proxy";
+$phpAds_ISO3166['A2'] = "Satelliten Anbieter";
+
+
+
+// Load localized strings
+if (file_exists(phpAds_path.'/language/'.$phpAds_config['language'].'/res-iso3166.lang.php'))
+	@include(phpAds_path.'/language/'.$phpAds_config['language'].'/res-iso3166.lang.php');
+
+?>
