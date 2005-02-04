036cef8cbfe3fdd7a0e925d4e7b6012e78817e45
ciaccia <ciaccia@69d48449-a624-0410-ae76-85ddf3b22ea5>
2005-02-04 15:03:14 +0000

    Fixed problems with JS delivery on IE when using non Latin encodings

--- adjs.php
+++ adjs.php
@@ -62,11 +62,7 @@ function enjavanate ($str, $limit = 60)
 		$line = substr ($str, 0, $limit);
 		$str  = substr ($str, $limit);
 		
-		$line = str_replace('\\', "\\\\", $line);
-		$line = str_replace('\'', "\\'", $line);
-		$line = str_replace("\r", '', $line);
-		$line = str_replace("\n", "\\n", $line);
-		$line = str_replace("\t", "\\t", $line);
+		$line = addcslashes($line, "\0..\37'\\@\177..\377");
 		$line = str_replace('<', "<'+'", $line);
 		
 		print "phpadsbanner += '$line';\n";
