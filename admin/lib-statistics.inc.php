871a8e207817d2733a53df28dc29f37720bc287f
ciaccia <ciaccia@69d48449-a624-0410-ae76-85ddf3b22ea5>
2005-02-04 11:53:50 +0000

    Fixed problems with IE and HTML/flash banners

--- admin/lib-statistics.inc.php
+++ admin/lib-statistics.inc.php
@@ -642,7 +642,7 @@ function phpAds_buildBannerCode ($bannerid, $fullpreview = false)
 			$activexbuffer .= "<script language='JavaScript' type='text/javascript'>\n";
 			$activexbuffer .= "<!--\n";
 			$activexbuffer .= "var phpads_activex = \"";
-			$activexbuffer .= addcslashes($buffer, "\0..\37\"");
+			$activexbuffer .= addcslashes($buffer, "\0..\37\"\\");
 			$activexbuffer .= "\";\n";
  			$activexbuffer .= "phpads_deliverActiveX(phpads_activex);\n";
  			$activexbuffer .= "//-->\n";
