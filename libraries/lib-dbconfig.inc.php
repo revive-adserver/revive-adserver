2b12dd979ad02deb2829302856416c1e72865950
ciaccia <ciaccia@69d48449-a624-0410-ae76-85ddf3b22ea5>
2005-02-08 10:58:33 +0000

    Preparing for 2.0.3 release

--- libraries/lib-dbconfig.inc.php
+++ libraries/lib-dbconfig.inc.php
@@ -18,8 +18,8 @@ define ('LIBDBCONFIG_INCLUDED', true);
 
 
 // Current phpAds version
-$phpAds_version = 200.220;
-$phpAds_version_readable = "2.0.2";
+$phpAds_version = 200.230;
+$phpAds_version_readable = "2.0.3";
 $phpAds_version_development = false;  
 $phpAds_productname = "phpAdsNew";
 $phpAds_producturl = "www.phpadsnew.com";
