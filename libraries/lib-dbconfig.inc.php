419815229d5429bd42bcd559b6d64ba61e43058a
ciaccia <ciaccia@69d48449-a624-0410-ae76-85ddf3b22ea5>
2005-05-12 06:24:02 +0000

    Preparing for 2.0.5-rc1

--- libraries/lib-dbconfig.inc.php
+++ libraries/lib-dbconfig.inc.php
@@ -18,8 +18,8 @@ define ('LIBDBCONFIG_INCLUDED', true);
 
 
 // Current phpAds version
-$phpAds_version = 200.246;
-$phpAds_version_readable = "2.0.5-dev";
+$phpAds_version = 200.247;
+$phpAds_version_readable = "2.0.5-rc1";
 $phpAds_version_development = true;  
 $phpAds_productname = "phpAdsNew";
 $phpAds_producturl = "www.phpadsnew.com";
