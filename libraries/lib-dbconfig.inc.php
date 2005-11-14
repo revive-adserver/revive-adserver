fc93c1c4efab8f3cb6b6d385728b5373ce99e7ab
ciaccia <ciaccia@69d48449-a624-0410-ae76-85ddf3b22ea5>
2005-11-14 11:49:32 +0000

    Bumped back release number

--- libraries/lib-dbconfig.inc.php
+++ libraries/lib-dbconfig.inc.php
@@ -18,8 +18,8 @@ define ('LIBDBCONFIG_INCLUDED', true);
 
 
 // Current phpAds version
-$phpAds_version = 200.270;
-$phpAds_version_readable = "2.0.7";
+$phpAds_version = 200.265;
+$phpAds_version_readable = "2.0.7 RC2";
 $phpAds_version_development = true;  
 
 $phpAds_productname = "phpAdsNew";
