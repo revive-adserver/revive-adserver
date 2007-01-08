556e33acecec9ebbfbcf4b41e56f7aea39315931
david.keen@openads.org <david.keen@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-01-08 15:03:27 +0000

    Removed @name tag from docblock so phpDocumentor works. See http://sourceforge.net/tracker/index.php?func=detail&aid=1542529&group_id=11194&atid=111194

--- lib/max/Delivery/output.php
+++ lib/max/Delivery/output.php
@@ -22,7 +22,7 @@
 | along with this program; if not, write to the Free Software               |
 | Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA |
 +---------------------------------------------------------------------------+
-$Id:$
+$Id$
 */
 
 /**
@@ -36,7 +36,6 @@ $Id:$
  * just comment out the if wrapper
  *
  * @author Monique Szpak
- * @name output.php
  * @package Max
  *
  */
