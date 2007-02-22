eaf59d1cce5c280674165cee2110afec87aa9fbb
matteo.beccati@openads.org <matteo.beccati@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-02-22 14:56:15 +0000

    svn merge -r4608:4612 https://svn.openads.org/openads/branches/max/branches/developers/chris/adselection

---
+++ simulation/data/v0.3.32-alpha_ChrisBasic.php
@@ -0,0 +1,39 @@
+<?php
+
+$aIterations[1]['request_objects'][1]   = new SimulationRequest(
+                                            "zone:1",
+                                            "",
+                                            "",
+                                            "",
+                                            "",
+                                            "",
+                                            "false",
+                                            "",
+                                            "true");
+$aIterations[1]['shuffle_requests']     = false;
+$aIterations[1]['max_requests']         = "50";
+
+$iterations = "24";
+
+for($i=2;$i<($iterations+1);$i++)
+{
+    $aIterations[$i] = $aIterations[1];
+}
+
+$precis=    '<pre>'."Chris basic scenario".'</pre>';
+
+$precis.=    '-- from database: '."max_adsel".'<br />'
+            .'-- max version: '."v0.3.32-alpha".'<br />'
+            .'-- iterations: '.$iterations.'<br />'
+            .'-- startdate: '."2000-01-01 00:00:00".'<br />'
+            .print_r($oRequest, true);
+
+$GLOBALS['_MAX']['CONF']['sim']['precis']       = $precis;
+$GLOBALS['_MAX']['CONF']['sim']['iterations']   = $iterations;
+$GLOBALS['_MAX']['CONF']['sim']['oRequest']     = $oRequest;
+$GLOBALS['_MAX']['CONF']['sim']['aIterations']  = $aIterations;
+$GLOBALS['_MAX']['CONF']['sim']['startdate']    = "2000-01-01 00:00:00";
+$GLOBALS['_MAX']['CONF']['sim']['startday']     = "2000-01-01";
+$GLOBALS['_MAX']['CONF']['sim']['starthour']    = "0";
+
+?>
