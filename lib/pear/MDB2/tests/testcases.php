b178e3227985ea05a82eac847bce82e7820ed6dd
monique.szpak@openads.org <monique.szpak@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-02-22 16:39:10 +0000

    changes to test_setup for MDB2 and MDB2_Schema

---
+++ lib/pear/MDB2/tests/testcases.php
@@ -0,0 +1,18 @@
+<?php
+
+$testcases = array(
+    'MDB2_internals_testcase',
+    'MDB2_api_testcase',
+    'MDB2_usage_testcase',
+    'MDB2_bugs_testcase',
+    'MDB2_extended_testcase',
+    'MDB2_datatype_testcase',
+    'MDB2_datatype_mysql_testcase',
+    'MDB2_declaration_mysql_testcase',
+    'MDB2_manager_testcase',
+    'MDB2_reverse_testcase',
+    'MDB2_function_testcase',
+    'MDB2_native_testcase',
+);
+
+?>
---
+++ lib/pear/MDB2_Schema/tests/testcases.php
@@ -0,0 +1,10 @@
+<?php
+
+// uncomment this line if you want to run both MDB2 and MDB2_Schema from a CVS checkout
+#ini_set('include_path', '../../MDB2/'.PATH_SEPARATOR.'..'.PATH_SEPARATOR.ini_get('include_path'));
+
+$testcases = array(
+    'MDB2_Schema_testcase',
+);
+
+?>
