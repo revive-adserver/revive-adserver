dbefd01095f62b1f3c65942542ba9dadfadea3f5
andrew.hill@openads.org <andrew.hill@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-03-05 15:58:09 +0000

    Added a new custom MDB2 datatype "openads_text", which ensures that current Openads text fields are defined in MySQL via the XML schema in the same way as they are currently defined in the SQL file. (This is the "wrong" way, but for now, this allows BC with the existing schema; this custom type will need to be removed once the schema is refactored into a sane state.) Refs #152.

--- lib/openads/Dal/CustomDatatypes/mysql.php
+++ lib/openads/Dal/CustomDatatypes/mysql.php
@@ -44,6 +44,7 @@ $aDatatypes = array(
     'openads_enum'      => 'openads_enum',
     'openads_mediumint' => 'openads_mediumint',
     'openads_set'       => 'openads_set',
+    'openads_text'      => 'openads_text',
     'openads_varchar'   => 'openads_varchar'
 );
 
@@ -56,6 +57,7 @@ $aNativetypes = array(
     'enum',
     'mediumint',
     'set',
+    'text',
     'varchar'
 );
 
@@ -101,7 +103,7 @@ function datatype_openads_char_callback(&$db, $method, $aParameters)
             $datatype = $db->datatype->mapPrepareDatatype($aParameters['type']);
             $declaration_options = $db->datatype->_getDeclarationOptions($aParameters['field']);
             $value = $name . ' ' . $datatype;
-            if (is_int($aParameters['field']['length'])) {
+            if (is_numeric($aParameters['field']['length'])) {
                 $value .= '(' . $aParameters['field']['length'] . ')';
             }
             $value .= $declaration_options;
@@ -217,7 +219,7 @@ function datatype_openads_mediumint_callback(&$db, $method, $aParameters)
             $datatype = $db->datatype->mapPrepareDatatype($aParameters['type']);
             $declaration_options = $db->datatype->_getDeclarationOptions($aParameters['field']);
             $value = $name . ' ' . $datatype;
-            if (is_int($aParameters['field']['length'])) {
+            if (is_numeric($aParameters['field']['length'])) {
                 $value .= '(' . $aParameters['field']['length'] . ')';
             }
             $value .= $declaration_options;
@@ -292,6 +294,66 @@ function datatype_openads_set_callback(&$db, $method, $aParameters)
 }
 
 /**
+ * A callback function to map the MDB2 datatype "openads_text" into
+ * the MySQL nativetype "TEXT".
+ *
+ * @param MDB2   $db         The MDB2 database reource object.
+ * @param string $method     The name of the MDB2_Driver_Datatype_Common method
+ *                           the callback function was called from. One of
+ *                           "getValidTypes", "convertResult", "getDeclaration",
+ *                           "quote" and "mapPrepareDatatype". See
+ *                           {@link MDB2_Driver_Datatype_Common} for the details
+ *                           of what each method does.
+ * @param array $aParameters An array of parameters, being the parameters that
+ *                           were passed to the method calling the callback
+ *                           function.
+ * @return mixed Returns the appropriate value depending on the method that
+ *               called the function. See {@link MDB2_Driver_Datatype_Common}
+ *               for details of the expected return values of the five possible
+ *               calling methods.
+ */
+function datatype_openads_text_callback(&$db, $method, $aParameters)
+{
+    // Ensure the datatype module is loaded
+    if (is_null($db->datatype)) {
+        $db->loadModule('Datatype', null, true);
+    }
+    // Lowercase method names for PHP4/PHP5 compatibility
+    $method = strtolower($method);
+    switch($method) {
+        case 'getvalidtypes':
+            // Return the default value for this custom datatype
+            return null;
+        case 'convertresult':
+            // Convert the nativetype value to a datatype value using the
+            // built in "text" datatype
+            return $db->datatype->convertResult($aParameters['value'], 'text', $aParameters['rtrim']);
+        case 'getdeclaration':
+            // Prepare and return the MySQL specific code needed to declare
+            // a column of this custom datatype
+            $name = $db->quoteIdentifier($aParameters['name'], true);
+            $datatype = $db->datatype->mapPrepareDatatype($aParameters['type']);
+            $declaration_options = $db->datatype->_getDeclarationOptions($aParameters['field']);
+            $value = $name . ' ' . $datatype;
+            if (is_numeric($aParameters['field']['length'])) {
+                $value .= '(' . $aParameters['field']['length'] . ')';
+            }
+            // Strip out any "DEFAULT NULL" value from the options
+            $declaration_options = preg_replace('/DEFAULT NULL /', $declaration_options);
+            $value .= $declaration_options;
+            return $value;
+        case 'quote':
+            // Convert the datatype value into a quoted nativetype value
+            // suitable for inserting into MySQL using the built in
+            // "text" datatype
+            return $db->datatype->quote($aParameters['value'], 'text');
+        case 'mappreparedatatype':
+            // Return the MySQL nativetype declaration for this custom datatype
+            return 'TEXT';
+    }
+}
+
+/**
  * A callback function to map the MDB2 datatype "openads_varchar" into
  * the MySQL nativetype "VARCHAR".
  *
@@ -333,7 +395,7 @@ function datatype_openads_varchar_callback(&$db, $method, $aParameters)
             $datatype = $db->datatype->mapPrepareDatatype($aParameters['type']);
             $declaration_options = $db->datatype->_getDeclarationOptions($aParameters['field']);
             $value = $name . ' ' . $datatype;
-            if (is_int($aParameters['field']['length'])) {
+            if (is_numeric($aParameters['field']['length'])) {
                 $value .= '(' . $aParameters['field']['length'] . ')';
             }
             $value .= $declaration_options;
@@ -531,6 +593,52 @@ function nativetype_set_callback(&$db, $aFields)
     $unsigned = null;
     // No fixed value needed
     $fixed = null;
+    return array($aType, $length, $unsigned, $fixed);
+}
+
+/**
+ * A callback function to map the MySQL nativetype "TEXT" into
+ * the extended MDB2 datatype "openads_text".
+ *
+ * @param MDB2 $db       The MDB2 database reource object.
+ * @param array $aFields The standard array of fields produced from the
+ *                       MySQL command "SHOW COLUMNS". See
+ *                       {@link http://dev.mysql.com/doc/refman/5.0/en/describe.html}
+ *                       for more details on the format of the fields.
+ *                          "type"      The nativetype column type
+ *                          "null"      "YES" or "NO"
+ *                          "key"       "PRI", "UNI", "MUL", or null
+ *                          "default"   The default value of the column
+ *                          "extra"     "auto_increment", or null
+ * @return array Returns an array of the following items:
+ *                  0 => An array of possible MDB2 datatypes. As this is
+ *                       a custom type, always has one entry, "openads_text".
+ *                  1 => The length of the type, if defined by the nativetype,
+ *                       otherwise null.
+ *                  2 => A boolean value indicating the "unsigned" nature of numeric
+ *                       fields. Always null in this case, as the type is not numeric.
+ *                  3 => A boolean value indicating the "fixed" nature of text
+ *                       fields. Always false in this case, as text is not
+ *                       of fixed length.
+ */
+function nativetype_text_callback(&$db, $aFields)
+{
+    // Prepare the type array
+    $aType = array();
+    $aType[] = 'openads_text';
+    // Can the length of the TEXT field be found?
+    $length = null;
+    $start = strpos($aFields['type'], '(');
+    $end = strpos($aFields['type'], ')');
+    if ($start && $end) {
+        $start++;
+        $chars = $end - $start;
+        $length = substr($aFields['type'], $start, $chars);
+    }
+    // No unsigned value needed
+    $unsigned = null;
+    // Set fixed to false
+    $fixed = false;
     return array($aType, $length, $unsigned, $fixed);
 }
 
