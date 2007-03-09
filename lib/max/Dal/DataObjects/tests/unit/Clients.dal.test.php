40c90703d24ad0549816e4f244acc2be42ae4e80
david.keen@openads.org <david.keen@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-03-09 11:34:35 +0000

    Added unit testing class for Clients DO. Removed unused included files from advertiser-delete.php. Refs #196

---
+++ lib/max/Dal/DataObjects/tests/unit/Clients.dal.test.php
@@ -0,0 +1,117 @@
+<?php
+/*
++---------------------------------------------------------------------------+
+| Max Media Manager v0.3                                                    |
+| =================                                                         |
+|                                                                           |
+| Copyright (c) 2003-2006 m3 Media Services Limited                         |
+| For contact details, see: http://www.m3.net/                              |
+|                                                                           |
+| This program is free software; you can redistribute it and/or modify      |
+| it under the terms of the GNU General Public License as published by      |
+| the Free Software Foundation; either version 2 of the License, or         |
+| (at your option) any later version.                                       |
+|                                                                           |
+| This program is distributed in the hope that it will be useful,           |
+| but WITHOUT ANY WARRANTY; without even the implied warranty of            |
+| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             |
+| GNU General Public License for more details.                              |
+|                                                                           |
+| You should have received a copy of the GNU General Public License         |
+| along with this program; if not, write to the Free Software               |
+| Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA |
++---------------------------------------------------------------------------+
+$Id$
+*/
+
+require_once MAX_PATH . '/lib/max/Dal/DataObjects/Clients.php';
+require_once 'DataObjectsUnitTestCase.php';
+
+/**
+ * A class for testing non standard DataObjects_Clients methods
+ *
+ * @package    MaxDal
+ * @subpackage TestSuite
+ *
+ */
+class DataObjects_ClientsTest extends DataObjectsUnitTestCase
+{
+     /**
+     * The constructor method.
+     */
+    function DataObjects_ClientsTest()
+    {
+        $this->UnitTestCase();
+    }
+    
+    /**
+     * Tests advertiser and any linked objects are deleted.
+     *
+     */
+    function testDelete()
+    {
+        // Insert advertiser
+        $doClients = MAX_DB::factoryDO('clients');
+        $doClients->clientname = 'test advertiser';
+        $doClients->agencyid = 0;
+        $clientId = DataGenerator::generateOne($doClients);
+        
+        // Insert advertiser_preferences
+        $doPreferenceAdvertiser = MAX_DB::factoryDO('preference_advertiser');
+        $doPreferenceAdvertiser->advertiser_id = $clientId;
+        $doPreferenceAdvertiser->preference = 'foo';
+        $doPreferenceAdvertiser->value = 'bar';
+        $preferenceAdvertiserId = DataGenerator::generateOne($doPreferenceAdvertiser);
+        
+        // Insert campaigns
+        $doCampaigns = MAX_DB::factoryDO('campaigns');
+        $doCampaigns->clientid = $clientId;
+        $aCampaignId = DataGenerator::generate($doCampaigns, 2);
+        
+        // Insert linked banners
+        $doBanners = MAX_DB::factoryDO('banners');
+        $doBanners->campaignid = $campaignId;
+        $aBannerId = DataGenerator::generate($doBanners, 2);
+        
+        // Insert linked trackers
+        $doTrackers = MAX_DB::factoryDO('trackers');
+        $doTrackers->clientid = $clientId;
+        $aTrackerId = DataGenerator::generate($doTrackers, 2);
+        
+        // Call delete on the inserted client.
+        $doClients = MAX_DB::staticGetDO('clients', $clientId);
+        $doClients->delete();
+        
+        // Check advertiser is deleted
+        $doClients = MAX_DB::staticGetDO('clients', $clientId);
+        $this->assertFalse($doClients, 'Client should not exist');
+        
+        // Check all campaigns are deleted
+        $doCampaigns = MAX_DB::factoryDO('campaigns');
+        $doCampaigns->clientid = $clientId;
+        $doCampaigns->find();
+        $this->assertEqual($doCampaigns->getRowCount(), 0, 'No campaigns should be found');
+        
+        // Check all banners are deleted
+        $doBanners = MAX_DB::factoryDO('banners');
+        $doBanners->whereAdd('campaignid in (' . implode(',', $aCampaignId) . ')');
+        $doBanners->find();
+        $this->assertEqual($doCampaigns->getRowCount(), 0, 'No banners should be found');
+
+        // Check all trackers are deleted
+        $doTrackers = MAX_DB::factoryDO('trackers');
+        $doTrackers->client = $clientId;
+        $doTrackers->find();
+        $this->assertEqual($doTrackers->getRowCount(), 0, 'No trackers should be found');
+        
+        // Check all preferences are deleted
+        $doPreferenceAdvertiser = MAX_DB::factoryDO('preference_advertiser');
+        $doPreferenceAdvertiser->advertiserid = $clientId;
+        $doPreferenceAdvertiser->find();
+        $this->assertEqual($doPreferenceAdvertiser->getRowCount(), 0, 'No advertiser preferences should be found');
+        
+        // Restore env
+        TestEnv::restoreEnv();
+    }
+    
+}
--- www/admin/advertiser-delete.php
+++ www/admin/advertiser-delete.php
@@ -33,9 +33,6 @@ require_once '../../init.php';
 
 // Required files
 require_once MAX_PATH . '/www/admin/config.php';
-require_once MAX_PATH . '/www/admin/lib-storage.inc.php';
-require_once MAX_PATH . '/www/admin/lib-zones.inc.php';
-require_once MAX_PATH . '/www/admin/lib-statistics.inc.php';
 require_once MAX_PATH . '/lib/max/Maintenance/Priority.php';
 require_once MAX_PATH . '/lib/max/DB.php';
 
@@ -58,7 +55,7 @@ if (isset($session['prefs']['advertiser-index.php']['nodes'])) {
 /* Main code                                             */
 /*-------------------------------------------------------*/
 
-if (isset($clientid) && $clientid != '') {
+if (!empty($clientid)) {
     $doClients = MAX_DB::factoryDO('clients');
     $doClients->clientid = $clientid;
     $doClients->delete();
