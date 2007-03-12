0fdf29e592b237d6efe83fcbcb5ef30d3eb71717
david.keen@openads.org <david.keen@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-03-12 15:40:58 +0000

    Added testDelete()

--- lib/max/Dal/DataObjects/tests/unit/DB_DataObjectCommon.dal.test.php
+++ lib/max/Dal/DataObjects/tests/unit/DB_DataObjectCommon.dal.test.php
@@ -42,4 +42,74 @@ class DB_DataObjectCommonTest extends UnitTestCase
     {
         $this->UnitTestCase();
     }
+    
+       /**
+     * Tests advertiser and any linked objects are deleted.
+     *
+     */
+    function testDelete()
+    {
+        // Insert advertiser
+        $doClients = MAX_DB::factoryDO('clients');
+        $doClients->clientname = 'test advertiser';
+        $doClients->agencyid = 0;
+        $clientId = DataGenerator::generateOne($doClients);
+        
+        // Insert advertiser_preferences
+        $doPreferenceAdvertiser = MAX_DB::factoryDO('preference_advertiser');
+        $doPreferenceAdvertiser->advertiser_id = $clientId;
+        $doPreferenceAdvertiser->preference = 'foo';
+        $doPreferenceAdvertiser->value = 'bar';
+        $preferenceAdvertiserId = DataGenerator::generateOne($doPreferenceAdvertiser);
+        
+        // Insert campaigns
+        $doCampaigns = MAX_DB::factoryDO('campaigns');
+        $doCampaigns->clientid = $clientId;
+        $aCampaignId = DataGenerator::generate($doCampaigns, 2);
+        
+        // Insert linked banners
+        $doBanners = MAX_DB::factoryDO('banners');
+        $doBanners->campaignid = $campaignId;
+        $aBannerId = DataGenerator::generate($doBanners, 2);
+        
+        // Insert linked trackers
+        $doTrackers = MAX_DB::factoryDO('trackers');
+        $doTrackers->clientid = $clientId;
+        $aTrackerId = DataGenerator::generate($doTrackers, 2);
+        
+        // Call delete on the inserted client.
+        $doClients = MAX_DB::staticGetDO('clients', $clientId);
+        $doClients->delete();
+        
+        // Check advertiser is deleted
+        $doClients = MAX_DB::staticGetDO('clients', $clientId);
+        $this->assertFalse($doClients, 'Client should not exist');
+        
+        // Check all campaigns are deleted
+        $doCampaigns = MAX_DB::factoryDO('campaigns');
+        $doCampaigns->clientid = $clientId;
+        $doCampaigns->find();
+        $this->assertEqual($doCampaigns->getRowCount(), 0, 'No campaigns should be found');
+        
+        // Check all banners are deleted
+        $doBanners = MAX_DB::factoryDO('banners');
+        $doBanners->whereAdd('campaignid in (' . implode(',', $aCampaignId) . ')');
+        $doBanners->find();
+        $this->assertEqual($doCampaigns->getRowCount(), 0, 'No banners should be found');
+
+        // Check all trackers are deleted
+        $doTrackers = MAX_DB::factoryDO('trackers');
+        $doTrackers->client = $clientId;
+        $doTrackers->find();
+        $this->assertEqual($doTrackers->getRowCount(), 0, 'No trackers should be found');
+        
+        // Check all preferences are deleted
+        $doPreferenceAdvertiser = MAX_DB::factoryDO('preference_advertiser');
+        $doPreferenceAdvertiser->advertiserid = $clientId;
+        $doPreferenceAdvertiser->find();
+        $this->assertEqual($doPreferenceAdvertiser->getRowCount(), 0, 'No advertiser preferences should be found');
+        
+        // Restore env
+        TestEnv::restoreEnv();
+    }
 }
