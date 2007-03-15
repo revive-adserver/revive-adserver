137613b964dea8cf3529330a9338c4c3a3cee31f
david.keen@openads.org <david.keen@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-03-15 17:35:21 +0000

    Added DataObjects_ImagesTest. Refs #196

---
+++ lib/max/Dal/DataObjects/tests/unit/Images.dal.test.php
@@ -0,0 +1,68 @@
+<?php
+/*
++---------------------------------------------------------------------------+
+| Max Media Manager v0.3                                                    |
+| =================                                                         |
+|                                                                           |
+| Copyright (c) 2003-2006 m3 Media Services Limited                         |
+| For contact details, see: http://www.m3.net/                              |
+|                                                                           |
+| This program is free software; you can redistribute it and/or modify      |
+| it under the terms of the GNU General Public License as published by      |
+| the Free Software Foundation; either version 2 of the License, or         |
+| (at your option) any later version.                                       |
+|                                                                           |
+| This program is distributed in the hope that it will be useful,           |
+| but WITHOUT ANY WARRANTY; without even the implied warranty of            |
+| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             |
+| GNU General Public License for more details.                              |
+|                                                                           |
+| You should have received a copy of the GNU General Public License         |
+| along with this program; if not, write to the Free Software               |
+| Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA |
++---------------------------------------------------------------------------+
+$Id$
+*/
+
+require_once MAX_PATH . '/lib/max/Dal/tests/util/DalUnitTestCase.php';
+
+/**
+ * A class for testing non standard DataObjects_Images methods
+ *
+ * @package    MaxDal
+ * @subpackage TestSuite
+ *
+ */
+class DataObjects_ImagesTest extends DalUnitTestCase
+{
+    /**
+     * The constructor method.
+     */
+    function DataObjects_ImagesTest()
+    {
+        $this->UnitTestCase();
+    }
+    
+    function tearDown()
+    {
+        DataGenerator::cleanUp();
+    }
+    
+    function testGetUniqueFileNameForDuplication()
+    {
+        // Insert an image
+        $doImages = MAX_DB::factoryDO('images');
+        $doImages->filename = 'foo.jpg';
+        $doImages->contents = '';
+        DataGenerator::generateOne($doImages);
+        
+        // Get the image out of the DB
+        $doImages = MAX_DB::staticGetDO('images', 'filename', 'foo.jpg');
+
+        // Check it
+        $expected = 'foo_2.jpg';
+        $this->assertEqual($doImages->getUniqueFileNameForDuplication(), $expected);
+    }
+    
+}
+?>
