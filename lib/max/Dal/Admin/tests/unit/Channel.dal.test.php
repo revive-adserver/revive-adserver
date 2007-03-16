1127de888e1337c54ebe7831c1ae946b10a59217
david.keen@openads.org <david.keen@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-03-16 15:05:55 +0000

    Added Channel.dal.test.php. Refs #227

---
+++ lib/max/Dal/Admin/tests/unit/Channel.dal.test.php
@@ -0,0 +1,78 @@
+<?php
+/*
++---------------------------------------------------------------------------+
+| Max Media Manager v0.3                                                    |
+| =================                                                         |
+|                                                                           |
+| Copyright (c) 2003-2006 m3 Media Services Limited                         |
+| For contact details, see: http://www.m3.net/                              |
+|                                                                           |
+| This program is free software; you can redistribute it and/or modify      |
+| it under the terms of the GNU General Public License as published by      |
+| the Free Software Foundation; either version 2 of the License, or         |
+| (at your option) any later version.                                       |
+|                                                                           |
+| This program is distributed in the hope that it will be useful,           |
+| but WITHOUT ANY WARRANTY; without even the implied warranty of            |
+| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             |
+| GNU General Public License for more details.                              |
+|                                                                           |
+| You should have received a copy of the GNU General Public License         |
+| along with this program; if not, write to the Free Software               |
+| Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA |
++---------------------------------------------------------------------------+
+$Id$
+*/
+
+require_once MAX_PATH . '/lib/max/Dal/tests/util/DalUnitTestCase.php';
+
+/**
+ * A class for testing DAL Channel methods
+ *
+ * @package    MaxDal
+ * @subpackage TestSuite
+ *
+ */
+class MAX_Dal_Admin_ChannelTest extends DalUnitTestCase
+{
+    var $dalChannel;
+    
+    /**
+     * The constructor method.
+     */
+    function MAX_Dal_Admin_ChannelTest()
+    {
+        $this->UnitTestCase();
+    }
+    
+    function setUp()
+    {
+        $this->dalChannel = MAX_DB::factoryDAL('channel');
+    }
+    
+    function tearDown()
+    {
+        DataGenerator::cleanUp();
+    }
+    
+    function testGetChannelsAndAffiliates()
+    {
+        // Insert 2 channels
+        $aChannelId = DataGenerator::generate('channel', 2, true);
+        
+        // Check the correct number of rows returned
+        $expectedRows = 2;
+        $rsChannel = $this->dalChannel->getChannelsAndAffiliates();
+        $rsChannel->find();
+        $actualRows = $rsChannel->getRowCount();
+        $this->assertEqual($actualRows, $expectedRows);
+        
+        // Check each row has the correct number of fields
+        $rsChannel->fetch();
+        $aChannel = $rsChannel->export();
+        $this->assertEqual(count($aChannel), 4);
+    }
+    
+    
+}
+?>
