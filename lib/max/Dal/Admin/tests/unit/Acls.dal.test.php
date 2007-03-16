cc88f946a9aacfa745902da70350b62cd073d35a
radek.maciaszek@openads.org <radek.maciaszek@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-03-16 14:43:57 +0000

    Added Acls tests

---
+++ lib/max/Dal/Admin/tests/unit/Acls.dal.test.php
@@ -0,0 +1,89 @@
+<?php
+/*
++---------------------------------------------------------------------------+
+| Max Media Manager v0.3                                                    |
+| =================                                                         |
+|                                                                           |
+| Copyright (c) 2003-2006 m3 Media Services Limited                         |
+| For contact details, see: http://www.m3.net/                              |
+|                                                                           |
+| This program is free software; you can redistribute it and/or modify      |
+| it under the terms of the GNU General Public License as published by      |
+| the Free Software Foundation; either version 2 of the License, or         |
+| (at your option) any later version.                                       |
+|                                                                           |
+| This program is distributed in the hope that it will be useful,           |
+| but WITHOUT ANY WARRANTY; without even the implied warranty of            |
+| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             |
+| GNU General Public License for more details.                              |
+|                                                                           |
+| You should have received a copy of the GNU General Public License         |
+| along with this program; if not, write to the Free Software               |
+| Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA |
++---------------------------------------------------------------------------+
+$Id$
+*/
+
+require_once MAX_PATH . '/lib/max/Dal/tests/util/DalUnitTestCase.php';
+require_once MAX_PATH . '/lib/max/Dal/Admin/Acls.php';
+
+/**
+ * A class for testing DAL Acls methods
+ *
+ * @package    MaxDal
+ * @subpackage TestSuite
+ *
+ */
+class MAX_Dal_Admin_AclsTest extends DalUnitTestCase
+{
+    var $dalAcls;
+    
+    /**
+     * The constructor method.
+     */
+    function MAX_Dal_Admin_AclsTest()
+    {
+        $this->UnitTestCase();
+    }
+    
+    function setUp()
+    {
+        $this->dalAcls = MAX_DB::factoryDAL('acls');
+    }
+    
+    function tearDown()
+    {
+        DataGenerator::cleanUp();
+    }
+    
+    function testGetAclsByDataValueType()
+    {
+        $type = 'Site:Channel';
+        $bannerId = 1;
+        
+        // Test it returns empty set if no data exists
+        $rsChannel = $this->dalAcls->getAclsByDataValueType($bannerId, $type);
+    	$rsChannel->reset();
+    	$this->assertEqual($rsChannel->getRowCount(), 0);
+    	
+    	// Generate acls, two of them with the same $bannerId
+    	$data = array(
+    	   'bannerid' => array($bannerId,$bannerId,3)
+    	);
+    	$dg = new DataGenerator();
+    	$dg->setData('acls', $data);
+    	
+    	// Add test data
+    	$doAcls = MAX_DB::factoryDO('acls');
+    	$doAcls->type = $type;
+    	$doAcls->data = implode(',', $data['bannerid']) . ',5,76';
+    	$dg->generate($doAcls, 3);
+    	
+    	// Test that $bannerId is in two sets
+        $rsChannel = $this->dalAcls->getAclsByDataValueType($bannerId, $type);
+    	$rsChannel->reset();
+    	$this->assertEqual($rsChannel->getRowCount(), 2);
+    }
+    
+}
+?>
