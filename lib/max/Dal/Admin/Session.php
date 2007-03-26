1a89e7c33da8d93ab5e9b2bd9a84c863af7e89e6
matteo.beccati@openads.org <matteo.beccati@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-03-26 16:30:52 +0000

    Fixed lib/max/Dal/Admin/Session.php so that it is MDB2 compatible

--- lib/max/Dal/Admin/Session.php
+++ lib/max/Dal/Admin/Session.php
@@ -8,18 +8,18 @@ require_once MAX_PATH . '/lib/max/Dal/Common.php';
 
 /**
  * A data access layer for storing and retrieving persistent Web sessions.
- * 
+ *
  * @todo Factor out the repetitive "session_table_name" calculation
  */
 class MAX_Dal_Admin_Session extends MAX_Dal_Common
 {
     var $table = 'session';
-	
+
 	/**
      * @param string $session_id
      * @return string A serialized array (probably)
-     * 
-     * @todo Consider raise an error when no session is found. 
+     *
+     * @todo Consider raise an error when no session is found.
      */
     function getSerializedSession($session_id)
     {
@@ -35,20 +35,20 @@ class MAX_Dal_Admin_Session extends MAX_Dal_Common
                 AND UNIX_TIMESTAMP(NOW())-UNIX_TIMESTAMP(lastused) < 3600
             ";
         $query_params = array($session_id);
-        $serialized_session_data = $this->dbh->getOne($query, $query_params);
+        $serialized_session_data = $this->oDbh->extended->getOne($query, null, $query_params);
         return $serialized_session_data;
     }
 
     /**
      * Reset "last used" timestamp on a session to prevent it from timing out.
-     * 
+     *
      * @param string $session_id
      * @return void
      */
     function refreshSession($session_id)
     {
         $conf = $GLOBALS['_MAX']['CONF'];
-        $session_table_name = $conf['table']['prefix'] . $conf['table']['session']; 
+        $session_table_name = $conf['table']['prefix'] . $conf['table']['session'];
         $query = "
                     UPDATE
                         $session_table_name
@@ -58,14 +58,14 @@ class MAX_Dal_Admin_Session extends MAX_Dal_Common
                         sessionid = ?
                     ";
         $query_params = array($session_id);
-        $result = $this->dbh->query($query, $query_params);
+        $result = $this->oDbh->extended->execParam($query, $query_params);
     }
-    
+
     /**
      * @param string $serialized_session_data
      * @param string $session_id
-     * 
-     * @todo Use ANSI SQL syntax, such as an UPDATE/INSERT cycle.  
+     *
+     * @todo Use ANSI SQL syntax, such as an UPDATE/INSERT cycle.
      * @todo Push down REPLACE INTO into a MySQL-specific DAL.
      */
     function storeSerializedSession($serialized_session_data, $session_id)
@@ -79,12 +79,12 @@ class MAX_Dal_Admin_Session extends MAX_Dal_Common
                 (?, ?)
             ";
         $query_params = array($session_id, $serialized_session_data);
-        $this->dbh->query($query, $query_params);
+        $this->oDbh->extended->execParam($query, $query_params);
     }
 
     /**
      * Remove many unused sessions from storage.
-     * 
+     *
      * @todo Use ANSI SQL syntax, such as NOW() + INTERVAL '12' HOUR
      */
     function pruneOldSessions()
@@ -96,9 +96,9 @@ class MAX_Dal_Admin_Session extends MAX_Dal_Common
                 WHERE
                     UNIX_TIMESTAMP(NOW()) - UNIX_TIMESTAMP(lastused) > 43200
                 ";
-        $this->dbh->query($query);
+        $this->oDbh->query($query);
     }
-    
+
     /**
      * Remove a specific session from storage.
      */
@@ -111,7 +111,7 @@ class MAX_Dal_Admin_Session extends MAX_Dal_Common
            WHERE sessionid=?
            ";
         $query_params = array($session_id);
-        $this->dbh->query($query, $query_params);
+        $this->oDbh->extended->execParam($query, $query_params);
     }
 
 }
