4781f0fabe46f6aa36f17b3b111e695e28544f64
matteo.beccati@openads.org <matteo.beccati@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-03-26 16:57:30 +0000

    Refactored maintenance/maintenance.php moving the code inside lib/max/Maintenance.php (refs #228)

--- lib/max/Maintenance.php
+++ lib/max/Maintenance.php
@@ -25,7 +25,15 @@
 $Id$
 */
 
+require_once MAX_PATH . '/lib/Max.php';
 require_once MAX_PATH . '/lib/max/core/ServiceLocator.php';
+require_once MAX_PATH . '/lib/max/DB.php';
+require_once MAX_PATH . '/lib/max/Maintenance/Priority.php';
+require_once MAX_PATH . '/lib/max/Maintenance/Statistics.php';
+require_once MAX_PATH . '/lib/max/OperationInterval.php';
+require_once MAX_PATH . '/lib/max/other/lib-reports.inc.php';
+require_once MAX_PATH . '/scripts/maintenance/translationStrings.php';
+require_once 'Date.php';
 
 /**
  * A library class for providing common maintenance process methods.
@@ -36,6 +44,34 @@ require_once MAX_PATH . '/lib/max/core/ServiceLocator.php';
  */
 class MAX_Maintenance
 {
+    var $oDbh;
+    var $conf;
+    var $pref;
+
+    /**
+     * PHP5 constructor
+     */
+    function __construct()
+    {
+        $this->conf =& $GLOBALS['_MAX']['CONF'];
+        $this->pref =& $GLOBALS['_MAX']['PREF'];
+
+        // Get a connection to the datbase
+        $this->oDbh = &MAX_DB::singleton();
+        if (PEAR::isError($this->oDbh)) {
+            // Unable to continue!
+            MAX::raiseError($this->oDbh, null, PEAR_ERROR_DIE);
+        }
+    }
+
+    /**
+     * PHP4 constructor
+     */
+
+    function MAX_Maintenance()
+    {
+       $this->__construct();
+    }
 
     /**
      * A method for premaring e-mails, advising of the activation of campaigns.
@@ -136,6 +172,200 @@ class MAX_Maintenance
         return $message;
     }
 
+    /**
+     * A method to run maintenance
+     */
+    function run()
+    {
+        MAX::debug('Running Maintenance Statistics and Priority', PEAR_LOG_INFO);
+
+        // Record the current time, and register with the ServiceLocator
+        $oDate = new Date();
+        $oServiceLocator = &ServiceLocator::instance();
+        $oServiceLocator->register('now', $oDate);
+
+        // Check the operation interval is valid
+        $result = MAX_OperationInterval::checkOperationIntervalValue($this->conf['maintenance']['operationInterval']);
+        if (PEAR::isError($result)) {
+            // Unable to continue!
+            MAX::raiseError($result, null, PEAR_ERROR_DIE);
+        }
+
+        // Create lockfile, if required
+        $this->getLock();
+
+        // Run the Maintenance Statistics Engine (MSE) process
+        $this->runMSE();
+
+        // Run Midnight phpAdsNew Tasks
+        $this->runMidnightTasks();
+
+        // Run the Maintenance Priority Engine (MPE) process, ensuring that the
+        // process always runs, even if instant update of priorities is disabled
+        $this->runMPE();
+
+        // Remove lockfile, if required
+        $this->releaseLock();
+
+        // Update the timestamp (for old maintenance code)
+        $this->updateLastRun();
+
+        MAX::debug('Maintenance Statistics and Priority Completed', PEAR_LOG_INFO);
+    }
+
+    /**
+     * A method to run MSE
+     */
+    function runMSE()
+    {
+        MAX_Maintenance_Statistics::run();
+    }
+
+    /**
+     * A method to run midnight maintenance tasks
+     */
+    function runMidnightTasks()
+    {
+        if (date('H') == 0) {
+            MAX::debug('Running Midnight Maintenance Tasks', PEAR_LOG_INFO);
+            $this->runReports();
+            $this->runOpenadsSync();
+            MAX::debug('Midnight Maintenance Tasks Completed', PEAR_LOG_INFO);
+        }
+    }
+
+    /**
+     * A method to run MPE
+     */
+    function runMPE()
+    {
+        MAX_Maintenance_Priority::run(true);
+    }
+
+    /**
+     * A method to send reports during maintenance
+     */
+    function runReports()
+    {
+        MAX::debug('  Starting to send advertiser reports.', PEAR_LOG_DEBUG);
+        $query = "
+            SELECT
+                clientid,
+                report,
+                reportinterval,
+                reportlastdate,
+                UNIX_TIMESTAMP(reportlastdate) AS reportlastdate_t
+            FROM
+                {$this->conf['table']['prefix']}{$this->conf['table']['clients']}
+            WHERE
+                report = 't'";
+        MAX::debug('  Getting details of when advertiser reports were last sent.', PEAR_LOG_DEBUG);
+        $rResult = phpAds_dbQuery($query);
+        if (phpAds_dbNumRows($rResult) > 0) {
+            while ($aAdvertiser = phpAds_dbFetchArray($rResult)) {
+                // Determine date of interval days ago
+                $intervaldaysago = mktime(0, 0, 0, date('m'), date('d'), date('Y')) - ($aAdvertiser['reportinterval'] * (60 * 60 * 24));
+                // Check if the date interval has been reached
+                if (($aAdvertiser['reportlastdate_t'] <= $intervaldaysago && $aAdvertiser['reportlastdate'] != '0000-00-00') || ($aAdvertiser['reportlastdate'] == '0000-00-00')) {
+                    // Determine first and last date
+                    $last_unixtimestamp  = mktime(0, 0, 0, date('m'), date('d'), date('Y')) - 1;
+                    $first_unixtimestamp = $aAdvertiser['reportlastdate_t'];
+                    // Send the advertiser's report
+                    phpAds_SendMaintenanceReport($aAdvertiser['clientid'], $first_unixtimestamp, $last_unixtimestamp, true);
+                }
+            }
+        }
+        MAX::debug('  Finished sending advertiser reports.', PEAR_LOG_DEBUG);
+    }
+
+    /**
+     * A method to run Openads Sync
+     */
+    function runOpenadsSync()
+    {
+        MAX::debug('  Starting Openads Sync process.', PEAR_LOG_DEBUG);
+
+        if ($pref['updates_enabled'] == 't') {
+            require_once (MAX_PATH . '/lib/max/OpenadsSync.php');
+
+            $oSync = new MAX_OpenadsSync($this->conf, $this->pref);
+            $res = $oSync->checkForUpdates(0, true);
+
+            if ($res[0] != 0 && $res[0] != 800) {
+                MAX::debug("Openads Sync error ($res[0]): $res[1]", PEAR_LOG_INFO);
+            }
+        }
+
+        MAX::debug('  Finished Openads Sync process.', PEAR_LOG_DEBUG);
+    }
+
+    /**
+     * A method to get maintenance lock
+     */
+    function getLock()
+    {
+        // If split tables, check for lockfile
+        if ($this->conf['table']['split']) {
+            MAX::debug('Checking for lockfile', PEAR_LOG_INFO);
+            $attempt = 0;
+            // Don't start if lockfile detected
+            while (file_exists($this->conf['table']['lockfile'])) {
+                if ($attempt > 2) {
+                    // Give up on maintenance
+                    MAX::debug('More than 3 attempts, sending email to admin user', PEAR_LOG_ERR);
+                    $message  = "Warning! Maintenance was unable to run - the lockfile was found\n";
+                    $message .= "in place while trying to start maintenance, even after 3 iterations.\n\n";
+                    $message .= "The lockfile used was: {$this->conf['table']['lockfile']}.\n\n";
+                    $query = "
+                        SELECT
+                            admin_email
+                        FROM
+                            {$this->conf['table']['prefix']}{$this->conf['table']['preference']}
+                        WHERE
+                            agencyid = 0
+                        ";
+                    $row = $this->oDbh->getRow($query);
+                    MAX::sendMail($row['admin_email'], '', 'Lockfile altert!', $message);
+                    MAX::raiseError('Aborting script execution', null, PEAR_ERROR_DIE);
+                }
+                // Pause for 30 secs
+                MAX::debug('Lockfile exists, sleeping for 30 secs. Iteration ' . $attempt, PEAR_LOG_INFO);
+                sleep(30);
+                $attempt ++;
+            }
+            // Write lockfile so table splitting scripts will abort if they
+            // attempt to start during a maintenance run
+            MAX::debug('No lockfile exists, writing lockfile', PEAR_LOG_INFO);
+            $fh = fopen($this->conf['table']['lockfile'], 'w');
+        }
+    }
+
+    /**
+     * A method to release maintenance lock
+     */
+    function releaseLock()
+    {
+        if ($this->conf['table']['split']) {
+            MAX::debug('Removing lockfile', PEAR_LOG_INFO);
+            unlink($this->conf['table']['lockfile']);
+        }
+    }
+
+    /**
+     * A method to update maintenance last run information for old maintenance code
+     */
+    function updateLastRun()
+    {
+        // Update the timestamp (for old maintenance code)
+        // TODO: Move this query to the DAL, so that other code (tests, installation) can call it.
+        MAX::debug('Updating the timestamp in the preference table', PEAR_LOG_DEBUG);
+        $query = "
+            UPDATE
+                {$this->conf['table']['prefix']}{$this->conf['table']['preference']}
+            SET
+                maintenance_timestamp = UNIX_TIMESTAMP(NOW())";
+        $result = $this->oDbh->query($query);
+    }
 }
 
 ?>
