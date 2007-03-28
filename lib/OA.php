e8dbe115b3bcd09cac73335924c0916d2b69572a
andrew.hill@openads.org <andrew.hill@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-03-28 12:45:12 +0000

    Improved methods to deal with error handling ignoring/enabling.

--- lib/OA.php
+++ lib/OA.php
@@ -129,13 +129,18 @@ class OA
 
     /**
      * A method to re-enable PEAR error handling by popping
-     * the null error handler off the top of the stack.
+     * a null error handler off the top of the stack.
      *
      * @static
      */
     function enableErrorHandling()
     {
-        PEAR::popErrorHandling();
+        // Ensure this method only acts when a null error handler exists
+        $stack = &$GLOBALS['_PEAR_error_handler_stack'];
+        list($mode, $options) = $stack[sizeof($stack) - 1];
+        if (is_null($mode) && is_null($options)) {
+            PEAR::popErrorHandling();
+        }
     }
 
 }
