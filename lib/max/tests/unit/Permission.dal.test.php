2626adc31e8aa994f140a7f060dbae835e19043f
andrew.hill@openads.org <andrew.hill@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-04-02 13:27:45 +0000

    Added required field to the test. Refs #242.

--- lib/max/tests/unit/Permission.dal.test.php
+++ lib/max/tests/unit/Permission.dal.test.php
@@ -37,7 +37,7 @@ require_once MAX_PATH . '/lib/max/tests/util/DataGenerator.php';
  */
 class MAX_PermissionTest extends UnitTestCase
 {
-    
+
     /**
      * The constructor method.
      */
@@ -45,82 +45,83 @@ class MAX_PermissionTest extends UnitTestCase
     {
         $this->UnitTestCase();
     }
-    
+
     function setUp()
     {
-        
+
     }
-    
+
     function tearDown()
     {
         DataGenerator::cleanUp();
     }
-    
+
     function testIsUsernameAllowed()
     {
         // Set up the preferences array
         $GLOBALS['pref'] = array();
         $GLOBALS['pref']['admin'] = 'admin';
-        
+
         // If the names are the same then true
         $this->assertTrue(MAX_Permission::isUsernameAllowed('foo', 'foo'));
-        
+
         // Don't let a user have the "admin" username
         $this->assertFalse(MAX_Permission::isUsernameAllowed('foo', 'admin'));
-        
+
         // Check users as client, affiliate, agency
         $doClients = MAX_DB::factoryDO('clients');
         $doClients->clientusername = 'bar';
+        $doClients->reportlastdate = '2007-04-02 12:00:00';
         $clientId = DataGenerator::generateOne($doClients);
-        
+
         $this->assertFalse(MAX_Permission::isUsernameAllowed('foo', 'bar'));
-        
+
         $doAffiliates = MAX_DB::factoryDO('affiliates');
         $doAffiliates->username = 'baz';
         $affiliateId = DataGenerator::generateOne($doAffiliates);
-        
+
         $this->assertFalse(MAX_Permission::isUsernameAllowed('foo', 'baz'));
-        
+
         $doAgency = MAX_DB::factoryDO('agency');
         $doAgency->username = 'quux';
         $agencyId = DataGenerator::generateOne($doAgency);
-        
+
         $this->assertFalse(MAX_Permission::isUsernameAllowed('foo', 'quux'));
-        
+
         $this->assertTrue(MAX_Permission::isUsernameAllowed('foo', 'newname'));
-        
+
     }
-    
+
     function testGetUniqueUserNames()
     {
          // Set up the preferences array
         $GLOBALS['pref'] = array();
         $GLOBALS['pref']['admin'] = 'admin';
-        
+
         $expected = array('admin');
         $actual = MAX_Permission::getUniqueUserNames();
         $this->assertEqual($actual, $expected);
-        
+
         // Insert some users
         $doClients = MAX_DB::factoryDO('clients');
         $doClients->clientusername = 'bar';
         $clientId = DataGenerator::generateOne($doClients);
-        
+
         $doAffiliates = MAX_DB::factoryDO('affiliates');
         $doAffiliates->username = 'baz';
         $affiliateId = DataGenerator::generateOne($doAffiliates);
-        
+
         $doAgency = MAX_DB::factoryDO('agency');
         $doAgency->username = 'quux';
         $agencyId = DataGenerator::generate($doAgency, 2); // Duplicate username
-        
+
         $expected = array('admin', 'bar', 'baz', 'quux');
         sort($expected);
         $actual = MAX_Permission::getUniqueUserNames();
         sort($actual);
         $this->assertEqual($actual, $expected);
     }
-    
+
     function testHasAccessToObject()
     {
         $userTables = array(
@@ -140,11 +141,11 @@ class MAX_PermissionTest extends UnitTestCase
         $agencyId = DataGenerator::getReferenceId('agency');
         // Test that admin always has access
         $this->assertTrue(MAX_Permission::hasAccessToObject('banners', 'booId', phpAds_Admin));
-        
+
         // Test users have access
         $this->assertTrue(MAX_Permission::hasAccessToObject('banners', $bannerId, phpAds_Client, $clientId));
         $this->assertTrue(MAX_Permission::hasAccessToObject('banners', $bannerId, phpAds_Agency, $agencyId));
-        
+
         // Create users who don't have access
         $clientId2 = DataGenerator::generateOne('clients');
         $agencyId2 = DataGenerator::generateOne('agency');
