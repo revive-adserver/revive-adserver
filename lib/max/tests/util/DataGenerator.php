6944b7b1ff56fd2d7cd4c960b30f1eb65341508a
radek.maciaszek@openads.org <radek.maciaszek@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-04-05 13:59:10 +0000

    Updated comments and made sure that DataGenerator do not generate default data for primary keys

--- lib/max/tests/util/DataGenerator.php
+++ lib/max/tests/util/DataGenerator.php
@@ -29,7 +29,9 @@ require_once MAX_PATH . '/lib/OA/Dal.php';
 define('MAX_DATAGENERATOR_DEFAULT_VALUE', 1);
 
 /**
- * A Data Generator class for easy
+ * A DataGenerator class for easy data generation
+ * 
+ * Online manual: https://developer.openads.org/wiki/DataGenerator
  *
  * @package    MaxDal
  * @subpackage TestSuite
@@ -38,60 +40,74 @@ define('MAX_DATAGENERATOR_DEFAULT_VALUE', 1);
 class DataGenerator
 {
     /**
+     * Data conteiner
      * Use this data to populate any new records
-     *
+     * @see getFieldValueFromDataContainer()
+     * 
      * @var array
      */
     var $data;
 
     /**
-     * Generate one record with default values and add insert it into database.
+     * Generate one record. Wrapper for: generate($do, 1, $generateParents)
      * Returns id of created record
      *
      * @see DB_DataObject::insert()
-     * @param DB_DataObjectCommon $do
-     * @param bool $generateReferences
-     * @return int
+     * @param DB_DataObjectCommon $do Either DataObject or table name (as string)
+     * @param bool $generateParents   Generate parent records
+     * @return int                    Id of created record
      * @access public
      * @static
      */
-    function generateOne($do, $generateReferences = false)
+    function generateOne($do, $generateParents = false)
     {
-        $ids = DataGenerator::generate($do, 1, $generateReferences);
+        $ids = DataGenerator::generate($do, 1, $generateParents);
         return array_pop($ids);
     }
 
     /**
-     * Generate many records with default values and insert them into database.
-     * Returns array ids of created records
-     *
-     * @todo generate parent references (all the records this record has a foreign key to)
+     * Generate records (populate them with either prepared or default values) and insert into database.
+     * This method could create parent records (we call "parent" all records in tables where generated
+     * record has foreign keys linked to).
+     * 
+     * Returns array ids of created records.
+     * 
+     * Examples:
+     * 
+     * // Lazy generate 7 records
+     * $aMytableIds = DataGenerator::generate('mytable', 7);
+     * 
+     * // Create dataobject and pass it into generator
+     * $doMyTable = OA_Dal::factoryDO('mytable');
+     * $doMyTable->column1 = 'some value';
+     * $aMytableIds = DataGenerator::generate($doMyTable, 7);
      *
      * @see DB_DataObject::insert()
-     * @param DB_DataObjectCommon $do
-     * @param int $numberOfCopies  How many records should be generated
-     * @param bool $generateReferences
-     * @return array
+     * @param mixed $do              Either DataObject or table name (as string)
+     * @param int $numberOfCopies    How many records should be generated
+     * @param bool $generateParents  Should parent records be generated?
+     * @return array                 Array ids of created records
      * @access public
      * @static
      */
-    function generate($do, $numberOfCopies = 1, $generateReferences = false)
+    function generate($do, $numberOfCopies = 1, $generateParents = false)
     {
         // Cleanup ancestor ids
         DataGenerator::getReferenceId();
 
         if (is_string($do)) {
+            // Lazy DataObject initialization
             $do = OA_Dal::factoryDO($do);
             if (PEAR::isError($do)) {
                 return array();
             }
         }
 
-        if ($generateReferences) {
+        if ($generateParents) {
             $links = $do->links();
         	foreach ($links as $key => $match) {
-        		list($table,$link) = explode(':', $match);
-        		$table = $do->getTableWithoutPrefix($table);
+        		list($tablePrefixed, $link) = explode(':', $match);
+        		$table = $do->getTableWithoutPrefix($tablePrefixed);
     	        $primaryKey = isset($do->$key) ? $do->$key : null;
         		$do->$key = DataGenerator::addAncestor($table, $primaryKey);
         	}
@@ -113,6 +129,8 @@ class DataGenerator
     /**
      * Remove the data from all tables where DataGenerator generated any records
      *
+     * @param array $addTablesToCleanUp  Array of any additional tables DataGenerator should
+     *                                   delete data from
      * @access public
      * @static
      */
@@ -125,13 +143,19 @@ class DataGenerator
             $do->whereAdd('1=1');
             $do->delete($useWhere = true);
         }
-        // cleanup ancestor ids
+        // Cleanup ancestor ids
         DataGenerator::getReferenceId();
     }
 
     /**
-     * This method allows to store and retreive any ids which were created
-     * for ancestors records (all records created atuomatically if $generateReferences is true)
+     * This method allows to store and retreive any primary key's ids which were created
+     * for ancestors records
+     * 
+     * Example of use:
+     * // Assume following line will create 'subtable' table and it's parent 'table' record
+     * $subtableId = DataGenerator::generate('subtable', 1, $generateParents = true);
+     * // Following command retrieves ID of 'table' record
+     * $tableId = DataGenerator::getReferenceId('table');
      *
      * @param string $table
      * @param int $id
@@ -152,10 +176,11 @@ class DataGenerator
     }
 
     /**
-     * Track tables where some data were generated so we could easily clean it up later
+     * This method is used for tracking tables where some data were generated
+     * so DataGenerator knows how and what to clean up later
      *
-     * @param string $table  If equal null it reset the static $tables
-     * @return array
+     * @param string $table     Table name to track, if it's equal null it reset all the data
+     * @return array            Array of stored tables names
      * @access package private
      * @static
      */
@@ -176,9 +201,15 @@ class DataGenerator
     }
 
     /**
-     * Method adds related records recursively
+     * Method adds related "parent/ancestor" records recursively.
+     * 
+     * It should be used only to create records which have only one primary key.
+     * 
+     * Note: in theory it should work for ancestor records with multiple primary keys
+     * but this behaviour is undefined.
      *
-     * @param string $table  Table name
+     * @param string $table       Table name
+     * @param string $primaryKey  Used as primary key for ancestor
      * @return int  New ID
      * @access package private
      */
@@ -193,9 +224,8 @@ class DataGenerator
 
         $links = $doAncestor->links();
     	foreach ($links as $key => $match) {
-    		list($ancestorTable,$link) = explode(':', $match);
-    		// remove prefix from it
-    		$ancestorTable = $doAncestor->getTableWithoutPrefix($ancestorTable);
+    		list($ancestorTableWithPrefix, $link) = explode(':', $match);
+    		$ancestorTable = $doAncestor->getTableWithoutPrefix($ancestorTableWithPrefix);
     		$doAncestor->$key = DataGenerator::addAncestor($ancestorTable);
     	}
     	DataGenerator::trackData($table);
@@ -205,16 +235,21 @@ class DataGenerator
     }
 
     /**
-     * Check if DataObject has defined "defaultValues" array and if it does
-     * set default values on DataObject. For all others fields
-     * which are not set in default values set global default values
-     * depends on field type.
+     * This method checks if there is any data prepared for record or it uses
+     * default data based on type of the field.
+     * 
+     * It looks for data for each field in record in following order:
+     * 1. Checks if data is set in DataObject
+     * 2. Checks data from data container (@see setData())
+     * 3. Checks data from $defaultValues variables defined in each DataObject
+     * 4. Generate data based on type of the field
      *
-     * This method doesn't change a value if DataObject already has a value assigned
-     * to some of it's fields.
-     *
-     * @param DataObject $do
-     * @return DataObject
+     * It populates primary keys fields only if data for key field was specifically
+     * set using any of methods defined in 1, 2 or 3 (it doesn't
+     * populate it with default data based on field type)
+     * 
+     * @param DataObject $do    DataObject to populate data in
+     * @param int $counter      Used to generate a key for data container array to retreive data
      * @access package private
      * @static
      */
@@ -226,23 +261,32 @@ class DataGenerator
             }
         }
         $fields = $do->table();
+        $keys = $do->keys();
+        $table = $do->getTableWithoutPrefix();
         foreach ($fields as $fieldName => $fieldType) {
             if (!isset($do->$fieldName)) {
-                $do->$fieldName = DataGenerator::getDefaultValue($do->getTableWithoutPrefix(),
-                    $fieldName, $fieldType, $counter);
+                $fieldValue = DataGenerator::getFieldValueFromDataContainer($table, $fieldName, $counter);
+                if(!isset($fieldValue) && !in_array($fieldName, $keys)) {
+                    $fieldValue = DataGenerator::getDefaultValueByType($fieldType);
+                }
+                if (isset($fieldValue)) {
+                    $do->$fieldName = $fieldValue;
+                }
             }
         }
     }
 
     /**
-     * Return default data for a specified field in the table.
+     * Return value for a specified field in the table
+     * if it was previously set in data container
      *
-     * @param string $fieldType
-     * @return mixed
+     * @param string $table     Table name
+     * @param string $fieldName Field (column) name
+     * @return mixed Data defined for field or null if data wasn't prepared in data container
      * @access package private
      * @static
      */
-    function getDefaultValue($table, $fieldName, $fieldType, $counter)
+    function getFieldValueFromDataContainer($table, $fieldName, $counter)
     {
         if (isset($this)) {
             if (isset($this->data[$table]) && isset($this->data[$table][$fieldName])) {
@@ -250,11 +294,12 @@ class DataGenerator
                 return $this->data[$table][$fieldName][$index];
             }
         }
-        return DataGenerator::getDefaultValueByType($fieldType);
+        return null;
     }
 
     /**
-     * Return default value by type.
+     * Return default field value based on field type
+     * 
      * @todo This could be refactored if we will decide to add more types here
      *
      * @param string $fieldType
@@ -277,6 +322,8 @@ class DataGenerator
      * Template variables:
      * %DATE_TIME% is replaced with date('Y-m-d H:i:s')
      *
+     * @todo This should be refactored if we will decide to add more templates
+     * 
      * @param string $val  Template variable
      * @return string
      * @static
@@ -292,11 +339,25 @@ class DataGenerator
     }
 
     /**
-     * This method sets data which is used by DataGenerator to populate records
+     * This method sets data in data container which is used by DataGenerator to populate records
+     * 
+     * Format of data to use. Example:
+     * $data = array(
+     *   'column1' => array('value for first record', 'value for second record'),
+     *   'column2' => array('populare all created records with this'),
+     * );
+     * $dg = new DataGenerator();
+     * $dg->setData('mytable', $data);
+     * $dg->generateOne('mytable');
+     * 
+     * Generator use data set in data container to populate fields for new
+     * records. It loops over columns arrays and reuse data for few records.
+     * For example if we will generate 10 records with data defined in example
+     * five records will consist 'value for first record' and five
+     * 'value for second record'.
      *
-     *
-     * @param string $table
-     * @param array $data
+     * @param string $table  Table name
+     * @param array $data    Save prepared data in data container
      * @access public
      */
     function setData($table = null, $data = array())
