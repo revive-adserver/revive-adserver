93a0f13f9a9c0adca3b446f3a65a2ea4548b532d
monique.szpak@openads.org <monique.szpak@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-04-13 14:51:12 +0000

    init files for new upgrade_mdb2

---
+++ www/devel/schema/util/init.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once '../../../../init.php';
+define('MAX_DEV', MAX_PATH.'/www/devel');
+
+require_once(MAX_DEV.'/schema/util/lib/error.inc.php');
+
+?>
---
+++ www/devel/schema/util/lib/init.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once '../../../../../init.php';
+define('MAX_DEV', MAX_PATH.'/www/devel');
+define('MAX_VAR', MAX_PATH.'/var');
+
+require_once(MAX_DEV.'/schema/util/lib/error.inc.php');
+
+?>
