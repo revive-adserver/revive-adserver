2e75f62d37d0f956f29cf6156388406d618e9724
monique.szpak@openads.org <monique.szpak@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-04-24 12:50:26 +0000

    schema upgrade package 510 for 2.3

---
+++ etc/changes/migration_tables_core_510.php
@@ -0,0 +1,97 @@
+<?php
+
+require_once(MAX_PATH.'/lib/OA/Upgrade/Migration.php');
+
+class Migration_510 extends Migration
+{
+
+    function Migration_510()
+    {
+        //$this->__construct();
+
+		$this->aTaskList_constructive[] = 'beforeAddTable__lb_local';
+		$this->aTaskList_constructive[] = 'afterAddTable__lb_local';
+		$this->aTaskList_constructive[] = 'beforeAddField__banners__keyword';
+		$this->aTaskList_constructive[] = 'afterAddField__banners__keyword';
+		$this->aTaskList_constructive[] = 'beforeAddField__banners__transparent';
+		$this->aTaskList_constructive[] = 'afterAddField__banners__transparent';
+		$this->aTaskList_constructive[] = 'beforeAddField__clients__lb_reporting';
+		$this->aTaskList_constructive[] = 'afterAddField__clients__lb_reporting';
+		$this->aTaskList_constructive[] = 'beforeAddField__preference__maintenance_cron_timestamp';
+		$this->aTaskList_constructive[] = 'afterAddField__preference__maintenance_cron_timestamp';
+		$this->aTaskList_constructive[] = 'beforeAddField__zones__what';
+		$this->aTaskList_constructive[] = 'afterAddField__zones__what';
+
+
+		$this->aObjectMap['banners']['keyword'] = array('fromTable'=>'banners', 'fromField'=>'keyword');
+		$this->aObjectMap['banners']['transparent'] = array('fromTable'=>'banners', 'fromField'=>'transparent');
+		$this->aObjectMap['clients']['lb_reporting'] = array('fromTable'=>'clients', 'fromField'=>'lb_reporting');
+		$this->aObjectMap['preference']['maintenance_cron_timestamp'] = array('fromTable'=>'preference', 'fromField'=>'maintenance_cron_timestamp');
+		$this->aObjectMap['zones']['what'] = array('fromTable'=>'zones', 'fromField'=>'what');
+    }
+
+
+
+	function beforeAddTable__lb_local()
+	{
+		return $this->beforeAddTable('lb_local');
+	}
+
+	function afterAddTable__lb_local()
+	{
+		return $this->afterAddTable('lb_local');
+	}
+
+	function beforeAddField__banners__keyword()
+	{
+		return $this->beforeAddField('banners', 'keyword');
+	}
+
+	function afterAddField__banners__keyword()
+	{
+		return $this->afterAddField('banners', 'keyword');
+	}
+
+	function beforeAddField__banners__transparent()
+	{
+		return $this->beforeAddField('banners', 'transparent');
+	}
+
+	function afterAddField__banners__transparent()
+	{
+		return $this->afterAddField('banners', 'transparent');
+	}
+
+	function beforeAddField__clients__lb_reporting()
+	{
+		return $this->beforeAddField('clients', 'lb_reporting');
+	}
+
+	function afterAddField__clients__lb_reporting()
+	{
+		return $this->afterAddField('clients', 'lb_reporting');
+	}
+
+	function beforeAddField__preference__maintenance_cron_timestamp()
+	{
+		return $this->beforeAddField('preference', 'maintenance_cron_timestamp');
+	}
+
+	function afterAddField__preference__maintenance_cron_timestamp()
+	{
+		return $this->afterAddField('preference', 'maintenance_cron_timestamp');
+	}
+
+	function beforeAddField__zones__what()
+	{
+		return $this->beforeAddField('zones', 'what');
+	}
+
+	function afterAddField__zones__what()
+	{
+		return $this->afterAddField('zones', 'what');
+	}
+
+}
+
+?>
