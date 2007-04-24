aca647200f8a03e9d59e97965f46c6f8085fd2e8
monique.szpak@openads.org <monique.szpak@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-04-24 16:58:34 +0000

    tables_core.xml: new version 511 with changeset

---
+++ etc/changes/migration_tables_core_511.php
@@ -0,0 +1,59 @@
+<?php
+
+require_once(MAX_PATH.'/lib/OA/Upgrade/Migration.php');
+
+class Migration_511 extends Migration
+{
+
+    function Migration_511()
+    {
+        //$this->__construct();
+
+		$this->aTaskList_constructive[] = 'beforeAddField__ad_zone_assoc__priority_factor';
+		$this->aTaskList_constructive[] = 'afterAddField__ad_zone_assoc__priority_factor';
+		$this->aTaskList_constructive[] = 'beforeAddField__ad_zone_assoc__to_be_delivered';
+		$this->aTaskList_constructive[] = 'afterAddField__ad_zone_assoc__to_be_delivered';
+		$this->aTaskList_constructive[] = 'beforeAddField__data_summary_ad_zone_assoc__to_be_delivered';
+		$this->aTaskList_constructive[] = 'afterAddField__data_summary_ad_zone_assoc__to_be_delivered';
+
+
+		$this->aObjectMap['ad_zone_assoc']['priority_factor'] = array('fromTable'=>'ad_zone_assoc', 'fromField'=>'priority_factor');
+		$this->aObjectMap['ad_zone_assoc']['to_be_delivered'] = array('fromTable'=>'ad_zone_assoc', 'fromField'=>'to_be_delivered');
+		$this->aObjectMap['data_summary_ad_zone_assoc']['to_be_delivered'] = array('fromTable'=>'data_summary_ad_zone_assoc', 'fromField'=>'to_be_delivered');
+    }
+
+
+
+	function beforeAddField__ad_zone_assoc__priority_factor()
+	{
+		return $this->beforeAddField('ad_zone_assoc', 'priority_factor');
+	}
+
+	function afterAddField__ad_zone_assoc__priority_factor()
+	{
+		return $this->afterAddField('ad_zone_assoc', 'priority_factor');
+	}
+
+	function beforeAddField__ad_zone_assoc__to_be_delivered()
+	{
+		return $this->beforeAddField('ad_zone_assoc', 'to_be_delivered');
+	}
+
+	function afterAddField__ad_zone_assoc__to_be_delivered()
+	{
+		return $this->afterAddField('ad_zone_assoc', 'to_be_delivered');
+	}
+
+	function beforeAddField__data_summary_ad_zone_assoc__to_be_delivered()
+	{
+		return $this->beforeAddField('data_summary_ad_zone_assoc', 'to_be_delivered');
+	}
+
+	function afterAddField__data_summary_ad_zone_assoc__to_be_delivered()
+	{
+		return $this->afterAddField('data_summary_ad_zone_assoc', 'to_be_delivered');
+	}
+
+}
+
+?>
