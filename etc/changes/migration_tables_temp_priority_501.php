b5a3a8ab8cd6851595bb93aa1a7485fe7b48c3ae
monique.szpak@openads.org <monique.szpak@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-04-24 16:38:38 +0000

    schema change: new field in temp priority table

---
+++ etc/changes/migration_tables_temp_priority_501.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once(MAX_PATH.'/lib/OA/Upgrade/Migration.php');
+
+class Migration_501 extends Migration
+{
+
+    function Migration_501()
+    {
+        //$this->__construct();
+
+		$this->aTaskList_constructive[] = 'beforeAddField__tmp_ad_zone_impression__to_be_delivered';
+		$this->aTaskList_constructive[] = 'afterAddField__tmp_ad_zone_impression__to_be_delivered';
+
+
+		$this->aObjectMap['tmp_ad_zone_impression']['to_be_delivered'] = array('fromTable'=>'tmp_ad_zone_impression', 'fromField'=>'to_be_delivered');
+    }
+
+
+
+	function beforeAddField__tmp_ad_zone_impression__to_be_delivered()
+	{
+		return $this->beforeAddField('tmp_ad_zone_impression', 'to_be_delivered');
+	}
+
+	function afterAddField__tmp_ad_zone_impression__to_be_delivered()
+	{
+		return $this->afterAddField('tmp_ad_zone_impression', 'to_be_delivered');
+	}
+
+}
+
+?>
