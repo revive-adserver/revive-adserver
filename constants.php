340d495b6b8d2f6483982144aa90da97ca99dc0e
aj@seagullproject.org <aj@seagullproject.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-05-09 16:03:21 +0000

    refactored code so that a timezone is always set and changed a check to !empty() so that an notice is not displayed when the variable is set but empty, refs #301

--- constants.php
+++ constants.php
@@ -222,22 +222,20 @@ function setupConstants()
         // screw up the dates after using the PEAR::compare() method -  also,
         // ensure that an appropriate timezone is set, if required, to allow
         // the time zone to be other than the time zone of the server
-        if (version_compare(phpversion(), '5.1.0', '>=')) {
-            if (isset($GLOBALS['_MAX']['CONF']['timezone']['location'])) {
+        if (!empty($GLOBALS['_MAX']['CONF']['timezone']['location'])) {
+            if (version_compare(phpversion(), '5.1.0', '>=')) {
                 // Set new time zone
                 date_default_timezone_set($GLOBALS['_MAX']['CONF']['timezone']['location']);
-            }
-        } else {
-            if (isset($GLOBALS['_MAX']['CONF']['timezone']['location'])) {
+            } else {
                 // Set new time zone
                 putenv("TZ={$GLOBALS['_MAX']['CONF']['timezone']['location']}");
-            } else {
-                // Ensure that at TZ variable is set, regardless
-                if (getenv('TZ') === false) {
+            }
+        } else {
+            // Ensure that at TZ variable is set, regardless
+            if (getenv('TZ') === false) {
 
-                    $diff = date('O') / 100;
-                    putenv('TZ=Etc/GMT'.($diff > 0 ? '-' : '+').abs($diff));
-                }
+                $diff = date('O') / 100;
+                putenv('TZ=Etc/GMT'.($diff > 0 ? '-' : '+').abs($diff));
             }
         }
 
