2cd3061dbdfe3bd4682e1f676604a5dd452a5b05
matteo.beccati@openads.org <matteo.beccati@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-05-16 13:47:00 +0000

    Added missing ad selection fields to 1xx packages (refs #234)

--- etc/changes/migration_tables_core_129.php
+++ etc/changes/migration_tables_core_129.php
@@ -11,14 +11,53 @@ class Migration_129 extends Migration
     {
         //$this->__construct();
 
+		$this->aTaskList_constructive[] = 'beforeAddField__ad_zone_assoc__priority_factor';
+		$this->aTaskList_constructive[] = 'afterAddField__ad_zone_assoc__priority_factor';
+		$this->aTaskList_constructive[] = 'beforeAddField__ad_zone_assoc__to_be_delivered';
+		$this->aTaskList_constructive[] = 'afterAddField__ad_zone_assoc__to_be_delivered';
+		$this->aTaskList_constructive[] = 'beforeAddField__data_summary_ad_zone_assoc__to_be_delivered';
+		$this->aTaskList_constructive[] = 'afterAddField__data_summary_ad_zone_assoc__to_be_delivered';
 		$this->aTaskList_constructive[] = 'beforeAddField__preference__warn_limit_days';
 		$this->aTaskList_constructive[] = 'afterAddField__preference__warn_limit_days';
 
 
+		$this->aObjectMap['ad_zone_assoc']['priority_factor'] = array('fromTable'=>'ad_zone_assoc', 'fromField'=>'priority_factor');
+		$this->aObjectMap['ad_zone_assoc']['to_be_delivered'] = array('fromTable'=>'ad_zone_assoc', 'fromField'=>'to_be_delivered');
+		$this->aObjectMap['data_summary_ad_zone_assoc']['to_be_delivered'] = array('fromTable'=>'data_summary_ad_zone_assoc', 'fromField'=>'to_be_delivered');
 		$this->aObjectMap['preference']['warn_limit_days'] = array('fromTable'=>'preference', 'fromField'=>'warn_limit_days');
     }
 
 
+
+	function beforeAddField__ad_zone_assoc__priority_factor()
+	{
+		return $this->beforeAddField('ad_zone_assoc', 'priority_factor');
+	}
+
+	function afterAddField__ad_zone_assoc__priority_factor()
+	{
+		return $this->afterAddField('ad_zone_assoc', 'priority_factor');
+	}
+
+	function beforeAddField__ad_zone_assoc__to_be_delivered()
+	{
+		return $this->beforeAddField('ad_zone_assoc', 'to_be_delivered');
+	}
+
+	function afterAddField__ad_zone_assoc__to_be_delivered()
+	{
+		return $this->afterAddField('ad_zone_assoc', 'to_be_delivered');
+	}
+
+	function beforeAddField__data_summary_ad_zone_assoc__to_be_delivered()
+	{
+		return $this->beforeAddField('data_summary_ad_zone_assoc', 'to_be_delivered');
+	}
+
+	function afterAddField__data_summary_ad_zone_assoc__to_be_delivered()
+	{
+		return $this->afterAddField('data_summary_ad_zone_assoc', 'to_be_delivered');
+	}
 
 	function beforeAddField__preference__warn_limit_days()
 	{
