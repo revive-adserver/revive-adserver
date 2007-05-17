564e9963e20e75fcf1dfc8c3936d77ebc50fd71d
andrew.hill@openads.org <andrew.hill@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-05-17 08:06:18 +0000

    Updated more copyright headers. Refs #306.

--- www/admin/lib-data-statistics.inc.php
+++ www/admin/lib-data-statistics.inc.php
@@ -1,21 +1,42 @@
 <?php
 
-/*----------------------------------------------------------------------*/
-/* phpAdsNew 2                                                          */
-/* ===========                                                          */
-/*                                                                      */
-/* Copyright (c) 2001 by TOMO <groove@spencernetwork.org>               */
-/* For more information visit: http://www.phpadsnew.com                 */
-/*----------------------------------------------------------------------*/
-
+/*
++---------------------------------------------------------------------------+
+| Openads v2.3                                                              |
+| ============                                                              |
+|                                                                           |
+| Copyright (c) 2003-2007 Openads Limited                                   |
+| For contact details, see: http://www.openads.org/                         |
+|                                                                           |
+| Copyright (c) 2000-2003 the phpAdsNew developers                          |
+| For contact details, see: http://www.phpadsnew.com/                       |
+|                                                                           |
+| Copyright (c) 2001 by TOMO <groove@spencernetwork.org>                    |
+|                                                                           |
+| This program is free software; you can redistribute it and/or modify      |
+| it under the terms of the GNU General Public License as published by      |
+| the Free Software Foundation; either version 2 of the License, or         |
+| (at your option) any later version.                                       |
+|                                                                           |
+| This program is distributed in the hope that it will be useful,           |
+| but WITHOUT ANY WARRANTY; without even the implied warranty of            |
+| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             |
+| GNU General Public License for more details.                              |
+|                                                                           |
+| You should have received a copy of the GNU General Public License         |
+| along with this program; if not, write to the Free Software               |
+| Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA |
++---------------------------------------------------------------------------+
+$Id$
+*/
 
 function phpAds_sortArray(&$array, $column=0, $ascending=TRUE)
 {
-	
+
 	for ($i=0; $i<sizeof($array); $i++)
 		if (isset($array[$i]['children']) && is_array($array[$i]['children']))
 			phpAds_sortArray($array[$i]['children'], $column, $ascending);
-	
+
 	phpAds_qsort($array, $column, $ascending);
 
 }
@@ -24,14 +45,14 @@ function phpAds_qsort(&$array, $column=0, $ascending=true, $first=0, $last=0)
 {
 	if ($last == 0)
 		$last = count($array) - 1;
-	
+
 	if ($last > $first)
 	{
 		$alpha = $first;
 		$omega = $last;
 		$mid = floor(($alpha+$omega)/2);
 		$guess = $array[$mid][$column];
-		
+
 		while ($alpha <= $omega)
 		{
 			if ($ascending)
@@ -48,7 +69,7 @@ function phpAds_qsort(&$array, $column=0, $ascending=true, $first=0, $last=0)
 				while ( ($array[$omega][$column] < $guess) && ($omega > $first) )
 					$omega--;
 			}
-			
+
 			if ($alpha <= $omega)
 			{
 				$temp = $array[$alpha];
@@ -59,7 +80,7 @@ function phpAds_qsort(&$array, $column=0, $ascending=true, $first=0, $last=0)
 				$omega--;
 			}
 		}
-		
+
 		if ($first < $omega)
 			phpAds_qsort($array, $column, $ascending, $first, $omega);
 		if ($alpha < $last)
