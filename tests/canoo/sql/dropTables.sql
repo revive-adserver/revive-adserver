291d55331bd297069d5cc137d8d4b74a780c297c
radek.maciaszek@openads.org <radek.maciaszek@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-05-21 09:08:15 +0000

    Added init step which creates testing delivery database

--- tests/canoo/sql/dropTables.sql
+++ tests/canoo/sql/dropTables.sql
@@ -1,1 +1,14 @@
-DROP TABLE IF EXISTS `acls`, `ad_zone_assoc`, `affiliates`, `agency`, `banners`, `campaigns`, `channel`, `clients`, `images`, `placement_zone_assoc`, `preference`, `zones`;
+DROP TABLE IF EXISTS 
+    acls,
+    ad_zone_assoc,
+    affiliates,
+    affiliates_extra, 
+    agency, 
+    banners, 
+    campaigns, 
+    channel, 
+    clients, 
+    images, 
+    placement_zone_assoc, 
+    preference, 
+    zones;
