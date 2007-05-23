6eb912da190aab526872fc3664d2125ed5671396
monique.szpak@openads.org <monique.szpak@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-05-23 12:07:46 +0000

    upgrade schema packages 515

---
+++ etc/changes/migration_tables_core_515.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once(MAX_PATH.'/lib/OA/Upgrade/Migration.php');
+
+class Migration_515 extends Migration
+{
+
+    function Migration_515()
+    {
+        //$this->__construct();
+
+		$this->aTaskList_constructive[] = 'beforeAlterField__preference__gui_invocation_3rdparty_default';
+		$this->aTaskList_constructive[] = 'afterAlterField__preference__gui_invocation_3rdparty_default';
+
+
+    }
+
+
+
+	function beforeAlterField__preference__gui_invocation_3rdparty_default()
+	{
+		return $this->beforeAlterField('preference', 'gui_invocation_3rdparty_default');
+	}
+
+	function afterAlterField__preference__gui_invocation_3rdparty_default()
+	{
+		return $this->afterAlterField('preference', 'gui_invocation_3rdparty_default');
+	}
+
+}
+
+?>
