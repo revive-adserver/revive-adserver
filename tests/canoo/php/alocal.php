0f4976df4c993cbab52d24e78e56f376b37dd140
radek.maciaszek@openads.org <radek.maciaszek@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-05-23 10:02:47 +0000

    Added tests for alocal.php

--- tests/canoo/php/alocal.php
+++ tests/canoo/php/alocal.php
@@ -1,5 +1,11 @@
 <?php
 
-define('MAX_PATH', '/path/to/max');
+define('MAX_PATH', './..');
+
+require_once MAX_PATH . '/www/delivery_dev/alocal.php';
+
+$bannercode = view_local($what = '', $zoneid = 0, $campaignid = 0, $bannerid = 3);
+
+echo $bannercode['html'];
 
 ?>
