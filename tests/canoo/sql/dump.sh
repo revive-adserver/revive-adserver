b1707158951f1ed14b9597f0cd899cef13c09dc2
radek.maciaszek@openads.org <radek.maciaszek@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-05-24 15:52:30 +0000

    Updated 'main' canoo task, it imports a test data and runs all the tests

--- tests/canoo/sql/dump.sh
+++ tests/canoo/sql/dump.sh
@@ -1,1 +1,9 @@
-/usr/local/mysql5/bin/mysqldump --compact -u root -p delivery acls ad_zone_assoc affiliates affiliates_extra agency banners campaigns channel clients images log_maintenance_statistics placement_zone_assoc preference zones  > delivery.sql
+#!/bin/sh
+#
+# Dumps testing database
+#
+# Help: 
+# --hex-blob - dumps data binary so image could be imported properly into the database
+# see mysqldump manual: http://dev.mysql.com/doc/refman/5.0/en/mysqldump.html
+
+/usr/local/mysql5/bin/mysqldump --compact --hex-blob -u root -p delivery acls ad_zone_assoc affiliates affiliates_extra agency banners campaigns channel clients images log_maintenance_statistics placement_zone_assoc preference zones  > delivery.sql
