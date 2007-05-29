936370fb18411cb846cb6cf8c75d245c02074e0c
matteo.beccati@openads.org <matteo.beccati@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-05-29 12:51:03 +0000

    Added 099 changeset (refs #442)

---
+++ etc/changes/migration_tables_core_099.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once(MAX_PATH.'/lib/OA/Upgrade/Migration.php');
+
+class Migration_99 extends Migration
+{
+
+    function Migration_99()
+    {
+        //$this->__construct();
+
+		$this->aTaskList_constructive[] = 'beforeAddTable__lb_local';
+		$this->aTaskList_constructive[] = 'afterAddTable__lb_local';
+		$this->aTaskList_constructive[] = 'beforeAddField__clients__lb_reporting';
+		$this->aTaskList_constructive[] = 'afterAddField__clients__lb_reporting';
+
+
+		$this->aObjectMap['clients']['lb_reporting'] = array('fromTable'=>'clients', 'fromField'=>'lb_reporting');
+    }
+
+
+
+	function beforeAddTable__lb_local()
+	{
+		return $this->beforeAddTable('lb_local');
+	}
+
+	function afterAddTable__lb_local()
+	{
+		return $this->afterAddTable('lb_local');
+	}
+
+	function beforeAddField__clients__lb_reporting()
+	{
+		return $this->beforeAddField('clients', 'lb_reporting');
+	}
+
+	function afterAddField__clients__lb_reporting()
+	{
+		return $this->afterAddField('clients', 'lb_reporting');
+	}
+
+}
+
+?>
