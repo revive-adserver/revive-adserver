7682a7cad6093cc3790c7b7171d39e98fd858c6b
andrzej.swedrzynski@openads.org <andrzej.swedrzynski@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-05-30 16:55:09 +0000

    Following Andrew's fix on #435 I modified the postscripts to return the value returned by the Maintenance Engine. Refs #435.

--- etc/changes/postscript_openads_upgrade_2.0.11_to_2.3.32_beta.php
+++ etc/changes/postscript_openads_upgrade_2.0.11_to_2.3.32_beta.php
@@ -13,19 +13,7 @@ class OA_UpgradePostscript
 
     function execute()
     {
-// activate campaigns
-// needs tests
-//        $oNowDate = new Date();
-//
-//        define('DISABLE_ALL_EMAILS', 1);
-//        $oDalMSF = new OA_Dal_Maintenance_Statistics_Factory();
-//        $oDalMaintenanceStatistics = $oDalMSF->factory('AdServer');
-//        $oDalMaintenanceStatistics->managePlacements($oNowDate);
-
-        MAX_Maintenance_Priority::run();
-        // return false
-        return true;
-
+        return MAX_Maintenance_Priority::run();
     }
 
 }
