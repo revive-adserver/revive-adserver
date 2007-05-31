13feaa71509cfc959a3c74c25fa770fbddd4e45f
matteo.beccati@openads.org <matteo.beccati@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-05-31 15:13:43 +0000

    Refactored the whole XML-RPC delivery with a new API while retaining both 0.3 and 2.0 BC (refs #482)

--- lib/max/Delivery/XML-RPC.php
+++ lib/max/Delivery/XML-RPC.php
@@ -8,6 +8,9 @@
 | Copyright (c) 2003-2007 Openads Limited                                   |
 | For contact details, see: http://www.openads.org/                         |
 |                                                                           |
+| Copyright (c) 2000-2003 the phpAdsNew developers                          |
+| For contact details, see: http://www.phpadsnew.com/                       |
+|                                                                           |
 | This program is free software; you can redistribute it and/or modify      |
 | it under the terms of the GNU General Public License as published by      |
 | the Free Software Foundation; either version 2 of the License, or         |
@@ -42,35 +45,233 @@ if (empty($GLOBALS['HTTP_RAW_POST_DATA'])) {
     $GLOBALS['HTTP_RAW_POST_DATA'] = file_get_contents('php://input');
 }
 
-$xmlRpcView_sig = array(
-                    array($GLOBALS['XML_RPC_String'],
-                          $GLOBALS['XML_RPC_String'],
-                          $GLOBALS['XML_RPC_String'],
-                          $GLOBALS['XML_RPC_String'],
-                          $GLOBALS['XML_RPC_Boolean'],
-                          $GLOBALS['XML_RPC_String'],
-                          $GLOBALS['XML_RPC_Struct']
-                    ),
-                    array($GLOBALS['XML_RPC_String'],
-                          $GLOBALS['XML_RPC_String'],
-                          $GLOBALS['XML_RPC_String'],
-                          $GLOBALS['XML_RPC_String'],
-                          $GLOBALS['XML_RPC_Boolean'],
-                          $GLOBALS['XML_RPC_String'],
-                          $GLOBALS['XML_RPC_Struct'],
-                          $GLOBALS['XML_RPC_Struct']
+/**
+ * New Openads signature / docs
+ *
+ * @since 2.3.32-beta
+ */
+$xmlRpcView_OA =
+    array(
+        'sig' => array(
+                    array(
+                        $GLOBALS['XML_RPC_Struct'],  // Return value
+                        $GLOBALS['XML_RPC_Struct'],  // Environment and cookies
+                        $GLOBALS['XML_RPC_String'],  // What
+                        $GLOBALS['XML_RPC_Int'],     // Campaignid
+                        $GLOBALS['XML_RPC_String'],  // Target
+                        $GLOBALS['XML_RPC_String'],  // Source
+                        $GLOBALS['XML_RPC_Boolean'], // WithText
+                        $GLOBALS['XML_RPC_Array']    // Context
                     )
-                  );
+                ),
+        'doc' => 'When passed the "environment/cookies" struct, "what", "campaignid", "target", "source", ' .
+                 '"withText", "context" returns the cookies to be set and the HTML code to display the ' .
+                 'appropriate advertisement.'
+    );
 
-$xmlRpcView_doc = 'When passed the "what", "target", "source", "withText", remote IP address and array ' .
-                  'of cookies, returns the cookies to be set and the HTML code to display the appropriate ' .
-                  'advertisement.';
+/**
+ * MMM 0.3 / Openads 2.3 backwards compatible signature / docs
+ */
+$xmlRpcView_Max =
+    array(
+        'sig' => array(
+                    array(
+                        $GLOBALS['XML_RPC_String'],  // Retrun value
+                        $GLOBALS['XML_RPC_String'],  // What
+                        $GLOBALS['XML_RPC_String'],  // Target
+                        $GLOBALS['XML_RPC_String'],  // Source
+                        $GLOBALS['XML_RPC_Boolean'], // WithText
+                        $GLOBALS['XML_RPC_String'],  // IP Address
+                        $GLOBALS['XML_RPC_Struct']   // Cookies
+                    ),
+                    array(
+                        $GLOBALS['XML_RPC_String'],  // Retrun value
+                        $GLOBALS['XML_RPC_String'],  // What
+                        $GLOBALS['XML_RPC_String'],  // Target
+                        $GLOBALS['XML_RPC_String'],  // Source
+                        $GLOBALS['XML_RPC_Boolean'], // WithText
+                        $GLOBALS['XML_RPC_String'],  // IP Address
+                        $GLOBALS['XML_RPC_Struct'],  // Cookies
+                        $GLOBALS['XML_RPC_Array']    // Context - @since late 2.3
+                    )
+                ),
+        'doc' => '2.3 backwards compatibility method - deprecated'
+    );
+
+/**
+ * PAN / Openads 2.0 backwards compatible signature / docs
+ */
+$xmlRpcView_PAN =
+    array(
+        'sig' => array(
+                    array(
+                        $GLOBALS['XML_RPC_Struct'],  // Return value
+                        $GLOBALS['XML_RPC_Struct'],  // Environment
+                        $GLOBALS['XML_RPC_String'],  // What
+                        $GLOBALS['XML_RPC_Int'],     // Campaignid
+                        $GLOBALS['XML_RPC_String'],  // Target
+                        $GLOBALS['XML_RPC_String'],  // Source
+                        $GLOBALS['XML_RPC_Boolean']  // WithText
+                    ),
+                    array(
+                        $GLOBALS['XML_RPC_Struct'],  // Return value
+                        $GLOBALS['XML_RPC_Struct'],  // Environment
+                        $GLOBALS['XML_RPC_String'],  // What
+                        $GLOBALS['XML_RPC_Int'],     // Campaignid
+                        $GLOBALS['XML_RPC_String'],  // Target
+                        $GLOBALS['XML_RPC_String'],  // Source
+                        $GLOBALS['XML_RPC_Boolean'], // WithText
+                        $GLOBALS['XML_RPC_Array']    // Context
+                    )
+                ),
+        'doc' => '2.0 Backwards compatibility method - deprecated'
+    );
+
 
 /**
  * A function to handle XML-RPC advertisement view requests.
  *
- * @param XML_RPC_Value $params An XML_RPC_Value of type "array" containing the parameters. The
- *                              expected parameters in the array are (in order):
+ * @param XML_RPC_Message $params An XML_RPC_Message containing the parameters. The expected parameters
+ *                              are (in order):
+ *                              - An XML_RPC_Value of type "struct"  containing remote informations
+ *                                which needs at least two members:
+ *                                - remote_addr (string) and
+ *                                - cookies     (struct);
+ *                              - An XML_RPC_Value of type "string"  containing the "what" value;
+ *                              - An XML_RPC_Value of type "int"     containing the "campaignid" value;
+ *                              - An XML_RPC_Value of type "string"  containing the "target" value;
+ *                              - An XML_RPC_Value of type "string"  containing the "source" value;
+ *                              - An XML_RPC_Value of type "boolean" containing the "withText" value;
+ *                              - An XML_RPC_Value of type "array"   containing the "context" value.
+ * @return XML_RPC_Response The response. The XML_RPC_Value of the response can be one of
+ *                          a number of different values:
+ *                          - Error Code 21: wrong number of parameters.
+ *                          - Error Code 22: remote_addr element missing from the remote info struct.
+ *                          - Error Code 23: cookies element missing from the remote info struct.
+ *                          - An XML_RPC_Value of type "struct" with the HTML details required
+ *                            for displaying the advertisement stored as in XML_RPC_Value of
+ *                            type "string" in the "html" index, and other elements returned by the
+ *                            MAX_asSelect call. A special "cookies" element is either:
+ *                            - An empty XML_RPC_Value if there are no cookies to be set, or
+ *                            - An XML_RPC_Value of type "array", containing a number of XML_RPC_Values
+ *                              of tpye "array", each with 3 items:
+ *                              - An XML_RPC_Value of type "string" with the cookie name;
+ *                              - An XML_RPC_Value of type "string" with the cookie value; and
+ *                              - An XML_RPC_Value of type "string" with the cookie expiration time.
+ */
+function OA_Delivery_XmlRpc_View($params)
+{
+    global $XML_RPC_erruser;
+    global $XML_RPC_String, $XML_RPC_Struct, $XML_RPC_Array;
+
+    $cookieCache =& $GLOBALS['_MAX']['COOKIE']['CACHE'];
+
+    // Check the parameters exist
+    $numParams = $params->getNumParams();
+    if ($numParams != 7) {
+        // Return an error
+        $errorCode = $XML_RPC_erruser + 21;
+        $errorMsg  = 'Incorrect number of parameters';
+        return new XML_RPC_Response(0, $errorCode, $errorMsg);
+    }
+
+    // Parse parameters
+    for ($i = 0; $i < $numParams; $i++)
+    {
+        $p = $params->getParam($i);
+
+        if ($i) {
+            // Put the decoded value the view arg array
+            $view_params[] = XML_RPC_decode($p);
+        } else {
+            // First parameter: environment information supplied be XML-RPC client
+            $p = XML_RPC_decode($p);
+
+            if (!isset($p['remote_addr'])) {
+                // Return an error
+                $errorCode = $XML_RPC_erruser + 22;
+                $errorMsg  = "Missing 'remote_addr' member";
+                return new XML_RPC_Response(0, $errorCode, $errorMsg);
+            }
+            if (!isset($p['cookies']) || !is_array($p['cookies'])) {
+                // Return an error
+                $errorCode = $XML_RPC_erruser + 23;
+                $errorMsg  = "Missing 'cookies' member";
+                return new XML_RPC_Response(0, $errorCode, $errorMsg);
+            }
+
+            $aServerVars = array(
+                'remote_addr'       => 'REMOTE_ADDR',
+                'remote_host'       => 'REMOTE_HOST',
+
+                // Headers used for ACLs
+                'request_uri'       => 'REQUEST_URI',
+                'https'             => 'HTTPS',
+                'server_name'       => 'SERVER_NAME',
+                'http_host'         => 'HTTP_HOST',
+                'accept_language'   => 'HTTP_ACCEPT_LANGUAGE',
+                'referer'           => 'HTTP_REFERER',
+                'user_agent'        => 'HTTP_USER_AGENT',
+
+                // Headers used for proxy lookup
+                'via'               => 'HTTP_VIA',
+                'forwarded'         => 'HTTP_FORWARDED',
+                'forwarded_for'     => 'HTTP_FORWARDED_FOR',
+                'x_forwarded'       => 'HTTP_X_FORWARDED',
+                'x_forwarded_for'   => 'HTTP_X_FORWARDED_FOR',
+                'client_ip'         => 'HTTP_CLIENT_IP'
+            );
+
+            // Extract environment vars to $_SERVER
+            foreach ($aServerVars as $xmlName => $varName) {
+                if (isset($p[$xmlName])) {
+                    $_SERVER[$varName] = $p[$xmlName];
+                }
+            }
+
+            // Extract cookie vars to $_COOKIE
+            foreach ($p['cookies'] as $key => $value) {
+                $_COOKIE[$key] = addslashes($value);
+            }
+        }
+    }
+
+    // Add $richMedia parameter
+    $view_params[] = true;
+    // Add $ct0 parameter
+    $view_params[] = '';
+    // Add $loc param
+    $view_params[] =
+        (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on' ? 'https' : 'http').'://'.
+        getHostName().
+        $_SERVER['REQUEST_URI'];
+    // Add $referer parameter
+    $view_params[] = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
+
+    // Call MAX_adSelect with supplied parameters
+    $output = call_user_func_array('MAX_adSelect', $view_params);
+
+    // Prepare output as PHP array
+    if (!is_array($output)) {
+        $output = array();
+    } elseif (isset($output['contenttype']) && $output['contenttype'] == 'swf') {
+        $output['html'] = MAX_flashGetFlashObjectExternal() . $output['html'];
+    }
+
+    // Add cookie information
+    $output['cookies'] = $cookieCache;
+
+    // Return response
+    return new XML_RPC_Response(XML_RPC_encode($output));
+}
+
+/**
+ * A function to handle XML-RPC advertisement view requests. 2.3 version
+ *
+ * @deprecated
+ *
+ * @param XML_RPC_Message $params An XML_RPC_Message containing the parameters. The expected parameters
+ *                              are (in order):
  *                              - An XML_RPC_Value of type "string" containing the "what" value;
  *                              - An XML_RPC_Value of type "string" containing the "target" value;
  *                              - An XML_RPC_Value of type "string" containing the "source" value;
@@ -78,7 +279,7 @@ $xmlRpcView_doc = 'When passed the "what", "target", "source", "withText", remot
  *                              - An XML_RPC_Value of type "string" containing the viewer's IP address; and
  *                              - An XML_RPC_Value of type "struct" containing the viewer's cookie values
  *                                (indexed by cookie name);
- *                              - An XML_RPC_Value of type "struct" containing the "context" value.
+ *                              - An XML_RPC_Value of type "array" containing the "context" value.
  * @return XML_RPC_Response The response. The XML_RPC_Value of the response can be one of
  *                          a number of different values:
  *                          - Error Code 1: The $params variable was not an XML_RPC_Value of
@@ -97,11 +298,10 @@ $xmlRpcView_doc = 'When passed the "what", "target", "source", "withText", remot
  *                              - An XML_RPC_Value of type "string" with the cookie value; and
  *                              - An XML_RPC_Value of type "string" with the cookie expiration time.
  */
-function xmlRpcView($params)
+function OA_Delivery_XmlRpc_View_Max($params)
 {
-    $cookieCache =& $GLOBALS['_MAX']['COOKIE']['CACHE'];
     global $XML_RPC_erruser;
-    global $XML_RPC_String, $XML_RPC_Struct, $XML_RPC_Array;
+    global $XML_RPC_String, $XML_RPC_Struct, $XML_RPC_Array, $XML_RPC_Int;
     // Check the parameters exist
     $numParams = $params->getNumParams();
     if ($numParams < 6) {
@@ -112,73 +312,116 @@ function xmlRpcView($params)
     }
     // Extract the what parameter
     $whatXmlRpcValue = $params->getParam(0);
-    $what = $whatXmlRpcValue->scalarval();
     // Extract the target parameter
     $targetXmlRpcValue = $params->getParam(1);
-    $target = $targetXmlRpcValue->scalarval();
     // Extract the source parameter
     $sourceXmlRpcValue = $params->getParam(2);
-    $source = $sourceXmlRpcValue->scalarval();
     // Extract the withText parameter
     $withTextXmlRpcValue = $params->getParam(3);
-    $withText = $withTextXmlRpcValue->scalarval();
     // Extract the remoteAddress parameter
     $remoteAddressXmlRpcValue = $params->getParam(4);
-    $_SERVER['REMOTE_ADDR'] = $remoteAddressXmlRpcValue->scalarval();
     // Extract the tunnelled cookies
     $cookiesXmlRpcValue = $params->getParam(5);
-    while (list($key, $value) = $cookiesXmlRpcValue->structeach()) {
-        $_COOKIE[$key] = $value->scalarval();
-    }
+    // Extract the context parameter, if any
     if ($numParams >= 7) {
-        $context = XML_RPC_decode($params->getParam(6));
+        $contextXmlRpcValue = $params->getParam(6);
     } else {
-        $context = array();
+        $contextXmlRpcValue = new XML_RPC_Value(array(), $XML_RPC_Array);
     }
-    // Find the ad display code
-    $output = MAX_adSelect($what, '', $target, $source, $withText, $context);
-    if ($output['contenttype'] == 'swf') {
-        $output['html'] = MAX_flashGetFlashObjectExternal() . $output['html'];
+    // Generate 0 campaignid parameter
+    $campaignidXmlRpcValue = new XML_RPC_Value(0, $XML_RPC_Int);
+
+    // Create environment array
+    $remoteInfoXmlRpcValue = new XML_RPC_Value(
+        array(
+            'remote_addr'   => $remoteAddressXmlRpcValue,
+            'cookies'       => $cookiesXmlRpcValue
+        ),
+        $XML_RPC_Struct
+    );
+
+    // Recreate XML-RPC message
+    $msg = new XML_RPC_Message('openads.view', array(
+        $remoteInfoXmlRpcValue,
+        $whatXmlRpcValue,
+        $campaignidXmlRpcValue,
+        $targetXmlRpcValue,
+        $sourceXmlRpcValue,
+        $withTextXmlRpcValue,
+        $contextXmlRpcValue
+    ));
+
+    // Relay call to openads.view
+    $xmlResponse = OA_Delivery_XmlRpc_View($msg);
+
+    //return new XML_RPC_Response(XML_RPC_encode(print_r($xmlResponse, true)));
+
+    // Check for errors
+    if ($xmlResponse->isError()) {
+        // Return error
+        return $xmlResponse;
     }
 
-    // Convert the output and cookies into XML_RPC_Values
-    if (count($output) > 0) {
-        foreach ($output as $key => $value) {
-            if (is_array($value)) {
-                $output[$key] = new XML_RPC_Value($value, $XML_RPC_Struct);
-            } else {
-                $output[$key] = new XML_RPC_Value($value, $XML_RPC_String);
-            }
-        }
-        $outputValue = new XML_RPC_Value($output, $XML_RPC_Struct);
-    }
-    if (count($cookieCache) > 0) {
-        $cookies = array();
-        foreach ($cookieCache as $key => $value) {
-            $cookie = array();
-            foreach ($value as $ikey => $ivalue) {
-                $cookie[$ikey] = new XML_RPC_Value($ivalue, $XML_RPC_String);
-            }
-            $cookies[$key] = new XML_RPC_Value($cookie, $XML_RPC_Struct);
-        }
-        $cookieValue = new XML_RPC_Value($cookies, $XML_RPC_Struct);
-    }
-    // Return the output and cookies
-    $returnArray = array();
-    if (count($output) > 0) {
-        $returnArray[] = $outputValue;
+    // Change the response
+    $output  = XML_RPC_decode($xmlResponse->value());
+    $cookies = $output['cookies'];
+    unset($output['cookies']);
+
+    // Return XML-RPC response
+    return new XML_RPC_Response(
+        new XML_RPC_Value(array(
+                XML_RPC_encode($output),
+                XML_RPC_encode($cookies)
+            ),
+            $XML_RPC_Array
+        )
+    );
+}
+
+
+/**
+ * A function to handle XML-RPC advertisement view requests. 2.0 version
+ *
+ * @deprecated
+ *
+ * @param XML_RPC_Message $params
+ * @return XML_RPC_Response
+ */
+function OA_Delivery_XmlRpc_View_PAN($params)
+{
+    // Extract the remote_info parameter
+    $remoteInfoXmlRpcValue = $params->getParam(0);
+    $remote_info = XML_RPC_Decode($params->getParam(0));
+
+    // Add empty cookies array
+    $remote_info['cookies'] = array();
+
+    // Create environment array
+    $remoteInfoXmlRpcValue = XML_RPC_encode($remote_info);
+
+    // Extract the context param
+    if ($params->getNumParams() > 6) {
+        $contextXmlRpcValue = $params->getParam(6);
     } else {
-        // Nothing to display
-        $returnArray[] = new XML_RPC_Value();
+        $contextXmlRpcValue = new XML_RPC_Value(array(), $XML_RPC_Array);
     }
-    if (count($cookieCache) > 0) {
-        $returnArray[] = $cookieValue;
-    } else {
-        // No cookies to set
-        $returnArray[] = new XML_RPC_Value();
-    }
-    $return = new XML_RPC_Value($returnArray, $XML_RPC_Array);
-    return new XML_RPC_Response($return);
+
+    // Recreate XML-RPC message
+    $msg = new XML_RPC_Message('phpAds.view', array(
+        $remoteInfoXmlRpcValue,
+        $params->getParam(1),
+        $params->getParam(2),
+        $params->getParam(3),
+        $params->getParam(4),
+        $params->getParam(5),
+        $contextXmlRpcValue
+    ));
+
+    // Relay call to openads.view
+    $xmlResponse = OA_Delivery_XmlRpc_View($msg);
+
+    // Check for errors as-is
+    return $xmlResponse;
 }
 
 ?>
--- www/delivery_dev/axmlrpc.php
+++ www/delivery_dev/axmlrpc.php
@@ -33,12 +33,25 @@ setupIncludePath();
 // Required files
 require_once MAX_PATH . '/lib/max/Delivery/XML-RPC.php';
 
-// Configure the XML-RPC server to use the xmlRpcView 
-// function to handle the XML-RPC ad view request
+// Configure the XML-RPC server to use the varous OA_Delivery_XmlRpc_View
+// function to handle the XML-RPC ad view requests
 $server = new XML_RPC_Server(array(
-                'max.view'  => array('function' => 'xmlRpcView'),
-                'signature' => $xmlRpcView_sig,
-                'docstring' => $xmlRpcView_doc
-              ));              
+            'openads.view'  => array(
+                'function'  => 'OA_Delivery_XmlRpc_View',
+                'signature' => $xmlRpcView_OA['sig'],
+                'docstring' => $xmlRpcView_OA['doc']
+            ),
+            'phpAds.view'  => array(
+                'function'  => 'OA_Delivery_XmlRpc_View_PAN',
+                'signature' => $xmlRpcView_PAN['sig'],
+                'docstring' => $xmlRpcView_PAN['doc']
+            ),
+            'max.view'  => array(
+                'function'  => 'OA_Delivery_XmlRpc_View_Max',
+                'signature' => $xmlRpcView_Max['sig'],
+                'docstring' => $xmlRpcView_Max['doc']
+            )
+    )
+);
 
 ?>
