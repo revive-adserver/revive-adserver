33dff45a1abfd33225cd0ea22e87f9e79bbbde0d
andrzej.swedrzynski@openads.org <andrzej.swedrzynski@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-06-01 09:10:22 +0000

    Reverted accidental change.

--- constants.php
+++ constants.php
@@ -38,7 +38,7 @@ $Id$
 function setupConstants()
 {
     // Define this version of Openads's constants
-    define('OA_VERSION', 'trunk-200706011002');
+    define('OA_VERSION' ,           '2.3.32-beta-rc10');
     define('MAX_PRODUCT_NAME',      'Openads');
     define('MAX_PRODUCT_URL',       'www.openads.org');
     define('MAX_PRODUCT_DOCSURL',   'docs.openads.org');
