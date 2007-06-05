9ef724621a9f9a0e3419ec5e8bedb5ee7e9c7831
andrew.hill@openads.org <andrew.hill@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-06-05 09:08:01 +0000

    Upgraded Simpletest to v1.0.1-beta. Refs #526.

---
+++ lib/simpletest/test/unit_tests.php
@@ -0,0 +1,16 @@
+<?php
+    // $Id$
+    if (! defined('TEST')) {
+        define('TEST', __FILE__);
+    }
+    require_once(dirname(__FILE__) . '/test_groups.php');
+    require_once(dirname(__FILE__) . '/../reporter.php');
+    
+    if (TEST == __FILE__) {
+        $test = &new UnitTests();
+        if (SimpleReporter::inCli()) {
+            exit ($test->run(new TextReporter()) ? 0 : 1);
+        }
+        $test->run(new HtmlReporter());
+    }
+?>
