5c5092a332bb67b6e3e624b49a62b3146873cc61
monique.szpak@openads.org <monique.szpak@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-06-08 17:20:05 +0000

    upgrade class: mmm 0.1 detection

--- lib/OA/Upgrade/Upgrade.php
+++ lib/OA/Upgrade/Upgrade.php
@@ -395,6 +395,29 @@ class OA_Upgrade
                 $this->existing_installation_status = OA_STATUS_PAN_VERSION_FAILED;
                 return false;
             }
+
+            $valid = (version_compare($this->versionInitialApplication,'0.100')>=0);
+            if ($valid)
+            {
+                $this->versionInitialSchema['tables_core'] = '300';
+                if (!$this->initDatabaseConnection())
+                {
+                    $this->existing_installation_status = OA_STATUS_PAN_DBCONNECT_FAILED;
+                    return false;
+                }
+                if (!$this->_checkDBIntegrity($this->versionInitialSchema['tables_core']))
+                {
+                    $this->existing_installation_status = OA_STATUS_PAN_DBINTEG_FAILED;
+                    return false;
+                }
+                $this->existing_installation_status = OA_STATUS_CAN_UPGRADE;
+                $this->aPackageList[0] = 'openads_upgrade_2.1.29_to_2.3.32_beta.xml';
+                $this->aDsn['database'] = $GLOBALS['_MAX']['CONF']['database'];
+                $this->aDsn['table']    = $GLOBALS['_MAX']['CONF']['table'];
+                return true;
+            }
+
+
             $valid = ( (version_compare($this->versionInitialApplication,'200.313')>=0)
                       ||
                        (version_compare($this->versionInitialApplication,'200.314')>=0)
@@ -404,7 +427,7 @@ class OA_Upgrade
                 $this->versionInitialSchema['tables_core'] = '099';
                 if (!$this->initDatabaseConnection())
                 {
-                    $this->existing_installation_status = OA_STATUS_MAX_DBCONNECT_FAILED;
+                    $this->existing_installation_status = OA_STATUS_PAN_DBCONNECT_FAILED;
                     return false;
                 }
                 if (!$this->_checkDBIntegrity($this->versionInitialSchema['tables_core']))
