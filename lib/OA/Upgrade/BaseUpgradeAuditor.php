8dec48ec15b8acade574f1d92c9f695c929addc6
matthieu.aubry@openads.org <matthieu.aubry@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-06-26 17:40:31 +0000

    Upgrade test : Add files to the svn, forgot previously

---
+++ lib/OA/Upgrade/BaseUpgradeAuditor.php
@@ -0,0 +1,81 @@
+<?php
+class OA_BaseUpgradeAuditor 
+{
+    
+    // needs to be defined in the child class
+    var $action_table_xml_filename;
+
+	function OA_BaseUpgradeAuditor()
+	{
+	}
+
+	/**
+     * audit actions taken
+     *
+     * @param array $aParams
+     * @return boolean
+     */
+    function logAuditAction($aParams=array())
+    {
+        $aParams = $this->_escapeParams($aParams);
+        $columns = implode(",", array_keys($this->aParams)).','.implode(",", array_keys($aParams));
+        $values  = implode(",", array_values($this->aParams)).','.implode(",", array_values($aParams));
+
+        $query = "INSERT INTO {$this->prefix}{$this->logTable} ({$columns}, updated) VALUES ({$values}, '". OA::getNow() ."')";
+        $result = $this->oDbh->exec($query);
+
+        if ($this->isPearError($result, "error inserting {$this->prefix}{$this->logTable}"))
+        {
+            return false;
+        }
+        return true;
+    }
+    
+    function setKeyParams($aParams='')
+    {
+        $this->aParams = $this->_escapeParams($aParams);
+    }
+    
+    /**
+     * the action_table_name table must exist for all upgrade events
+     * currently the schema is stored in a separate xml file which is not part of an upgrade pkg
+     * eventually this table schema should be merged into the core tables schema
+     *
+     * @return boolean
+     */
+    function _createAuditTable()
+    {
+        $xmlfile = MAX_PATH.$this->action_table_xml_filename;
+
+        $oTable = new OA_DB_Table();
+        $oTable->init($xmlfile);
+        return $oTable->createTable($this->logTable);
+    }
+
+    function _checkCreateAuditTable()
+    {
+        $this->aDBTables = $this->oDbh->manager->listTables();
+        if (!in_array($this->prefix.$this->logTable, $this->aDBTables))
+        {
+            $this->log('creating upgrade_action audit table');
+            if (!$this->_createAuditTable())
+            {
+                $this->logError('failed to create upgrade_action audit table');
+                return false;
+            }
+            $this->log('successfully created upgrade_action audit table');
+        }
+        return true;
+    }
+    
+    function _escapeParams($aParams)
+    {
+        foreach ($aParams AS $k => $v)
+        {
+            $aParams[$k] = $this->oDbh->quote($v);
+        }
+        return $aParams;
+    }
+    
+}
+?>
