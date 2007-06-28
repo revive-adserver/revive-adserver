34a85cc31446942b6cc703fa8b5240e09f2ce2f1
arlen.coupland@openads.org <arlen.coupland@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-06-28 14:24:49 +0000

    Fixed #671. Fixed display differences between IE/FF/etc for dbLoader bar after changes.

--- www/admin/js/openads.js
+++ www/admin/js/openads.js
@@ -1,5 +1,6 @@
 function showLoader(f) {
     var dbLoader = new getObj('dbLoader');
+    window.location = '#mid';
     dbLoader.style.display='block';
-    dbLoader.style.height='570px';
+    dbLoader.style.height='700px';
 }
