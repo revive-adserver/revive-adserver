8ec7e56286811386bb290bfedb0e38495f1f5dcb
david.keen@openads.org <david.keen@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-07-02 09:02:58 +0000

    A new release OA_VERSION constants auto-replacement.

--- constants.php
+++ constants.php
@@ -38,7 +38,7 @@ $Id$
 function setupConstants()
 {
     // Define this version of Openads's constants
-    define('OA_VERSION', '2.3.34-beta-rc1');
+    define('OA_VERSION', '2.3.34-beta-rc2');
     define('MAX_PRODUCT_NAME',      'Openads');
     define('MAX_PRODUCT_URL',       'www.openads.org');
     define('MAX_PRODUCT_DOCSURL',   'docs.openads.org');
