5077c1ee94b58e511e6806476e02e00d28faa054
radek.maciaszek@openads.org <radek.maciaszek@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-07-03 13:15:46 +0000

    Added a link to FAQ

--- www/admin/install.php
+++ www/admin/install.php
@@ -63,7 +63,7 @@ if (!$oSystemMgr->checkMemory()) {
     $memory = getMinimumRequiredMemory() / 1048576;
     echo '<link rel="stylesheet" type="text/css" href="css/install.css"/><br />';
     echo '<div class="sysmessage sysinfoerror" style="text-align: center;">The minimum amount of memory <a href="http://docs.openads.org/openads-2.3-guide/requirements.html" target="_blank" style="color: #990000">required</a> by Openads is <b>'. $memory
-        .' MB</b>. <br />Please increase your PHP memory_limit before continuing.</div>';
+        .' MB</b>. <br />Please <a href="http://www.openads.org/support/faq.html" target="_blank" style="color: #990000">increase</a> your PHP memory_limit before continuing.</div>';
     exit(1);
 }
 
