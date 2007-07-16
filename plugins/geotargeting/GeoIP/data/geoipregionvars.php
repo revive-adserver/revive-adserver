776953ee0ffccb702e870f0cfa44524a6a281257
matteo.beccati@openads.org <matteo.beccati@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-07-16 13:46:59 +0000

    Refs #820 - Updated MaxMind GeoIP libraries

--- plugins/geotargeting/GeoIP/data/geoipregionvars.php
+++ plugins/geotargeting/GeoIP/data/geoipregionvars.php
@@ -1,13 +1,6 @@
 <?php
-
-/**
- * @package    MaxPlugin
- * @subpackage Geotargeting
- * @author     MaxMind LLC
- */
-
 # Copyright 2003 Maxmind LLC All Rights Reserved
-$ISO = array(
+$GEOIP_REGION_NAME = array(
 "CA" => array(
 "AB" => "Alberta",
 "BC" => "British Columbia",
@@ -84,8 +77,7 @@ $ISO = array(
 "WA" => "Washington",
 "WV" => "West Virginia",
 "WI" => "Wisconsin",
-"WY" => "Wyoming"));
-$FIPS = array(
+"WY" => "Wyoming"),
 "AD" => array(
 "02" => "Canillo",
 "03" => "Encamp",
@@ -1140,7 +1132,7 @@ $FIPS = array(
 "53" => "Tigray",
 "54" => "YeDebub Biheroch Bihereseboch na Hizboch"),
 "FI" => array(
-"01" => "ï¿½?land",
+"01" => "Åland",
 "06" => "Lapland",
 "08" => "Oulu",
 "13" => "Southern Finland",
@@ -3925,58 +3917,6 @@ $FIPS = array(
 "69" => "Katakwi",
 "73" => "Nakasongola",
 "74" => "Sembabule"),
-"US" => array(
-"01" => "Alabama",
-"02" => "Alaska",
-"04" => "Arizona",
-"05" => "Arkansas",
-"06" => "California",
-"08" => "Colorado",
-"09" => "Connecticut",
-"10" => "Delaware",
-"11" => "District of Columbia",
-"12" => "Florida",
-"13" => "Georgia",
-"15" => "Hawaii",
-"16" => "Idaho",
-"17" => "Illinois",
-"18" => "Indiana",
-"19" => "Iowa",
-"20" => "Kansas",
-"21" => "Kentucky",
-"22" => "Louisiana",
-"23" => "Maine",
-"24" => "Maryland",
-"25" => "Massachusetts",
-"26" => "Michigan",
-"27" => "Minnesota",
-"28" => "Mississippi",
-"29" => "Missouri",
-"30" => "Montana",
-"31" => "Nebraska",
-"32" => "Nevada",
-"33" => "New Hampshire",
-"34" => "New Jersey",
-"35" => "New Mexico",
-"36" => "New York",
-"37" => "North Carolina",
-"38" => "North Dakota",
-"39" => "Ohio",
-"40" => "Oklahoma",
-"41" => "Oregon",
-"42" => "Pennyslvania",
-"44" => "Rhode Island",
-"45" => "South Carolina",
-"46" => "South Dakota",
-"47" => "Tennessee",
-"48" => "Texas",
-"49" => "Utah",
-"50" => "Vermont",
-"51" => "Virginia",
-"53" => "Washington",
-"54" => "West Virginia",
-"55" => "Wisconsin",
-"56" => "Wyoming"),
 "UY" => array(
 "01" => "Artigas",
 "02" => "Canelones",
