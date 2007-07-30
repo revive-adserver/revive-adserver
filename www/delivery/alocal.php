ebfce5d8069c166d99e56e134186b9a94c9a836a
post_commit <post_commit@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-07-30 13:33:16 +0000

    Commit of auto-generated delivery files.

--- www/delivery/alocal.php
+++ www/delivery/alocal.php
@@ -175,7 +175,7 @@ return;
 $checkIfAlreadySet = true;
 // Define the PEAR installation path
 $existingPearPath = ini_get('include_path');
-$newPearPath = MAX_PATH . '/lib/pear';
+$newPearPath = MAX_PATH . DIRECTORY_SEPARATOR.'lib' . DIRECTORY_SEPARATOR . 'pear';
 if (!empty($existingPearPath)) {
 $newPearPath .= PATH_SEPARATOR . $existingPearPath;
 }
