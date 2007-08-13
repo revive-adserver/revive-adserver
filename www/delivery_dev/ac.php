d4ff3900e4a29fd59bcb9526068fbc75595cf976
andrew.hill@openads.org <andrew.hill@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-08-13 13:23:30 +0000

    Restored the delivery engine.

--- www/delivery_dev/ac.php
+++ www/delivery_dev/ac.php
@@ -29,7 +29,7 @@ $Id$
 */
 
 // Require the initialisation file
-//require_once '../../init-delivery.php';
+require_once '../../init-delivery.php';
 
 // Required files
 require_once MAX_PATH . '/lib/max/Delivery/adSelect.php';
