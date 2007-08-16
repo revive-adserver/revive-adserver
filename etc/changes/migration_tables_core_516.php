bf4774d296a1b2d8a84ab8037a964f9cf089a319
andrew.hill@openads.org <andrew.hill@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-08-16 14:35:22 +0000

    Removed tables from the core schema that have never been used, and have no purpose. Will make the job of defining which tables to prune easier. Refs #946.

---
+++ etc/changes/migration_tables_core_516.php
@@ -0,0 +1,164 @@
+<?php
+
+require_once(MAX_PATH.'/lib/OA/Upgrade/Migration.php');
+
+class Migration_516 extends Migration
+{
+
+    function Migration_516()
+    {
+        //$this->__construct();
+
+		$this->aTaskList_destructive[] = 'beforeRemoveTable__data_summary_zone_country_daily';
+		$this->aTaskList_destructive[] = 'afterRemoveTable__data_summary_zone_country_daily';
+		$this->aTaskList_destructive[] = 'beforeRemoveTable__data_summary_zone_country_forecast';
+		$this->aTaskList_destructive[] = 'afterRemoveTable__data_summary_zone_country_forecast';
+		$this->aTaskList_destructive[] = 'beforeRemoveTable__data_summary_zone_country_monthly';
+		$this->aTaskList_destructive[] = 'afterRemoveTable__data_summary_zone_country_monthly';
+		$this->aTaskList_destructive[] = 'beforeRemoveTable__data_summary_zone_domain_page_daily';
+		$this->aTaskList_destructive[] = 'afterRemoveTable__data_summary_zone_domain_page_daily';
+		$this->aTaskList_destructive[] = 'beforeRemoveTable__data_summary_zone_domain_page_forecast';
+		$this->aTaskList_destructive[] = 'afterRemoveTable__data_summary_zone_domain_page_forecast';
+		$this->aTaskList_destructive[] = 'beforeRemoveTable__data_summary_zone_domain_page_monthly';
+		$this->aTaskList_destructive[] = 'afterRemoveTable__data_summary_zone_domain_page_monthly';
+		$this->aTaskList_destructive[] = 'beforeRemoveTable__data_summary_zone_site_keyword_daily';
+		$this->aTaskList_destructive[] = 'afterRemoveTable__data_summary_zone_site_keyword_daily';
+		$this->aTaskList_destructive[] = 'beforeRemoveTable__data_summary_zone_site_keyword_forecast';
+		$this->aTaskList_destructive[] = 'afterRemoveTable__data_summary_zone_site_keyword_forecast';
+		$this->aTaskList_destructive[] = 'beforeRemoveTable__data_summary_zone_site_keyword_monthly';
+		$this->aTaskList_destructive[] = 'afterRemoveTable__data_summary_zone_site_keyword_monthly';
+		$this->aTaskList_destructive[] = 'beforeRemoveTable__data_summary_zone_source_daily';
+		$this->aTaskList_destructive[] = 'afterRemoveTable__data_summary_zone_source_daily';
+		$this->aTaskList_destructive[] = 'beforeRemoveTable__data_summary_zone_source_forecast';
+		$this->aTaskList_destructive[] = 'afterRemoveTable__data_summary_zone_source_forecast';
+		$this->aTaskList_destructive[] = 'beforeRemoveTable__data_summary_zone_source_monthly';
+		$this->aTaskList_destructive[] = 'afterRemoveTable__data_summary_zone_source_monthly';
+
+
+    }
+
+
+
+	function beforeRemoveTable__data_summary_zone_country_daily()
+	{
+		return $this->beforeRemoveTable('data_summary_zone_country_daily');
+	}
+
+	function afterRemoveTable__data_summary_zone_country_daily()
+	{
+		return $this->afterRemoveTable('data_summary_zone_country_daily');
+	}
+
+	function beforeRemoveTable__data_summary_zone_country_forecast()
+	{
+		return $this->beforeRemoveTable('data_summary_zone_country_forecast');
+	}
+
+	function afterRemoveTable__data_summary_zone_country_forecast()
+	{
+		return $this->afterRemoveTable('data_summary_zone_country_forecast');
+	}
+
+	function beforeRemoveTable__data_summary_zone_country_monthly()
+	{
+		return $this->beforeRemoveTable('data_summary_zone_country_monthly');
+	}
+
+	function afterRemoveTable__data_summary_zone_country_monthly()
+	{
+		return $this->afterRemoveTable('data_summary_zone_country_monthly');
+	}
+
+	function beforeRemoveTable__data_summary_zone_domain_page_daily()
+	{
+		return $this->beforeRemoveTable('data_summary_zone_domain_page_daily');
+	}
+
+	function afterRemoveTable__data_summary_zone_domain_page_daily()
+	{
+		return $this->afterRemoveTable('data_summary_zone_domain_page_daily');
+	}
+
+	function beforeRemoveTable__data_summary_zone_domain_page_forecast()
+	{
+		return $this->beforeRemoveTable('data_summary_zone_domain_page_forecast');
+	}
+
+	function afterRemoveTable__data_summary_zone_domain_page_forecast()
+	{
+		return $this->afterRemoveTable('data_summary_zone_domain_page_forecast');
+	}
+
+	function beforeRemoveTable__data_summary_zone_domain_page_monthly()
+	{
+		return $this->beforeRemoveTable('data_summary_zone_domain_page_monthly');
+	}
+
+	function afterRemoveTable__data_summary_zone_domain_page_monthly()
+	{
+		return $this->afterRemoveTable('data_summary_zone_domain_page_monthly');
+	}
+
+	function beforeRemoveTable__data_summary_zone_site_keyword_daily()
+	{
+		return $this->beforeRemoveTable('data_summary_zone_site_keyword_daily');
+	}
+
+	function afterRemoveTable__data_summary_zone_site_keyword_daily()
+	{
+		return $this->afterRemoveTable('data_summary_zone_site_keyword_daily');
+	}
+
+	function beforeRemoveTable__data_summary_zone_site_keyword_forecast()
+	{
+		return $this->beforeRemoveTable('data_summary_zone_site_keyword_forecast');
+	}
+
+	function afterRemoveTable__data_summary_zone_site_keyword_forecast()
+	{
+		return $this->afterRemoveTable('data_summary_zone_site_keyword_forecast');
+	}
+
+	function beforeRemoveTable__data_summary_zone_site_keyword_monthly()
+	{
+		return $this->beforeRemoveTable('data_summary_zone_site_keyword_monthly');
+	}
+
+	function afterRemoveTable__data_summary_zone_site_keyword_monthly()
+	{
+		return $this->afterRemoveTable('data_summary_zone_site_keyword_monthly');
+	}
+
+	function beforeRemoveTable__data_summary_zone_source_daily()
+	{
+		return $this->beforeRemoveTable('data_summary_zone_source_daily');
+	}
+
+	function afterRemoveTable__data_summary_zone_source_daily()
+	{
+		return $this->afterRemoveTable('data_summary_zone_source_daily');
+	}
+
+	function beforeRemoveTable__data_summary_zone_source_forecast()
+	{
+		return $this->beforeRemoveTable('data_summary_zone_source_forecast');
+	}
+
+	function afterRemoveTable__data_summary_zone_source_forecast()
+	{
+		return $this->afterRemoveTable('data_summary_zone_source_forecast');
+	}
+
+	function beforeRemoveTable__data_summary_zone_source_monthly()
+	{
+		return $this->beforeRemoveTable('data_summary_zone_source_monthly');
+	}
+
+	function afterRemoveTable__data_summary_zone_source_monthly()
+	{
+		return $this->afterRemoveTable('data_summary_zone_source_monthly');
+	}
+
+}
+
+?>
