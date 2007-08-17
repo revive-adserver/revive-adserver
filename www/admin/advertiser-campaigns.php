2394cd0c54dcc350e6f851e4f56b2c33aa68d693
chris.nutting@openads.org <chris.nutting@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-08-17 10:55:44 +0000

    Refs #957 (Reopened) Reverted [8996] "fix" was incorrect

--- www/admin/advertiser-campaigns.php
+++ www/admin/advertiser-campaigns.php
@@ -81,7 +81,7 @@ while ($doClients->fetch() && $row = $doClients->toArray()) {
 	phpAds_PageContext(
 		phpAds_buildName ($row['clientid'], $row['clientname']),
 		"advertiser-campaigns.php?clientid=".$row['clientid'],
-		$clientid = $row['clientid']
+		$clientid == $row['clientid']
 	);
 }
 
