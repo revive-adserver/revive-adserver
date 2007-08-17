cd06a0c0308ba0c4704299646677580a315f3bf4
matteo.beccati@openads.org <matteo.beccati@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-08-17 11:17:07 +0000

    svn merge -r9054:9055 https://svn.openads.org/openads/branches/2.6/branches/developers/matteo/277_oac_xml_rpc

---
+++ lib/OA/Central/Rpc.php
@@ -0,0 +1,164 @@
+<?php
+
+/*
++---------------------------------------------------------------------------+
+| Openads v2.5                                                              |
+| ============                                                              |
+|                                                                           |
+| Copyright (c) 2003-2007 Openads Limited                                   |
+| For contact details, see: http://www.openads.org/                         |
+|                                                                           |
+| This program is free software; you can redistribute it and/or modify      |
+| it under the terms of the GNU General Public License as published by      |
+| the Free Software Foundation; either version 2 of the License, or         |
+| (at your option) any later version.                                       |
+|                                                                           |
+| This program is distributed in the hope that it will be useful,           |
+| but WITHOUT ANY WARRANTY; without even the implied warranty of            |
+| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             |
+| GNU General Public License for more details.                              |
+|                                                                           |
+| You should have received a copy of the GNU General Public License         |
+| along with this program; if not, write to the Free Software               |
+| Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA |
++---------------------------------------------------------------------------+
+$Id$
+*/
+
+require_once MAX_PATH.'/lib/OA.php';
+require_once 'XML/RPC.php';
+
+
+define('OA_CENTRAL_PROTOCOL_VERSION', 3);
+
+define('OA_CENTRAL_AUTH_NONE',    0);
+define('OA_CENTRAL_AUTH_SSO',     1);
+define('OA_CENTRAL_AUTH_CAPTCHA', 2);
+
+
+/**
+ * OAP to OAC communication class
+ *
+ */
+class OA_Central_Rpc
+{
+    /**
+     * @var XML_RPC_Client
+     */
+    var $oXml;
+
+    /**
+     * Class constructor
+     *
+     * @return OA_Central_Rpc
+     */
+    function OA_Central_Rpc()
+    {
+        $this->oXml = new XML_RPC_Client(
+            '/oap/',
+            'https://oac.openads.org'
+        );
+    }
+
+    /**
+     * A method to perform a call to the OAC XML-RPC server
+     *
+     * @param string $methodName The RPC method name
+     * @param int    $authType   Type of required authentication, see constants
+     * @param array  $aParams    Array of XML_RPC_Values
+     * @return mixed The returned value or PEAR_Error on error
+     */
+    function call($methodName, $authType, $aParams = null)
+    {
+        $oMsg = new XML_RPC_Message('oac.'.$methodName);
+
+        $aHeader = array(
+            'protocolVersion'   => OA_CENTRAL_PROTOCOL_VERSION,
+            'platformHash'      => sha1('foobar')
+        );
+
+        if ($authType & OA_CENTRAL_AUTH_SSO) {
+            $aHeader['ssoUsername'] = 'foo';
+            $aHeader['ssoPassword'] = 'bar';
+        }
+        if ($authType & OA_CENTRAL_AUTH_CAPTCHA) {
+            $aHeader['ssoCaptcha']  = 'baz';
+        }
+
+        $oMsg->addParam(XML_RPC_encode($aHeader));
+
+        if (is_array($aParams)) {
+            foreach ($aParams as $oParam) {
+                $oMsg->addParam($oParam);
+            }
+        }
+
+        $oResponse = $this->oXml->send($oMsg);
+
+        if ($oResponse->faultCode()) {
+            return PEAR::raiseError($oResponse->faultString(), $oResponse->faultCode());
+        }
+
+        return XML_RPC_decode($oResponse->value());
+    }
+
+    /**
+     * Wrapper to the call method using no authentication
+     *
+     * @see call()
+     *
+     * @param string $methodName The RPC method name
+     * @param array  $aParams    Array of XML_RPC_Values
+     * @return mixed The returned value or PEAR_Error on error
+     */
+    function callNoAuth($methodName, $aParams = null)
+    {
+        return $this->call($methodName, OA_CENTRAL_AUTH_NONE, $aParams);
+    }
+
+    /**
+     * Wrapper to the call method using SSO authentication
+     *
+     * @see call()
+     *
+     * @param string $methodName The RPC method name
+     * @param array  $aParams    Array of XML_RPC_Values
+     * @return mixed The returned value or PEAR_Error on error
+     */
+    function callSso($methodName, $aParams = null)
+    {
+        return $this->call($methodName, OA_CENTRAL_AUTH_SSO, $aParams);
+    }
+
+    /**
+     * Wrapper to the call method using Capthca authentication
+     *
+     * @see call()
+     *
+     * @param string $methodName The RPC method name
+     * @param array  $aParams    Array of XML_RPC_Values
+     * @return mixed The returned value or PEAR_Error on error
+     */
+    function callCaptcha($methodName, $aParams = null)
+    {
+        return $this->call($methodName, OA_CENTRAL_AUTH_CAPTCHA, $aParams);
+    }
+
+    /**
+     * Wrapper to the call method using SSO + Capthca authentication
+     *
+     * @see call()
+     *
+     * @param string $methodName The RPC method name
+     * @param array  $aParams    Array of XML_RPC_Values
+     * @return mixed The returned value or PEAR_Error on error
+     */
+    function callCaptchaSso($methodName, $aParams = null)
+    {
+        return $this->call($methodName, OA_CENTRAL_AUTH_SSO | OA_CENTRAL_AUTH_CAPTCHA, $aParams);
+    }
+}
+
+
+
+?>
