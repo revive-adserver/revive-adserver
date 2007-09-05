dfe1589323b962133b4e03952a5810ef4c052505
matteo.beccati@openads.org <matteo.beccati@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-09-05 11:36:44 +0000

    Added correct URLs

--- www/admin/dashboard.php
+++ www/admin/dashboard.php
@@ -57,8 +57,8 @@ $oTpl->assign('dashboardURL', MAX::constructURL(MAX_URL_ADMIN, 'dashboard.php?if
 if ($iframe) {
     $oTpl->assign('ssoAdmin',     OA_Dal_ApplicationVariables::get('sso_admin'));
     $oTpl->assign('ssoPasswd',    OA_Dal_ApplicationVariables::get('sso_password'));
-    $oTpl->assign('casLoginURL',  'https://login.openads.org:8443/cas-server/login');
-    $oTpl->assign('serviceURL',   'http://forum.openads.org/index.php?act=module&module=cas');
+    $oTpl->assign('casLoginURL',  'https://login.openads.org/sso/login');
+    $oTpl->assign('serviceURL',   'https://login.openads.org/account/account.auth');
 }
 
 $oTpl->display();
