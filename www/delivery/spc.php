ffde95ae9df24f6ce70a10335b52e4ea19ff28bb
post_commit <post_commit@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-09-11 09:23:13 +0000

    Commit of auto-generated delivery files.

--- www/delivery/spc.php
+++ www/delivery/spc.php
@@ -25,7 +25,7 @@
 | along with this program; if not, write to the Free Software               |
 | Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA |
 +---------------------------------------------------------------------------+
-$Id: template.php 8911 2007-08-10 09:47:46Z andrew.hill@openads.org $
+$Id: template.php 9558 2007-09-11 06:50:55Z aj.tarachanowicz@openads.org $
 */
 
 /**
