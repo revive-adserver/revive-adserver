d72fbea861062d769f02cc152c33a1966e80168e
matteo.beccati@openads.org <matteo.beccati@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-09-17 15:52:02 +0000

    Added upgrade package for 2.5.5 - Added OAC fields to the affiliate table

---
+++ etc/changes/migration_tables_core_533.php
@@ -0,0 +1,59 @@
+<?php
+
+require_once(MAX_PATH.'/lib/OA/Upgrade/Migration.php');
+
+class Migration_533 extends Migration
+{
+
+    function Migration_533()
+    {
+        //$this->__construct();
+
+		$this->aTaskList_constructive[] = 'beforeAddField__affiliates__oac_country_code';
+		$this->aTaskList_constructive[] = 'afterAddField__affiliates__oac_country_code';
+		$this->aTaskList_constructive[] = 'beforeAddField__affiliates__oac_language_id';
+		$this->aTaskList_constructive[] = 'afterAddField__affiliates__oac_language_id';
+		$this->aTaskList_constructive[] = 'beforeAddField__affiliates__oac_category_id';
+		$this->aTaskList_constructive[] = 'afterAddField__affiliates__oac_category_id';
+
+
+		$this->aObjectMap['affiliates']['oac_country_code'] = array('fromTable'=>'affiliates', 'fromField'=>'oac_country_code');
+		$this->aObjectMap['affiliates']['oac_language_id'] = array('fromTable'=>'affiliates', 'fromField'=>'oac_language_id');
+		$this->aObjectMap['affiliates']['oac_category_id'] = array('fromTable'=>'affiliates', 'fromField'=>'oac_category_id');
+    }
+
+
+
+	function beforeAddField__affiliates__oac_country_code()
+	{
+		return $this->beforeAddField('affiliates', 'oac_country_code');
+	}
+
+	function afterAddField__affiliates__oac_country_code()
+	{
+		return $this->afterAddField('affiliates', 'oac_country_code');
+	}
+
+	function beforeAddField__affiliates__oac_language_id()
+	{
+		return $this->beforeAddField('affiliates', 'oac_language_id');
+	}
+
+	function afterAddField__affiliates__oac_language_id()
+	{
+		return $this->afterAddField('affiliates', 'oac_language_id');
+	}
+
+	function beforeAddField__affiliates__oac_category_id()
+	{
+		return $this->beforeAddField('affiliates', 'oac_category_id');
+	}
+
+	function afterAddField__affiliates__oac_category_id()
+	{
+		return $this->afterAddField('affiliates', 'oac_category_id');
+	}
+
+}
+
+?>
