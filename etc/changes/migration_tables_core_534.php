10c24fc2e5ce53fab19041acfe04f888ef0726ed
andrew.hill@openads.org <andrew.hill@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-09-19 10:18:21 +0000

    Added new "est" column to the data_summary_zone_impression_history table, so that rows which are based on the default ZIF value can be determined. Resf #1047.

---
+++ etc/changes/migration_tables_core_534.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once(MAX_PATH.'/lib/OA/Upgrade/Migration.php');
+
+class Migration_534 extends Migration
+{
+
+    function Migration_534()
+    {
+        //$this->__construct();
+
+		$this->aTaskList_constructive[] = 'beforeAddField__data_summary_zone_impression_history__est';
+		$this->aTaskList_constructive[] = 'afterAddField__data_summary_zone_impression_history__est';
+
+
+		$this->aObjectMap['data_summary_zone_impression_history']['est'] = array('fromTable'=>'data_summary_zone_impression_history', 'fromField'=>'est');
+    }
+
+
+
+	function beforeAddField__data_summary_zone_impression_history__est()
+	{
+		return $this->beforeAddField('data_summary_zone_impression_history', 'est');
+	}
+
+	function afterAddField__data_summary_zone_impression_history__est()
+	{
+		return $this->afterAddField('data_summary_zone_impression_history', 'est');
+	}
+
+}
+
+?>
