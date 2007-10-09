6273b1d560d6a2e8c3a6486865ee605ab0e9bce9
radek.maciaszek@openads.org <radek.maciaszek@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-10-09 16:08:30 +0000

    Fixed handling error messages in login module

--- lib/OA/Dashboard/Widgets/Login.php
+++ lib/OA/Dashboard/Widgets/Login.php
@@ -28,7 +28,14 @@ $Id$
 require_once MAX_PATH . '/lib/OA/Dashboard/Widget.php';
 require_once MAX_PATH . '/lib/max/Admin/Redirect.php';
 require_once MAX_PATH . '/lib/OA/Central/AdNetworks.php';
+require_once MAX_PATH . '/lib/OA/Central/Dashboard.php';
 
+define('OA_SSO_USERNAME', 'sso_username');
+define('OA_SSO_PASSWORD', 'sso_password');
+
+define('OA_SSO_ERROR_WRONG_CREDENTIALS', 1);
+define('OA_SSO_ERROR_WRONG_PARAMETERS', 2);
+define('OA_SSO_ERROR_PLATFORM_TAKEN', 3);
 
 /**
  * A class to display the dashboard iframe content
@@ -36,8 +43,7 @@ require_once MAX_PATH . '/lib/OA/Central/AdNetworks.php';
  */
 class OA_Dashboard_Widget_Login extends OA_Dashboard_Widget
 {
-    var $wrongCredentials;
-    var $wrongParameters;
+    var $errorCode = null;
 
     var $lastUsedUserName;
 
@@ -50,27 +56,33 @@ class OA_Dashboard_Widget_Login extends OA_Dashboard_Widget
         $this->getCredentials();
 
         $this->wrongParameters = false;
-        if (!empty($aParams['sso_password'])) {
+        
+        if (!empty($aParams['login'])) {
+            if ((empty($aParams[OA_SSO_USERNAME]) && !$this->ssoAdmin) || empty($aParams[OA_SSO_PASSWORD])) {
+                $this->errorCode = OA_SSO_ERROR_WRONG_PARAMETERS;
+                return;
+            }
+        
             if (!$this->ssoAdmin) {
-                $this->wrongParameters = true;
-                if (!empty($aParams['sso_username'])) {
-                    $this->lastUsedUserName = $aParams['sso_username'];
-                    $passwordHash = md5($aParams['sso_password']);
+                // user didn't connect platform yet
+                $this->lastUsedUserName = $aParams[OA_SSO_USERNAME];
+                $passwordHash = md5($aParams[OA_SSO_PASSWORD]);
 
-                    $oAdNetworks = new OA_Central_AdNetworks();
-                    $result = $oAdNetworks->connectOAPToOAC($aParams['sso_username'], $passwordHash);
+                $oAdNetworks = new OA_Central_AdNetworks();
+                $result = $oAdNetworks->connectOAPToOAC($aParams[OA_SSO_USERNAME], $passwordHash);
 
-                    if (!PEAR::isError($result)) {
-                        OA_Dal_ApplicationVariables::set('sso_admin', $aParams['sso_username']);
-                        OA_Dal_ApplicationVariables::set('sso_password', $passwordHash);
-                        $this->wrongParameters = false;
-                    }
+                if (!PEAR::isError($result)) {
+                    OA_Dal_ApplicationVariables::set('sso_admin', $aParams[OA_SSO_USERNAME]);
+                    OA_Dal_ApplicationVariables::set('sso_password', $passwordHash);
+                } else {
+                    $this->errorCode = $result->getCode();
                 }
             } else {
+                // user already connected - just changing password
                 OA_Dal_ApplicationVariables::set('sso_password', md5($aParams['sso_password']));
             }
 
-            if (!$this->wrongParameters) {
+            if (!$this->errorCode) {
                 $url = $this->buildUrl($GLOBALS['_MAX']['CONF']['oacDashboard']);
                 MAX_Admin_Redirect::redirect('ssoProxy.php?url='.urlencode($url));
                 exit;
@@ -92,26 +104,42 @@ class OA_Dashboard_Widget_Login extends OA_Dashboard_Widget
         $oTpl->assign('forgotUrl', $this->buildUrl($aConf['oacSSO'], 'forgot'));
         if (!empty($this->lastUsedUserName)) {
             $oTpl->assign('ssoAdmin', $this->lastUsedUserName);
-        } else {
+        } else if (!empty($this->ssoAdmin)) {
             $oTpl->assign('ssoAdmin', $this->ssoAdmin);
+            $oTpl->assign('disabledUsername', true);
         }
-
-        if ($this->wrongCredentials || $this->wrongParameters)
-        {
-            $oTpl->assign('displayError', true);
-
-            if ($this->ssoAdmin) {
-                $errorMessage = "Sorry, this platform is already connected to a different user.";
-            } elseif ($this->wrongParameters) {
-                $errorMessage = "You must enter both username and passwsord.";
-            } else {
-                $errorMessage = "Check credentials, wrong username or password.";
-            }
-
-            $oTpl->assign('errorMessage', $errorMessage);
-        }
+        $oTpl->assign('errorMessage', $this->getErrorMessage());
 
         $oTpl->display();
+    }
+    
+    /**
+     * Build error message from error code
+     *
+     * @return string
+     */
+    function getErrorMessage()
+    {
+        if (!empty($_GET['error'])) {
+            $this->errorCode = OA_CENTRAL_ERROR_ERROR_NOT_AUTHORIZED;
+        }
+        if (empty($this->errorCode)) {
+            return null;
+        }
+        switch($this->errorCode) {
+            case OA_CENTRAL_ERROR_USERNAME_DOES_NOT_MATCH_PLATFORM:
+                $msg = 'Sorry, this platform is already connected to a different user.';
+                break;
+            case OA_SSO_ERROR_WRONG_PARAMETERS:
+                $msg = 'You must enter both username and passwsord.';
+                break;
+            case OA_CENTRAL_ERROR_ERROR_NOT_AUTHORIZED:
+                $msg = 'Check credentials, wrong username or password.';
+                break;
+            default:
+                $msg = 'Error while authenticating user';
+        }
+        return $msg;
     }
 }
 
