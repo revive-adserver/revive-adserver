7419f9e655e038b34b1dce2619fbce5d98915353
monique.szpak@openads.org <monique.szpak@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-10-16 13:27:00 +0000

    upgrade integration test: enhanced another detectOpenads assert fail msg

--- lib/OA/Upgrade/tests/integration/Upgrade.up.test.php
+++ lib/OA/Upgrade/tests/integration/Upgrade.up.test.php
@@ -434,14 +434,14 @@ class Test_OA_Upgrade extends UnitTestCase
 
         $this->_createTestAppVarRecord('oa_version','2.3.31-beta');
         $this->assertTrue($oUpgrade->detectOpenads(),'');
-        $this->assertEqual($oUpgrade->versionInitialApplication,'2.3.31-beta','wrong initial application version');
+        $this->assertEqual($oUpgrade->versionInitialApplication,'2.3.31-beta','wrong initial application version '.$oUpgrade->versionInitialApplication);
         $this->assertEqual($oUpgrade->existing_installation_status, OA_STATUS_CAN_UPGRADE,'wrong upgrade status code, expected '.OA_STATUS_CAN_UPGRADE.' got '.$oUpgrade->existing_installation_status);
         $this->assertEqual($oUpgrade->aPackageList[0], 'openads_upgrade_2.3.32-beta-rc2.xml','wrong package file assigned');
         $this->_deleteTestAppVarRecordAllNames('oa_version');
 
         $this->_createTestAppVarRecord('oa_version','2.3.32-beta-rc5');
         $this->assertTrue($oUpgrade->detectOpenads(),'');
-        $this->assertEqual($oUpgrade->versionInitialApplication,'2.3.32-beta-rc5','wrong initial application version');
+        $this->assertEqual($oUpgrade->versionInitialApplication,'2.3.32-beta-rc5','wrong initial application version '.$oUpgrade->versionInitialApplication);
         $this->assertEqual($oUpgrade->existing_installation_status, OA_STATUS_CAN_UPGRADE,'wrong upgrade status code, expected '.OA_STATUS_CAN_UPGRADE.' got '.$oUpgrade->existing_installation_status);
         $this->assertEqual($oUpgrade->aPackageList[0], 'openads_upgrade_2.3.32-beta-rc10.xml','wrong package file assigned');
         $this->_deleteTestAppVarRecordAllNames('oa_version');
@@ -449,7 +449,7 @@ class Test_OA_Upgrade extends UnitTestCase
         // testing installation is up to date, no upgrade required
         $this->_createTestAppVarRecord('oa_version',OA_VERSION);
         $this->assertFalse($oUpgrade->detectOpenads(),'');
-        $this->assertEqual($oUpgrade->versionInitialApplication,OA_VERSION,'wrong initial application version');
+        $this->assertEqual($oUpgrade->versionInitialApplication,OA_VERSION,'wrong initial application version '.$oUpgrade->versionInitialApplication);
         $this->assertEqual($oUpgrade->existing_installation_status, OA_STATUS_CURRENT_VERSION,'wrong upgrade status code, expected '.OA_STATUS_CURRENT_VERSION.' got '.$oUpgrade->existing_installation_status);
         $this->assertEqual($oUpgrade->aPackageList[0], '', 'wrong package file assigned');
         $this->_deleteTestAppVarRecordAllNames('oa_version');
