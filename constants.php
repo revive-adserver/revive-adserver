5b63b82025f8184634d91a8d2820d76d81100f7d
andrzej.swedrzynski@openads.org <andrzej.swedrzynski@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-10-31 12:16:20 +0000

    Release script commit.

--- constants.php
+++ constants.php
@@ -38,7 +38,7 @@ $Id$
 function setupConstants()
 {
     // Define this version of Openads's constants
-    define('OA_VERSION', '2.5.16-beta-rc5');
+    define('OA_VERSION', '2.5.16-beta-rc6');
     define('MAX_PRODUCT_NAME',      'Openads');
     define('MAX_PRODUCT_URL',       'www.openads.org');
     define('MAX_PRODUCT_DOCSURL',   'docs.openads.org');
