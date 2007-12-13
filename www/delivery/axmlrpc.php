d0a50d7072a530f047973cab41154092413ebff8
post_commit <post_commit@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-12-13 17:23:09 +0000

    Commit of auto-generated delivery files.

--- www/delivery/axmlrpc.php
+++ www/delivery/axmlrpc.php
@@ -1567,7 +1567,7 @@ $msg .= '<div style="clear:both"></div>';
 }
 if (defined('TEST_ENVIRONMENT_RUNNING')) {
 // It's a test, stop execution
-echo $oError->getMessage()."\n".$oError->getDebugInfo();
+echo $message."\n===\n".$debugInfo."\n===\n".print_r($oError->getBacktrace(), true);
 exit(1);
 } else {
 // Send the error to the screen
