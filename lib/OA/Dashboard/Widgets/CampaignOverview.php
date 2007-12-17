27a604083b6eaaf56814dc2995e67f6050b17d37
bernard.lange@openads.org <bernard.lange@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2007-12-17 13:29:43 +0000

    added templates for OA-344

---
+++ lib/OA/Dashboard/Widgets/CampaignOverview.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
++---------------------------------------------------------------------------+
+| Openads v${RELEASE_MAJOR_MINOR}                                                              |
+| ============                                                              |
+|                                                                           |
+| Copyright (c) 2003-2007 Openads Limited                                   |
+| For contact details, see: http://www.openads.org/                         |
+|                                                                           |
+| This program is free software; you can redistribute it and/or modify      |
+| it under the terms of the GNU General Public License as published by      |
+| the Free Software Foundation; either version 2 of the License, or         |
+| (at your option) any later version.                                       |
+|                                                                           |
+| This program is distributed in the hope that it will be useful,           |
+| but WITHOUT ANY WARRANTY; without even the implied warranty of            |
+| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             |
+| GNU General Public License for more details.                              |
+|                                                                           |
+| You should have received a copy of the GNU General Public License         |
+| along with this program; if not, write to the Free Software               |
+| Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA |
++---------------------------------------------------------------------------+
+$Id: $
+*/
+
+require_once MAX_PATH . '/lib/OA/Dashboard/Widget.php';
+
+/**
+ * A dashboard widget to diplay an RSS feed of the Openads Blog
+ *
+ */
+class OA_Dashboard_Widget_CampaignOverview extends OA_Dashboard_Widget
+{
+    /**
+     * @var OA_Admin_Template
+     */
+    var $oTpl;
+
+    /**
+     * The class constructor
+     *
+     * @param array $aParams The parameters array, usually $_REQUEST
+     * @return OA_Dashboard_Widget_Campaign_Overview
+     */
+    function OA_Dashboard_Widget_CampaignOverview($aParams)
+    {
+        parent::OA_Dashboard_Widget($aParams);
+        $this->oTpl = new OA_Admin_Template('dashboard/campaignOverview.html');
+    }
+}
+
+?>
