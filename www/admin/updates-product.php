17fbed69972d92b220362e958806869c90854251
radek.maciaszek@openads.org <radek.maciaszek@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-01-04 12:49:37 +0000

    Fixed OA-463 - access denied for channel managementwhen working as admin

--- www/admin/updates-product.php
+++ www/admin/updates-product.php
@@ -44,8 +44,9 @@ OA_Permission::enforceAccount(OA_ACCOUNT_ADMIN);
 /*-------------------------------------------------------*/
 
 phpAds_PageHeader("5.5");
-phpAds_ShowSections(array("5.1", "5.2", "5.4", "5.5", "5.3", "5.6", "5.7"));
+phpAds_ShowSections(array("5.1", "5.2", "5.4", "5.5", "5.3"));
 phpAds_MaintenanceSelection("product", "updates");
+
 
 /*-------------------------------------------------------*/
 /* Main code                                             */
