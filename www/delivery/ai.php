7b97486e4276223501343dc4cd988c345d640115
post_commit <post_commit@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-01-10 13:02:04 +0000

    Commit of auto-generated delivery files.

--- www/delivery/ai.php
+++ www/delivery/ai.php
@@ -1593,9 +1593,9 @@ MAX_sendStatusCode(304);
 } else {
 // Filename not specified, show the admin user's default banner
 // (as the agency cannot be determined from a filename)
-$pref = OA_Admin_Preferences::loadPrefs(0);
-if ($pref['default_banner_url'] != "") {
-MAX_redirect($pref['default_banner_url']);
+$aPref = OA_Preferences::loadAdminAccountPreferences(true);
+if ($aPref['default_banner_url'] != "") {
+MAX_redirect($aPref['default_banner_url']);
 }
 }
 
