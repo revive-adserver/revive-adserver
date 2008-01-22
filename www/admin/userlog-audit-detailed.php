5da77c7169a3756acb1696530c27f3365cb75803
radek.maciaszek@openads.org <radek.maciaszek@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-01-22 11:17:23 +0000

    Fixed OA-810 - problem with tab layout

--- www/admin/userlog-audit-detailed.php
+++ www/admin/userlog-audit-detailed.php
@@ -47,8 +47,15 @@ OA_Permission::enforceAccount(OA_ACCOUNT_ADMIN, OA_ACCOUNT_MANAGER);
 /* HTML framework                                        */
 /*-------------------------------------------------------*/
 
-phpAds_PageHeader("5.2");
-phpAds_ShowSections(array("5.1", "5.3", "5.4", "5.2", "5.5", "5.6"));
+phpAds_PageHeader("5.3");
+if (OA_Permission::isAccount(OA_ACCOUNT_ADMIN)) {
+    // Show all "My Account" sections
+    phpAds_ShowSections(array("5.1", "5.2", "5.4", "5.5", "5.3"));
+    phpAds_UserlogSelection("index");
+} else {
+    // Show the "Preferences", "User Log" and "Channel Management" sections of the "My Account" sections
+    phpAds_ShowSections(array("5.1", "5.3", "5.7"));
+}
 
 //  initialize parameters
 $pageName = basename($_SERVER['PHP_SELF']);
