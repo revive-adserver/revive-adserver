dbedc7c366b453e6f0aa2f8d841b73d09d825c0d
matteo.beccati@openads.org <matteo.beccati@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-01-31 18:06:08 +0000

    OA-186 - Added Windows compatibility, a few security fixes + ignore cache properties

--- www/admin/assets/combine.php
+++ www/admin/assets/combine.php
@@ -25,53 +25,60 @@
 $Id$
 */
 
-// For performance reasons this file is standalone from the rest of the 
+// For performance reasons this file is standalone from the rest of the
 // product and does not include any configuration files or libraries!
 
+
+
+
 // Enable advanced caching control using ETag headers
-define ('_STRATEGY_ETAG_', true);			
+define ('_STRATEGY_ETAG_', true);
 
 // Enable advanced caching control using Modified headers
-define ('_STRATEGY_MODIFIED_', true);			
+define ('_STRATEGY_MODIFIED_', true);
 
 // Enable caching the combined files on the server
-define ('_STRATEGY_CACHE_', true);			
+define ('_STRATEGY_CACHE_', true);
 
 // Determine the type
 $type = $_GET['type'];
+if ($type != 'js' && $type != 'css') {
+	header ("HTTP/1.0 403 Forbidden");
+	exit;
+}
 
-// Build our paths 
+// Build our paths
 $base = realpath(dirname(dirname(__FILE__)));
 $cache = realpath(dirname(dirname($base)) . '/var/cache');
 
 // Check if files exist
 $elements = explode(',', $_GET['files']);
 $files = array();
+
 while (list(,$element) = each($elements)) {
 	$path = realpath($base . '/' . $element);
 
-	if (substr($path, 0, strlen($base) + 8) == $base . '/images/' ||
-		substr($path, 0, strlen($base) + 5) == $base . '/css/' ||
-		substr($path, 0, strlen($base) + 4) == $base . '/js/') {
+	$dirSep = preg_quote(DIRECTORY_SEPARATOR);
+	if (preg_match("/^{$dirSep}(images|css|js){$dirSep}/", substr($path, strlen($base)))) {
 		$files[] = $path;
 	} else {
-		header ("HTTP/1.0 404 Not Found");
+		header ("HTTP/1.0 403 Forbidden");
 		exit;
 	}
 }
 
 // Determine supported compression methods
-$encoding = strstr($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip') ? 'gzip' : 
+$encoding = strstr($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip') ? 'gzip' :
 			(strstr($_SERVER['HTTP_ACCEPT_ENCODING'], 'deflate') ? 'deflate' : 'none');
 
-if (!strstr($_SERVER['HTTP_USER_AGENT'], 'Opera') && 
+if (!strstr($_SERVER['HTTP_USER_AGENT'], 'Opera') &&
 	preg_match('/^Mozilla\/4\.0 \(compatible; MSIE ([0-9]\.[0-9])/i', $_SERVER['HTTP_USER_AGENT'], $matches)) {
 	$version = floatval($matches[1]);
-	
+
 	if ($version < 6)
 		$encoding = 'none';
-		
-	if ($version == 6 && !strstr($_SERVER['HTTP_USER_AGENT'], 'EV1')) 
+
+	if ($version == 6 && !strstr($_SERVER['HTTP_USER_AGENT'], 'EV1'))
 		$encoding = 'none';
 }
 
@@ -92,38 +99,35 @@ if (_STRATEGY_MODIFIED_) {
 		header ("HTTP/1.0 304 Not Modified");
 		header ('Content-Length: 0');
 		exit;
-	} 
+	}
 }
 
 if (_STRATEGY_ETAG_) {
 	header ("ETag: \"" . $hash . "\"");
-	
-	if (isset($_SERVER['HTTP_IF_NONE_MATCH']) && get_magic_quotes_gpc()) {
-		$_SERVER['HTTP_IF_NONE_MATCH'] = stripslashes($_SERVER['HTTP_IF_NONE_MATCH']);
-	}
 
 	if (isset($_SERVER['HTTP_IF_NONE_MATCH']) && ($_SERVER['HTTP_IF_NONE_MATCH'] == $hash || $_SERVER['HTTP_IF_NONE_MATCH'] == '"' . $hash . '"')) {
 		header ("HTTP/1.0 304 Not Modified");
 		header ('Content-Length: 0');
 		exit;
-	} 
+	}
 }
-	
-if (_STRATEGY_CACHE_ && file_exists($cache . '/' . $hash)) {
-	$contents = file_get_contents($cache . '/' . $hash);
+
+$cacheFile = $cache . '/combine_' . $hash;
+if (_STRATEGY_CACHE_ && file_exists($cacheFile)) {
+	$contents = file_get_contents($cacheFile);
 } else {
 	$contents = '';
-	reset ($files); 
+	reset ($files);
 	while (list(,$file) = each($files)) {
 		$contents .= "\n\n" . file_get_contents($file);
 	}
-	
+
 	if ($encoding != 'none') {
 		$contents = gzencode($contents, 9, $encoding == 'gzip' ? FORCE_GZIP : FORCE_DEFLATE);
-	} 
-	
+	}
+
 	if (_STRATEGY_CACHE_) {
-		if ($fp = fopen($cache . '/' . $hash, 'wb')) {
+		if ($fp = fopen($cacheFile, 'wb')) {
 			fwrite($fp, $contents);
 			fclose($fp);
 		}
