1cd6a471a3c1f9dd2b0a7c8b8bd41ae5e4502bfa
andrew.hill@openads.org <andrew.hill@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-02-05 14:56:21 +0000

    OA-1025, OA-1026.

--- scripts/updateDocs.sh
+++ scripts/updateDocs.sh
@@ -3,8 +3,8 @@
 # A shell script to generate the Openads documentation.
 # Requires phpDocumentor be installed.
 #
-# Copyright (c) 2003-2007 Openads Limited
-# For contact details, see: http://www.openads.org/
+# Copyright (c) 2003-2008 m3 Media Services Ltd
+# For contact details, see: http://www.openx.org/
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
