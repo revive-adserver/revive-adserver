01266bba4457322a3c7996a1c59c0893df30b355
andrew.hill@openads.org <andrew.hill@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-02-06 18:12:09 +0000

    OA-1025.

--- www/admin/js/boxrow.js
+++ www/admin/js/boxrow.js
@@ -8,9 +8,6 @@
 | Copyright (c) 2003-2008 m3 Media Services Ltd                             |
 | For contact details, see: http://www.openx.org/                           |
 |                                                                           |
-| Copyright (c) 2000-2003 the phpAdsNew developers                          |
-| For contact details, see: http://www.phpadsnew.com/                       |
-|                                                                           |
 | This program is free software; you can redistribute it and/or modify      |
 | it under the terms of the GNU General Public License as published by      |
 | the Free Software Foundation; either version 2 of the License, or         |
@@ -106,7 +103,7 @@ function boxrow_click(e)
     if (o.tagName == 'IMG') {
         return;
     }
-    
+
     // Find the DIV
     while (o.tagName != "DIV") {
         o = o.parentNode;
