aac14b11f5b26cc183e32a9f3bb6ee5ac2d996c9
chris.nutting@openads.org <chris.nutting@openads.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-02-07 11:10:09 +0000

    Refs OA-1011 Updated language file headers in 2.6

--- lib/max/language/czech/invocation.lang.php
+++ lib/max/language/czech/invocation.lang.php
@@ -8,9 +8,6 @@
 | Copyright (c) 2003-2008 m3 Media Services Ltd                             |
 | For contact details, see: http://www.openx.org/                           |
 |                                                                           |
-| Copyright (c) 2000-2003 the phpAdsNew developers                          |
-| For contact details, see: http://www.phpadsnew.com/                       |
-|                                                                           |
 | This program is free software; you can redistribute it and/or modify      |
 | it under the terms of the GNU General Public License as published by      |
 | the Free Software Foundation; either version 2 of the License, or         |
@@ -26,7 +23,7 @@
 | Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA |
 +---------------------------------------------------------------------------+
 $Id$
-*/ 
+*/
 
 // Invocation Types
 $GLOBALS['strInvocationRemote']			= "Vzd�len� vol�n�";
--- lib/max/language/hebrew/invocation.lang.php
+++ lib/max/language/hebrew/invocation.lang.php
@@ -8,9 +8,6 @@
 | Copyright (c) 2003-2008 m3 Media Services Ltd                             |
 | For contact details, see: http://www.openx.org/                           |
 |                                                                           |
-| Copyright (c) 2000-2003 the phpAdsNew developers                          |
-| For contact details, see: http://www.phpadsnew.com/                       |
-|                                                                           |
 | This program is free software; you can redistribute it and/or modify      |
 | it under the terms of the GNU General Public License as published by      |
 | the Free Software Foundation; either version 2 of the License, or         |
@@ -27,8 +24,6 @@
 +---------------------------------------------------------------------------+
 $Id$
 */
-
-
 
 // Invocation Types
 $GLOBALS['strInvocationRemote']			= "����� �����";
