0cf315a66f4e9538e7a4105cf84352c67aeb88eb
chris.nutting@openx.org <chris.nutting@openx.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-02-28 15:58:38 +0000

    Fixed OX-1424 in /trunk Updated FlashObject (now SwfObject) library

--- www/delivery_dev/fl.js
+++ www/delivery_dev/fl.js
@@ -1,7 +1,7 @@
 /**
- * FlashObject v1.3d: Flash detection and embed - http://blog.deconcept.com/flashobject/
+ * SWFObject v1.5: Flash Player detection and embed - http://blog.deconcept.com/swfobject/
  *
- * FlashObject is (c) 2006 Geoff Stearns and is released under the MIT License:
+ * SWFObject is (c) 2007 Geoff Stearns and is released under the MIT License:
  * http://www.opensource.org/licenses/mit-license.php
  *
  * Extended for use in Openads
@@ -12,185 +12,233 @@
  * and update the copy in /www/delivery/fl.js
  *
  */
-
-if(typeof net == "undefined") var net = new Object();
-if(typeof net.m3 == "undefined") net.m3 = new Object();
-if(typeof net.m3.util == "undefined") net.m3.util = new Object();
-if(typeof net.m3.FlashObjectUtil == "undefined") net.m3.FlashObjectUtil = new Object();
-net.m3.FlashObject = function(swf, id, w, h, ver, c, useExpressInstall, quality, xiRedirectUrl, redirectUrl, detectKey){
-  if (!document.createElement || !document.getElementById) return;
-  this.DETECT_KEY = detectKey ? detectKey : 'detectflash';
-  this.skipDetect = net.m3.util.getRequestParameter(this.DETECT_KEY);
-  this.params = new Object();
-  this.variables = new Object();
-  this.attributes = new Array();
-  this.useExpressInstall = useExpressInstall;
-
-  if(swf) this.setAttribute('swf', swf);
-  if(id) this.setAttribute('id', id);
-  if(w) this.setAttribute('width', w);
-  if(h) this.setAttribute('height', h);
-  if(ver) this.setAttribute('version', new net.m3.PlayerVersion(ver.toString().split(".")));
-  this.installedVer = net.m3.FlashObjectUtil.getPlayerVersion(this.getAttribute('version'), useExpressInstall);
-  if(c) this.addParam('bgcolor', c);
-  var q = quality ? quality : 'high';
-  this.addParam('quality', q);
-  var xir = (xiRedirectUrl) ? xiRedirectUrl : window.location;
-  this.setAttribute('xiRedirectUrl', xir);
-  this.setAttribute('redirectUrl', '');
-  if(redirectUrl) this.setAttribute('redirectUrl', redirectUrl);
+if(typeof org == "undefined") var org = new Object();
+if(typeof org.openx == "undefined") org.openx = new Object();
+if(typeof org.openx.util == "undefined") org.openx.util = new Object();
+if(typeof org.openx.SWFObjectUtil == "undefined") org.openx.SWFObjectUtil = new Object();
+org.openx.SWFObject = function(swf, id, w, h, ver, c, quality, xiRedirectUrl, redirectUrl, detectKey) {
+	if (!document.getElementById) { return; }
+	this.DETECT_KEY = detectKey ? detectKey : 'detectflash';
+	this.skipDetect = org.openx.util.getRequestParameter(this.DETECT_KEY);
+	this.params = new Object();
+	this.variables = new Object();
+	this.attributes = new Array();
+	if(swf) { this.setAttribute('swf', swf); }
+	if(id) { this.setAttribute('id', id); }
+	if(w) { this.setAttribute('width', w); }
+	if(h) { this.setAttribute('height', h); }
+	if(ver) { this.setAttribute('version', new org.openx.PlayerVersion(ver.toString().split("."))); }
+	this.installedVer = org.openx.SWFObjectUtil.getPlayerVersion();
+	if (!window.opera && document.all && this.installedVer.major > 7) {
+		// only add the onunload cleanup if the Flash Player version supports External Interface and we are in IE
+		org.openx.SWFObject.doPrepUnload = true;
+	}
+	if(c) { this.addParam('bgcolor', c); }
+	var q = quality ? quality : 'high';
+	this.addParam('quality', q);
+	this.setAttribute('useExpressInstall', false);
+	this.setAttribute('doExpressInstall', false);
+	var xir = (xiRedirectUrl) ? xiRedirectUrl : window.location;
+	this.setAttribute('xiRedirectUrl', xir);
+	this.setAttribute('redirectUrl', '');
+	if(redirectUrl) { this.setAttribute('redirectUrl', redirectUrl); }
 }
-net.m3.FlashObject.prototype = {
-  setAttribute: function(name, value){
-    this.attributes[name] = value;
-  },
-  getAttribute: function(name){
-    return this.attributes[name];
-  },
-  addParam: function(name, value){
-    this.params[name] = value;
-  },
-  getParams: function(){
-    return this.params;
-  },
-  addVariable: function(name, value){
-    this.variables[name] = value;
-  },
-  getVariable: function(name){
-    return this.variables[name];
-  },
-  getVariables: function(){
-    return this.variables;
-  },
-  createParamTag: function(n, v){
-    var p = document.createElement('param');
-    p.setAttribute('name', n);
-    p.setAttribute('value', v);
-    return p;
-  },
-  getVariablePairs: function(){
-    var variablePairs = new Array();
-    var key;
-    var variables = this.getVariables();
-    for(key in variables){
-      variablePairs.push(key +"="+ variables[key]);
-    }
-    return variablePairs;
-  },
-  getFlashHTML: function() {
-    var flashNode = "";
-    if (navigator.plugins && navigator.mimeTypes && navigator.mimeTypes.length) { // netscape plugin architecture
-      if (this.getAttribute("doExpressInstall")) this.addVariable("MMplayerType", "PlugIn");
-      flashNode = '<embed type="application/x-shockwave-flash" src="'+ this.getAttribute('swf') +'" width="'+ this.getAttribute('width') +'" height="'+ this.getAttribute('height') +'"';
-      flashNode += ' id="'+ this.getAttribute('id') +'" name="'+ this.getAttribute('id') +'" ';
-      var params = this.getParams();
-       for(var key in params){ flashNode += [key] +'="'+ params[key] +'" '; }
-      var pairs = this.getVariablePairs().join("&");
-       if (pairs.length > 0){ flashNode += 'flashvars="'+ pairs +'"'; }
-      flashNode += '/>';
-    } else { // PC IE
-      if (this.getAttribute("doExpressInstall")) this.addVariable("MMplayerType", "ActiveX");
-      flashNode = '<object id="'+ this.getAttribute('id') +'" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="'+ this.getAttribute('width') +'" height="'+ this.getAttribute('height') +'">';
-      flashNode += '<param name="movie" value="'+ this.getAttribute('swf') +'" />';
-      var params = this.getParams();
-       for(var key in params) {
-        flashNode += '<param name="'+ key +'" value="'+ params[key] +'" />';
-       }
-      var pairs = this.getVariablePairs().join("&");
-       if(pairs.length > 0) flashNode += '<param name="flashvars" value="'+ pairs +'" />';
-      flashNode += "</object>";
-    }
-    return flashNode;
-  },
-  write: function(elementId){
-    if(this.useExpressInstall) {
-      // check to see if we need to do an express install
-      var expressInstallReqVer = new net.m3.PlayerVersion([6,0,65]);
-      if (this.installedVer.versionIsValid(expressInstallReqVer) && !this.installedVer.versionIsValid(this.getAttribute('version'))) {
-        this.setAttribute('doExpressInstall', true);
-        this.addVariable("MMredirectURL", escape(this.getAttribute('xiRedirectUrl')));
-        document.title = document.title.slice(0, 47) + " - Flash Player Installation";
-        this.addVariable("MMdoctitle", document.title);
-      }
-    } else {
-      this.setAttribute('doExpressInstall', false);
-    }
-    if(this.skipDetect || this.getAttribute('doExpressInstall') || this.installedVer.versionIsValid(this.getAttribute('version'))){
-      var n = (typeof elementId == 'string') ? document.getElementById(elementId) : elementId;
-      n.innerHTML = this.getFlashHTML();
-    }else{
-      if(this.getAttribute('redirectUrl') != "") {
-        document.location.replace(this.getAttribute('redirectUrl'));
-      }
-    }
-  }
+org.openx.SWFObject.prototype = {
+	useExpressInstall: function(path) {
+		this.xiSWFPath = !path ? "expressinstall.swf" : path;
+		this.setAttribute('useExpressInstall', true);
+	},
+	setAttribute: function(name, value){
+		this.attributes[name] = value;
+	},
+	getAttribute: function(name){
+		return this.attributes[name];
+	},
+	addParam: function(name, value){
+		this.params[name] = value;
+	},
+	getParams: function(){
+		return this.params;
+	},
+	addVariable: function(name, value){
+		this.variables[name] = value;
+	},
+	getVariable: function(name){
+		return this.variables[name];
+	},
+	getVariables: function(){
+		return this.variables;
+	},
+	getVariablePairs: function(){
+		var variablePairs = new Array();
+		var key;
+		var variables = this.getVariables();
+		for(key in variables){
+			variablePairs[variablePairs.length] = key +"="+ variables[key];
+		}
+		return variablePairs;
+	},
+	getSWFHTML: function() {
+		var swfNode = "";
+		if (navigator.plugins && navigator.mimeTypes && navigator.mimeTypes.length) { // netscape plugin architecture
+			if (this.getAttribute("doExpressInstall")) {
+				this.addVariable("MMplayerType", "PlugIn");
+				this.setAttribute('swf', this.xiSWFPath);
+			}
+			swfNode = '<embed type="application/x-shockwave-flash" src="'+ this.getAttribute('swf') +'" width="'+ this.getAttribute('width') +'" height="'+ this.getAttribute('height') +'" style="'+ this.getAttribute('style') +'"';
+			swfNode += ' id="'+ this.getAttribute('id') +'" name="'+ this.getAttribute('id') +'" ';
+			var params = this.getParams();
+			 for(var key in params){ swfNode += [key] +'="'+ params[key] +'" '; }
+			var pairs = this.getVariablePairs().join("&");
+			 if (pairs.length > 0){ swfNode += 'flashvars="'+ pairs +'"'; }
+			swfNode += '/>';
+		} else { // PC IE
+			if (this.getAttribute("doExpressInstall")) {
+				this.addVariable("MMplayerType", "ActiveX");
+				this.setAttribute('swf', this.xiSWFPath);
+			}
+			swfNode = '<object id="'+ this.getAttribute('id') +'" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="'+ this.getAttribute('width') +'" height="'+ this.getAttribute('height') +'" style="'+ this.getAttribute('style') +'">';
+			swfNode += '<param name="movie" value="'+ this.getAttribute('swf') +'" />';
+			var params = this.getParams();
+			for(var key in params) {
+			 swfNode += '<param name="'+ key +'" value="'+ params[key] +'" />';
+			}
+			var pairs = this.getVariablePairs().join("&");
+			if(pairs.length > 0) {swfNode += '<param name="flashvars" value="'+ pairs +'" />';}
+			swfNode += "</object>";
+		}
+		return swfNode;
+	},
+	write: function(elementId){
+		if(this.getAttribute('useExpressInstall')) {
+			// check to see if we need to do an express install
+			var expressInstallReqVer = new org.openx.PlayerVersion([6,0,65]);
+			if (this.installedVer.versionIsValid(expressInstallReqVer) && !this.installedVer.versionIsValid(this.getAttribute('version'))) {
+				this.setAttribute('doExpressInstall', true);
+				this.addVariable("MMredirectURL", escape(this.getAttribute('xiRedirectUrl')));
+				document.title = document.title.slice(0, 47) + " - Flash Player Installation";
+				this.addVariable("MMdoctitle", document.title);
+			}
+		}
+		if(this.skipDetect || this.getAttribute('doExpressInstall') || this.installedVer.versionIsValid(this.getAttribute('version'))){
+			var n = (typeof elementId == 'string') ? document.getElementById(elementId) : elementId;
+			n.innerHTML = this.getSWFHTML();
+			return true;
+		}else{
+			if(this.getAttribute('redirectUrl') != "") {
+				document.location.replace(this.getAttribute('redirectUrl'));
+			}
+		}
+		return false;
+	}
 }
 
 /* ---- detection functions ---- */
-net.m3.FlashObjectUtil.getPlayerVersion = function(reqVer, xiInstall){
-  var PlayerVersion = new net.m3.PlayerVersion(0,0,0);
-  if(navigator.plugins && navigator.mimeTypes.length){
-    var x = navigator.plugins["Shockwave Flash"];
-    if(x && x.description) {
-      PlayerVersion = new net.m3.PlayerVersion(x.description.replace(/([a-z]|[A-Z]|\s)+/, "").replace(/(\s+r|\s+b[0-9]+)/, ".").split("."));
-    }
-  }else{
-    try{
-      var axo = new ActiveXObject("ShockwaveFlash.ShockwaveFlash");
-      for (var i=3; axo!=null; i++) {
-        axo = new ActiveXObject("ShockwaveFlash.ShockwaveFlash."+i);
-        PlayerVersion = new net.m3.PlayerVersion([i,0,0]);
-      }
-    }catch(e){}
-    if (reqVer && PlayerVersion.major > reqVer.major) return PlayerVersion; // version is ok, skip minor detection
-    // this only does the minor rev lookup if the user's major version 
-    // is not 6 or we are checking for a specific minor or revision number
-    // see http://blog.deconcept.com/2006/01/11/getvariable-setvariable-crash-internet-explorer-flash-6/
-    if (!reqVer || ((reqVer.minor != 0 || reqVer.rev != 0) && PlayerVersion.major == reqVer.major) || PlayerVersion.major != 6 || xiInstall) {
-      try{
-        PlayerVersion = new net.m3.PlayerVersion(axo.GetVariable("$version").split(" ")[1].split(","));
-      }catch(e){}
-    }
-  }
-  return PlayerVersion;
+org.openx.SWFObjectUtil.getPlayerVersion = function(){
+	var PlayerVersion = new org.openx.PlayerVersion([0,0,0]);
+	if(navigator.plugins && navigator.mimeTypes.length){
+		var x = navigator.plugins["Shockwave Flash"];
+		if(x && x.description) {
+			PlayerVersion = new org.openx.PlayerVersion(x.description.replace(/([a-zA-Z]|\s)+/, "").replace(/(\s+r|\s+b[0-9]+)/, ".").split("."));
+		}
+	}else if (navigator.userAgent && navigator.userAgent.indexOf("Windows CE") >= 0){ // if Windows CE
+		var axo = 1;
+		var counter = 3;
+		while(axo) {
+			try {
+				counter++;
+				axo = new ActiveXObject("ShockwaveFlash.ShockwaveFlash."+ counter);
+//				document.write("player v: "+ counter);
+				PlayerVersion = new org.openx.PlayerVersion([counter,0,0]);
+			} catch (e) {
+				axo = null;
+			}
+		}
+	} else { // Win IE (non mobile)
+		// do minor version lookup in IE, but avoid fp6 crashing issues
+		// see http://blog.deconcept.com/2006/01/11/getvariable-setvariable-crash-internet-explorer-flash-6/
+		try{
+			var axo = new ActiveXObject("ShockwaveFlash.ShockwaveFlash.7");
+		}catch(e){
+			try {
+				var axo = new ActiveXObject("ShockwaveFlash.ShockwaveFlash.6");
+				PlayerVersion = new org.openx.PlayerVersion([6,0,21]);
+				axo.AllowScriptAccess = "always"; // error if player version < 6.0.47 (thanks to Michael Williams @ Adobe for this code)
+			} catch(e) {
+				if (PlayerVersion.major == 6) {
+					return PlayerVersion;
+				}
+			}
+			try {
+				axo = new ActiveXObject("ShockwaveFlash.ShockwaveFlash");
+			} catch(e) {}
+		}
+		if (axo != null) {
+			PlayerVersion = new org.openx.PlayerVersion(axo.GetVariable("$version").split(" ")[1].split(","));
+		}
+	}
+	return PlayerVersion;
 }
-net.m3.PlayerVersion = function(arrVersion){
-  this.major = parseInt(arrVersion[0]) || 0;
-  this.minor = parseInt(arrVersion[1]) || 0;
-  this.rev = parseInt(arrVersion[2]) || 0;
+org.openx.PlayerVersion = function(arrVersion){
+	this.major = arrVersion[0] != null ? parseInt(arrVersion[0]) : 0;
+	this.minor = arrVersion[1] != null ? parseInt(arrVersion[1]) : 0;
+	this.rev = arrVersion[2] != null ? parseInt(arrVersion[2]) : 0;
 }
-net.m3.PlayerVersion.prototype.versionIsValid = function(fv){
-  if(this.major < fv.major) return false;
-  if(this.major > fv.major) return true;
-  if(this.minor < fv.minor) return false;
-  if(this.minor > fv.minor) return true;
-  if(this.rev < fv.rev) return false;
-  return true;
+org.openx.PlayerVersion.prototype.versionIsValid = function(fv){
+	if(this.major < fv.major) return false;
+	if(this.major > fv.major) return true;
+	if(this.minor < fv.minor) return false;
+	if(this.minor > fv.minor) return true;
+	if(this.rev < fv.rev) return false;
+	return true;
 }
 /* ---- get value of query string param ---- */
-net.m3.util = {
-  getRequestParameter: function(param){
-    var q = document.location.search || document.location.href.hash;
-    if(q){
-      var startIndex = q.indexOf(param +"=");
-      var endIndex = (q.indexOf("&", startIndex) > -1) ? q.indexOf("&", startIndex) : q.length;
-      if (q.length > 1 && startIndex > -1) {
-        return q.substring(q.indexOf("=", startIndex)+1, endIndex);
-      }
-    }
-    return "";
-  },
-  removeChildren: function(n){
-    while (n.hasChildNodes()) n.removeChild(n.firstChild);
-  }
+org.openx.util = {
+	getRequestParameter: function(param) {
+		var q = document.location.search || document.location.hash;
+		if (param == null) { return q; }
+		if(q) {
+			var pairs = q.substring(1).split("&");
+			for (var i=0; i < pairs.length; i++) {
+				if (pairs[i].substring(0, pairs[i].indexOf("=")) == param) {
+					return pairs[i].substring((pairs[i].indexOf("=")+1));
+				}
+			}
+		}
+		return "";
+	}
 }
-
-/* add Array.push if needed (ie5) */
-if (Array.prototype.push == null) { Array.prototype.push = function(item) { this[this.length] = item; return this.length; }}
+/* fix for video streaming bug */
+org.openx.SWFObjectUtil.cleanupSWFs = function() {
+	var objects = document.getElementsByTagName("OBJECT");
+	for (var i = objects.length - 1; i >= 0; i--) {
+		objects[i].style.display = 'none';
+		for (var x in objects[i]) {
+			if (typeof objects[i][x] == 'function') {
+				objects[i][x] = function(){};
+			}
+		}
+	}
+}
+// fixes bug in some fp9 versions see http://blog.deconcept.com/2006/07/28/swfobject-143-released/
+if (org.openx.SWFObject.doPrepUnload) {
+	if (!org.openx.unloadSet) {
+		org.openx.SWFObjectUtil.prepUnload = function() {
+			__flash_unloadHandler = function(){};
+			__flash_savedUnloadHandler = function(){};
+			window.attachEvent("onunload", org.openx.SWFObjectUtil.cleanupSWFs);
+		}
+		window.attachEvent("onbeforeunload", org.openx.SWFObjectUtil.prepUnload);
+		org.openx.unloadSet = true;
+	}
+}
+/* add document.getElementById if needed (mobile IE < 5) */
+if (!document.getElementById && document.all) { document.getElementById = function(id) { return document.all[id]; }}
 
 /* add some aliases for ease of use/backwards compatibility */
-var getQueryParamValue = net.m3.util.getRequestParameter;
-var FlashObject = net.m3.FlashObject;
+var getQueryParamValue = org.openx.util.getRequestParameter;
+var FlashObject = org.openx.SWFObject; // for legacy support
+var SWFObject = org.openx.SWFObject;
 
 /* Set a document variable to track if this code has been included on the current page */
 document.mmm_fo = 1;
