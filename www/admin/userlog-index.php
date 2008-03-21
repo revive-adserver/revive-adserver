d967be238aab575826f8ccc5e7c9549e1891c233
bernard.lange@openx.org <bernard.lange@openx.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-03-21 10:20:19 +0000

    Fix for OX-1805 (part of OX-1679)

--- www/admin/userlog-index.php
+++ www/admin/userlog-index.php
@@ -65,8 +65,8 @@ $advertiserId   = MAX_getValue('advertiserId',    0);
 $campaignId     = MAX_getValue('campaignId',      0);
 $publisherId    = MAX_getValue('publisherId',     0);
 $zoneId         = MAX_getValue('zoneId',          0);
-$startDate      = MAX_getStoredValue('period_start');
-$endDate        = MAX_getStoredValue('period_end');
+$startDate      = MAX_getStoredValue('period_start', null);
+$endDate        = MAX_getStoredValue('period_end', null);
 $periodPreset   = MAX_getValue('period_preset', 'all_events');
 
 //  paging related input variables
