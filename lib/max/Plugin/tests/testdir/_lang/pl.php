4870377ebc3a04accebee8190d5797c2fc1cd3ed
chris.nutting@openx.org <chris.nutting@openx.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-03-25 17:20:26 +0000

    Refs OX-1399 - In /trunk : Updated the Plugin translation tests to use the new mechanism, also added tests for fallbacks

--- lib/max/Plugin/tests/testdir/_lang/pl.php
+++ lib/max/Plugin/tests/testdir/_lang/pl.php
@@ -25,6 +25,11 @@
 $Id$
 */
 
+/**
+ * This file lets us test that the translate cascade mechanism is working
+ * So if a translation is requested for a string that doesn't exist in this language, the english is returned
+ * if no english, the key is returned
+ */
     $words = array(
         'translate me' => 'translated text',
     );
