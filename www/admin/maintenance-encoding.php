e7f6d5ded6db205d09abd59ebf55638d963dbd93
chris.nutting@openx.org <chris.nutting@openx.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-04-01 11:01:25 +0000

    Fixed OX-1872 in /trunk (Improved maintenance-encoding tool)

--- www/admin/maintenance-encoding.php
+++ www/admin/maintenance-encoding.php
@@ -48,43 +48,79 @@ phpAds_MaintenanceSelection("encoding");
 /*-------------------------------------------------------*/
 
 // Note: Taken from EncodingMigration.php (with the idfields and joinon arrays removed and updated for use with DataObjects)
+/**
+ * An array to hold all the tables and fields that should be converted
+ *
+ * @var array of arrays(
+ *  'fields' => array: field names that should be converted,
+ *  'idfields' => array: fields identifying the primary (multi?) key used for the WHERE clause on update
+ *  'joinon' string: Identifies which field in the table should be used to build the join up (to get the agency ID where applicable)
+ */
 $aTableFields = array(
-    'acls' => array('fields' => array('data')),
-    'acls_channel' => array('fields'    => array('data')),
-    'affiliates' =>  array('fields' =>  array('name', 'mnemonic', 'comments', 'contact', 'email', 'website')),
-    'agency' => array('fields' => array('name', 'contact', 'email', 'username', 'logout_url')),
-    'application_variable' => array('fields' => array('name', 'value')),
-    'banners' => array('fields' => array('htmltemplate','htmlcache','target','url','alt','bannertext','description','append','comments','keyword','statustext')),
-    'campaigns' => array('fields' => array('campaignname','comments')),
-    'channel' => array('fields' => array('name','description','comments')),
-    'clients' => array('fields' => array('clientname','contact','email','comments')),
-    'tracker_append' => array('fields' => array('tagcode')),
-    'trackers' => array('fields' => array('trackername','description','appendcode')),
-    'userlog' => array('fields' => array('details')),
-    'variables' => array('fields' => array('name','description','variablecode')),
-    'zones' => array('fields' => array('zonename','description','prepend','append','comments','what')),
-    'users' => array('fields' => array('contact_name', 'email_address', 'username', 'comments')),
+    'acls' => array(
+        'fields' => array('data'),
+        'idfields' => array('bannerid', 'executionorder'),
+    ),
+    'acls_channel' => array(
+        'fields'    => array('data'),
+        'idfields'  => array('channelid', 'executionorder'),
+    ),
+    'affiliates' =>  array(
+        'fields' =>  array('name', 'mnemonic', 'comments', 'contact', 'email', 'website'),
+        'idfields'  => array('affiliateid'),
+    ),
+    'agency' => array(
+        'fields' => array('name', 'contact', 'email'),
+        'idfields'  => array('agencyid'),
+    ),
+    'application_variable' => array(
+        'fields' => array('name', 'value'),
+        'idfields'  => array('name'),
+    ),
+    'banners' => array(
+        'fields' => array('htmltemplate','htmlcache','target','url','alt','bannertext','description','append','comments','keyword','statustext'),
+        'idfields'  => array('bannerid'),
+    ),
+    'campaigns' => array(
+        'fields' => array('campaignname','comments'),
+        'idfields'  => array('campaignid'),
+    ),
+    'channel' => array(
+        'fields' => array('name','description','comments'),
+        'idfields'  => array('channelid'),
+    ),
+    'clients' => array(
+        'fields' => array('clientname','contact','email','comments'),
+        'idfields'  => array('clientid'),
+    ),
+    'tracker_append' => array(
+        'fields' => array('tagcode'),
+        'idfields'  => array('tracker_append_id'),
+    ),
+    'trackers' => array(
+        'fields' => array('trackername','description','appendcode'),
+        'idfields'  => array('trackerid'),
+    ),
+    'userlog' => array(
+        'fields' => array('details'),
+        'idfields'  => array('userlogid'),
+    ),
+    'users' => array(
+        'fields' => array('contact_name', 'email_address', 'comments'),
+        'idfields'  => array('user_id'),
+    ),
+
+    'variables' => array(
+        'fields' => array('name','description','variablecode'),
+        'idfields'  => array('variableid'),
+    ),
+    'zones' => array(
+        'fields' => array('zonename','description','prepend','append','comments','what'),
+        'idfields'  => array('zoneid'),
+    ),
 );
 
-if (!empty($_POST['encTest'])) {
-    echo "<form action='maintenance-encoding.php' method='POST'>";
-    echo "<input type='hidden' name='encFrom' value='{$_POST['encFrom']}' />\n";
-    echo "{$GLOBALS['strConvertThese']}<br /><br /><table border='1' padding='1'><tr><th>{$GLOBALS['strFrom']}</th><th>" . ucfirst($GLOBALS['strTo']) . "</th></tr>\n";
-    $aChanged = _iterateTableFields($aTableFields, false);
-
-    foreach ($aChanged as $changed) {
-        echo "<tr>
-                    <td>" . (htmlspecialchars($changed['from'])) . "</td>
-                    <td>" . (htmlspecialchars($changed['to'])) . "</td>
-                </tr>\n";
-    }
-    echo "</table><br />";
-    echo "<input type='submit' name='encConfirm' value='{$GLOBALS['strConvert']}' /> <input type='button' name='encCancel' value='{$GLOBALS['strCancel']}' onclick='javascript:document.location = \"" . $_SERVER['PHP_SELF'] . "\";' />";
-    phpAds_ShowBreak();
-    phpAds_PageFooter();
-
-    exit;
-} elseif (!empty($_POST['encConfirm'])) {
+if (!empty($_POST['encConfirm'])) {
     _iterateTableFields($aTableFields, true);
     Header("Location: maintenance-maintenance.php");
 }
@@ -92,12 +128,15 @@ if (!empty($_POST['encTest'])) {
 /**
  * This function iterates over the table fields, and either gathers data which would be changed, or changes the data
  *
- * @param unknown_type $aTableFields
- * @return unknown
+ * @param array $aTableFields
+ * @return array
  */
 function _iterateTableFields($aTableFields, $execute = false)
 {
     $aChanged = array();
+    $encTo =   (!empty($_POST['encTo']))   ? $_POST['encTo']   : 'UTF-8';
+    $encFrom = (!empty($_POST['encFrom'])) ? $_POST['encFrom'] : 'UTF-8';
+
     foreach ($aTableFields as $table => $tableData)
     {
         $doTable = OA_Dal::factoryDO($table);
@@ -105,9 +144,17 @@ function _iterateTableFields($aTableFields, $execute = false)
         while ($doTable->fetch()) {
             $changed = false;
             foreach ($tableData['fields'] as $field) {
-                $converted = MAX_commonConvertEncoding($doTable->$field, 'UTF-8', $_POST['encFrom']);
+                $converted = MAX_commonConvertEncoding($doTable->$field, $encTo, $encFrom);
                 if ($converted === $doTable->$field) { continue; }
-                $aChanged[] = array('from' => $doTable->$field, 'to' => $converted);
+                $id = array();
+                foreach ($tableData['idfields'] as $idField) {
+                    $id[$idField] = $doTable->$idField;
+                }
+                if ($execute && !in_array(implode('.', $id), $_POST['aExecute'][$table][$field])) {
+                    continue;
+                }
+
+                $aChanged[$table][$field][] = array('from' => $doTable->$field, 'to' => $converted, 'id' => $id);
                 $doTable->$field = $converted;
                 $changed = true;
             }
@@ -126,16 +173,52 @@ echo "<br /><br />";
 
 phpAds_ShowBreak();
 echo "<form action='maintenance-encoding.php' method='POST'>";
-echo $strEncodingConvertFrom . " <select name='encFrom'>\n";
 $oOptions = new Plugins_InvocationTagsOptions();
 $aEncodings = $oOptions->_getAvailableCharsets();
+
+$selectedFrom = (!empty($_POST['encFrom'])) ? $_POST['encFrom'] : 'UTF-8';
+$selectedTo   = (!empty($_POST['encTo']))   ? $_POST['encTo']   : 'UTF-8';
+
+echo $strEncodingConvertFrom . " <select name='encFrom'>\n";
 foreach ($aEncodings as $encCode => $encName) {
-    echo "    <option value='{$encCode}'>{$encName}</option>\n";
+    $selected = ($encCode == $selectedFrom) ? " selected='selected'" : '';
+    echo "    <option value='{$encCode}' {$selected}>{$encName}</option>\n";
 }
 echo "</select> ";
 
+//echo $strEncodingConvertTo . " <select name='encTo'>\n";
+//foreach ($aEncodings as $encCode => $encName) {
+//    $selected = ($encCode == $selectedTo) ? " selected='selected'" : '';
+//    echo "    <option value='{$encCode}' {$selected}>{$encName}</option>\n";
+//}
+//echo "</select> ";
+
 echo "<input type='submit' name='encTest' value='{$strEncodingConvertTest}' />";
-echo "</form>";
+
+// Show the results of the encoding
+if (!empty($_POST['encTest'])) {
+    phpAds_ShowBreak();
+
+    $aChanged = _iterateTableFields($aTableFields, false);
+
+    echo "{$GLOBALS['strConvertThese']}<br /><br />
+        <table border='1' padding='1'>
+            <tr><th>{$GLOBALS['strInclude']}</th><th>{$GLOBALS['strFrom']}</th><th>" . ucfirst($GLOBALS['strTo']) . "</th></tr>\n";
+
+    foreach ($aChanged as $table => $aFields) {
+        foreach ($aFields as $field => $aItems) {
+            foreach ($aItems as $idx => $aItem) {
+                echo "<tr>
+                            <td><input type='checkbox' name='aExecute[{$table}][{$field}][]' value='" . implode('.', $aItem['id']) . "' checked='checked' /></td>
+                            <td>" . (htmlspecialchars($aItem['from'])) . "</td>
+                            <td>" . (htmlspecialchars($aItem['to'])) . "</td>
+                        </tr>\n";
+            }
+        }
+    }
+    echo "</table><br />";
+    echo "<input type='submit' name='encConfirm' value='{$GLOBALS['strConvert']}' /> <input type='button' name='encCancel' value='{$GLOBALS['strCancel']}' onclick='javascript:document.location = \"" . $_SERVER['PHP_SELF'] . "\";' />";
+}
 
 phpAds_ShowBreak();
 
