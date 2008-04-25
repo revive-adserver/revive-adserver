340ecd3e5b29ece6458be412e9487fc70b5377c3
andrew.hill@openx.org <andrew.hill@openx.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-04-25 16:16:50 +0000

    Merged in fix for OX-1562.
    "svn merge -r19478:19479 https://svn.openx.org/openx/branches/2.6/trunk ."

--- plugins/geotargeting/ModGeoIP/ModGeoIP.delivery.php
+++ plugins/geotargeting/ModGeoIP/ModGeoIP.delivery.php
@@ -27,7 +27,7 @@ $Id$
 
 /**
  * The method to look up the GeoTargeting information already obtained
- * by the mod_geoip Apache module.
+ * by the mod_geoip Apache / lighttpd module.
  *
  * @return array An array that will contain the results of the
  *               GeoTargeting lookup.
@@ -35,42 +35,56 @@ $Id$
 function OA_Geo_ModGeoIP_getInfo() {
     $result = array();
     if (isset($_SERVER['GEOIP_COUNTRY_CODE'])) {
-	  $result['country_code'] = $_SERVER['GEOIP_COUNTRY_CODE'];
-	}
-	if (isset($_SERVER['GEOIP_COUNTRY_NAME'])) {
-	  $result['country_name'] = $_SERVER['GEOIP_COUNTRY_NAME'];
-	}
-	if (isset($_SERVER['GEOIP_REGION'])) {
-	  $result['region'] = $_SERVER['GEOIP_REGION'];
-	}
-	if (isset($_SERVER['GEOIP_CITY'])) {
-	  $result['city'] = $_SERVER['GEOIP_CITY'];
-	}
-	if (isset($_SERVER['GEOIP_POSTAL_CODE'])) {
-	  $result['postal_code'] = $_SERVER['GEOIP_POSTAL_CODE'];
-	}
-	if (isset($_SERVER['GEOIP_LATITUDE'])) {
-	  $result['latitude'] = $_SERVER['GEOIP_LATITUDE'];
-	}
-	if (isset($_SERVER['GEOIP_LONGITUDE'])) {
-	  $result['longitude'] = $_SERVER['GEOIP_LONGITUDE'];
-	}
-	if (isset($_SERVER['GEOIP_DMA_CODE'])) {
-	  $result['dma_code'] = $_SERVER['GEOIP_DMA_CODE'];
-	}
-	if (isset($_SERVER['GEOIP_AREA_CODE'])) {
-	  $result['area_code'] = $_SERVER['GEOIP_AREA_CODE'];
-	}
-	if (isset($_SERVER['GEOIP_ORGANIZATION'])) {
-	  $result['organisation'] = $_SERVER['GEOIP_ORGANIZATION'];
-	}
-	if (isset($_SERVER['GEOIP_ISP'])) {
-	  $result['isp'] = $_SERVER['GEOIP_ISP'];
-	}
-	if (isset($_SERVER['GEOIP_NETSPEED'])) {
-	  $result['netspeed'] = $_SERVER['GEOIP_NETSPEED'];
-	}
-	return $result;
+          $result['country_code'] = $_SERVER['GEOIP_COUNTRY_CODE'];
+    }
+    if (isset($_SERVER['GEOIP_COUNTRY_NAME'])) {
+        $result['country_name'] = $_SERVER['GEOIP_COUNTRY_NAME'];
+    }
+    if (isset($_SERVER['GEOIP_REGION'])) {
+        $result['region'] = $_SERVER['GEOIP_REGION'];
+    } else if (isset($_SERVER['GEOIP_CITY_REGION'])) {
+        $result['region'] = $_SERVER['GEOIP_CITY_REGION'];
+    }
+    if (isset($_SERVER['GEOIP_CITY'])) {
+        $result['city'] = $_SERVER['GEOIP_CITY'];
+    } else if (isset($_SERVER['GEOIP_CITY_NAME'])) {
+        $result['city'] = $_SERVER['GEOIP_CITY_NAME'];
+    }
+    if (isset($_SERVER['GEOIP_POSTAL_CODE'])) {
+        $result['postal_code'] = $_SERVER['GEOIP_POSTAL_CODE'];
+    } else if (isset($_SERVER['GEOIP_CITY_POSTAL_CODE'])) {
+        $result['postal_code'] = $_SERVER['GEOIP_CITY_POSTAL_CODE'];
+    }
+    if (isset($_SERVER['GEOIP_LATITUDE'])) {
+        $result['latitude'] = $_SERVER['GEOIP_LATITUDE'];
+    } else if (isset($_SERVER['GEOIP_CITY_LATITUDE'])) {
+        $result['latitude'] = $_SERVER['GEOIP_CITY_LATITUDE'];
+    }
+    if (isset($_SERVER['GEOIP_LONGITUDE'])) {
+        $result['longitude'] = $_SERVER['GEOIP_LONGITUDE'];
+    } else if (isset($_SERVER['GEOIP_CITY_LONG_LATITUDE'])) {
+        $result['longitude'] = $_SERVER['GEOIP_CITY_LONG_LATITUDE'];
+    }
+    if (isset($_SERVER['GEOIP_DMA_CODE'])) {
+        $result['dma_code'] = $_SERVER['GEOIP_DMA_CODE'];
+    } else if (isset($_SERVER['GEOIP_CITY_DMA_CODE'])) {
+        $result['dma_code'] = $_SERVER['GEOIP_CITY_DMA_CODE'];
+    }
+    if (isset($_SERVER['GEOIP_AREA_CODE'])) {
+        $result['area_code'] = $_SERVER['GEOIP_AREA_CODE'];
+    } else if (isset($_SERVER['GEOIP_CITY_AREA_CODE'])) {
+        $result['area_code'] = $_SERVER['GEOIP_CITY_AREA_CODE'];
+    }
+    if (isset($_SERVER['GEOIP_ORGANIZATION'])) {
+        $result['organisation'] = $_SERVER['GEOIP_ORGANIZATION'];
+    }
+    if (isset($_SERVER['GEOIP_ISP'])) {
+        $result['isp'] = $_SERVER['GEOIP_ISP'];
+    }
+    if (isset($_SERVER['GEOIP_NETSPEED'])) {
+        $result['netspeed'] = $_SERVER['GEOIP_NETSPEED'];
+    }
+    return $result;
 }
 
 ?>
--- plugins/geotargeting/tests/unit/ModGeoIP.plg.test.php
+++ plugins/geotargeting/tests/unit/ModGeoIP.plg.test.php
@@ -36,15 +36,15 @@ require_once MAX_PATH . '/lib/max/Plugin.php';
  */
 class Delivery_TestOfPlugins_Geotargeting_ModGeoIP_ModGeoIP extends UnitTestCase
 {
-    
+
     /**
      * The constructor method.
      */
     function Delivery_TestOfPlugins_Geotargeting_ModGeoIP_ModGeoIPP()
-    {        
+    {
         $this->UnitTestCase();
     }
-    
+
     /**
      * Test the getModuleInfo method.
      */
@@ -55,12 +55,13 @@ class Delivery_TestOfPlugins_Geotargeting_ModGeoIP_ModGeoIP extends UnitTestCase
             'MaxMind mod_apache GeoIP'
         );
     }
-    
+
     /**
      * Test the getInfo method.
      */
     function testGetInfo()
     {
+        // Test the old style database codes
         $_SERVER['GEOIP_COUNTRY_CODE']  = 'Test Country';
         $_SERVER['GEOIP_COUNTRY_NAME']  = 'Test Name';
         $_SERVER['GEOIP_REGION']        = 'Test Region';
@@ -72,20 +73,73 @@ class Delivery_TestOfPlugins_Geotargeting_ModGeoIP_ModGeoIP extends UnitTestCase
         $_SERVER['GEOIP_AREA_CODE']     = 'Test Area';
         $_SERVER['GEOIP_ORGANIZATION']  = 'Test Org';
         $_SERVER['GEOIP_NETSPEED']      = 'Test Speed';
+
         $result = MAX_Plugin::callStaticMethod('geotargeting', 'ModGeoIP', 'ModGeoIP', 'getInfo');
+
         $this->assertEqual($result['country_code'], 'Test Country');
         $this->assertEqual($result['country_name'], 'Test Name');
-        $this->assertEqual($result['region'], 'Test Region');
-        $this->assertEqual($result['city'], 'Test City');
-        $this->assertEqual($result['postal_code'], 'Test PC');
-        $this->assertEqual($result['latitude'], 'Test Lat');
-        $this->assertEqual($result['longitude'], 'Test Long');
-        $this->assertEqual($result['dma_code'], 'Test Code');
-        $this->assertEqual($result['area_code'], 'Test Area');
+        $this->assertEqual($result['region'],       'Test Region');
+        $this->assertEqual($result['city'],         'Test City');
+        $this->assertEqual($result['postal_code'],  'Test PC');
+        $this->assertEqual($result['latitude'],     'Test Lat');
+        $this->assertEqual($result['longitude'],    'Test Long');
+        $this->assertEqual($result['dma_code'],     'Test Code');
+        $this->assertEqual($result['area_code'],    'Test Area');
         $this->assertEqual($result['organisation'], 'Test Org');
-        $this->assertEqual($result['netspeed'], 'Test Speed');
+        $this->assertEqual($result['netspeed'],     'Test Speed');
+
+        unset($_SERVER['GEOIP_COUNTRY_CODE']);
+        unset($_SERVER['GEOIP_COUNTRY_NAME']);
+        unset($_SERVER['GEOIP_REGION']);
+        unset($_SERVER['GEOIP_CITY']);
+        unset($_SERVER['GEOIP_POSTAL_CODE']);
+        unset($_SERVER['GEOIP_LATITUDE']);
+        unset($_SERVER['GEOIP_LONGITUDE']);
+        unset($_SERVER['GEOIP_DMA_CODE']);
+        unset($_SERVER['GEOIP_AREA_CODE']);
+        unset($_SERVER['GEOIP_ORGANIZATION']);
+        unset($_SERVER['GEOIP_NETSPEED']);
+
+        // Test the new style database codes
+        $_SERVER['GEOIP_COUNTRY_CODE']       = 'Test Country';
+        $_SERVER['GEOIP_COUNTRY_NAME']       = 'Test Name';
+        $_SERVER['GEOIP_CITY_REGION']        = 'Test Region';
+        $_SERVER['GEOIP_CITY_NAME']          = 'Test City';
+        $_SERVER['GEOIP_CITY_POSTAL_CODE']   = 'Test PC';
+        $_SERVER['GEOIP_CITY_LATITUDE']      = 'Test Lat';
+        $_SERVER['GEOIP_CITY_LONG_LATITUDE'] = 'Test Long';
+        $_SERVER['GEOIP_CITY_DMA_CODE']      = 'Test Code';
+        $_SERVER['GEOIP_CITY_AREA_CODE']     = 'Test Area';
+        $_SERVER['GEOIP_ORGANIZATION']       = 'Test Org';
+        $_SERVER['GEOIP_NETSPEED']           = 'Test Speed';
+
+        $result = MAX_Plugin::callStaticMethod('geotargeting', 'ModGeoIP', 'ModGeoIP', 'getInfo');
+
+        $this->assertEqual($result['country_code'], 'Test Country');
+        $this->assertEqual($result['country_name'], 'Test Name');
+        $this->assertEqual($result['region'],       'Test Region');
+        $this->assertEqual($result['city'],         'Test City');
+        $this->assertEqual($result['postal_code'],  'Test PC');
+        $this->assertEqual($result['latitude'],     'Test Lat');
+        $this->assertEqual($result['longitude'],    'Test Long');
+        $this->assertEqual($result['dma_code'],     'Test Code');
+        $this->assertEqual($result['area_code'],    'Test Area');
+        $this->assertEqual($result['organisation'], 'Test Org');
+        $this->assertEqual($result['netspeed'],     'Test Speed');
+
+        unset($_SERVER['GEOIP_COUNTRY_CODE']);
+        unset($_SERVER['GEOIP_COUNTRY_NAME']);
+        unset($_SERVER['GEOIP_CITY_REGION']);
+        unset($_SERVER['GEOIP_CITY_NAME']);
+        unset($_SERVER['GEOIP_CITY_POSTAL_CODE']);
+        unset($_SERVER['GEOIP_CITY_LATITUDE']);
+        unset($_SERVER['GEOIP_CITY_LONG_LATITUDE']);
+        unset($_SERVER['GEOIP_CITY_DMA_CODE']);
+        unset($_SERVER['GEOIP_CITY_AREA_CODE']);
+        unset($_SERVER['GEOIP_ORGANIZATION']);
+        unset($_SERVER['GEOIP_NETSPEED']);
     }
-    
+
 }
 
 ?>
