0a3cf187644a67721b8bc74734313356bae8865c
andrzej.swedrzynski@openx.org <andrzej.swedrzynski@openx.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-05-14 15:49:54 +0000

    OX-1803. svn merge -r 19914:20120 https://svn.openx.org/openx/branches/2.6/branches/developers/wooky/OX-1803_script_regenerateAdServerStatistics.php_does_not_clear_data_intermediate_ad

--- scripts/maintenance/tools/regenerateAdServerStatistics.php
+++ scripts/maintenance/tools/regenerateAdServerStatistics.php
@@ -59,9 +59,8 @@ require_once $path . '/../../../init.php';
 
 // Required files
 
-require_once MAX_PATH . '/lib/OA/DB.php';
+require_once MAX_PATH . '/lib/OA/Maintenance/Regenerate.php';
 require_once MAX_PATH . '/lib/OA/Maintenance/Statistics.php';
-require_once MAX_PATH . '/lib/OA/OperationInterval.php';
 require_once MAX_PATH . '/lib/OA/ServiceLocator.php';
 require_once MAX_PATH . '/lib/pear/Date.php';
 
@@ -74,59 +73,8 @@ $oNowDate->addSeconds(5);
 $oServiceLocator =& OA_ServiceLocator::instance();
 $oServiceLocator->register('now', $oNowDate);
 
-// Check start/end dates - note that check is the reverse of normal check:
-// if the operation interval is <= 60, must be start/end of an hour, to
-// make sure we update all the operation intervals in the hour, and if
-// the operation interval > 60, must be the start/end of an operation
-// interval, to make sure we update all the hours in the operation interval.
-$datesOkay = true;
-$operationInterval = $conf['maintenance']['operation_interval'];
-if ($operationInterval <= 60) {
-    // Must ensure that only one hour is being summarised
-    if (!OA_OperationInterval::checkDatesInSameHour($oStartDate, $oEndDate)) {
-        $datesOkay = false;
-    }
-    // Now check that the start and end dates are match the start and
-    // end of the hour
-    $oHourStart = new Date();
-    $oHourStart->setYear($oStartDate->getYear());
-    $oHourStart->setMonth($oStartDate->getMonth());
-    $oHourStart->setDay($oStartDate->getDay());
-    $oHourStart->setHour($oStartDate->getHour());
-    $oHourStart->setMinute('00');
-    $oHourStart->setSecond('00');
-    $oHourEnd = new Date();
-    $oHourEnd->setYear($oEndDate->getYear());
-    $oHourEnd->setMonth($oEndDate->getMonth());
-    $oHourEnd->setDay($oEndDate->getDay());
-    $oHourEnd->setHour($oEndDate->getHour());
-    $oHourEnd->setMinute('59');
-    $oHourEnd->setSecond('59');
-    if (!$oStartDate->equals($oHourStart)) {
-        $datesOkay = false;
-    }
-    if (!$oEndDate->equals($oHourEnd)) {
-        $datesOkay = false;
-    }
-} else {
-    // Must ensure that only one operation interval is being summarised
-    $operationIntervalID =
-        OA_OperationInterval::convertDaySpanToOperationIntervalID($oStartDate, $oEndDate, $operationInterval);
-    if (is_bool($operationIntervalID) && !$operationIntervalID) {
-        $datesOkay = false;
-    }
-    // Now check that the start and end dates match the start and end
-    // of the operation interval
-    list($oOperationIntervalStart, $oOperationIntervalEnd) =
-        OA_OperationInterval::convertDateToOperationIntervalStartAndEndDates($oStartDate, $operationInterval);
-    if (!$oStartDate->equals($oOperationIntervalStart)) {
-        $datesOkay = false;
-    }
-    if (!$oEndDate->equals($oOperationIntervalEnd)) {
-        $datesOkay = false;
-    }
-}
-if ($datesOkay == false) {
+
+if (OA_Maintenance_Regenerate::checkDates($oStartDate, $oEndDate) == false) {
     echo "\n" . 'Error: The dates set are not valid. See code comments.' . "\n";
     exit();
 }
@@ -140,76 +88,7 @@ sleep(10);
 
 /***************************************************************************/
 
-// Create a Data Access Layer object
-$oDbh = &OA_DB::singleton();
-
-// Find the connections (if any) in the data_intermediate_ad_connection table
-$query = "
-    SELECT
-        data_intermediate_ad_connection_id
-    FROM
-        {$conf['table']['prefix']}{$conf['table']['data_intermediate_ad_connection']}
-    WHERE
-        tracker_date_time >= '" . $oStartDate->format('%Y-%m-%d %H:%M:%S') . "'
-        AND tracker_date_time <= '" . $oEndDate->format('%Y-%m-%d %H:%M:%S') . "'";
-$rc = $oDbh->query($query);
-
-// Delete any variable values that are attached to these connections
-while ($row = $rc->fetchRow()) {
-    $query = "
-        DELETE FROM
-            {$conf['table']['prefix']}{$conf['table']['data_intermediate_ad_variable_value']}
-        WHERE
-            data_intermediate_ad_connection_id = {$row['data_intermediate_ad_connection_id']}";
-    $rows = $oDbh->exec($query);
-}
-
-// Delete any connections in the data_intermediate_ad_connection table
-$query = "
-    DELETE FROM
-        {$conf['table']['prefix']}{$conf['table']['data_intermediate_ad_connection']}
-    WHERE
-        tracker_date_time >= '" . $oStartDate->format('%Y-%m-%d %H:%M:%S') . "'
-        AND tracker_date_time <= '" . $oEndDate->format('%Y-%m-%d %H:%M:%S') . "'";
-$rows = $oDbh->exec($query);
-
-// Delete any summary rows from the data_intermediate_ad table
-$query = "
-    DELETE FROM
-        {$conf['table']['prefix']}{$conf['table']['data_intermediate_ad']}
-    WHERE
-        interval_start >= '" . $oStartDate->format('%Y-%m-%d %H:%M:%S') . "'
-        AND interval_end <= '" . $oEndDate->format('%Y-%m-%d %H:%M:%S') . "'";
-$rows = $oDbh->exec($query);
-
-// Delete any summary rows from the data_summary_ad_hourly table
-$query = "
-    DELETE FROM
-        {$conf['table']['prefix']}{$conf['table']['data_summary_ad_hourly']}
-    WHERE
-        day >= '" . $oStartDate->format('%Y-%m-%d') . "'
-        AND hour >= " . $oStartDate->format('%H') . "
-        AND day <= '" . $oEndDate->format('%Y-%m-%d') . "'
-        AND hour <= " . $oEndDate->format('%H');
-$rows = $oDbh->exec($query);
-
-// Delete any impression history data from the data_summary_zone_impression_history table
-$query = "
-    DELETE FROM
-        {$conf['table']['prefix']}{$conf['table']['data_summary_zone_impression_history']}
-    WHERE
-        interval_start >= '" . $oStartDate->format('%Y-%m-%d %H:%M:%S') . "'
-        AND interval_end <= '" . $oEndDate->format('%Y-%m-%d %H:%M:%S') . "'";
-$rows = $oDbh->exec($query);
-
-// Delete any impression history data from the data_summary_ad_zone_assoc table
-$query = "
-    DELETE FROM
-        {$conf['table']['prefix']}{$conf['table']['data_summary_ad_zone_assoc']}
-    WHERE
-        interval_start >= '" . $oStartDate->format('%Y-%m-%d %H:%M:%S') . "'
-        AND interval_end <= '" . $oEndDate->format('%Y-%m-%d %H:%M:%S') . "'";
-$rows = $oDbh->exec($query);
+OA_Maintenance_Regenerate::clearIntermediateAndSummaryTables($oStartDate, $oEndDate);
 
 // Ensure emails are not sent due to activation/deactivation effect
 define('DISABLE_ALL_EMAILS', 1);
