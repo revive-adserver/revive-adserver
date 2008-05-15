576216d1eacce4584033693ede6b8f00fc36bda1
aj.tarachanowicz@openx.org <aj.tarachanowicz@openx.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-05-15 06:56:02 +0000

    added Persian translations - OX-2714

---
+++ lib/max/language/fa/invocation.lang.php
@@ -0,0 +1,151 @@
+<?php
+
+/*
++---------------------------------------------------------------------------+
+| Openads v2.4                                                              |
+| ============                                                              |
+|                                                                           |
+| Copyright (c) 2003-2007 Openads Limited                                   |
+| For contact details, see: http://www.openads.org/                         |
+|   Translation :  Adel Shia Ali  http://MasterDesign.ir                    |
++---------------------------------------------------------------------------+
+$Id$
+*/
+
+/**
+ * A file for holding the "invocation code" English translation information.
+ *
+ * @package    MaxUI
+ * @subpackage Languages
+ */
+
+// Other
+$GLOBALS['strCopyToClipboard']			= "کپی کردن به حافظه";
+$GLOBALS['strCopy']                                 = "کپی";
+
+// Measures
+$GLOBALS['strAbbrPixels']			= "px";
+$GLOBALS['strAbbrSeconds']			= "ثانیه";
+
+// Common Invocation Parameters
+$GLOBALS['strInvocationWhat']                       = "انتخاب بنر";
+$GLOBALS['strInvocationPreview']                    = "نمایش بنر";
+$GLOBALS['strInvocationClientID']                   = "آگهی دهنده";
+$GLOBALS['strInvocationCampaignID']                 = "داخلی";
+$GLOBALS['strInvocationTarget']                     = "نحوه نمایش";
+$GLOBALS['strInvocationSource']                     = "منشا";
+$GLOBALS['strInvocationWithText']                   = "نمایش متن در زیر بنر";
+$GLOBALS['strInvocationDontShowAgain']              = "نمایش ندادن مجدد بنر در صورتی که یکسان باشد در صفحه";
+$GLOBALS['strInvocationDontShowAgainCampaign']		= "عدم نمایش بنر از campaign در صفحه مشابه";
+$GLOBALS['strInvocationTemplate'] 		= "ذخیره بنر درون یک متغیر که قابل استفاده در قالب صفحه باشد.";
+$GLOBALS['strInvocationBannerID']                   = "آی دی بنر";
+$GLOBALS['strInvocationComments']                   = "قراردادن توضیحات";
+
+// Iframe
+$GLOBALS['strIFrameRefreshAfter']		= "رفرش بعد از";
+$GLOBALS['strIframeResizeToBanner']		= "تغییر اندازه iframe برای ابعاد بنر";
+$GLOBALS['strIframeMakeTransparent']		= "ساختن شفافیت ( Transparency ) برای iframe";
+$GLOBALS['strIframeIncludeNetscape4']		= "همساز با مرورگر نت اسکپ 4";
+
+
+// PopUp
+$GLOBALS['strPopUpStyle']			= " نوع Pop-up";
+$GLOBALS['strPopUpStylePopUp']			= "Pop-up";
+$GLOBALS['strPopUpStylePopUnder']		= "Pop-under";
+$GLOBALS['strPopUpCreateInstance']		= "نمونه وقتی که  pop-up ایجاد شد";
+$GLOBALS['strPopUpImmediately']			= "بی درنگ";
+$GLOBALS['strPopUpOnClose']			= "وقتی که این صفحه بسته می شود";
+$GLOBALS['strPopUpAfterSec']			= "بعد از";
+$GLOBALS['strAutoCloseAfter']			= "اتوماتیک بسته شود بعد از";
+$GLOBALS['strPopUpTop']				= "Initial position (top)";
+$GLOBALS['strPopUpLeft']			= "Initial position (left)";
+$GLOBALS['strWindowOptions']			= "گزینه های تنظیم پنجره";
+$GLOBALS['strShowToolbars']			= "نوار ابزار";
+$GLOBALS['strShowLocation']			= "مکان";
+$GLOBALS['strShowMenubar']			= "نوار منو";
+$GLOBALS['strShowStatus']			= "وضعیت";
+$GLOBALS['strWindowResizable']			= "قابلیت عوض کردن اندازه";
+$GLOBALS['strShowScrollbars']			= "اسکرول";
+
+
+// XML-RPC
+$GLOBALS['strXmlRpcLanguage']       = "زبان هاست";
+$GLOBALS['strXmlRpcProtocol']       = "Use HTTPS to contact XML-RPC Server";
+$GLOBALS['strXmlRpcTimeout']        = "XML-RPC Timeout (Seconds)";
+
+
+// AdLayer
+$GLOBALS['strAdLayerStyle']			= "استایل";
+
+$GLOBALS['strAlignment']			= "تراز";
+$GLOBALS['strHAlignment']			= "تراز افقی";
+$GLOBALS['strLeft']				= "چپ";
+$GLOBALS['strCenter']				= "مرکز";
+$GLOBALS['strRight']				= "راست";
+
+$GLOBALS['strVAlignment']			= "تراز عمودی";
+$GLOBALS['strTop']				= "بالا";
+$GLOBALS['strMiddle']				= "میانی";
+$GLOBALS['strBottom']				= "دکمه";
+
+$GLOBALS['strAutoCollapseAfter']		= "به طور اتوماتیک متلاشی شود بعد از";
+$GLOBALS['strCloseText']			= "متن دکمه بستن";
+$GLOBALS['strClose']				= "[بستن]";
+$GLOBALS['strBannerPadding']			= "لایه گذاری بنر";
+
+$GLOBALS['strHShift']				= "انتقال افقی";
+$GLOBALS['strVShift']				= "انتقال عمودی";
+
+$GLOBALS['strShowCloseButton']			= "نشان دادن دکمه [بستن]";
+$GLOBALS['strBackgroundColor']			= "رنگ پس زمینه";
+$GLOBALS['strBorderColor']			= "رنگ حاشیه";
+
+$GLOBALS['strDirection']			= "جهت";
+$GLOBALS['strLeftToRight']			= "چپ به راست";
+$GLOBALS['strRightToLeft']			= "راست به چپ";
+$GLOBALS['strLooping']				= "به صورت چرخشی";
+$GLOBALS['strAlwaysActive']			= "همیشه فعال";
+$GLOBALS['strSpeed']				= "سرعت";
+$GLOBALS['strPause']				= "توقف";
+$GLOBALS['strLimited']				= "محدود";
+$GLOBALS['strLeftMargin']			= "حاشیه سمت چپمح";
+$GLOBALS['strRightMargin']			= "حاشیه سمت راست";
+$GLOBALS['strTransparentBackground']		= "پس زمینه شفاف";
+
+$GLOBALS['strSmoothMovement']		= "حرکت صاف";
+$GLOBALS['strHideNotMoving']		= "مخفی کردن بنر وقتی که مکان نما در حال حرکت نیست";
+$GLOBALS['strHideDelay']			= "تاخیر قبل از مخفی شدن بنر";
+$GLOBALS['strHideTransparancy']		= "شفافیت بنر مخفی شده";
+
+
+$GLOBALS['strAdLayerStyleName']	= array(
+	'geocities'		=> "Geocities",
+	'simple'		=> "ساده",
+	'cursor'		=> "مکان نما",
+	'floater'		=> "شناور"
+);
+
+// Support for 3rd party server clicktracking
+$GLOBALS['str3rdPartyTrack']		 = "Support 3rd Party Server Clicktracking";
+
+// Support for cachebusting code
+$GLOBALS['strCacheBuster']		    = "قراردادن کد Cache-Busting";
+
+// Non-Img creatives Warning for zone image-only invocation
+$GLOBALS['strNonImgWarningZone']	= "Warning: There are banners attached to this zone which are not images. These banners will not be rotated using this tag.";
+$GLOBALS['strNonImgWarning']        = "Warning: This tag will not work because this banner is not an image.";
+
+// unkown HTML tag type Warning for zone invocation
+$GLOBALS['strUnknHtmlWarning']      = "Warning: This banner is an unkown HTML ad format.";
+
+// sql/web banner-type warning for clickonly zone invocation
+$GLOBALS['strWebBannerWarning']     = "Warning: This banner must be downloaded and you must notify us the correct URL for the banner.
+<br /> 1) Download the banner:";
+$GLOBALS['strDwnldWebBanner']       = "Right-click here and choose Save Target As";
+$GLOBALS['strWebBannerWarning2']    = "<br /> 2) Upload the banner to your webserver and write its location here: ";
+
+// IMG invocation selected for tracker with appended code
+$GLOBALS['strWarning'] = "Warning";
+$GLOBALS['strImgWithAppendWarning'] = "This tracker has appended code, appended code will <strong>only</strong> work with JavaScript tags";
+
+?>
---
+++ lib/max/language/fa/report.lang.php
@@ -0,0 +1,82 @@
+<?php
+
+/*
++---------------------------------------------------------------------------+
+| Openads v2.4                                                              |
+| ============                                                              |
+|                                                                           |
+| Copyright (c) 2003-2007 Openads Limited                                   |
+| For contact details, see: http://www.openads.org/                         |
+|   Translation :  Adel Shia Ali  http://MasterDesign.ir                    |
++---------------------------------------------------------------------------+
+$Id$
+*/
+
+/**
+ * A file for holding the "reports" English translation information.
+ *
+ * @package    MaxUI
+ * @subpackage Languages
+ */
+
+$GLOBALS['strAdvertiserCampaignHistoryReport'] = 'Campaign History Report';
+$GLOBALS['strAdvertiserCampaignHistoryDescription'] = 'A daily breakdown of all activity for a specific campaign';
+$GLOBALS['strAdvertiserHistoryReport'] = 'History Report';
+$GLOBALS['strAdvertiserHistoryDescription'] = "A daily breakdown of all activity for a specific advertiser";
+$GLOBALS['strAdvertiserKeywordHistoryReport'] = 'Keyword Campaign History Report';
+$GLOBALS['strAdvertiserKeywordHistoryDescription'] = "A detailed breakdown of a keyword campaign, including conversion tracking data";
+$GLOBALS['strAdvertiserTrackerHistoryReport'] = 'Tracking Report';
+$GLOBALS['strAdvertiserTrackerHistoryDescription'] = 'A detailed breakdown of conversion tracking.';
+$GLOBALS['strAdvertiserZoneAnalysisReport'] = 'Zone Analysis';
+$GLOBALS['strAdvertiserZoneAnalysisDescription'] = 'This table shows total campaign activity by site and zone.';
+$GLOBALS['strAdvertiserDayOfWeekAnalysisReport'] = 'Day of Week Analysis';
+$GLOBALS['strAdvertiserDayOfWeekAnalysisDescription'] = 'This report shows average campaign activity by day of week.';
+$GLOBALS['strAdvertiserHourOfDayAnalysisReport'] = 'Hour of Day Analysis';
+$GLOBALS['strAdvertiserHourOfDayAnalysisDescription'] = 'This table shows total campaign activity by hour of day.';
+$GLOBALS['strAdvertiserCreativeSummaryReport'] = 'Creative Summary';
+$GLOBALS['strAdvertiserCreativeSummaryDescription'] = 'This report is a summary of the campaign by creative.';
+$GLOBALS['strAdvertiserDailyViewsAnalysisReport'] = 'Daily Views Analysis';
+$GLOBALS['strAdvertiserDailyViewsAnalysisDescription'] = 'This table is an analysis of the total activity down by day.';
+$GLOBALS['strAdvertiserCampaignSummaryReport'] = 'Campaign Summary';
+$GLOBALS['strAdvertiserCampaignSummaryDescription'] = 'Campaign Summary';
+$GLOBALS['strAdvertiserCampaignAnalysisReport'] = 'Campaign Analysis';
+$GLOBALS['strAdvertiserCampaignAnalysisDescription'] = 'This report is a summary of the campaign by placement.';
+$GLOBALS['strAdvertiserConversionTrackingAnalysisReport'] = 'Conversion Tracking Analysis';
+$GLOBALS['strAdvertiserConversionTrackingAnalysisDescription'] = 'This report shows a breakdown of all conversion tracking activity.';
+
+$GLOBALS['strPublisherAdvertisingSummaryReport'] = 'Advertising Summary';
+$GLOBALS['strPublisherAdvertisingSummaryDescription'] = 'This is a summary of the advertising activity on the site.';
+$GLOBALS['strPublisherBannerTypeAnalysisReport'] = 'Banner Type Analysis';
+$GLOBALS['strPublisherBannerTypeAnalysisDescription'] = 'This report is a summary of the campaigns by placement.';
+$GLOBALS['strPublisherBannerAnalysisReport'] = 'Banner Analysis';
+$GLOBALS['strPublisherBannerAnalysisDescription'] = 'This report is a summary of the campaigns by placement.';
+$GLOBALS['strPublisherCampaignAnalysisReport'] = 'Campaign Analysis';
+$GLOBALS['strPublisherCampaignAnalysisDescription'] = 'This table is an summary of the campaigns on the site.';
+$GLOBALS['strPublisherDayOfWeekAnalysisReport'] = 'Day of Week Analysis';
+$GLOBALS['strPublisherDayOfWeekAnalysisDescription'] = 'The report is average advertising activity by day of week.';
+$GLOBALS['strPublisherHourOfDayAnalysisReport'] = 'Hour of Day Analysis';
+$GLOBALS['strPublisherHourOfDayAnalysisDescription'] = 'The report is average advertising activity by hour of day.';
+$GLOBALS['strPublisherRequestViewsAnalysisReport'] = 'Request / Views Analysis';
+$GLOBALS['strPublisherRequestViewsAnalysisDescription'] = 'The table is comparison between total ad requests and total actual views by date.';
+$GLOBALS['strPublisherZoneAnalysisReport'] = 'Zone Report';
+$GLOBALS['strPublisherZoneAnalysisDescription'] = 'This report shows total advertising activity broken down by zone.';
+$GLOBALS['strPublisherHistoryReport'] = 'History Report';
+$GLOBALS['strPublisherHistoryDescription'] = 'Report of publisher distribution for an advertiser.';
+$GLOBALS['strPublisherZoneHistoryReport'] = 'Zone History Report';
+$GLOBALS['strPublisherZoneHistoryDescription'] = 'Report of publisher and zone distribution for an advertiser.';
+$GLOBALS['strPublisherConversionTrackingAnalysisReport'] = 'Conversion Tracking Analysis';
+$GLOBALS['strPublisherConversionTrackingAnalysisDescription'] = 'This report shows a breakdown of all conversion tracking activity for a particular publisher (affiliate).';
+
+$GLOBALS['strAgencyCampaignDeliveryReport'] = 'Campaign Delivery Report';
+$GLOBALS['strAgencyCampaignDeliveryDescription'] = "Trafficking report which shows all campaign activity for a given day, including how well it met its delivery targets.";
+$GLOBALS['strAgencyCampaignUnderdeliveryReport'] = 'Campaign Underdelivery Report';
+$GLOBALS['strAgencyCampaignUnderdeliveryDescription'] = 'Trafficking report which shows campaign activity for campaigns that did not meet targets for a given day.';
+$GLOBALS['strAgencyHistoryReport'] = 'Overall History Report';
+$GLOBALS['strAgencyHistoryDescription']  = 'Overall advertising history, broken down by day.';
+
+$GLOBALS['strCampaignPredictedFullDeliveryMessage']  = 'On track';
+
+$GLOBALS['strStatsAnalysisReport']  = 'Stats Analysis Report';
+
+
+?>
