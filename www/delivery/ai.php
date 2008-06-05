d33e56b836108a453539c6fdfa4d38245cb8908d
post_commit <post_commit@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-06-05 14:36:26 +0000

    Commit of auto-generated delivery files.

--- www/delivery/ai.php
+++ www/delivery/ai.php
@@ -1719,6 +1719,30 @@ $output = OA_Delivery_Cache_store_return($sName, $output);
 }
 return $output;
 }
+$file = '/lib/OA/Delivery/image.php';
+$GLOBALS['_MAX']['FILES'][$file] = true;
+function MAX_imageServe($aCreative, $filename, $contenttype)
+{
+// Check if the browser sent a If-Modified-Since header and if the image was
+// modified since that date
+if (!isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) ||
+$aCreative['t_stamp'] > strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE'])) {
+MAX_header("Last-Modified: ".gmdate('D, d M Y H:i:s', $aCreative['t_stamp']).' GMT');
+if (isset($contenttype) && $contenttype != '') {
+switch ($contenttype) {
+case 'swf': MAX_header('Content-type: application/x-shockwave-flash; name='.$filename); break;
+case 'dcr': MAX_header('Content-type: application/x-director; name='.$filename); break;
+case 'rpm': MAX_header('Content-type: audio/x-pn-realaudio-plugin; name='.$filename); break;
+case 'mov': MAX_header('Content-type: video/quicktime; name='.$filename); break;
+default:	MAX_header('Content-type: image/'.$contenttype.'; name='.$filename); break;
+}
+}
+echo $aCreative['contents'];
+} else {
+// Send "Not Modified" status header
+MAX_sendStatusCode(304);
+}
+}
 //Register any script specific input variables
 MAX_commonRegisterGlobalsArray(array('filename', 'contenttype'));
 if (!empty($filename)) {
