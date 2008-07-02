d0918c543f6e476793f6341443face62a04c4fe4
andrew.hill@openx.org <andrew.hill@openx.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-07-02 09:34:33 +0000

    Merged in fix for OX-3479.
    "svn merge -r22083:22084 https://svn.openx.org/openx/branches/2.6/branches/developers/miguel/OX-3479_Size_drop_down_values_are_empty ."

--- lib/max/resources/res-iab.inc.php
+++ lib/max/resources/res-iab.inc.php
@@ -25,26 +25,21 @@
 $Id$
 */
 
-// This is by no means a complete list of all possible banner sized, but
-// an the current list of all offical banner sized defined by the IAB.
-// If you have corrections or additions to this list, please send them
-// to niels@creatype.nl
-
-$phpAds_IAB['IAB Full Banner (468 x 60)'] 		 	= array ('width' => 468, 'height' => 60);
-$phpAds_IAB['IAB Skyscraper (120 x 600)'] 		   	= array ('width' => 120, 'height' => 600);
-$phpAds_IAB['IAB Leaderboard (728 x 90)'] 		 	= array ('width' => 728, 'height' => 90);
-$phpAds_IAB['IAB Button 1 (120 x 90)'] 		   		= array ('width' => 120, 'height' => 90);
-$phpAds_IAB['IAB Button 2 (120 x 60)'] 		   		= array ('width' => 120, 'height' => 60);
-$phpAds_IAB['IAB Half Banner (234 x 60)'] 		   	= array ('width' => 234, 'height' => 60);
-$phpAds_IAB['IAB Leader Board (728 x 90) *']    	= array ('width' => 728, 'height' => 90);
-$phpAds_IAB['IAB Micro Bar (88 x 31)'] 		   		= array ('width' => 88,  'height' => 31);
-$phpAds_IAB['IAB Square Button (125 x 125)'] 	   	= array ('width' => 125, 'height' => 125);
-$phpAds_IAB['IAB Vertical Banner (120 x 240)']    	= array ('width' => 120, 'height' => 240);
-$phpAds_IAB['IAB Rectangle (180 x 150) *'] 		   	= array ('width' => 180, 'height' => 150);
-$phpAds_IAB['IAB Medium Rectangle (300 x 250) *']  	= array ('width' => 300, 'height' => 250);
-$phpAds_IAB['IAB Large Rectangle (336 x 280)']   	= array ('width' => 336, 'height' => 280);
-$phpAds_IAB['IAB Vertical Rectangle (240 x 400)'] 	= array ('width' => 240, 'height' => 400);
-$phpAds_IAB['IAB Square Pop-up (250 x 250)'] 	   	= array ('width' => 250, 'height' => 250);
-$phpAds_IAB['IAB Wide Skyscraper (160 x 600) *']    = array ('width' => 160, 'height' => 600);
+$phpAds_IAB['IAB_FullBanner(468x60)'] 		 	= array ('width' => 468, 'height' => 60);
+$phpAds_IAB['IAB_Skyscraper(120x600)'] 		   	= array ('width' => 120, 'height' => 600);
+$phpAds_IAB['IAB_Leaderboard(728x90)'] 		 	= array ('width' => 728, 'height' => 90);
+$phpAds_IAB['IAB_Button1(120x90)'] 		   		= array ('width' => 120, 'height' => 90);
+$phpAds_IAB['IAB_Button2(120x60)'] 		   		= array ('width' => 120, 'height' => 60);
+$phpAds_IAB['IAB_HalfBanner(234x60)'] 		   	= array ('width' => 234, 'height' => 60);
+$phpAds_IAB['IAB_LeaderBoard(728x90)*']    	= array ('width' => 728, 'height' => 90);
+$phpAds_IAB['IAB_MicroBar(88x31)'] 		   		= array ('width' => 88,  'height' => 31);
+$phpAds_IAB['IAB_SquareButton(125x125)'] 	   	= array ('width' => 125, 'height' => 125);
+$phpAds_IAB['IAB_VerticalBanner(120x240)']    	= array ('width' => 120, 'height' => 240);
+$phpAds_IAB['IAB_Rectangle(180x150)*'] 		   	= array ('width' => 180, 'height' => 150);
+$phpAds_IAB['IAB_MediumRectangle(300x250)*']  	= array ('width' => 300, 'height' => 250);
+$phpAds_IAB['IAB_LargeRectangle(336x280)']   	= array ('width' => 336, 'height' => 280);
+$phpAds_IAB['IAB_VerticalRectangle(240x400)'] 	= array ('width' => 240, 'height' => 400);
+$phpAds_IAB['IAB_SquarePop-up(250x250)'] 	   	= array ('width' => 250, 'height' => 250);
+$phpAds_IAB['IAB_WideSkyscraper(160x600)*']    = array ('width' => 160, 'height' => 600);
 
 ?>
