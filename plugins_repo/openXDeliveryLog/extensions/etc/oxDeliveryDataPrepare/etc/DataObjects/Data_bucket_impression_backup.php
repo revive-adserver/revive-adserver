334aa7e3b01b6a9f51a3c4df3858a4ade9643045
radek.maciaszek@openx.org <radek.maciaszek@openx.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-07-23 16:48:25 +0000

    OX-3188 - refactoring

--- plugins_repo/openXDeliveryLog/extensions/etc/oxDeliveryDataPrepare/etc/DataObjects/Data_bucket_impression_backup.php
+++ plugins_repo/openXDeliveryLog/extensions/etc/oxDeliveryDataPrepare/etc/DataObjects/Data_bucket_impression_backup.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * Table Definition for data_bucket_fb_impression
+ * Table Definition for data_bucket_impression_backup
  */
 require_once MAX_PATH.'/lib/max/Dal/DataObjects/DB_DataObjectCommon.php';
 
-class DataObjects_Data_bucket_fb_impression extends DB_DataObjectCommon
+class DataObjects_Data_bucket_impression_backup extends DB_DataObjectCommon
 {
     ###START_AUTOCODE
     /* the code below is auto generated do not remove the above tag */
 
-    public $__table = 'data_bucket_fb_impression';       // table name
+    public $__table = 'data_bucket_impression_backup';       // table name
     public $interval_start;                  // DATETIME() => openads_datetime => 142
     public $primary_creative_id;             // BIGINT(20) => openads_bigint => 129
     public $creative_id;                     // BIGINT(20) => openads_bigint => 129
@@ -17,7 +17,7 @@ class DataObjects_Data_bucket_fb_impression extends DB_DataObjectCommon
     public $count;                           // INTEGER(11) => openads_int => 1
 
     /* Static get */
-    function staticGet($k,$v=NULL) { return DB_DataObject::staticGet('DataObjects_Data_bucket_fb_impression',$k,$v); }
+    function staticGet($k,$v=NULL) { return DB_DataObject::staticGet('DataObjects_Data_bucket_impression_backup',$k,$v); }
 
     var $defaultValues = array(
                 'interval_start' => '',
