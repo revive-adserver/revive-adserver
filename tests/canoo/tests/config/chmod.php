2d38fe6d2e82668af0cddbbbce384d81aa2c80bc
lukasz.dybcio@openx.org <lukasz.dybcio@openx.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-07-23 11:37:25 +0000

    OX-3389: chmod executed on extensions dir

--- tests/canoo/tests/config/chmod.php
+++ tests/canoo/tests/config/chmod.php
@@ -1,6 +1,8 @@
 <?php
-	$files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($argv[2]));
+
+	$files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($argv[1]));
 	foreach($files as $file) {
+		echo("File: " . $file . ": ");
 		chmod($file, 0666);
-	} 
+	}
 ?> 
