4e695e487d516e572413d560a66858b59e9b86af
radek.maciaszek@openx.org <radek.maciaszek@openx.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-07-24 16:00:49 +0000

    OX-3188

---
+++ extensions/deliveryLog/logCommon.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once MAX_PATH . '/lib/OA/Plugin/Component.php';
+
+class Plugins_DeliveryLog_LogCommon extends OX_Component
+{
+    abstract function getDependencies() {}
+
+    /**
+     * Can carry on any additional post-installs actions
+     * (for example install postgres specific stored procedures)
+     *
+     * @return boolean  True on success otherwise false
+     */
+    public function postInstall()
+    {
+        return true;
+    }
+}
+
+?>
