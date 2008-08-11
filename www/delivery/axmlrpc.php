8c9fc44040e274d076aa11f93a83ef0761d21b41
post_commit <post_commit@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-08-11 12:31:03 +0000

    Commit of auto-generated delivery files.

--- www/delivery/axmlrpc.php
+++ www/delivery/axmlrpc.php
@@ -150,13 +150,7 @@ setupConfigVariables();
 }
 function OA_setTimeZone($timezone)
 {
-if (version_compare(phpversion(), '5.1.0', '>=')) {
-// Set new time zone
 date_default_timezone_set($timezone);
-} else {
-// Set new time zone
-putenv("TZ={$timezone}");
-}
 // Set PEAR::Date_TimeZone default as well
 //
 // Ideally this should be a Date_TimeZone::setDefault() call, but for optimization
