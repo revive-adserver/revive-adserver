40f1b259b0c4df4cbabf79980a81038158d5a3c8
radek.maciaszek@openx.org <radek.maciaszek@openx.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-08-12 14:30:01 +0000

    PRE-339 - plugins refactoring

--- plugins_repo/OpenXThorium/extensions/deliveryAdRender/oxIndium/beaconService.delivery.php
+++ plugins_repo/OpenXThorium/extensions/deliveryAdRender/oxIndium/beaconService.delivery.php
@@ -2,7 +2,7 @@
 
 require_once MAX_PATH . '/extensions/deliveryAdRender/oxThorium/marketplace.php';
 
-function Plugin_deliveryAdRender_openXIndium_beaconService_Delivery_postAdRender($code)
+function Plugin_deliveryAdRender_oxIndium_beaconService_Delivery_postAdRender($code)
 {
     if (MAX_marketplaceNeedsIndium()) {
         $code .= MAX_adRenderImageBeacon(MAX_commonGetDeliveryUrl('in.php'), 'indium_');
