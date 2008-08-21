ea21c4d23e31da07f8f607849007d8ee9cac9cb3
monique.szpak@openx.org <monique.szpak@openx.org@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-08-21 08:58:26 +0000

    added demoExtension to plugins_repo

---
+++ plugins_repo/demoExtension/www/admin/plugins/demoUserInterface/demoUI-index.php
@@ -0,0 +1,42 @@
+<?php
+/*
++---------------------------------------------------------------------------+
+| Openads v${RELEASE_MAJOR_MINOR}                                                              |
+| ============                                                              |
+|                                                                           |
+| Copyright (c) 2003-2007 Openads Limited                                   |
+| For contact details, see: http://www.openads.org/                         |
+|                                                                           |
+| Copyright (c) 2000-2003 the phpAdsNew developers                          |
+| For contact details, see: http://www.phpadsnew.com/                       |
+|                                                                           |
+| This program is free software; you can redistribute it and/or modify      |
+| it under the terms of the GNU General Public License as published by      |
+| the Free Software Foundation; either version 2 of the License, or         |
+| (at your option) any later version.                                       |
+|                                                                           |
+| This program is distributed in the hope that it will be useful,           |
+| but WITHOUT ANY WARRANTY; without even the implied warranty of            |
+| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             |
+| GNU General Public License for more details.                              |
+|                                                                           |
+| You should have received a copy of the GNU General Public License         |
+| along with this program; if not, write to the Free Software               |
+| Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA |
++---------------------------------------------------------------------------+
+$Id$
+*/
+
+require_once 'demoUI-common.php';
+
+phpAds_PageHeader("demo-menu-i",'','../../');
+
+$oTpl    = new OA_Plugin_Template('demoUI.html','demoUserInterface');
+
+$message = 'OpenX User Interface Demonstration';
+$oTpl->assign('message',$message);
+$oTpl->display();
+
+phpAds_PageFooter();
+
+?>
---
+++ plugins_repo/demoExtension/www/admin/plugins/demoXajax/demoXajax-index.php
@@ -0,0 +1,74 @@
+<?php
+/*
++---------------------------------------------------------------------------+
+| OpenX v${RELEASE_MAJOR_MINOR}                                                                |
+| =======${RELEASE_MAJOR_MINOR_DOUBLE_UNDERLINE}                                                                |
+|                                                                           |
+| Copyright (c) 2003-2008 OpenX Limited                                     |
+| For contact details, see: http://www.openx.org/                           |
+|                                                                           |
+| This program is free software; you can redistribute it and/or modify      |
+| it under the terms of the GNU General Public License as published by      |
+| the Free Software Foundation; either version 2 of the License, or         |
+| (at your option) any later version.                                       |
+|                                                                           |
+| This program is distributed in the hope that it will be useful,           |
+| but WITHOUT ANY WARRANTY; without even the implied warranty of            |
+| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             |
+| GNU General Public License for more details.                              |
+|                                                                           |
+| You should have received a copy of the GNU General Public License         |
+| along with this program; if not, write to the Free Software               |
+| Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA |
++---------------------------------------------------------------------------+
+/**
+ * OpenX xAjax tester
+ *
+ * @author     Monique Szpak <monique.szpak@openx.org>
+ *
+ * $Id$
+ *
+ */
+
+require_once '../../../../init.php';
+require_once '../../config.php';
+require_once 'lib/demoXajax.inc.php';
+//$xajax->debugOn();
+$xajax->debugOff();
+
+$show = $_REQUEST['show'];
+
+phpAds_PageHeader("demo-xajax-{$show}",'','../../');
+
+switch ($show)
+{
+    case 'menu': // top level menu
+    case 'home': // info page
+        include "templates/home.html";
+        break;
+    case 'noframe': // 1st menu
+        include "templates/noframe.html";
+        break;
+    case 'frame': // 2nd menu
+        $src = $_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']).'/demoXajax-frame.php';
+        include "templates/frame.html";
+        break;
+    case 'frame-smarty': // 3rd menu
+        require_once MAX_PATH . '/lib/OA/Admin/TemplatePlugin.php';
+        $oTpl = new OA_Plugin_Template('frame-smarty.html','demoXajax');
+        $oTpl->debugging = false;
+        $src = $_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']).'/demoXajax-frame.php';
+        $oTpl->assign('src', $src);
+        $oTpl->display();
+        break;
+    case 'noframe-smarty': // 4th menu
+        require_once MAX_PATH . '/lib/OA/Admin/TemplatePlugin.php';
+        $oTpl = new OA_Plugin_Template('noframe.html','demoXajax');
+        $oTpl->debugging = false;
+        $oTpl->display();
+        break;
+}
+
+phpAds_PageFooter();
+
+?>
