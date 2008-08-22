d89c2fe0b589d46927c474f609b7fa7d3827c1e7
post_commit <post_commit@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-08-22 15:01:06 +0000

    Commit of auto-generated delivery files.

--- www/delivery/axmlrpc.php
+++ www/delivery/axmlrpc.php
@@ -140,6 +140,9 @@ define('MAX_PATH', dirname(__FILE__).'/../..');
 if (!defined('OX_PATH')) {
 define('OX_PATH', dirname(__FILE__).'/../..');
 }
+if (!defined('LIB_PATH')) {
+define('LIB_PATH', MAX_PATH. DIRECTORY_SEPARATOR. 'lib'. DIRECTORY_SEPARATOR. 'OX');
+}
 // Ensure that the initialisation has not been run before
 if ( !(isset($GLOBALS['_MAX']['CONF']))) {
 // Parse the Max configuration file
