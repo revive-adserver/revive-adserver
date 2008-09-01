158485af23fcc9cd47ce493fb61c5604c3e99cf1
bernard.lange <bernard.lange@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-09-01 14:37:30 +0000

    fix for OX-3731

--- www/admin/channel-edit.php
+++ www/admin/channel-edit.php
@@ -102,8 +102,20 @@ function buildChannelForm($channel)
     $translation = new OA_Translation();
     $nameRequiredMsg = $translation->translate($GLOBALS['strXRequiredField'], array($GLOBALS['strName'])); 
     $form->addRule('name', $nameRequiredMsg, 'required');
-
     
+    // Get unique affiliate
+    $doChannels = OA_Dal::factoryDO('channel');
+    if (!empty($channel['affiliateid'])) {
+        $doChannels->affiliateid = $channel['affiliateid'];
+    } 
+    else {
+        $doChannels->agencyid = OA_Permission::getAgencyId();
+    }
+    $aUnique_names = $doChannels->getUniqueValuesFromColumn('name',
+        empty($channel['channelid'])? '': $channel['name']);
+    $nameUniqueMsg = $translation->translate($GLOBALS['strXUniqueField'],
+        array($GLOBALS['strChannel'], strtolower($GLOBALS['strName'])));
+    $form->addRule('name', $nameUniqueMsg, 'unique', $aUnique_names);
     return $form;    
     
 }
