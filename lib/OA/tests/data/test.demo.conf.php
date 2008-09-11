7fa975474f2c8dd869243b62eb735120b9f13851
monique.szpak <monique.szpak@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-09-11 10:05:00 +0000

    OX-3780 added test files for 2.8 fix to merge conf files for delivery

---
+++ lib/OA/tests/data/test.demo.conf.php
@@ -0,0 +1,7 @@
+;<?php exit; ?>
+realConfig=test.real
+
+[database]
+username=demo_user
+password=demo_pass
+name=demo_name
---
+++ lib/OA/tests/data/test.real.conf.php
@@ -0,0 +1,10 @@
+;<?php exit; ?>
+;*** DO NOT REMOVE THE LINE ABOVE ***
+[database]
+username=main_user
+password=main_pass
+name=main_name
+
+[real]
+key1=val1
+key2=val2
