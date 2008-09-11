c4ca47d99a4ad1a322725f1e73dc305a9a37fb19
david.keen <david.keen@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-09-11 14:09:25 +0000

    Removed extra menu items. OX-3994

--- lib/OA/Admin/Menu/config.php
+++ lib/OA/Admin/Menu/config.php
@@ -97,8 +97,6 @@ function _buildNavigation($accountType)
                 $oMenu->addTo("agency-index", new OA_Admin_Menu_Section("agency-access", $GLOBALS['strUserAccess'], "agency-access.php?agencyid={agencyid}", false, "inventory/directSelection"));
                 $oMenu->addTo("inventory", new OA_Admin_Menu_Section("admin-generate", $GLOBALS['strGenerateBannercode'], "admin-generate.php"));
                 $oMenu->addTo("inventory", new OA_Admin_Menu_Section("admin-access", $GLOBALS['strAdminAccess'], "admin-access.php"));
-                $oMenu->addTo("inventory", new OA_Admin_Menu_Section("admin-user", $GLOBALS['strUserProperties'], "admin-user.php?userid={userid}", false, null, null, 1, true));
-                $oMenu->addTo("inventory", new OA_Admin_Menu_Section("agency-user", $GLOBALS['strUserProperties'], "agency-user.php?userid={userid}&agencyid={agencyid}", false, null, null, 1, true));
 
             $oMenu->add(new OA_Admin_Menu_Section("account-index", $GLOBALS['strMyAccount'], "account-index.php", false, "settings/preferences"));
                 $oMenu->addTo("account-index", new OA_Admin_Menu_Section("account-user-index", $GLOBALS['strUserPreferences'], "account-user-index.php", false, ""));
