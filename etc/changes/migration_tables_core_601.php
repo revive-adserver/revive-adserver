820c2b66b0b6759b79140a3cf21898e71fedacba
chris.nutting <chris.nutting@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-09-16 11:28:35 +0000

    Fixed broken upgrade migration class 601 on postgresql (unnecessary table alias)

--- etc/changes/migration_tables_core_601.php
+++ etc/changes/migration_tables_core_601.php
@@ -1,5 +1,30 @@
 <?php
 
+/*
++---------------------------------------------------------------------------+
+| OpenX v${RELEASE_MAJOR_MINOR}                                                                |
+| =======${RELEASE_MAJOR_MINOR_DOUBLE_UNDERLINE}                                                                |
+|                                                                           |
+| Copyright (c) 2003-2008 OpenX Limited                                     |
+| For contact details, see: http://www.openx.org/                           |
+|                                                                           |
+| This program is free software; you can redistribute it and/or modify      |
+| it under the terms of the GNU General Public License as published by      |
+| the Free Software Foundation; either version 2 of the License, or         |
+| (at your option) any later version.                                       |
+|                                                                           |
+| This program is distributed in the hope that it will be useful,           |
+| but WITHOUT ANY WARRANTY; without even the implied warranty of            |
+| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             |
+| GNU General Public License for more details.                              |
+|                                                                           |
+| You should have received a copy of the GNU General Public License         |
+| along with this program; if not, write to the Free Software               |
+| Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA |
++---------------------------------------------------------------------------+
+$Id$
+*/
+
 require_once(MAX_PATH.'/lib/OA/Upgrade/Migration.php');
 
 class Migration_601 extends Migration
@@ -82,17 +107,17 @@ class Migration_601 extends Migration
         {
             $updateQuery = "
                 UPDATE
-                    " . $this->oDBH->quoteIdentifier($tblCampaigns) . " AS c
+                    " . $this->oDBH->quoteIdentifier($tblCampaigns) . "
                 SET
                     viewwindow = '{$aCampaignTrackers['max_viewwindow']}',
                     clickwindow = '{$aCampaignTrackers['max_clickwindow']}'
                 WHERE
                     campaignid = '{$aCampaignTrackers['campaign_id']}'
             ";
-            
+
             $this->oDBH->query($updateQuery);
         }
- 
+
 		return $this->beforeRemoveField('campaigns_trackers', 'viewwindow');
 	}
 
@@ -111,7 +136,7 @@ class Migration_601 extends Migration
 	{
 		return $this->beforeRemoveField('campaigns_trackers', 'clickwindow');
 	}
-	
+
 	function afterRemoveField__campaigns_trackers__clickwindow()
 	{
 		return $this->afterRemoveField('campaigns_trackers', 'clickwindow');
