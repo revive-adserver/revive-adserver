383fb03cd8888cb10d0ef4f53e2f7869948d4d26
aj.tarachanowicz <aj.tarachanowicz@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-09-22 02:03:24 +0000

    added new db tables - AC-155

--- plugins_repo/openXThorium/extensions/etc/oxThorium/etc/DataObjects/Ext_thorium_campaign_pref.php
+++ plugins_repo/openXThorium/extensions/etc/oxThorium/etc/DataObjects/Ext_thorium_campaign_pref.php
@@ -9,15 +9,16 @@ class DataObjects_Ext_thorium_campaign_pref extends DB_DataObjectCommon
     ###START_AUTOCODE
     /* the code below is auto generated do not remove the above tag */
 
-    public $__table = 'ext_thorium_campaign_pref';    // table name
-    public $campaignid;                      // MEDIUMINT(9) => openads_mediumint => 129
-    public $is_enabled;                      // SMALLINT(1) => openads_smallint => 17
-    public $floor_price;                     // DECIMAL(10,4) => openads_decimal => 1
+    public $__table = 'ext_thorium_campaign_pref';       // table name
+    public $campaignid;                      // MEDIUMINT(9) => openads_mediumint => 129 
+    public $is_enabled;                      // SMALLINT(1) => openads_smallint => 17 
+    public $floor_price;                     // DECIMAL(10,4) => openads_decimal => 1 
 
     /* Static get */
     function staticGet($k,$v=NULL) { return DB_DataObject::staticGet('DataObjects_Ext_thorium_campaign_pref',$k,$v); }
 
     var $defaultValues = array(
+                'campaignid' => 0,
                 'is_enabled' => 0,
                 );
 
--- plugins_repo/openXThorium/extensions/etc/oxThorium/etc/DataObjects/Ext_thorium_website_pref.php
+++ plugins_repo/openXThorium/extensions/etc/oxThorium/etc/DataObjects/Ext_thorium_website_pref.php
@@ -9,12 +9,17 @@ class DataObjects_Ext_thorium_website_pref extends DB_DataObjectCommon
     ###START_AUTOCODE
     /* the code below is auto generated do not remove the above tag */
 
-    public $__table = 'ext_thorium_campaign_pref';    // table name
-    public $affiliateid;                      // MEDIUMINT(9) => openads_mediumint => 129
-    public $publisher_id;                     // MEDIUMINT(9) => openads_mediumint => 129
+    public $__table = 'ext_thorium_website_pref';        // table name
+    public $affiliateid;                     // MEDIUMINT(9) => openads_mediumint => 129 
+    public $website_id;                      // CHAR(36) => openads_char => 130 
 
     /* Static get */
     function staticGet($k,$v=NULL) { return DB_DataObject::staticGet('DataObjects_Ext_thorium_website_pref',$k,$v); }
+
+    var $defaultValues = array(
+                'affiliateid' => 0,
+                'website_id' => '',
+                );
 
     /* the code above is auto generated do not remove the tag below */
     ###END_AUTOCODE
