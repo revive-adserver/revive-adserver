b1dfe99898df491d179d267b5ea5e1274de38973
monique.szpak <monique.szpak@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-10-10 12:45:33 +0000

    regenerated correct Banners DataObject (fixes broken 2.8 tests)

--- lib/max/Dal/DataObjects/Banners.php
+++ lib/max/Dal/DataObjects/Banners.php
@@ -1,11 +1,41 @@
 <?php
+
+/*
++---------------------------------------------------------------------------+
+| OpenX v${RELEASE_MAJOR_MINOR}                                                                |
+| =======${RELEASE_MAJOR_MINOR_DOUBLE_UNDERLINE}                                                                |
+|                                                                           |
+| Copyright (c) 2003-2008 OpenX Limited                                     |
+| For contact details, see: http://www.openx.org/                           |
+|                                                                           |
+| This program is free software; you can redistribute it and/or modify      |
+| it under the terms of the GNU General Public License as published by      |
+| the Free Software Foundation; either version 2 of the License, or         |
+| (at your option) any later version.                                       |
+|                                                                           |
+| This program is distributed in the hope that it will be useful,           |
+| but WITHOUT ANY WARRANTY; without even the implied warranty of            |
+| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             |
+| GNU General Public License for more details.                              |
+|                                                                           |
+| You should have received a copy of the GNU General Public License         |
+| along with this program; if not, write to the Free Software               |
+| Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA |
++---------------------------------------------------------------------------+
+$Id$
+*/
+
 /**
  * Table Definition for banners
  */
-require_once MAX_PATH.'/lib/max/Dal/DataObjects/DB_DataObjectCommon.php';
+require_once 'DB_DataObjectCommon.php';
+include_once MAX_PATH . '/www/admin/lib-banner.inc.php';
+include_once MAX_PATH . '/www/admin/lib-storage.inc.php';
 
 class DataObjects_Banners extends DB_DataObjectCommon
 {
+    var $onDeleteCascade = true;
+    var $refreshUpdatedFieldIfExists = true;
     ###START_AUTOCODE
     /* the code below is auto generated do not remove the above tag */
 
@@ -279,4 +309,5 @@ class DataObjects_Banners extends DB_DataObjectCommon
     }
 
 }
+
 ?>
