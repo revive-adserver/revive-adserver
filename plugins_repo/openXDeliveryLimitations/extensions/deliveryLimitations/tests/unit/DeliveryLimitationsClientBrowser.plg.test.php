667655e7e3b5bbe6551b9a0769927bb5700230a7
monique.szpak <monique.szpak@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-11-07 13:00:06 +0000

    moved deprecated unit test for remotehost into delivery limitation plugin repo

--- plugins_repo/openXDeliveryLimitations/extensions/deliveryLimitations/tests/unit/DeliveryLimitationsClientBrowser.plg.test.php
+++ plugins_repo/openXDeliveryLimitations/extensions/deliveryLimitations/tests/unit/DeliveryLimitationsClientBrowser.plg.test.php
@@ -67,5 +67,41 @@ class Plugins_TestOfPlugins_DeliveryLimitations_Client_Browser extends UnitTestC
         $this->assertFalse(MAX_checkClient_Browser('LX,LI', '=~'));
         $this->assertTrue(MAX_checkClient_Browser('LX,FF', '=~'));
     }
+
+    /**
+     *@todo test case for different user agents
+     *
+     * A function to set the viewer's useragent information in the
+     * $GLOBALS['_MAX']['CLIENT'] global variable, if the option to use
+     * phpSniff to extract useragent information is set in the
+     * configuration file.
+     */
+    function test_MAX_remotehostSetClientInfo()
+    {
+        $GLOBALS['_MAX']['CONF']['logging']['sniff'] = true;
+        $http_user_agent = 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.8)' .
+                           ' Gecko/20061109 CentOS/1.5.0.8-0.1.el4.centos4 Firefox/1.5.0.8 pango-text';
+        $_SERVER['HTTP_USER_AGENT'] = $http_user_agent;
+        MAX_remotehostSetClientInfo();
+        $this->assertIsA($GLOBALS['_MAX']['CLIENT'], 'array');
+        $this->assertEqual($GLOBALS['_MAX']['CLIENT']['browser'], 'fx');
+        $this->assertEqual($GLOBALS['_MAX']['CLIENT']['cookies'], 'Unknown');
+        $this->assertEqual($GLOBALS['_MAX']['CLIENT']['gecko'], '20061109');
+        $this->assertEqual($GLOBALS['_MAX']['CLIENT']['gecko_ver'], '1.8.0.8');
+        $this->assertEqual($GLOBALS['_MAX']['CLIENT']['javascript'], '1.5');
+        // Not testing the language result from phpSniff, as always seems to
+        // add en-us when testing via the web browser...
+        // $this->assertEqual($GLOBALS['_MAX']['CLIENT']['language'], 'en-us,en');
+        $this->assertEqual($GLOBALS['_MAX']['CLIENT']['long_name'], 'firefox');
+        $this->assertEqual($GLOBALS['_MAX']['CLIENT']['maj_ver'], '1');
+        $this->assertEqual($GLOBALS['_MAX']['CLIENT']['min_ver'], '.5.0.8');
+        $this->assertEqual($GLOBALS['_MAX']['CLIENT']['os'], 'linux');
+        $this->assertEqual($GLOBALS['_MAX']['CLIENT']['platform'], '*nix');
+        $this->assertEqual($GLOBALS['_MAX']['CLIENT']['ss_cookies'], 'Unknown');
+        $this->assertEqual($GLOBALS['_MAX']['CLIENT']['st_cookies'], 'Unknown');
+        $this->assertEqual($GLOBALS['_MAX']['CLIENT']['ua'], strtolower($http_user_agent));
+        $this->assertEqual($GLOBALS['_MAX']['CLIENT']['version'], '1.5.0.8');
+    }
+
 }
 ?>
