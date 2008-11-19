1a741aa035366d7754cb56cff30dc79e4ec0be5f
pawel.dachterski <pawel.dachterski@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-11-19 16:28:56 +0000

    XMLRPC API tests refactoring

--- www/api/v1/xmlrpc/tests/unit/src/test/java/org/openx/banner/TestTargeting.java
+++ www/api/v1/xmlrpc/tests/unit/src/test/java/org/openx/banner/TestTargeting.java
@@ -8,110 +8,116 @@ import org.apache.xmlrpc.XmlRpcException;
 import org.openx.utils.ErrorMessage;
 
 public class TestTargeting extends BannerTestCase {
-    
-    @SuppressWarnings("unchecked")
-    public void testGetAndSetTargetingWithEmptyArrays() 
-        throws MalformedURLException, XmlRpcException {
-        
-        assertNotNull(campaignId);
-        Map<String, Object> myBanner = getBannerParams("test1");
-        myBanner.put(STATUS, 1);
 
-        Object[] XMLRPCMethodParameters = new Object[] { sessionId, myBanner };
-        final Integer bannerId = (Integer) execute(ADD_BANNER_METHOD, XMLRPCMethodParameters);
-        assertNotNull(bannerId);
+	public void testGetAndSetTargetingWithEmptyArrays()
+			throws MalformedURLException, XmlRpcException {
 
-        try {
-            Object[] targeting = new Object[0];
-            XMLRPCMethodParameters = new Object[] { sessionId, bannerId, targeting};
-            final Boolean result = (Boolean) execute(SET_BANNER_TARGETING, XMLRPCMethodParameters);
-            assertTrue(result);
-            
-            XMLRPCMethodParameters = new Object[] { sessionId, bannerId};
-            final Object[] targetingResult = (Object[]) execute(GET_BANNER_TARGETING, 
-                XMLRPCMethodParameters);
-            assertEquals(targetingResult.length, 0);
+		assertNotNull(campaignId);
+		Map<String, Object> myBanner = getBannerParams("test1");
+		myBanner.put(STATUS, 1);
 
-        } finally {
-            deleteBanner(bannerId);
-        }
-    }
-    
-    @SuppressWarnings("unchecked")
-    public void testGetAndSetTargeting() 
-        throws MalformedURLException, XmlRpcException {
-        
-        assertNotNull(campaignId);
-        Map<String, Object> myBanner = getBannerParams("test1");
-        myBanner.put(STATUS, 1);
+		Object[] XMLRPCMethodParameters = new Object[] { sessionId, myBanner };
+		final Integer bannerId = (Integer) execute(ADD_BANNER_METHOD,
+				XMLRPCMethodParameters);
+		
+		assertNotNull(bannerId);
 
-        Object[] XMLRPCMethodParameters = new Object[] { sessionId, myBanner };
-        final Integer bannerId = (Integer) execute(ADD_BANNER_METHOD, XMLRPCMethodParameters);
-        assertNotNull(bannerId);
+		try {
+			Object[] targeting = new Object[0];
+			XMLRPCMethodParameters = new Object[] { sessionId, bannerId,
+					targeting };
+			
+			final Boolean result = (Boolean) execute(SET_BANNER_TARGETING,
+					XMLRPCMethodParameters);
+			
+			assertTrue(result);
+			XMLRPCMethodParameters = new Object[] { sessionId, bannerId };
+			final Object[] targetingResult = (Object[]) execute(
+					GET_BANNER_TARGETING, XMLRPCMethodParameters);
+			
+			assertEquals(targetingResult.length, 0);
+		} finally {
+			deleteBanner(bannerId);
+		}
+	}
 
-        try {
-            
-            Map<String, Object> targetingInfo1 = new HashMap<String, Object>();
-            targetingInfo1.put(TARGETING_LOGICAL, "and");
-            targetingInfo1.put(TARGETING_TYPE, "deliveryLimitations:Geo:Country");
-            targetingInfo1.put(TARGETING_COMPATISON, "=");
-            targetingInfo1.put(TARGETING_DATA, "");
-            
-            Object[] targeting = new Object[] {targetingInfo1};
-            
-            XMLRPCMethodParameters = new Object[] { sessionId, bannerId, targeting};
-            final Boolean result = (Boolean) execute(SET_BANNER_TARGETING, XMLRPCMethodParameters);
-            assertTrue(result);
-            
-            XMLRPCMethodParameters = new Object[] { sessionId, bannerId};
-            final Object[] targetingResult = (Object[]) execute(GET_BANNER_TARGETING, 
-                XMLRPCMethodParameters);
-            assertEquals(targetingResult.length, 1);
-            
-            assertEquals(((Map) targetingResult[0]).get(TARGETING_LOGICAL), "and");
-            assertEquals(((Map) targetingResult[0]).get(TARGETING_TYPE), 
-                "deliveryLimitations:Geo:Country");
-            assertEquals(((Map) targetingResult[0]).get(TARGETING_COMPATISON), "=");
-            assertEquals(((Map) targetingResult[0]).get(TARGETING_DATA), "");
+	@SuppressWarnings("unchecked")
+	public void testGetAndSetTargeting() throws MalformedURLException,
+			XmlRpcException {
 
-        } finally {
-            deleteBanner(bannerId);
-        }
-    }
-    
-    @SuppressWarnings("unchecked")
-    public void testGetAndSetTargetingWithError() 
-        throws MalformedURLException, XmlRpcException {
-        
-        assertNotNull(campaignId);
-        Map<String, Object> myBanner = getBannerParams("test1");
-        myBanner.put(STATUS, 1);
+		assertNotNull(campaignId);
+		Map<String, Object> myBanner = getBannerParams("test1");
+		myBanner.put(STATUS, 1);
 
-        Object[] XMLRPCMethodParameters = new Object[] { sessionId, myBanner };
-        final Integer bannerId = (Integer) execute(ADD_BANNER_METHOD, XMLRPCMethodParameters);
-        assertNotNull(bannerId);
+		Object[] XMLRPCMethodParameters = new Object[] { sessionId, myBanner };
+		final Integer bannerId = (Integer) execute(ADD_BANNER_METHOD,
+				XMLRPCMethodParameters);
+		
+		assertNotNull(bannerId);
 
-        try {
-            
-            Map<String, Object> targetingInfo1 = new HashMap<String, Object>();
-            targetingInfo1.put(TARGETING_LOGICAL, "and");
-            targetingInfo1.put(TARGETING_TYPE, "deliveryLimitations:Geo:Country");
-            targetingInfo1.put(TARGETING_COMPATISON, "=");
-            
-            Object[] targeting = new Object[] {targetingInfo1};
+		try {
 
-            try {
-                XMLRPCMethodParameters = new Object[] { sessionId, bannerId, targeting};
-                execute(SET_BANNER_TARGETING, XMLRPCMethodParameters);
-                fail();
-            } catch (XmlRpcException e) {
-                assertEquals(ErrorMessage.getMessage(
-                    ErrorMessage.FIELD_IN_STRUCTURE_DOES_NOT_EXISTS, "data"),
-                    e.getMessage());
-            }
-        } finally {
-            deleteBanner(bannerId);
-        }
-    }
-    
+			Map<String, Object> targetingInfo1 = new HashMap<String, Object>();
+			targetingInfo1.put(TARGETING_LOGICAL, "and");
+			targetingInfo1.put(TARGETING_TYPE, "deliveryLimitations:Geo:Country");
+			targetingInfo1.put(TARGETING_COMPATISON, "=");
+			targetingInfo1.put(TARGETING_DATA, "");
+
+			Object[] targeting = new Object[] { targetingInfo1 };
+
+			XMLRPCMethodParameters = new Object[] { sessionId, bannerId,
+					targeting };
+			final Boolean result = (Boolean) execute(SET_BANNER_TARGETING,
+					XMLRPCMethodParameters);
+			assertTrue(result);
+
+			XMLRPCMethodParameters = new Object[] { sessionId, bannerId };
+			final Map<String, Object> targetingResult = (Map<String, Object>) execute(
+					GET_BANNER_TARGETING, XMLRPCMethodParameters);
+			
+			assertEquals(targetingResult.size(), 1);
+			checkParameter(targetingResult, targetingInfo1, TARGETING_LOGICAL);
+			checkParameter(targetingResult, targetingInfo1, TARGETING_TYPE);
+			checkParameter(targetingResult, targetingInfo1, TARGETING_COMPATISON);
+			checkParameter(targetingResult, targetingInfo1, TARGETING_DATA);
+		} finally {
+			deleteBanner(bannerId);
+		}
+	}
+
+	public void testGetAndSetTargetingWithError() throws MalformedURLException,
+			XmlRpcException {
+
+		assertNotNull(campaignId);
+		Map<String, Object> myBanner = getBannerParams("test1");
+		myBanner.put(STATUS, 1);
+
+		Object[] XMLRPCMethodParameters = new Object[] { sessionId, myBanner };
+		final Integer bannerId = (Integer) execute(ADD_BANNER_METHOD,
+				XMLRPCMethodParameters);
+		assertNotNull(bannerId);
+
+		try {
+
+			Map<String, Object> targetingInfo1 = new HashMap<String, Object>();
+			targetingInfo1.put(TARGETING_LOGICAL, "and");
+			targetingInfo1.put(TARGETING_TYPE, "deliveryLimitations:Geo:Country");
+			targetingInfo1.put(TARGETING_COMPATISON, "=");
+
+			Object[] targeting = new Object[] { targetingInfo1 };
+
+			try {
+				XMLRPCMethodParameters = new Object[] { sessionId, bannerId,
+						targeting };
+				execute(SET_BANNER_TARGETING, XMLRPCMethodParameters);
+				fail();
+			} catch (XmlRpcException e) {
+				assertEquals(ErrorMessage.getMessage(ErrorMessage.FIELD_IN_STRUCTURE_DOES_NOT_EXISTS,
+						"data"), e.getMessage());
+			}
+		} finally {
+			deleteBanner(bannerId);
+		}
+	}
+
 }
