29c9c3351ff64641a3152144f1f811d5a01e271e
lukasz.wikierski <lukasz.wikierski@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-11-27 15:50:30 +0000

    Fix OX-4403 case with variables beginning from space

--- lib/max/Delivery/querystring.php
+++ lib/max/Delivery/querystring.php
@@ -169,7 +169,7 @@ function MAX_querystringGetDestinationUrl($adId = null)
             foreach ($destQuery as $destPair) {
                 list($destName, $destValue) = explode('=', $destPair);
                 // http://php.net/parse_str#76978 dots and spaces are auto-replaced by underscores
-                $aValidVariables[] = str_replace(array('.', ' '), '_', $destName);
+                $aValidVariables[] = str_replace(array('.', ' '), '_', ltrim($destName));
             }
         }
     }
