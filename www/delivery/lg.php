42a8a3641a7d81913bf077b0a6d97acfb3383aeb
post_commit <post_commit@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-11-27 15:50:33 +0000

    Commit of auto-generated delivery files.

--- www/delivery/lg.php
+++ www/delivery/lg.php
@@ -1946,7 +1946,7 @@ if (!empty($destQuery)) {
 foreach ($destQuery as $destPair) {
 list($destName, $destValue) = explode('=', $destPair);
 // http://php.net/parse_str#76978 dots and spaces are auto-replaced by underscores
-$aValidVariables[] = str_replace(array('.', ' '), '_', $destName);
+$aValidVariables[] = str_replace(array('.', ' '), '_', ltrim($destName));
 }
 }
 }
