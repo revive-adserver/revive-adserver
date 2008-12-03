eb0f7db427fdf38d08a0971ddad916843bf98d21
lukasz.wikierski <lukasz.wikierski@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-12-03 09:18:19 +0000

    Reverting changes done for OX-4403

--- lib/max/Delivery/querystring.php
+++ lib/max/Delivery/querystring.php
@@ -168,8 +168,7 @@ function MAX_querystringGetDestinationUrl($adId = null)
         if (!empty($destQuery)) {
             foreach ($destQuery as $destPair) {
                 list($destName, $destValue) = explode('=', $destPair);
-                // http://php.net/parse_str#76978 dots and spaces are auto-replaced by underscores
-                $aValidVariables[] = str_replace(array('.', ' '), '_', ltrim($destName));
+                $aValidVariables[] = $destName;
             }
         }
     }
