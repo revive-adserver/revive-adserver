d90772d7197bc4e79d843e56011c3dbde65e241d
post_commit <post_commit@69d48449-a624-0410-ae76-85ddf3b22ea5>
2008-12-12 17:53:42 +0000

    Commit of auto-generated delivery files.

--- www/delivery/axmlrpc.php
+++ www/delivery/axmlrpc.php
@@ -3082,6 +3082,20 @@ else
 return $pathWithSuffix;
 }
 }
+function realPathRelative($path) {
+$path = str_replace(array('/', '\\'), DIRECTORY_SEPARATOR, $path);
+$parts = array_filter(explode(DIRECTORY_SEPARATOR, $path), 'strlen');
+$absolutes = array();
+foreach ($parts as $part) {
+if ('.' == $part) continue;
+if ('..' == $part) {
+array_pop($absolutes);
+} else {
+$absolutes[] = $part;
+}
+}
+return implode(DIRECTORY_SEPARATOR, $absolutes);
+}
 }
 require_once 'XML/RPC/Server.php';
 // Set a global variable to let the other functions know
