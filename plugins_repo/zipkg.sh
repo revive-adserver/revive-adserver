3fe9617ee70d90aa5e3a6977d4a66acd7601fb55
chris.nutting <chris.nutting@69d48449-a624-0410-ae76-85ddf3b22ea5>
2009-01-07 14:27:15 +0000

    Restored the command-line zip tools so ant is no longer required to package plugins, also added exclusions for the .template and default.properties file patterns

---
+++ plugins_repo/zipkg.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+  arg="$1"
+
+  if [ -z "$arg" ]; then
+    arg="--help"
+  fi
+
+  case $arg in
+  --help)
+    echo \
+    "Usage: zipkg [FOLDER]"
+    exit 0
+    ;;
+  esac
+
+  #echo "$arg"
+
+  rm "$arg".zip
+
+  cd "$arg"
+
+  zip -D -r ../"$arg".zip . -x \*.svn/* \*tests/* \*/packages/* \*.template.xml \*default.properties
+
+  cd ..
+
+  exit 0
