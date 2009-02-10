0e26ca99103bcdb48e70a635a5c09be2668b4930
bernard.lange <bernard.lange@69d48449-a624-0410-ae76-85ddf3b22ea5>
2009-02-10 13:57:52 +0000

    Fix for OX-4899

--- www/admin/assets/js/ox.help.js
+++ www/admin/assets/js/ox.help.js
@@ -6,62 +6,120 @@
   };  
   
   $.fn.helpObject = function(help, settings) {
-   $(help).hide();
-   var $wrapper = wrap($(help));
-	 $("[help='" + help.id + "']").addClass("popup-help-link")
-	   .bind('click', {'elementId' : $wrapper.get(0).id}, show);
-	 $wrapper.click(hide);      
-  };
-  
-  function show(event) {
-    var $wrapper = $("#" + event.data['elementId']);
+    var defaults = {
+      'parentXOffset' : 0,
+      'parentYOffset' : 0
+    }
+    var options = $.extend({ }, defaults, settings);
     
-    //hide other help popups
-    $(".popup-help").fadeOut("fast");
-    var posX = event.pageX;
-    var posY = event.pageY + 5;
+     $(help).hide();
+     var $wrapper = wrap($(help));
+  	 $("[help='" + help.id + "']").addClass("popup-help-link")
+  	   .bind('click', {'elementId' : $wrapper.get(0).id}, show);
+  	 $wrapper.click(hide);      
+    
+      function show(event) {
+        var $wrapper = $("#" + event.data['elementId']);
+        
+        //hide other help popups
+        $(".popup-help").fadeOut("fast");
+//        var posX = event.pageX - options.parentXOffset; 
+//        var posY = event.pageY + 5 - options.parentYOffset;
+    
+        var clickPosition = getClickPositionInDocument(event);
+        var posX = clickPosition.X - options.parentXOffset; 
+        var posY = clickPosition.Y + 5 - options.parentYOffset;
+    
+    
+        $wrapper.fadeIn("fast").css("display", "block");
+        
+        var wrapperWidth = $wrapper.outerWidth({ margin: true });
+        var wrapperHeight = $wrapper.outerHeight({ margin: true });
+        var scrollY = event.pageY - event.clientY; 
+        
+        var xDiff = $(window).width() - (posX + wrapperWidth + 10); 
+        var yDiff = $(window).height() - (posY - scrollY + wrapperHeight);
+        
+        
+        if (xDiff <= 0) {
+          posX -= Math.abs(xDiff);
+          posX = Math.max(10, posX);
+        }
+        
+        if (yDiff <= 0) {
+          posY -= (wrapperHeight + 10);
+          posY = Math.max(10 + scrollY, posY);
+        }
+        
+        $wrapper.css({top: posY, left: posX});
+      }
+  
+      function hide()
+      {
+          $(this).fadeOut("fast");
+      }
+	
+      function wrap($help)
+      {
+         var $wrapper = $help.wrap("<div class='popup-help'></div>").parent(); 
+            $wrapper.attr("id", $help.get(0).id + "wrapper");
+            $wrapper.prepend("<div class='close'> <span class='link'>[x]</span> </div>");
+            $wrapper.hide();
+            $help.addClass("help-content").show();
+            return $wrapper;	
+      }
 
-    $wrapper.fadeIn("fast").css("display", "block");
-    
-    var wrapperWidth = $wrapper.outerWidth({ margin: true });
-    var wrapperHeight = $wrapper.outerHeight({ margin: true });
-    var scrollY = event.pageY - event.clientY; 
-    
-    var xDiff = $(window).width() - (posX + wrapperWidth + 10); 
-    var yDiff = $(window).height() - (posY - scrollY + wrapperHeight);
-    
-    if (xDiff <= 0) {
-      posX -= Math.abs(xDiff);
-      posX = Math.max(10, posX);
-    }
-    
-    if (yDiff <= 0) {
-      posY -= (wrapperHeight + 10);
-      posY = Math.max(10 + scrollY, posY);
-    }
-    
-    $wrapper.css({top: posY, left: posX});
-  }
   
-	function hide()
-	{
-    $(this).fadeOut("fast");
-	}
-	
-	function wrap($help)
-	{
-	   var $wrapper = $help.wrap("<div class='popup-help'></div>").parent(); 
-    $wrapper.attr("id", $help.get(0).id + "wrapper");
-    $wrapper.prepend("<div class='close'> <span class='link'>[x]</span> </div>");
-    $wrapper.hide();
-    $help.addClass("help-content").show();
-    return $wrapper;	
-	}
-	
+      function getClickPositionInDocument(e)
+      {
+          //determine x click
+          if (e.pageX) {
+              clickX = e.pageX;
+          }
+          else if (e.clientX) {
+              if (document.documentElement.scrollLeft) {
+                  scrollX = document.documentElement.scrollLeft;
+              }
+              else {
+                  scrollX = document.body.scrollLeft;
+              }
+              clickX = e.clientX + scrollX;
+          }
+          
+          //determine y click 
+          if (e.pageY) {
+              clickY = e.pageY;
+          }
+          else if (e.clientY) {
+              if (document.documentElement.scrollTop) {
+                  scrollY = document.documentElement.scrollTop;
+              }
+              else {
+                  scrollY = document.body.scrollTop;
+              }
+              clickY = e.clientY + scrollY;
+          }
+          
+          if (clickX < 0) {
+              clickX = 0;
+          }
+          if (clickY < 0) { 
+              clickY = 0;
+          }
+          
+          return {'X' : clickX, 'Y' : clickY};
+      }
+  };              
 })(jQuery);
 
 (function($) {
   $(document).ready(function(){
-    $("[id^='help-']").help();
+    //init help, apply offset for thirdLevelContent to get abs positioning right
+    
+    $("[id^='help-']").help({
+        'parentXOffset' : 200,
+        'parentYOffset' : 200
+        }
+    );
   });
 })(jQuery);
