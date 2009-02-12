0c43664200d6797c1240e35be4f68909a728d17d
chris.nutting <chris.nutting@69d48449-a624-0410-ae76-85ddf3b22ea5>
2009-02-12 12:21:50 +0000

    Refs OX-4867 Manually recompiled the delivery engine in /trunk (post-commit hook is broken)

--- www/delivery/alocal.php
+++ www/delivery/alocal.php
@@ -1232,7 +1232,9 @@ return $converted ? $converted : $content;
 function MAX_commonSendContentTypeHeader($type = 'text/html', $charset = null)
 {
 $header = 'Content-type: ' . $type;
-if (!empty($charset)) { $header .= '; charset=' . $charset; }
+if (!empty($charset) && preg_match('/^[a-zA-Z0-9_-]+$/D', $charset)) {
+$header .= '; charset=' . $charset;
+}
 MAX_header($header);
 }
 function MAX_commonSetNoCacheHeaders()
