0923bb50958ee83e0969cc5a46f5ab15f6ad8ae2
andrew.hill <andrew.hill@69d48449-a624-0410-ae76-85ddf3b22ea5>
2009-02-23 09:15:01 +0000

    Updated header.

--- etc/changes/migration_tables_core_605.php
+++ etc/changes/migration_tables_core_605.php
@@ -1,5 +1,30 @@
 <?php
 
+/*
++---------------------------------------------------------------------------+
+| OpenX v${RELEASE_MAJOR_MINOR}                                                                |
+| =======${RELEASE_MAJOR_MINOR_DOUBLE_UNDERLINE}                                                                |
+|                                                                           |
+| Copyright (c) 2003-2009 OpenX Limited                                     |
+| For contact details, see: http://www.openx.org/                           |
+|                                                                           |
+| This program is free software; you can redistribute it and/or modify      |
+| it under the terms of the GNU General Public License as published by      |
+| the Free Software Foundation; either version 2 of the License, or         |
+| (at your option) any later version.                                       |
+|                                                                           |
+| This program is distributed in the hope that it will be useful,           |
+| but WITHOUT ANY WARRANTY; without even the implied warranty of            |
+| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             |
+| GNU General Public License for more details.                              |
+|                                                                           |
+| You should have received a copy of the GNU General Public License         |
+| along with this program; if not, write to the Free Software               |
+| Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA |
++---------------------------------------------------------------------------+
+$Id$
+*/
+
 require_once(MAX_PATH.'/lib/OA/Upgrade/Migration.php');
 
 class Migration_605 extends Migration
@@ -21,7 +46,7 @@ class Migration_605 extends Migration
         'tradedoubler'  => '3rdPartyServers:ox3rdPartyServers:tradedoubler',
         'ypn'           => '3rdPartyServers:ox3rdPartyServers:ypn',
     );
-    
+
     function Migration_605()
     {
         //$this->__construct();
@@ -44,7 +69,7 @@ class Migration_605 extends Migration
 	{
 	    $prefix = $this->getPrefix();
 	    $table = $this->oDBH->quoteIdentifier($prefix . 'banners', true);
-	    
+
 	    foreach ($this->adserverMap as $name => $pci) {
 	        $query = "UPDATE {$table} SET adserver = '" . $pci . "' WHERE adserver = '" . $name . "'";
 	        $result = $this->oDBH->exec($query);
