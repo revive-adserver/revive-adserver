ca82c233ad06c083e8661d7869a0ec5415a071a0
andrew.hill <andrew.hill@69d48449-a624-0410-ae76-85ddf3b22ea5>
2009-04-01 09:53:51 +0000

    Release script commit.

--- constants.php
+++ constants.php
@@ -38,7 +38,7 @@ $Id$
 function setupConstants()
 {
     // Define this version of Openads's constants
-    define('OA_VERSION', '2.7.31-beta-rc8');
+    define('OA_VERSION', '2.7.31-beta-rc9');
     define('MAX_PRODUCT_NAME',      'OpenX');
     define('MAX_PRODUCT_URL',       'www.openx.org');
     if (preg_match('/^(\d+\.\d+)/', OA_VERSION, $aMatches)) {
