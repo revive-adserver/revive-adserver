13a4502aa793c95933d05230ca9245dea19fdc5b
andrew.hill <andrew.hill@69d48449-a624-0410-ae76-85ddf3b22ea5>
2009-04-01 16:24:39 +0000

    Fixed header in file.

--- constants.php
+++ constants.php
@@ -2,8 +2,8 @@
 
 /*
 +---------------------------------------------------------------------------+
-| OpenX v2.7                                                                |
-| ==========                                                                |
+| OpenX v${RELEASE_MAJOR_MINOR}                                                                |
+| =======${RELEASE_MAJOR_MINOR_DOUBLE_UNDERLINE}                                                                |
 |                                                                           |
 | Copyright (c) 2003-2009 OpenX Limited                                     |
 | For contact details, see: http://www.openx.org/                           |
