6e2d0cc0fd3cad17de9c7700be4a4debc0c12f63
matthieu.aubry <matthieu.aubry@69d48449-a624-0410-ae76-85ddf3b22ea5>
2009-05-26 06:58:20 +0000

    [video] adding unversionned files

---
+++ plugins_repo/openXVideoAds/plugins/etc/vastInlineBannerTypeHtml/etc/DataObjects/Stats_vast.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Table Definition for Stats_vast
+ */
+require_once MAX_PATH.'/lib/max/Dal/DataObjects/DB_DataObjectCommon.php';
+
+class DataObjects_Stats_vast extends DB_DataObjectCommon 
+{
+    ###START_AUTOCODE
+    /* the code below is auto generated do not remove the above tag */
+
+    public $__table = 'stats_vast';                 // table name
+    public $interval_start;                  // DATETIME() => openads_datetime => 142 
+    public $creative_id;                     // MEDIUMINT(20) => openads_mediumint => 129 
+    public $zone_id;                         // MEDIUMINT(20) => openads_mediumint => 129 
+    public $vast_event_id;                   // MEDIUMINT(20) =>
+    public $count;                           // INT(11) => openads_int => 129 
+
+    /* Static get */
+    function staticGet($k,$v=NULL) { return DB_DataObject::staticGet('DataObjects_Stats_vast',$k,$v); }
+
+    var $defaultValues = array(
+                'interval_start' => '%NO_DATE_TIME%',
+                'vast_event_id' => 0,
+                'count' => 0,
+                );
+
+    /* the code above is auto generated do not remove the tag below */
+    ###END_AUTOCODE
+}
+?>
