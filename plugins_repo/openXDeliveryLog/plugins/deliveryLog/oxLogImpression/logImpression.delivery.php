8e4c5d7c5e2e053ee37e0dabf8513af00462f38d
andrew.hill <andrew.hill@69d48449-a624-0410-ae76-85ddf3b22ea5>
2009-06-15 12:44:21 +0000

    Fixes OXPL-340.

--- plugins_repo/openXDeliveryLog/plugins/deliveryLog/oxLogImpression/logImpression.delivery.php
+++ plugins_repo/openXDeliveryLog/plugins/deliveryLog/oxLogImpression/logImpression.delivery.php
@@ -30,7 +30,7 @@ $Id$
  * @subpackage openxDeliveryLog
  */
 
-function Plugin_deliveryLog_OxLogImpression_LogImpression_Delivery_logImpression($adId = 0, $zoneId = 0, $aAd = array(), $okToLog = true)
+function Plugin_deliveryLog_OxLogImpression_LogImpression_Delivery_logImpression($adId = 0, $zoneId = 0, $okToLog = true)
 {
     if (!$okToLog) { return false; }
     $aData = $GLOBALS['_MAX']['deliveryData'];
