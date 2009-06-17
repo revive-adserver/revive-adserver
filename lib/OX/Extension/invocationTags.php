2674120b60a69862a317906306e4eb72101420a4
chris.nutting <chris.nutting@69d48449-a624-0410-ae76-85ddf3b22ea5>
2009-06-17 19:19:36 +0000

    Fixed OX-5538 Added invocationTags.php to cause the deliveryHooks to be updated when enabling/disabling an invocationTags plugin

---
+++ lib/OX/Extension/invocationTags.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
++---------------------------------------------------------------------------+
+| OpenX v2.8                                                                |
+| ==========                                                                |
+|                                                                           |
+| Copyright (c) 2003-2009 OpenX Limited                                     |
+| For contact details, see: http://www.openx.org/                           |
+|                                                                           |
+| This program is free software; you can redistribute it and/or modify      |
+| it under the terms of the GNU General Public License as published by      |
+| the Free Software Foundation; either version 2 of the License, or         |
+| (at your option) any later version.                                       |
+|                                                                           |
+| This program is distributed in the hope that it will be useful,           |
+| but WITHOUT ANY WARRANTY; without even the implied warranty of            |
+| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             |
+| GNU General Public License for more details.                              |
+|                                                                           |
+| You should have received a copy of the GNU General Public License         |
+| along with this program; if not, write to the Free Software               |
+| Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA |
++---------------------------------------------------------------------------+
+$Id: deliveryAdRender.php 35158 2009-04-15 00:42:08Z grig.gheorghiu $
+*/
+
+require_once(LIB_PATH.'/Extension/ExtensionDelivery.php');
+
+/**
+ * @package    OpenXExtension
+ * @subpackage invocationTags
+ */
+class OX_Extension_invocationTags extends OX_Extension_Delivery
+{
+
+    function __construct()
+    {
+
+    }
+
+}
+
+?>
