9048b9dfa0ffa3798877bd0176961c56d20cf6c7
david.keen <david.keen@69d48449-a624-0410-ae76-85ddf3b22ea5>
2009-07-15 18:33:05 +0000

    Added migrateBucketData script.

---
+++ lib/OX/Maintenance/Statistics/MigrateBucketData.php
@@ -0,0 +1,137 @@
+<?php
+
+require_once LIB_PATH . '/Plugin/Component.php';
+require_once LIB_PATH . '/Plugin/PluginManager.php';
+require_once LIB_PATH . '/Dal/Maintenance/Statistics/Factory.php';
+require_once LIB_PATH . '/OperationInterval.php';
+require_once OX_PATH . '/lib/OA/Dal.php';
+require_once OX_PATH . '/lib/OA/DB/Table.php';
+
+
+/**
+ * A class that implements the functions used by the migrateBucketData.php script.
+ *
+ * @package    OpenXMaintenance
+ * @subpackage Tools
+ * @author     David Keen <david.keen@openx.org>
+ */
+class OX_Maintenance_Statistics_MigrateBucketData
+{
+    /**
+     * Connection to the database.
+     *
+     * @var MDB2_Driver_Common
+     */
+    public $oDbh;
+    private $aPackages;
+
+    function __construct()
+    {
+        // Get a connection to the datbase
+        $this->oDbh =& OA_DB::singleton();
+
+        // Locate all plugins (packages) that have been installed
+        $oPluginManager = new OX_PluginManager();
+        $this->aPackages = $oPluginManager->getPackagesList();
+    }
+
+    /**
+     * Summarise the data from the bucket tables into the stats tables.
+     *
+     * @param <type> $oStartDate
+     * @param <type> $oEndDate
+     */
+    public function summarise($oStartDate, $oEndDate)
+    {
+//        $oComponent =& OX_Component::factory('deliveryLog', 'oxLogCountry', 'logImpressionCountry');
+//        $statisticsTableName = $GLOBALS['_MAX']['CONF']['table']['prefix'] .
+//            $oComponent->getStatisticsName();
+//        $aMigrationMaps = array(
+//            $oComponent->getStatisticsMigration());
+
+        $aRunComponents = $this->prepareMaps($this->locateComponents());
+
+        // TODO: make it work for a range of dates
+        $aDates = array(
+            'start' => $oStartDate,
+            'end' => $oEndDate
+        );
+
+       // Prepare the DAL object
+        $oFactory = new OX_Dal_Maintenance_Statistics_Factory();
+        $oDalMaintenanceStatistics = $oFactory->factory();
+
+        $oNowDate = new Date();
+
+        foreach ($aRunComponents as $statisticsTable => $aMaps) {
+            $result = $oDalMaintenanceStatistics->summariseBucketsAggregate($statisticsTable, $aMaps, $aDates);
+        }
+    }
+
+    /**
+     * A private method to locate and instantiate all components that
+     * use the deliveryLog extension, and are in installed plugins, storing
+     * them in an array, grouped by the component's destination statistics
+     * table -- this will provide all of the plugin components that use
+     * the deliveryLog extension (i.e. have bucket-based delivery data that
+     * needs to be migrated to statistics tables), grouped by the detsination
+     * statistics tables (so that all components that have data migrated to
+     * the same table can be migrated in the one call).
+     *
+     * @access private
+     * @return array An array of plugins components, as described above.
+     */
+    private function locateComponents()
+    {
+        $aSummariseComponents = array();
+        foreach ($this->aPackages as $aPluginInfo) {
+            foreach ($aPluginInfo['contents'] as $aContents) {
+                if ($aContents['extends'] == 'deliveryLog') {
+                    foreach ($aContents['components'] as $aComponent) {
+                        $oComponent =& OX_Component::factory('deliveryLog', $aContents['name'], $aComponent['name']);
+                        if ($oComponent->enabled) {
+                            $destinationTable = $oComponent->getStatisticsTableName();
+                            $aSummariseComponents[$destinationTable][get_class($oComponent)] = $oComponent;
+                        }
+                    }
+                }
+            }
+        }
+        return $aSummariseComponents;
+    }
+
+    /**
+     * A private method to take the output of the
+     * OX_Maintenance_Statistics_Task_MigrateBucketData::_locateComponents()
+     * method, and convert it into an array of migration maps suitable for the
+     * required migration type.
+     *
+     * NOTE: Only handles aggregate bucket types.
+     *
+     * @access private
+     * @param array $aSummariseComponents An array of components from the
+     *                                    OX_Maintenance_Statistics_Task_MigrateBucketData::_locateComponents()
+     *                                    method.
+     * @return array An array of migration maps.
+     */
+    private function prepareMaps($aSummariseComponents)
+    {
+        $aRunComponents = array();
+
+        foreach ($aSummariseComponents as $statisticsTable => $aComponents) {
+            foreach ($aComponents as $oComponent) {
+                $aMap = $oComponent->getStatisticsMigration();
+                if (!$oComponent->testStatisticsMigration($aMap)) {
+                    continue;
+                }
+                if ($aMap['method'] == 'aggregate') {
+                    // Nice! We can migrate aggregate data
+                    $aRunComponents[$statisticsTable][get_class($oComponent)] = $aMap;
+                }
+            }
+        }
+        return $aRunComponents;
+    }
+
+
+}
---
+++ scripts/maintenance/tools/migrateBucketData.php
@@ -0,0 +1,110 @@
+<?php
+
+$scriptName = $argv[0];
+
+/**
+ * The start and end date/times of the first and last operation intervals to
+ * operate over must be defined in this script before running. For example,
+ * with a standard operation interval of 60 minutes, if INTERVAL_START is set
+ * to"2009-02-10 12:00:00" and INTERVAL_END is set to "2009-02-11 10:59:59",
+ * then all of the data between "2009-02-10 12:00:00" and
+ * "2009-02-11 10:59:59" will be inspected and corrected as required.
+ *
+ * The define statements below will need to be un-commented for the script
+ * to be able to be run!
+ */
+define('INTERVAL_START', '2009-07-15 00:00:00');
+define('INTERVAL_END',   '2009-07-15 00:59:59');
+
+// Initialise the OpenX environment....
+$path = dirname(__FILE__);
+require_once $path . '/../../../init.php';
+
+// Required files
+require_once LIB_PATH . '/OperationInterval.php';
+require_once LIB_PATH . '/Maintenance/Statistics/MigrateBucketData.php';
+require_once OX_PATH . '/lib/pear/Date.php';
+
+// Standard messages
+$haltMessage = "\nThe {$scriptName} script will NOT be run.\n\n";
+
+if ($argc !== 2) {
+    echo "
+Requires the hostname to be passed in as a string, as per the standard maintenance CLI script.
+";
+    echo $haltMessage;
+    exit;
+}
+
+if (!defined('INTERVAL_START') || !defined('INTERVAL_END')) {
+    echo "
+Please ensure that you have read the comments in the {$scriptName} script.
+
+You will also find out how to make this script work by reading the comments. :-)
+";
+    echo $haltMessage;
+    exit;
+}
+
+// Initialise the script
+OX::disableErrorHandling();
+$result = OX_OperationInterval::checkOperationIntervalValue(OX_OperationInterval::getOperationInterval());
+OX::enableErrorHandling();
+if (PEAR::isError($result)) {
+    $message = "\nThe operation interval in your OpenX configuration file is not valid. Please see the OpenX\ndocumentation for more details on valid operation interval values.\n";
+    echo $message;
+    echo $haltMessage;
+    exit;
+}
+
+$oStartDate = new Date(INTERVAL_START);
+$result = OX_OperationInterval::checkDateIsStartDate($oStartDate);
+if (!$result) {
+    $message = "\nThe start date defined in the {$scriptName} script is not a valid operation interval start date.\nPlease edit the statisticsTestAndCorrect.php script before running.\n";
+    echo $message;
+    echo $haltMessage;
+    exit;
+}
+
+$oEndDate   = new Date(INTERVAL_END);
+$result = OX_OperationInterval::checkDateIsEndDate($oEndDate);
+if (!$result) {
+    $message = "\nThe end date defined in the {$scriptName} script is not a valid operation interval start date.\nPlease edit the statisticsTestAndCorrect.php script before running.\n";
+    echo $message;
+    echo $haltMessage;
+    exit;
+}
+
+$oMigrateBucketData = new OX_Maintenance_Statistics_MigrateBucketData();
+if (PEAR::isError($oMigrateBucketData->oDbh)) {
+    $message = "\nUnable to connect to the OpenX database.\n";
+    echo $message;
+    echo $haltMessage;
+    exit;
+}
+
+// Check date range?
+
+// Advise the user of the operations that will be performed, and ask for
+// permission before running.
+echo "
+The {$scriptName} script will summarise the aggegate bucket data in your
+OpenX database for all operation intervals between '" . $oStartDate->format('%Y-%m-%d %H:%M:%S') . "' and '" . $oEndDate->format('%Y-%m-%d %H:%M:%S') . "'.
+
+Please ensure that you have read the comments in the {$scriptName} script.
+
+Depending on the speed of your database, this may take a long time to run. You may want to temporarily
+disable your central maintenance script while this script runs.
+
+Do you want to proceed with the summarisation process? [y/N]: ";
+
+$response = trim(fgets(STDIN));
+if (!($response == 'y' || $response == 'Y')) {
+    echo $haltMessage;
+    exit;
+}
+echo "\n";
+
+// For each OA in the range
+
+$oMigrateBucketData->summarise($oStartDate, $oEndDate);
