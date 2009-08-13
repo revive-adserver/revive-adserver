a119774c05fd2bd610d1c9c4ae314913358fd9c4
matteo.beccati <matteo.beccati@69d48449-a624-0410-ae76-85ddf3b22ea5>
2009-08-13 10:04:06 +0000

    OX-4928 - Fixed failing tests

--- lib/max/Dal/Admin/tests/unit/Clients.dal.test.php
+++ lib/max/Dal/Admin/tests/unit/Clients.dal.test.php
@@ -178,7 +178,7 @@ class MAX_Dal_Admin_ClientsTest extends DalUnitTestCase
         // Test 3
         $aClients = $this->dalClients->getAdvertiserDetails(2);
         $this->assertTrue(is_array($aClients));
-        $this->assertEqual(count($aClients), 16);
+        $this->assertEqual(count($aClients), 15);
         $this->assertEqual($aClients['clientid'], 2);
         $this->assertTrue(array_key_exists('agencyid', $aClients));
         $this->assertTrue(array_key_exists('clientname', $aClients));
