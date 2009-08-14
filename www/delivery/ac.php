d5f1a5ef713be6cd8ba55ee662b3bd40caa08800
post_commit <post_commit@69d48449-a624-0410-ae76-85ddf3b22ea5>
2009-08-14 10:30:21 +0000

    Commit of auto-generated delivery files.

--- www/delivery/ac.php
+++ www/delivery/ac.php
@@ -842,6 +842,8 @@ z.block AS block_zone,
 z.capping AS cap_zone,
 z.session_capping AS session_cap_zone,
 z.ext_adselection AS ext_adselection,
+z.affiliateid AS publisher_id,
+a.agencyid AS agency_id,
 a.account_id AS trafficker_account_id,
 m.account_id AS manager_account_id
 FROM
