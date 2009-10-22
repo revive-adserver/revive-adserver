9275c40314443560e1c02e0d4295f4c815b494f2
matthieu.aubry <matthieu.aubry@69d48449-a624-0410-ae76-85ddf3b22ea5>
2009-10-22 10:00:08 +0000

    Fixes OX-5798 the UI was not showing the right probabilities for ECPM remnant campaigns

--- www/admin/zone-probability.php
+++ www/admin/zone-probability.php
@@ -272,7 +272,8 @@ function phpAds_showZoneBanners ($zoneId)
             echo "</tr>";
             echo "<tr height='1'><td colspan='5' bgcolor='#888888'><img src='" . OX::assetPath() . "/images/break.gif' height='1' width='100%'></td></tr>";
 
-            foreach($aZoneLinkedAds['eAds'] as $adId => $aLinkedAd) {
+            $do = OA_Dal::factoryDO('Campaigns');
+            foreach($aZoneLinkedAds['eAds'][DataObjects_Campaigns::PRIORITY_ECPM] as $adId => $aLinkedAd) {
                 $name = phpAds_getBannerName ($adId, 60, false);
                 echo "<tr height='1'><td colspan='5' bgcolor='#888888'><img src='" . OX::assetPath() . "/images/break-l.gif' height='1' width='100%'></td></tr>";
                 echo "<tr height='25' ".($i%2==0?"bgcolor='#F6F6F6'":"").">";
