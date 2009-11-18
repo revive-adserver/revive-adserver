1f8277eaa334378c1a0ecba1b43316f58ae3645a
post_commit <post_commit@69d48449-a624-0410-ae76-85ddf3b22ea5>
2009-11-18 18:08:44 +0000

    Commit of auto-generated delivery files.

--- www/delivery/alocal.php
+++ www/delivery/alocal.php
@@ -630,9 +630,10 @@ break;
 }
 if (!empty($ip)) {
 // The "remote IP" may be a list, ensure that
-// only the last item is used in that case
-$ip = explode(',', $ip);
-$ip = trim($ip[count($ip) - 1]);
+// only the FIRST non-private value is used in that case
+// See http://en.wikipedia.org/wiki/X-Forwarded-For#Format
+foreach (explode(',', $ip) as $ip) {
+$ip = trim($ip);
 // If the found address is not unknown or a private network address
 if (($ip != 'unknown') && (!MAX_remotehostPrivateAddress($ip))) {
 // Set the "real" remote IP address, and unset
@@ -642,6 +643,8 @@ if (($ip != 'unknown') && (!MAX_remotehostPrivateAddress($ip))) {
 $_SERVER['REMOTE_ADDR'] = $ip;
 $_SERVER['REMOTE_HOST'] = '';
 $_SERVER['HTTP_VIA']    = '';
+break;
+}
 }
 }
 }
