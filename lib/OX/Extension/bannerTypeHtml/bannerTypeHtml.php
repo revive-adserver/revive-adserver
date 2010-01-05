c1f3e301ee9a93c0a26321950e1cfd2d925201b5
miguel.correa <miguel.correa@69d48449-a624-0410-ae76-85ddf3b22ea5>
2010-01-05 18:00:03 +0000

    Increased the size of the dropdown Alter HTML to enable click tracking for on Banner properties tab.

--- lib/OX/Extension/bannerTypeHtml/bannerTypeHtml.php
+++ lib/OX/Extension/bannerTypeHtml/bannerTypeHtml.php
@@ -88,7 +88,7 @@ class Plugins_BannerTypeHTML extends OX_Component
                 'class' =>'code', 'cols'=>'45', 'rows'=>'10', 'wrap'=>'off',
                 'dir' => 'ltr', 'style'=>'width:550px;'
             ));
-        $aSelectAttributes = array('id'=>'adserver', 'style' => 'margin-left: 15px;width:115px');
+        $aSelectAttributes = array('id'=>'adserver', 'style' => 'margin-left: 15px;width:230px');
         $htmlG['select'] = HTML_QuickForm::createElement('select', 'adserver', $GLOBALS['strAlterHTML'], $adPluginsList, $aSelectAttributes);
         $form->addGroup($htmlG, 'html_banner_g', null, array("<br>", ""), false);
 
