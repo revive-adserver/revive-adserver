2ca1c95b060ec8941d5d2ae81b8c9a0666860b50
michael.lum <michael.lum@69d48449-a624-0410-ae76-85ddf3b22ea5>
2010-05-26 00:51:22 +0000

    OX-5979. changing XMLRPC library to LGPL.

--- lib/xmlrpc/java/ApacheLib3/org/openads/proxy/CampaignService.java
+++ lib/xmlrpc/java/ApacheLib3/org/openads/proxy/CampaignService.java
@@ -3,20 +3,20 @@
 | OpenX v${RELEASE_MAJOR_MINOR}                                                                |
 | ======${RELEASE_MAJOR_MINOR_DOUBLE_UNDERLINE}                                                                 |
 |                                                                           |
-| Copyright (c) 2003-2009 OpenX Limited                                     |
+| Copyright (c) 2003-2010 OpenX Limited                                     |
 | For contact details, see: http://www.openx.org/                           |
 |                                                                           |
 | This program is free software; you can redistribute it and/or modify      |
-| it under the terms of the GNU General Public License as published by      |
-| the Free Software Foundation; either version 2 of the License, or         |
+| it under the terms of the GNU Lesser General Public License as published  |
+| by the Free Software Foundation; either version 2 of the License, or      |
 | (at your option) any later version.                                       |
 |                                                                           |
 | This program is distributed in the hope that it will be useful,           |
 | but WITHOUT ANY WARRANTY; without even the implied warranty of            |
 | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             |
-| GNU General Public License for more details.                              |
+| GNU Lesser General Public License for more details.                       |
 |                                                                           |
-| You should have received a copy of the GNU General Public License         |
+| You should have received a copy of the GNU Lesser General Public License  |
 | along with this program; if not, write to the Free Software               |
 | Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA |
 +---------------------------------------------------------------------------+
@@ -33,7 +33,6 @@ import org.apache.xmlrpc.client.XmlRpcClient;
 /**
  * Base class for all campaign web service tests.
  *
- * @author     Andriy Petlyovanyy <apetlyovanyy@lohika.com>
  */
 class CampaignService extends AbstractService {
 	private static final String CAMPAIGN_SERVICE = "CampaignXmlRpcService.php";
