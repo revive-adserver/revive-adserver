7b0737005626d09a7abad21f47db180b50b9cb5a
chris.nutting <chris.nutting@69d48449-a624-0410-ae76-85ddf3b22ea5>
2012-05-08 04:55:24 +0000

    Added (empty and unused, but required) $value to the _showHiddenField() call

--- lib/OA/Admin/Option.php
+++ lib/OA/Admin/Option.php
@@ -567,7 +567,7 @@ class OA_Admin_Option
             }
             $this->_showEndSection($sectionHasRequiredField);
         }
-        $this->_showHiddenField(array('name' => 'token', 'value' => phpAds_SessionGetToken()));
+        $this->_showHiddenField(array('name' => 'token', 'value' => phpAds_SessionGetToken()), '');
 
         if (OA_INSTALLATION_STATUS == OA_INSTALLATION_STATUS_INSTALLED)
         {
