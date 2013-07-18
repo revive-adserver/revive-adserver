1c71d83edfa527dcb8d097718d4464c5d330ecd9
Andrew Hill <andrew@fornax.net>
2013-07-18 21:18:49 +0100

    Updated file headers. #2

--- lib/max/Dal/DataObjects/Account_user_permission_assoc.php
+++ lib/max/Dal/DataObjects/Account_user_permission_assoc.php
@@ -1,4 +1,15 @@
 <?php
+
+/*
++---------------------------------------------------------------------------+
+| Revive Adserver                                                           |
+| http://www.revive-adserver.com                                            |
+|                                                                           |
+| Copyright: See the COPYRIGHT.txt file.                                    |
+| License: GPLv2 or later, see the LICENSE.txt file.                        |
++---------------------------------------------------------------------------+
+*/
+
 /**
  * Table Definition for account_preference_assoc
  */
