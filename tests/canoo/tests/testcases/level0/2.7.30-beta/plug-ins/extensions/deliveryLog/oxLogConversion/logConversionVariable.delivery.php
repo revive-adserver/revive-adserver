8c5931d3c5d8c9531642487b048e759377ae6a9c
Andrew Hill <andrew@fornax.net>
2013-07-19 22:04:02 +0100

    Updated file headers. #2

--- tests/canoo/tests/testcases/level0/2.7.30-beta/plug-ins/extensions/deliveryLog/oxLogConversion/logConversionVariable.delivery.php
+++ tests/canoo/tests/testcases/level0/2.7.30-beta/plug-ins/extensions/deliveryLog/oxLogConversion/logConversionVariable.delivery.php
@@ -2,27 +2,12 @@
 
 /*
 +---------------------------------------------------------------------------+
-| OpenX v${RELEASE_MAJOR_MINOR}                                                                |
-| =======${RELEASE_MAJOR_MINOR_DOUBLE_UNDERLINE}                                                                |
+| Revive Adserver                                                           |
+| http://www.revive-adserver.com                                            |
 |                                                                           |
-| Copyright (c) 2003-2009 OpenX Limited                                     |
-| For contact details, see: http://www.openx.org/                           |
-|                                                                           |
-| This program is free software; you can redistribute it and/or modify      |
-| it under the terms of the GNU General Public License as published by      |
-| the Free Software Foundation; either version 2 of the License, or         |
-| (at your option) any later version.                                       |
-|                                                                           |
-| This program is distributed in the hope that it will be useful,           |
-| but WITHOUT ANY WARRANTY; without even the implied warranty of            |
-| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             |
-| GNU General Public License for more details.                              |
-|                                                                           |
-| You should have received a copy of the GNU General Public License         |
-| along with this program; if not, write to the Free Software               |
-| Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA |
+| Copyright: See the COPYRIGHT.txt file.                                    |
+| License: GPLv2 or later, see the LICENSE.txt file.                        |
 +---------------------------------------------------------------------------+
-$Id: logConversionVariable.delivery.php 30820 2009-01-13 19:02:17Z andrew.hill $
 */
 
 /**
@@ -43,9 +28,9 @@ MAX_Dal_Delivery_Include();
  */
 function Plugin_deliveryLog_oxLogConversion_logConversionVariable_Delivery_logConversionVariable($aVariables, $trackerId, $serverConvId, $serverRawIp)
 {
-    // Initiate the connection to the database (before using mysql_real_escape_string) 
+    // Initiate the connection to the database (before using mysql_real_escape_string)
  	OA_Dal_Delivery_connect('rawDatabase');
- 	
+
  	$table = $GLOBALS['_MAX']['CONF']['table']['prefix'] . 'data_bkt_a_var';
 
     if (empty($GLOBALS['_MAX']['NOW'])) {
