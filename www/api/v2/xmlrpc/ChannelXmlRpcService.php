fd6524ad95f6dcaf00b47d44b07e4431fa84b3f7
Andrew Hill <andrew@fornax.net>
2013-07-20 00:17:23 +0100

    Updated file headers. #2

--- www/api/v2/xmlrpc/ChannelXmlRpcService.php
+++ www/api/v2/xmlrpc/ChannelXmlRpcService.php
@@ -2,27 +2,12 @@
 
 /*
 +---------------------------------------------------------------------------+
-| OpenX v${RELEASE_MAJOR_MINOR}                                             |
-| =======${RELEASE_MAJOR_MINOR_DOUBLE_UNDERLINE}                            |
+| Revive Adserver                                                           |
+| http://www.revive-adserver.com                                            |
 |                                                                           |
-| Copyright (c) 2003-2009 OpenX Limited                                     |
-| For contact details, see: http://www.openx.org/                           |
-|                                                                           |
-| This program is free software; you can redistribute it and/or modify      |
-| it under the terms of the GNU General Public License as published by      |
-| the Free Software Foundation; either version 2 of the License, or         |
-| (at your option) any later version.                                       |
-|                                                                           |
-| This program is distributed in the hope that it will be useful,           |
-| but WITHOUT ANY WARRANTY; without even the implied warranty of            |
-| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             |
-| GNU General Public License for more details.                              |
-|                                                                           |
-| You should have received a copy of the GNU General Public License         |
-| along with this program; if not, write to the Free Software               |
-| Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA |
+| Copyright: See the COPYRIGHT.txt file.                                    |
+| License: GPLv2 or later, see the LICENSE.txt file.                        |
 +---------------------------------------------------------------------------+
-$Id$
 */
 
 /**
@@ -45,7 +30,7 @@ require_once MAX_PATH . '/lib/OA/Dll/Channel.php';
  */
 class ChannelXmlRpcService extends BaseChannelService
 {
-    
+
     /**
      * Adds details for a new channel to the channel
      * object and returns either the channel ID or an error message.
@@ -223,7 +208,7 @@ class ChannelXmlRpcService extends BaseChannelService
             return XmlRpcUtils::generateError($this->_oChannelServiceImp->getLastError());
         }
     }
-    
+
     /**
      * This method return targeting limitations for channel.
      *
@@ -243,7 +228,7 @@ class ChannelXmlRpcService extends BaseChannelService
         }
 
         $aTargeting = null;
-        if ($this->_oChannelServiceImp->getChannelTargeting($sessionId, 
+        if ($this->_oChannelServiceImp->getChannelTargeting($sessionId,
             $channelId, $aTargeting)) {
 
             return XmlRpcUtils::getArrayOfEntityResponse($aTargeting);
@@ -253,7 +238,7 @@ class ChannelXmlRpcService extends BaseChannelService
             return XmlRpcUtils::generateError($this->_oChannelServiceImp->getLastError());
         }
     }
-    
+
     /**
      * This method sets targeting limitations for a channel.
      * It overrides existing limitations.
@@ -269,15 +254,15 @@ class ChannelXmlRpcService extends BaseChannelService
         $oResponseWithError = null;
         $aTargeting = array();
         if (!XmlRpcUtils::getScalarValues(array(&$sessionId, &$channelId),
-            array(true, true), $oParams, $oResponseWithError) || 
-            !XmlRpcUtils::getArrayOfStructuresScalarFields($aTargeting, 
-                'OA_Dll_TargetingInfo', $oParams, 2, array('logical', 'type', 
+            array(true, true), $oParams, $oResponseWithError) ||
+            !XmlRpcUtils::getArrayOfStructuresScalarFields($aTargeting,
+                'OA_Dll_TargetingInfo', $oParams, 2, array('logical', 'type',
                     'comparison', 'data'), $oResponseWithError)) {
 
             return $oResponseWithError;
         }
 
-        if ($this->_oChannelServiceImp->setChannelTargeting($sessionId, 
+        if ($this->_oChannelServiceImp->setChannelTargeting($sessionId,
             $channelId, $aTargeting)) {
 
             return XmlRpcUtils::booleanTypeResponse(true);
