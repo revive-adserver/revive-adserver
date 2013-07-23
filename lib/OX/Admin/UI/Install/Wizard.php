1652c5a370917ad4ad8126192c8ea5d9b7f4aab7
Andrew Hill <andrew@fornax.net>
2013-07-23 22:50:23 +0100

    Started removing old OpenX Market plugin code from the installer. The installer no longer asks for an SSO registration, and the OpenX Market plugin fails to install. However, so do some other plugins. Need to re-test installation to check it was not a result of old plugins being present, and need to update code to not try to install the OpenX Market plugin at all. #8

--- lib/OX/Admin/UI/Install/Wizard.php
+++ lib/OX/Admin/UI/Install/Wizard.php
@@ -11,31 +11,30 @@
 */
 
 require_once MAX_PATH . '/lib/OX/Admin/UI/Wizard.php';
-require_once MAX_PATH . '/lib/OX/Dal/Market/MarketPluginTools.php';
 
 /**
  * @package OX_Admin_UI
  * @subpackage Install
- * @author Bernard Lange <bernard@openx.org> 
+ * @author Bernard Lange <bernard@openx.org>
  */
 class OX_Admin_UI_Install_Wizard
-    extends OX_Admin_UI_Wizard  
+    extends OX_Admin_UI_Wizard
 {
     public function __construct($oInstallStatus, $currentStepId = null)
     {
-        $oStorage = OX_Admin_UI_Install_InstallUtils::getSessionStorage();        
+        $oStorage = OX_Admin_UI_Install_InstallUtils::getSessionStorage();
         $aSteps = $this->initSteps($oInstallStatus, $oStorage);
-        
+
         $aParams = array(
-                'steps' => $aSteps['steps'],      
+                'steps' => $aSteps['steps'],
                 'stepsMetadata' => $aSteps['meta'],
                 'current' => $currentStepId,
-        ); 
-        
+        );
+
         parent::__construct('install', $aParams, $oStorage);
     }
-    
-    
+
+
     /**
      * Builds step array. Uses information from InstallStatus (eg. if it's upgrade
      * or install) and also storage properties 'isMarketStepVisible', 'isLoginStepVisible'
@@ -47,56 +46,49 @@ class OX_Admin_UI_Install_Wizard
     protected function initSteps($oStatus, $oStorage)
     {
         $aMeta = array();
-        
+
         if ($oStatus->isRecovery()) {
             $aSteps = array(
                 'recovery' => 'Recovery',
-                'finish' => 'Finish' 
+                'finish' => 'Finish'
             );
         }
         if ($oStatus->isInstall()) {
             $aSteps = array(
                 'welcome' => 'Welcome',
-                'register' => 'Registration',
-                'database' => 'Database', 
-                'configuration' => 'Configuration', 
-                'finish' => 'Finish' 
+                'database' => 'Database',
+                'configuration' => 'Configuration',
+                'finish' => 'Finish'
             );
             $aMeta = array(
-                'finish' => array('secured' => true) 
+                'finish' => array('secured' => true)
             );
         }
         else if ($oStatus->isUpgrade()) {
             $aSteps = array(
                 'welcome' => 'Welcome',
                 'login' => 'Administrator Login',
-                'database' => 'Database', 
-                'configuration' => 'Configuration', 
-                'register' => 'Registration',
-                'finish' => 'Finish' 
+                'database' => 'Database',
+                'configuration' => 'Configuration',
+                'finish' => 'Finish'
             );
 
             $aMeta = array(
                 'welcome' => array('secured' => false),
                 'login' => array('secured' => false),
-                'register' => array('secured' => true),
-                'database' => array('secured' => true), 
+                'database' => array('secured' => true),
                 'configuration' => array('secured' => true),
-                'finish' => array('secured' => true) 
+                'finish' => array('secured' => true)
             );
 
-            
+
             //hide steps which are not required
-            if (!$oStorage->get('isMarketStepVisible')) {
-                unset($aSteps['register']);
-                unset($aMeta['register']);
-            }
             if (!$oStorage->get('isLoginStepVisible')) {
-                unset($aSteps['login']); 
+                unset($aSteps['login']);
                 unset($aMeta['login']);
             }
             if (!$oStorage->get('isConfigStepVisible')) {
-                unset($aSteps['configuration']); 
+                unset($aSteps['configuration']);
                 unset($aMeta['configuration']);
             }
         }
