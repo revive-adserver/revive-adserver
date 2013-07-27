39674eee689e050878986e5acb8a5cf0c3b13668
Andrew Hill <andrew@fornax.net>
2013-07-27 23:05:22 +0100

    Removed deprecated widgets; updated code to load widgets correctly from constructed, instead of hard-coded, URL for the local Revive installation. #24

--- lib/OA/Dashboard/Widgets/Grid.php
+++ lib/OA/Dashboard/Widgets/Grid.php
@@ -28,6 +28,7 @@ class OA_Dashboard_Widget_Grid extends OA_Dashboard_Widget
     {
         $oTpl = new OA_Admin_Template('dashboard/grid.html');
 
+        $oTpl->assign('dashboardURL', MAX::constructURL(MAX_URL_ADMIN, 'dashboard.php'));
         $oTpl->assign('cssURL', OX::assetPath() . "/css");
         $oTpl->assign('imageURL', OX::assetPath() . "/images");
         $oTpl->assign('jsURL', OX::assetPath() . "/js");
