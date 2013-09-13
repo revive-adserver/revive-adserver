f9a63498b5378eff33c80b1e2d82298bc654838f
Matteo Beccati <matteo@beccati.com>
2013-09-13 13:52:26 +0200

    Compile acls using var_export (vs addslashes)

--- lib/OX/Extension/deliveryLimitations/DeliveryLimitations.php
+++ lib/OX/Extension/deliveryLimitations/DeliveryLimitations.php
@@ -302,7 +302,7 @@ class Plugins_DeliveryLimitations extends OX_Component
      */
     function compileData($data)
     {
-        return 'MAX_check' . ucfirst($this->group) . '_' . $this->component . "('".addslashes($data)."', '".addslashes($this->comparison)."')";
+        return 'MAX_check' . ucfirst($this->group) . '_' . $this->component . "(".var_export($data, true).", ".var_export($this->comparison, true).")";
     }
 
     /**
