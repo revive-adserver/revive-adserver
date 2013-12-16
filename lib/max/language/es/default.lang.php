e082f02831976b4bbd1dbe0c2b6106429289bf63
Juan Jose Pablos <juanjo@apertus.es>
2013-12-16 21:12:41 +0100

    Fix Wrong date format for Spanish

--- lib/max/language/es/default.lang.php
+++ lib/max/language/es/default.lang.php
@@ -20,11 +20,11 @@ $GLOBALS['phpAds_ThousandsSeperator']		= ',';
 
 
 // Date & time configuration
-$GLOBALS['date_format']						= "%m/%d/%Y";
+$GLOBALS['date_format']						= "%d/%m/%Y";
 $GLOBALS['time_format']						= "%H:%M:%S";
 $GLOBALS['minute_format']					= "%H:%M";
 $GLOBALS['month_format']					= "%m/%Y";
-$GLOBALS['day_format']						= "%m/%d";
+$GLOBALS['day_format']						= "%d/%m";
 $GLOBALS['week_format']						= "%W/%Y";
 $GLOBALS['weekiso_format']					= "%V/%G";
 
