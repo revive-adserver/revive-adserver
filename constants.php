f79e9be9eaf66d375c2a7967858de7f235caa059
Matteo Beccati <matteo@beccati.com>
2014-03-13 17:01:12 +0100

    Bumped version to 3.0.4-dev

--- constants.php
+++ constants.php
@@ -22,7 +22,7 @@
 function setupConstants()
 {
     // Define this version of Revive Adserver's constants
-    define('VERSION',           '3.0.3');
+    define('VERSION',           '3.0.4-dev');
     define('PRODUCT_NAME',      'Revive Adserver');
     define('PRODUCT_URL',       'www.revive-adserver.com');
     define('PRODUCT_DOCSURL',   'http://documentation.revive-adserver.com');
