31aaa285ee4abe48b3fe5781786085d33487613e
Frickelbruder <top@secret.in>
2014-04-22 14:07:57 +0200

    Changed unknown header Content-Size to valid HTTP- header Content-Length, since Content-Size forced apache to header Content-Encoding: chunked

--- plugins_repo/openXVideoAds/plugins/bannerTypeHtml/vastInlineBannerTypeHtml/vastInlineHtml.delivery.php
+++ plugins_repo/openXVideoAds/plugins/bannerTypeHtml/vastInlineBannerTypeHtml/vastInlineHtml.delivery.php
@@ -171,12 +171,12 @@ if ( !empty($format) && $format == 'vast'){
         $spc_output .=  getVastXMLFooter();
         // Setup the banners for this page
         MAX_commonSendContentTypeHeader("application/xml", $charset);
-        header("Content-Size: ".strlen($spc_output));
+        header("Content-Length: ".strlen($spc_output));
     }
     else {
         // Setup the banners for this page
         MAX_commonSendContentTypeHeader("application/x-javascript", $charset);
-        header("Content-Size: ".strlen($spc_output));
+        header("Content-Length: ".strlen($spc_output));
     }
     $spc_output .= getClientMessages();
     echo $spc_output;
