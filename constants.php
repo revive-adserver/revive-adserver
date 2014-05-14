c7af0d59e52a583d32bc264094ef29ac8ade609e
Andrew Hill <andrew@fornax.net>
2014-05-14 20:31:47 +0100

    Prepared release date & version ready for release.

--- constants.php
+++ constants.php
@@ -22,7 +22,7 @@
 function setupConstants()
 {
     // Define this version of Revive Adserver's constants
-    define('VERSION',           '3.0.5-dev');
+    define('VERSION',           '3.0.5');
     define('PRODUCT_NAME',      'Revive Adserver');
     define('PRODUCT_URL',       'www.revive-adserver.com');
     define('PRODUCT_DOCSURL',   'http://documentation.revive-adserver.com');
