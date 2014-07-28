5a210b6115c4983cf44f8e0e8fe4ca893551f07c
Andrew Hill <andrew@fornax.net>
2014-07-28 20:51:30 +0100

    Moved the sync process to run after the MSE and MPE processes are completed and logged. #373

--- lib/OX/Maintenance.php
+++ lib/OX/Maintenance.php
@@ -104,8 +104,10 @@ class OX_Maintenance
             // Run the Maintenance Statistics Engine (MSE) process
             $this->_runMSE();
             // Run the "midnight" tasks, if required
+            $runSync = false;
             if ($this->isMidnightMaintenance($oLastRun)) {
                 $this->_runMidnightTasks();
+                $runSync = true;
             }
             // Release lock before starting MPE
             $oLock->release();
@@ -122,6 +124,15 @@ class OX_Maintenance
                       ', taking ' . $oDateSpan->format('%H:%M:%S') .
                       ')', PEAR_LOG_INFO);
             OA::switchLogIdent();
+            // Run the Revive Adserver Sync process, to get details of any updates
+            // to Revive Adserver, if the process is due to be run (because the
+            // "midnight" tasks were triggered); this happens as the very last
+            // thing to ensure that network issues or timeouts with the sync server
+            // don't affect the normal running of the MSE & MPE, which are important
+            // to have run properly!
+            if ($runSync) {
+                $this->_runReviveSync();
+            }
         } else {
             OA::switchLogIdent('maintenance');
 			OA::debug('Maintenance Engine not run: could not acquire lock', PEAR_LOG_INFO);
@@ -210,7 +221,6 @@ class OX_Maintenance
         $this->_runGeneralPruning();
         $this->_runPriorityPruning();
         $this->_runDeleteUnverifiedAccounts();
-        $this->_runReviveSync();
         OA::debug('Midnight Maintenance Tasks Completed', PEAR_LOG_INFO);
     }
 
@@ -281,8 +291,6 @@ class OX_Maintenance
         $oSync->checkForUpdates(0);
         OA::debug('Finished ' . PRODUCT_NAME . ' Sync process.', PEAR_LOG_INFO);
     }
-
-
 
     function _runPriorityPruning()
     {
