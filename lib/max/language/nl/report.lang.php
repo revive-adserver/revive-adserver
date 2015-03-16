3e141767b63c40f1516a60ff2e5d78561aa92461
Post Commit <matteo@beccati.com>
2015-03-16 08:39:10 +0100

    Added new translation files

---
+++ lib/max/language/nl/report.lang.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+  +---------------------------------------------------------------------------+
+  | Revive Adserver                                                           |
+  | http://www.revive-adserver.com                                            |
+  |                                                                           |
+  | Copyright: See the COPYRIGHT.txt file.                                    |
+  | License: GPLv2 or later, see the LICENSE.txt file.                        |
+  +---------------------------------------------------------------------------+
+ */
+
+$GLOBALS['strAdvertiserCampaignHistoryReport'] = 'Rapport over campagnegeschiedenis';
+$GLOBALS['strAdvertiserCampaignHistoryDescription'] = 'Een uitsplitsing per dag van alle activiteit van een bepaalde campagne';
+$GLOBALS['strAdvertiserHistoryReport'] = 'Rapport over statistieken';
+$GLOBALS['strAdvertiserHistoryDescription'] = "Een uitsplitsing per dag over alle activiteiten van een bepaalde adverteerder";
+$GLOBALS['strAdvertiserKeywordHistoryReport'] = 'Keyword Campaign History Report';
+$GLOBALS['strAdvertiserKeywordHistoryDescription'] = "A detailed breakdown of a keyword campaign, including conversion tracking data";
+$GLOBALS['strAdvertiserTrackerHistoryReport'] = 'Rapport over tracking';
+$GLOBALS['strAdvertiserTrackerHistoryDescription'] = 'Een gedetailleerde uitsplitsing van conversie metingen.';
+$GLOBALS['strAdvertiserZoneAnalysisReport'] = 'Zone analyse';
+$GLOBALS['strAdvertiserZoneAnalysisDescription'] = 'Deze tabel toont de totale campagne activiteit, per site en zone.';
+$GLOBALS['strAdvertiserDayOfWeekAnalysisReport'] = 'Analyse per dag van de week';
+$GLOBALS['strAdvertiserDayOfWeekAnalysisDescription'] = 'Dit rapport toont gemiddelde campagneactiviteit per dag van de week.';
+$GLOBALS['strAdvertiserHourOfDayAnalysisReport'] = 'Analyse per uur van de dag';
+$GLOBALS['strAdvertiserHourOfDayAnalysisDescription'] = 'Deze tabel toont de totale campagneactiviteit per uur van de dag.';
+$GLOBALS['strAdvertiserCreativeSummaryReport'] = 'Samenvatting per creative';
+$GLOBALS['strAdvertiserCreativeSummaryDescription'] = 'Dit rapport is een samenvatting van de campagne per creative.';
+$GLOBALS['strAdvertiserDailyViewsAnalysisReport'] = 'Analyse van de dagelijkse impressies';
+$GLOBALS['strAdvertiserDailyViewsAnalysisDescription'] = 'Dit rapport toont een analyse van de totale activiteit uitgesplitst per dag.';
+$GLOBALS['strAdvertiserCampaignSummaryReport'] = 'Samenvatting van de campagne';
+$GLOBALS['strAdvertiserCampaignSummaryDescription'] = 'Samenvatting van de campagne';
+$GLOBALS['strAdvertiserCampaignAnalysisReport'] = 'Campagne analyse';
+$GLOBALS['strAdvertiserCampaignAnalysisDescription'] = 'Dit rapport bevat een samenvatting van de campagne per plaatsing.';
+$GLOBALS['strAdvertiserConversionTrackingAnalysisReport'] = 'Conversie Tracking analyse';
+$GLOBALS['strAdvertiserConversionTrackingAnalysisDescription'] = 'Dit rapport toont een overzicht van alle activiteiten van conversies.';
+
+$GLOBALS['strPublisherBannerAnalysisReport'] = 'Banner analyse';
+$GLOBALS['strPublisherCampaignAnalysisReport'] = 'Campagne analyse';
+$GLOBALS['strPublisherDayOfWeekAnalysisReport'] = 'Analyse per dag van de week';
+$GLOBALS['strPublisherHourOfDayAnalysisReport'] = 'Analyse per uur van de dag';
+$GLOBALS['strPublisherHourOfDayAnalysisDescription'] = 'Een rapport van de gemiddelde reclameactiviteiten per uur van de dag.';
+$GLOBALS['strPublisherRequestViewsAnalysisReport'] = 'Analyse van requests / impressies';
+$GLOBALS['strPublisherRequestViewsAnalysisDescription'] = 'Dit rapport toont een vergelijken tussen de totale aantallen requests en totale aantallen werkelijke impressies per datum.';
+$GLOBALS['strPublisherZoneAnalysisReport'] = 'Zone rapport';
+$GLOBALS['strPublisherZoneAnalysisDescription'] = 'Dit verslag toont de totale reclame-activiteiten uitgesplitst per zone.';
+
+$GLOBALS['strAgencyCampaignDeliveryReport'] = 'Campagne Uitleveringsrapport';
+
+
+
+// Error messages
