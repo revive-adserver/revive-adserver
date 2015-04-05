a11547c321abc5f6bf7d4a0e385b998e0cae622a
Matteo Beccati <matteo@beccati.com>
2015-04-05 11:10:59 +0200

    Cleaned up transaltions (again)

--- lib/max/language/en/invocation.lang.php
+++ lib/max/language/en/invocation.lang.php
@@ -1,73 +1,99 @@
 <?php
 
 /*
-  +---------------------------------------------------------------------------+
-  | Revive Adserver                                                           |
-  | http://www.revive-adserver.com                                            |
-  |                                                                           |
-  | Copyright: See the COPYRIGHT.txt file.                                    |
-  | License: GPLv2 or later, see the LICENSE.txt file.                        |
-  +---------------------------------------------------------------------------+
- */
++---------------------------------------------------------------------------+
+| Revive Adserver                                                           |
+| http://www.revive-adserver.com                                            |
+|                                                                           |
+| Copyright: See the COPYRIGHT.txt file.                                    |
+| License: GPLv2 or later, see the LICENSE.txt file.                        |
++---------------------------------------------------------------------------+
+*/
 
 // Other
-$GLOBALS['strCopyToClipboard'] = "Copy to clipboard";
-$GLOBALS['strCopy'] = "copy";
-$GLOBALS['strChooseTypeOfInvocation'] = "Please choose the type of invocation";
-$GLOBALS['strChooseTypeOfBannerInvocation'] = "Please choose the type of banner invocation";
+$GLOBALS['strCopyToClipboard']                      = "Copy to clipboard";
+$GLOBALS['strCopy']                                 = "copy";
+$GLOBALS['strChooseTypeOfInvocation']               = "Please choose the type of invocation";
+$GLOBALS['strChooseTypeOfBannerInvocation']         = "Please choose the type of banner invocation";
 
 // Measures
-$GLOBALS['strAbbrPixels'] = "px";
-$GLOBALS['strAbbrSeconds'] = "sec";
+$GLOBALS['strAbbrPixels']                           = "px";
+$GLOBALS['strAbbrSeconds']                          = "sec";
 
 // Common Invocation Parameters
-$GLOBALS['strInvocationWhat'] = "Banner selection";
-$GLOBALS['strInvocationCampaignID'] = "Campaign";
-$GLOBALS['strInvocationTarget'] = "Target frame";
-$GLOBALS['strInvocationSource'] = "Source";
-$GLOBALS['strInvocationWithText'] = "Show text below banner";
-$GLOBALS['strInvocationDontShowAgain'] = "Don't show the banner again on the same page";
-$GLOBALS['strInvocationDontShowAgainCampaign'] = "Don't show a banner from the same campaign again on the same page";
-$GLOBALS['strInvocationTemplate'] = "Store the banner inside a variable so it can be used in a template";
-$GLOBALS['strInvocationBannerID'] = "Banner ID";
-$GLOBALS['strInvocationComments'] = "Include comments";
+$GLOBALS['strInvocationWhat']                       = "Banner selection";
+$GLOBALS['strInvocationCampaignID']                 = "Campaign";
+$GLOBALS['strInvocationTarget']                     = "Target frame";
+$GLOBALS['strInvocationSource']                     = "Source";
+$GLOBALS['strInvocationWithText']                   = "Show text below banner";
+$GLOBALS['strInvocationDontShowAgain']              = "Don't show the banner again on the same page";
+$GLOBALS['strInvocationDontShowAgainCampaign']      = "Don't show a banner from the same campaign again on the same page";
+$GLOBALS['strInvocationTemplate']                   = "Store the banner inside a variable so it can be used in a template";
+$GLOBALS['strInvocationBannerID']                   = "Banner ID";
+$GLOBALS['strInvocationComments']                   = "Include comments";
 
 // Iframe
-$GLOBALS['strIFrameRefreshAfter'] = "Refresh after";
-$GLOBALS['strIframeResizeToBanner'] = "Resize iframe to banner dimensions";
-$GLOBALS['strIframeMakeTransparent'] = "Make the iframe transparent";
-$GLOBALS['strIframeIncludeNetscape4'] = "Include Netscape 4 compatible ilayer";
-$GLOBALS['strIframeGoogleClickTracking'] = "Include code to track Google AdSense clicks";
+$GLOBALS['strIFrameRefreshAfter']                   = "Refresh after";
+$GLOBALS['strIframeResizeToBanner']                 = "Resize iframe to banner dimensions";
+$GLOBALS['strIframeMakeTransparent']                = "Make the iframe transparent";
+$GLOBALS['strIframeIncludeNetscape4']               = "Include Netscape 4 compatible ilayer";
+$GLOBALS['strIframeGoogleClickTracking']            = "Include code to track Google AdSense clicks";
+
 
 // PopUp
-$GLOBALS['strPopUpStyle'] = "Pop-up type";
-$GLOBALS['strPopUpStylePopUp'] = "Pop-up";
-$GLOBALS['strPopUpStylePopUnder'] = "Pop-under";
-$GLOBALS['strPopUpCreateInstance'] = "Instance when the pop-up is created";
-$GLOBALS['strPopUpImmediately'] = "Immediately";
-$GLOBALS['strPopUpOnClose'] = "When the page is closed";
-$GLOBALS['strPopUpAfterSec'] = "After";
-$GLOBALS['strAutoCloseAfter'] = "Automatically close after";
-$GLOBALS['strPopUpTop'] = "Initial position (top)";
-$GLOBALS['strPopUpLeft'] = "Initial position (left)";
-$GLOBALS['strWindowOptions'] = "Window options";
-$GLOBALS['strShowToolbars'] = "Toolbars";
-$GLOBALS['strShowLocation'] = "Location";
-$GLOBALS['strShowMenubar'] = "Menubar";
-$GLOBALS['strShowStatus'] = "Status";
-$GLOBALS['strWindowResizable'] = "Resizable";
-$GLOBALS['strShowScrollbars'] = "Scrollbars";
+$GLOBALS['strPopUpStyle']			= "Pop-up type";
+$GLOBALS['strPopUpStylePopUp']			= "Pop-up";
+$GLOBALS['strPopUpStylePopUnder']		= "Pop-under";
+$GLOBALS['strPopUpCreateInstance']		= "Instance when the pop-up is created";
+$GLOBALS['strPopUpImmediately']			= "Immediately";
+$GLOBALS['strPopUpOnClose']			= "When the page is closed";
+$GLOBALS['strPopUpAfterSec']			= "After";
+$GLOBALS['strAutoCloseAfter']			= "Automatically close after";
+$GLOBALS['strPopUpTop']				= "Initial position (top)";
+$GLOBALS['strPopUpLeft']			= "Initial position (left)";
+$GLOBALS['strWindowOptions']		= "Window options";
+$GLOBALS['strShowToolbars']			= "Toolbars";
+$GLOBALS['strShowLocation']			= "Location";
+$GLOBALS['strShowMenubar']			= "Menubar";
+$GLOBALS['strShowStatus']			= "Status";
+$GLOBALS['strWindowResizable']		= "Resizable";
+$GLOBALS['strShowScrollbars']		= "Scrollbars";
+
 
 // XML-RPC
-$GLOBALS['strXmlRpcLanguage'] = "Host Language";
-$GLOBALS['strXmlRpcProtocol'] = "Use HTTPS to contact XML-RPC Server";
-$GLOBALS['strXmlRpcTimeout'] = "XML-RPC Timeout (Seconds)";
+$GLOBALS['strXmlRpcLanguage']       = "Host Language";
+$GLOBALS['strXmlRpcProtocol']       = "Use HTTPS to contact XML-RPC Server";
+$GLOBALS['strXmlRpcTimeout']        = "XML-RPC Timeout (Seconds)";
+
+
+// AdLayer
+
+
+
+
+
+
+
+
+
+	'geocities'		=> "Geocities",
+	'simple'		=> "Simple",
+	'cursor'		=> "Cursor",
+	'floater'		=> "Floater"
+);
 
 // Support for 3rd party server clicktracking
-$GLOBALS['str3rdPartyTrack'] = "Support 3rd Party Server Clicktracking";
+$GLOBALS['str3rdPartyTrack']		 = "Support 3rd Party Server Clicktracking";
 
 // Support for cachebusting code
-$GLOBALS['strCacheBuster'] = "Insert Cache-Busting code";
+$GLOBALS['strCacheBuster']		    = "Insert Cache-Busting code";
+
+// Non-Img creatives Warning for zone image-only invocation
+
+// unkown HTML tag type Warning for zone invocation
+
+// sql/web banner-type warning for clickonly zone invocation
+<br /> 1) Download the banner:";
 
 // IMG invocation selected for tracker with appended code
 $GLOBALS['strWarning'] = "Warning";
@@ -81,3 +107,5 @@ and that you have a config file for the domain of the site showing the ads (in M
 
 $GLOBALS['strIABNoteLocalInvocation'] = "<b>Note:</b> Impression data generated from using Local Mode invocation tags are not compliant with IAB guidelines for ad impression measurements.";
 $GLOBALS['strIABNoteXMLRPCInvocation'] = "<b>Note:</b> Impression data generated from using XML-RPC invocation tags are not compliant with IAB guidelines for ad impression measurements.";
+
+?>
