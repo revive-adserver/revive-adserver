1d441592d08bfa18a5818604a40801dbbabef910
Matteo Beccati <matteo@beccati.com>
2015-04-13 11:15:21 +0200

    Fix #515 - Create sql function on install/upgrade

---
+++ plugins_repo/openXVideoAds/plugins/etc/vastInlineBannerTypeHtml/etc/changes/postscript_videoads_1.11.1.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
++---------------------------------------------------------------------------+
+| Revive Adserver                                                           |
+| http://www.revive-adserver.com                                            |
+|                                                                           |
+| Copyright: See the COPYRIGHT.txt file.                                    |
+| License: GPLv2 or later, see the LICENSE.txt file.                        |
++---------------------------------------------------------------------------+
+*/
+
+require __DIR__.'/../../../oxLogVast/etc/postscript_install_oxLogVast.php';
