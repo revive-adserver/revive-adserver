c5635e02588aa98a0253842e6cfa1ed8930e266e
Atlassian Applications <noreply@beccati.com>
2015-07-20 08:01:40 +0200

    Updated translations from Crowdin

---
+++ lib/max/language/vi/settings.lang.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+  +---------------------------------------------------------------------------+
+  | Revive Adserver                                                           |
+  | http://www.revive-adserver.com                                            |
+  |                                                                           |
+  | Copyright: See the COPYRIGHT.txt file.                                    |
+  | License: GPLv2 or later, see the LICENSE.txt file.                        |
+  +---------------------------------------------------------------------------+
+ */
+
+// Installer translation strings
+
+
+
+
+
+
+
+/* ------------------------------------------------------- */
+/* Configuration translations                            */
+/* ------------------------------------------------------- */
+
+// Global
+$GLOBALS['strEditConfigNotPossible'] = "It is not possible to edit all settings because the configuration file is locked for security reasons. " .
+    "If you want to make changes, you may need to unlock the configuration file for this installation first.";
+$GLOBALS['strEditConfigPossible'] = "It is possible to edit all settings because the configuration file is not locked, but this could lead to security issues. " .
+    "If you want to secure your system, you need to lock the configuration file for this installation.";
+
+// Configuration Settings
+
+// Administrator Settings
+
+// Database Settings
+
+// Email Settings
+
+// Audit Trail Settings
+
+// Debug Logging Settings
+
+// Delivery Settings
+
+// General Settings
+
+// Geotargeting Settings
+
+// Interface Settings
+
+// Invocation Settings
+
+// Banner Delivery Settings
+
+// Banner Logging Settings
+
+// Banner Storage Settings
+
+// Campaign ECPM settings
+
+// Statistics & Maintenance Settings
+
+// UI Settings
+
+// Regenerate Platfor Hash script
+
+// Plugin Settings
