ab6fb3007d87f3537f2540b8d58d28136c984360
gnits <sting@prog.hu>
2016-01-02 23:26:25 +0100

    Small fix to the PHP 7 compatibility patch
    The patch didn't work correctly if the old MySQL extension was actually
    present, because in this case the is_resource_custom() function did not
    get defined at all.

--- lib/OA/DB-MySQLWrapper.php
+++ lib/OA/DB-MySQLWrapper.php
@@ -7,4 +7,11 @@ if (!function_exists('mysql_connect'))
 	require_once MAX_PATH . '/lib/mysql/MySQL_Functions.php';
 }
 
+if (!function_exists('is_resource_custom'))
+{
+	function is_resource_custom($resource)
+	{
+		return is_resource($resource);
+	}
+}
 
