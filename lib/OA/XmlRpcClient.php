edd3a62d0322bc26c491ff7f272a5093fab9cf38
Andrew Hill <andrew@fornax.net>
2016-01-18 20:29:58 +0000

    Fixed an issue with Revive Adserver failing to connect correctly to the sync service, which advises users about upgrades to the product. #662

--- lib/OA/XmlRpcClient.php
+++ lib/OA/XmlRpcClient.php
@@ -40,8 +40,8 @@ class OA_XML_RPC_Client extends XML_RPC_Client
             $this->hasOpenssl = in_array('openssl', $aExtensions);
         }
 
-        $this->verifyPeer = false;
-        $this->caFile     = MAX_PATH . '/etc/curl-ca-bundle.crt';
+        $this->verifyPeer = true;
+        $this->caFile     = RV_PATH . '/etc/curl-ca-bundle.crt';
         parent::__construct($path, $server, $port);
     }
 
