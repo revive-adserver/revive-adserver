deada97a0220e625ea96c0dbbe08516dd5270da0
Atlassian Applications <noreply@beccati.com>
2016-02-22 08:02:42 +0100

    Updated translations from Crowdin

--- lib/max/language/nl/settings.lang.php
+++ lib/max/language/nl/settings.lang.php
@@ -54,6 +54,8 @@ $GLOBALS['strUpgrade'] = "Upgrade";
 
 // Global
 $GLOBALS['strChooseSection'] = "Kies sectie";
+$GLOBALS['strEditConfigNotPossible'] = "Het is niet mogelijk om alle instellingen te bewerken omdat het configuratiebestand is vergrendeld om veiligheidsredenen. Indien u wijzigingen wil aanbrengen, moet u wellicht het configuratiebestand voor deze installatie eerst ontgrendelen.";
+$GLOBALS['strEditConfigPossible'] = "Het is mogelijk om alle instellingen te bewerken omdat het configuratiebestand niet isvergrendeld, wat tot beveiligingsproblemen zou kunnen. Indien u uw systeem wilt beveiligen, moet u het configurattiebestand van deze installatie vergrendelen.";
 $GLOBALS['strUnableToWriteConfig'] = "Wegschrijven van wijzigingen in de config file was niet mogelijk";
 $GLOBALS['strUnableToWritePrefs'] = "Wegschrijven van voorkeursinstellingen in de database was niet mogelijk";
 $GLOBALS['strImageDirLockedDetected'] = "De ingevoerde <b>Images Folder</b> is niet beschrijfbaar door de server. <br>U kunt niet verder gaan tot u ofwel de permissies van de folder heeft aangepast, of de betreffende folder heeft aangemaakt.";
