bca6c5e3fa66e45daf736e7416902ba493454b6f
Andrew Hill <a.hill@servicepower.com>
2017-04-21 16:00:09 +0930

    Improved the warning messages used when generating Local Mode and XML-RPC invocation tags to clarify why these tags are not compliant with IAB guidelines for ad impression measurement (i.e. the ad request comes from the server, not the client). #833

--- lib/max/language/en/invocation.lang.php
+++ lib/max/language/en/invocation.lang.php
@@ -79,5 +79,5 @@ is on the same physical machine as the adserver</span><br />
 Check that the MAX_PATH defined in the code below points to the base directory of your MAX installation<br />
 and that you have a config file for the domain of the site showing the ads (in MAX_PATH/var)";
 
-$GLOBALS['strIABNoteLocalInvocation'] = "<b>Note:</b> Impression data generated from using Local Mode invocation tags are not compliant with IAB guidelines for ad impression measurements.";
-$GLOBALS['strIABNoteXMLRPCInvocation'] = "<b>Note:</b> Impression data generated from using XML-RPC invocation tags are not compliant with IAB guidelines for ad impression measurements.";
+$GLOBALS['strIABNoteLocalInvocation'] = "<b>Note:</b> Local Mode invocation tags mean banner requests come from the web server, rather than the client. As a result, statistics are not compliant with IAB guidelines for ad impression measurement.";
+$GLOBALS['strIABNoteXMLRPCInvocation'] = "<b>Note:</b> XML-RPC invocation tags mean banner requests come from the web server, rather than the client. As a result, statistics are not compliant with IAB guidelines for ad impression measurement.";
