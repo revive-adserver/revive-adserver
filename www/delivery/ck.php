432248192a6d144e5229bf22c27ff43017cfbc6b
Post Commit <matteo@beccati.com>
2017-06-30 02:31:31 +0200

    Regenerated delivery scripts
    Commits:
     * Updated mechanism for testing if campaign/banner is active when clicks are processed, based on feedback in PR #850. Ticket #826.

--- www/delivery/ck.php
+++ www/delivery/ck.php
@@ -1220,7 +1220,8 @@ $query = "
         c.clickwindow AS clickwindow,
         c.viewwindow AS viewwindow,
         m.advertiser_limitation AS advertiser_limitation,
-        m.agencyid AS agency_id
+        m.agencyid AS agency_id,
+        c.status AS campaign_status
     FROM
         ".OX_escapeIdentifier($conf['table']['prefix'].$conf['table']['banners'])." AS d,
         ".OX_escapeIdentifier($conf['table']['prefix'].$conf['table']['campaigns'])." AS c,
@@ -3265,16 +3266,8 @@ $creativeId[$i] = 0;
 }
 if (($adId[$i] > 0 || $adId[$i] == -1) && ($conf['logging']['adClicks']) && !(isset($_GET['log']) && ($_GET['log'] == 'no'))) {
 if (isset($GLOBALS['conf']['logging']['blockInactiveBannerClicks'])) {
-$exitProcessing = true;
-$aZoneLinkedAds = MAX_cacheGetZoneLinkedAds($zoneId);
-$aAds = array_merge($aZoneLinkedAds['xAds'], $aZoneLinkedAds['ads'], $aZoneLinkedAds['lAds']);
-foreach ($aAds as $aAdInfo) {
-if ($adId[$i] == $aAdInfo['ad_id']) {
-$exitProcessing = false;
-break;
-}
-}
-if ($exitProcessing) {
+$aAdInfo = MAX_cacheGetAd($adId[$i]);
+if ($aAdInfo['status'] != 0 || $aAdInfo['campaign_status'] != 0) {
 return;
 }
 }
--- www/delivery/tv.php
+++ www/delivery/tv.php
@@ -1220,7 +1220,8 @@ $query = "
         c.clickwindow AS clickwindow,
         c.viewwindow AS viewwindow,
         m.advertiser_limitation AS advertiser_limitation,
-        m.agencyid AS agency_id
+        m.agencyid AS agency_id,
+        c.status AS campaign_status
     FROM
         ".OX_escapeIdentifier($conf['table']['prefix'].$conf['table']['banners'])." AS d,
         ".OX_escapeIdentifier($conf['table']['prefix'].$conf['table']['campaigns'])." AS c,
