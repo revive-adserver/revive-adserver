7d542efece7c87cb70ee43ca5ea4665a16093556
Matteo Beccati <matteo@beccati.com>
2017-10-02 16:43:41 +0200

    Fixed async on IE11 - ref #894

--- www/delivery_dev/async.js
+++ www/delivery_dev/async.js
@@ -3,6 +3,25 @@
 
     win.reviveAsync = win.reviveAsync || {};
 
+    /*
+     * IE backwards compatibility
+     */
+    (function (w) {
+
+        if ( typeof w.CustomEvent === "function" ) return false;
+
+        function CustomEvent ( event, params ) {
+            params = params || { bubbles: false, cancelable: false, detail: undefined };
+            var evt = document.createEvent( 'CustomEvent' );
+            evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+            return evt;
+        }
+
+        CustomEvent.prototype = w.Event.prototype;
+
+        w.CustomEvent = CustomEvent;
+    })(win);
+
     try {
         if (!win.reviveAsync.hasOwnProperty(ID)) {
             var rv = win.reviveAsync[ID] = {
