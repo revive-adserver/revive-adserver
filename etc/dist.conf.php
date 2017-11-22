c1e361abca9ef013e92c99c42677dc2468e97ad9
Matteo Beccati <matteo@beccati.com>
2017-11-22 16:08:49 +0100

    Fix #913 - Other Adsense tracking leftovers
    Also ref #855

--- etc/dist.conf.php
+++ etc/dist.conf.php
@@ -79,7 +79,6 @@ conversionvars                      = tv.php
 content                             = ac.php
 conversion                          = ti.php
 conversionjs                        = tjs.php
-google                              = ag.php
 frame                               = afr.php
 image                               = ai.php
 js                                  = ajs.php
