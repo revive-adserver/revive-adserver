9a4d3fd26553820a65ded8f9290f892c3824cd64
Matteo Beccati <matteo@beccati.com>
2018-06-28 09:22:57 +0200

    Fix #983 - Added en_US language

---
+++ lib/max/language/en_US/index.lang.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+  +---------------------------------------------------------------------------+
+  | Revive Adserver                                                           |
+  | http://www.revive-adserver.com                                            |
+  |                                                                           |
+  | Copyright: See the COPYRIGHT.txt file.                                    |
+  | License: GPLv2 or later, see the LICENSE.txt file.                        |
+  +---------------------------------------------------------------------------+
+ */
+
+// Meta information
+$translation_readable = "English (US)";
+$translation_maintainer = "Revive Adserver Team";
+$translation_contact = "noreply@revive-adserver.com";
