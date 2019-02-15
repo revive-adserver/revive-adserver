2627d677d3db3a73a73e8330221bccec2448d5bf
Jerome Brunel <jb.jeromebrunel@gmail.com>
2019-02-15 16:56:44 -0500

    Use 0 as width and height

--- www/delivery_dev/afr.php
+++ www/delivery_dev/afr.php
@@ -89,17 +89,21 @@ if (isset($refresh) && is_numeric($refresh) && $refresh > 0) {
 }
 
 if (isset($resize) && $resize == 1) {
+	// If no banner found, use 0 as width and height
+	$bannerWidth = empty($banner['width']) ? 0 : $banner['width'];
+	$bannerHeight = empty($banner['height']) ? 0 : $banner['height'];
+
 	$outputHtml .= "<script type='text/javascript'>\n";
 	$outputHtml .= "<!--// <![CDATA[ \n";
 	$outputHtml .= "\tfunction MAX_adjustframe(frame) {\n";
 	$outputHtml .= "\t\tif (document.all) {\n";
-    $outputHtml .= "\t\t\tparent.document.all[frame.name].width = ".$banner['width'].";\n";
-    $outputHtml .= "\t\t\tparent.document.all[frame.name].height = ".$banner['height'].";\n";
-  	$outputHtml .= "\t\t}\n";
-  	$outputHtml .= "\t\telse if (document.getElementById) {\n";
-    $outputHtml .= "\t\t\tparent.document.getElementById(frame.name).width = ".$banner['width'].";\n";
-    $outputHtml .= "\t\t\tparent.document.getElementById(frame.name).height = ".$banner['height'].";\n";
-  	$outputHtml .= "\t\t}\n";
+	$outputHtml .= "\t\t\tparent.document.all[frame.name].width = ".$bannerWidth.";\n";
+	$outputHtml .= "\t\t\tparent.document.all[frame.name].height = ".$bannerHeight.";\n";
+	$outputHtml .= "\t\t}\n";
+	$outputHtml .= "\t\telse if (document.getElementById) {\n";
+	$outputHtml .= "\t\t\tparent.document.getElementById(frame.name).width = ".$bannerWidth.";\n";
+	$outputHtml .= "\t\t\tparent.document.getElementById(frame.name).height = ".$bannerHeight.";\n";
+	$outputHtml .= "\t\t}\n";
 	$outputHtml .= "\t}\n";
 	$outputHtml .= "// ]]> -->\n";
 	$outputHtml .= "</script>\n";
