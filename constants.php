0c98116a29bcb19dccbc8ec49d3cbb9cafd7a17b
Matteo Beccati <matteo@beccati.com>
2019-05-20 14:41:46 +0200

    Preparing for 4.2.1

--- constants.php
+++ constants.php
@@ -22,7 +22,7 @@
 function setupConstants()
 {
     // Define this version of Revive Adserver's constants
-    define('VERSION', '4.3.0-dev');
+    define('VERSION', '4.2.1');
     define('PRODUCT_NAME', 'Revive Adserver');
     define('PRODUCT_URL', 'www.revive-adserver.com');
     define('PRODUCT_DOCSURL', 'http://documentation.revive-adserver.com');
