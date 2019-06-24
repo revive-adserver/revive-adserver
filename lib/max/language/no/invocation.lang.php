eebd56b77e2d890d2ef11461589b3b0e28017521
Atlassian Applications <noreply@beccati.com>
2019-06-24 08:00:42 +0200

    Updated translations from Crowdin

---
+++ lib/max/language/no/invocation.lang.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+  +---------------------------------------------------------------------------+
+  | Revive Adserver                                                           |
+  | http://www.revive-adserver.com                                            |
+  |                                                                           |
+  | Copyright: See the COPYRIGHT.txt file.                                    |
+  | License: GPLv2 or later, see the LICENSE.txt file.                        |
+  +---------------------------------------------------------------------------+
+ */
+
+// Other
+$GLOBALS['strCopyToClipboard'] = "Kopier til utklippstavlen";
+$GLOBALS['strCopy'] = "kopier";
+
+// Measures
+
+// Common Invocation Parameters
+$GLOBALS['strInvocationCampaignID'] = "Kampanje";
+$GLOBALS['strInvocationSource'] = "Kilde";
+
+// Iframe
+
+// PopUp
+$GLOBALS['strPopUpAfterSec'] = "Etter";
+$GLOBALS['strWindowOptions'] = "Vindusvalg";
+$GLOBALS['strShowToolbars'] = "Verktøylinjer";
+$GLOBALS['strShowLocation'] = "Posisjon";
+$GLOBALS['strShowMenubar'] = "Menybar";
+$GLOBALS['strShowStatus'] = "Status";
+
+// XML-RPC
+
+// Support for 3rd party server clicktracking
+
+// Support for cachebusting code
+
+// IMG invocation selected for tracker with appended code
+$GLOBALS['strWarning'] = "Advarsel";
+
+// Local Invocation
+
