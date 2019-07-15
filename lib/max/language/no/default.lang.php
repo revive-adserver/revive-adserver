a1598f3bb041710193df659abb604e63e8848bb6
Atlassian Applications <noreply@beccati.com>
2019-07-15 08:00:44 +0200

    Updated translations from Crowdin

--- lib/max/language/no/default.lang.php
+++ lib/max/language/no/default.lang.php
@@ -21,93 +21,260 @@
 /* Translations                                          */
 /* ------------------------------------------------------- */
 
+$GLOBALS['strHome'] = "Hjem";
 $GLOBALS['strHelp'] = "Hjelp";
 $GLOBALS['strStartOver'] = "Begynn forfra";
 $GLOBALS['strShortcuts'] = "Snarveier";
+$GLOBALS['strActions'] = "Handlinger";
+$GLOBALS['strAndXMore'] = "og %s til";
+$GLOBALS['strAdminstration'] = "Beholdning";
 $GLOBALS['strMaintenance'] = "Vedlikehold";
 $GLOBALS['strProbability'] = "Sannsynlighet";
 $GLOBALS['strInvocationcode'] = "Aktiveringskode";
+$GLOBALS['strBasicInformation'] = "Grunnleggende informasjon";
 $GLOBALS['strOverview'] = "Oversikt";
 $GLOBALS['strSearch'] = "<u>S</u>&oslash;k";
 $GLOBALS['strDetails'] = "Detaljer";
+$GLOBALS['strUpdateSettings'] = "Oppdater innstillinger";
+$GLOBALS['strCheckForUpdates'] = "Se etter oppdateringer";
+$GLOBALS['strCompact'] = "Kompakt";
 $GLOBALS['strUser'] = "Bruker";
 $GLOBALS['strDuplicate'] = "Dupliser";
+$GLOBALS['strCopyOf'] = "Kopi av";
 $GLOBALS['strMoveTo'] = "Flytt til";
 $GLOBALS['strDelete'] = "Slett";
 $GLOBALS['strActivate'] = "Aktiver";
 $GLOBALS['strConvert'] = "Konverter";
+$GLOBALS['strRefresh'] = "Oppdater";
 $GLOBALS['strSaveChanges'] = "Lagre endringer";
 $GLOBALS['strUp'] = "Opp";
 $GLOBALS['strDown'] = "Ned";
 $GLOBALS['strSave'] = "Lagre";
 $GLOBALS['strCancel'] = "Avbryt";
+$GLOBALS['strBack'] = "Tilbake";
 $GLOBALS['strPrevious'] = "Forrige";
 $GLOBALS['strNext'] = "Neste";
 $GLOBALS['strYes'] = "Ja";
 $GLOBALS['strNo'] = "Nei";
 $GLOBALS['strNone'] = "Ingen";
+$GLOBALS['strCustom'] = "Egendefinert";
+$GLOBALS['strDefault'] = "Standard";
+$GLOBALS['strUnknown'] = "Ukjent";
+$GLOBALS['strUnlimited'] = "Ubegrenset";
+$GLOBALS['strUntitled'] = "Uten navn";
+$GLOBALS['strAll'] = "alle";
 $GLOBALS['strAverage'] = "Gjennomsnittlig";
+$GLOBALS['strOverall'] = "Total";
 $GLOBALS['strTotal'] = "Totalt";
+$GLOBALS['strFrom'] = "Fra";
 $GLOBALS['strTo'] = "til";
+$GLOBALS['strAdd'] = "Legg til";
 $GLOBALS['strDaysLeft'] = "Dager igjen";
 $GLOBALS['strCheckAllNone'] = "Merk alle / ingen";
 $GLOBALS['strExpandAll'] = "<u>U</u>tvid alle";
 $GLOBALS['strShowAll'] = "Vis alle";
+$GLOBALS['strMiscellaneous'] = "Forskjellig";
+$GLOBALS['strCollectedAllStats'] = "All statistikk";
+$GLOBALS['strCollectedToday'] = "I dag";
+$GLOBALS['strCollectedYesterday'] = "I går";
+$GLOBALS['strCollectedThisWeek'] = "Denne uken";
+$GLOBALS['strCollectedLastWeek'] = "Forrige uke";
+$GLOBALS['strCollectedThisMonth'] = "Denne måneden";
+$GLOBALS['strCollectedLastMonth'] = "Forrige måned";
+$GLOBALS['strCollectedLast7Days'] = "Siste 7 dager";
+$GLOBALS['strCollectedSpecificDates'] = "Valgt dato";
+$GLOBALS['strValue'] = "Verdi";
+$GLOBALS['strWarning'] = "Advarsel";
+$GLOBALS['strNotice'] = "Merk";
 
 // Dashboard
 // Dashboard Errors
+$GLOBALS['strDashboardErrorCode'] = "kode";
+$GLOBALS['strDashboardSystemMessage'] = "Systemmelding";
 
 // Priority
+$GLOBALS['strPriority'] = "Prioritet";
+$GLOBALS['strPriorityLevel'] = "Prioritetsnivå";
+$GLOBALS['strLimitations'] = "Leveranseregler";
+$GLOBALS['strNoLimitations'] = "Ingen leveranseregler";
 
 // Properties
+$GLOBALS['strName'] = "Navn";
+$GLOBALS['strSize'] = "Størrelse";
+$GLOBALS['strWidth'] = "Bredde";
+$GLOBALS['strHeight'] = "Høyde";
+$GLOBALS['strTarget'] = "Mål";
+$GLOBALS['strLanguage'] = "Språk";
+$GLOBALS['strDescription'] = "Beskrivelse";
+$GLOBALS['strVariables'] = "Variabler";
+$GLOBALS['strID'] = "ID";
+$GLOBALS['strComments'] = "Kommentarer";
 
 // User access
+$GLOBALS['strSwitchTo'] = "Bytt til";
+$GLOBALS['strLinkUser'] = "Legg til bruker";
+$GLOBALS['strLinkUserHelpUser'] = "brukernavn";
+$GLOBALS['strLinkUserHelpEmail'] = "e-postadresse";
+$GLOBALS['strLastLoggedIn'] = "Sist innlogget";
 
 // Login & Permissions
+$GLOBALS['strUserAccess'] = "Brukertilgang";
+$GLOBALS['strAdminAccess'] = "Admin-tilgang";
+$GLOBALS['strUserProperties'] = "Bruker innstillinger";
+$GLOBALS['strPermissions'] = "Rettigheter";
+$GLOBALS['strAuthentification'] = "Autentisering";
+$GLOBALS['strWelcomeTo'] = "Velkommen til";
+$GLOBALS['strEnterUsername'] = "Angi brukernavn og passord for å logge inn";
+$GLOBALS['strEnterBoth'] = "Vennligst fyll inn både brukernavn og passord";
+$GLOBALS['strLogin'] = "Logg inn";
+$GLOBALS['strLogout'] = "Logg ut";
+$GLOBALS['strUsername'] = "Brukernavn";
+$GLOBALS['strPassword'] = "Passord";
+$GLOBALS['strPasswordRepeat'] = "Bekreft passord";
+$GLOBALS['strAccessDenied'] = "Tilgang nektet";
+$GLOBALS['strPasswordWrong'] = "Feil passord";
+$GLOBALS['strRepeatPassword'] = "Gjenta passord";
+$GLOBALS['strDeadLink'] = "Linken er ugyldig.";
 
 // General advertising
+$GLOBALS['strRequests'] = "Forespørsler";
+$GLOBALS['strClicks'] = "Klikk";
+$GLOBALS['strConversions'] = "Konverteringer";
+$GLOBALS['strBanners'] = "Bannere";
+$GLOBALS['strCampaigns'] = "Kampanjer";
+$GLOBALS['strCampaignID'] = "Kampanje ID";
+$GLOBALS['strCampaignName'] = "Kampanjenavn";
+$GLOBALS['strCountry'] = "Land";
+$GLOBALS['strStatsAction'] = "Handling";
+$GLOBALS['strStatsVariables'] = "Variabler";
 
 // Finance
 
 // Time and date related
+$GLOBALS['strDate'] = "Dato";
+$GLOBALS['strDay'] = "Dag";
+$GLOBALS['strDays'] = "Dager";
+$GLOBALS['strWeek'] = "Uke";
+$GLOBALS['strWeeks'] = "Uker";
+$GLOBALS['strSingleMonth'] = "Måned";
+$GLOBALS['strMonths'] = "Måneder";
+$GLOBALS['strDayOfWeek'] = "Ukedag";
 
 
 if (!isset($GLOBALS['strDayFullNames'])) {
     $GLOBALS['strDayFullNames'] = array();
 }
+$GLOBALS['strDayFullNames'][0] = 'Søndag';
+$GLOBALS['strDayFullNames'][1] = 'Mandag';
+$GLOBALS['strDayFullNames'][2] = 'Tirsdag';
+$GLOBALS['strDayFullNames'][3] = 'Onsdag';
+$GLOBALS['strDayFullNames'][4] = 'Torsdag';
+$GLOBALS['strDayFullNames'][5] = 'Fredag';
+$GLOBALS['strDayFullNames'][6] = 'Lørdag';
 
 if (!isset($GLOBALS['strDayShortCuts'])) {
     $GLOBALS['strDayShortCuts'] = array();
 }
+$GLOBALS['strDayShortCuts'][0] = 'Sø';
+$GLOBALS['strDayShortCuts'][1] = 'Ma';
+$GLOBALS['strDayShortCuts'][2] = 'Ti';
+$GLOBALS['strDayShortCuts'][3] = 'Ons';
+$GLOBALS['strDayShortCuts'][4] = 'Tors';
+$GLOBALS['strDayShortCuts'][5] = 'Fre';
+$GLOBALS['strDayShortCuts'][6] = 'Lø';
 
+$GLOBALS['strHour'] = "Time";
+$GLOBALS['strSeconds'] = "sekunder";
+$GLOBALS['strMinutes'] = "minutter";
 
 // Advertiser
+$GLOBALS['strClient'] = "Annonsør";
+$GLOBALS['strClients'] = "Annonsører";
+$GLOBALS['strAddClient'] = "Legg til ny annonsør";
+$GLOBALS['strClientProperties'] = "Annonsør innstillinger";
+$GLOBALS['strClientHistory'] = "Annonsør statistikk";
+$GLOBALS['strInactiveAdvertisersHidden'] = "inaktive annonsør(er) er skjult";
 
 // Advertisers properties
+$GLOBALS['strContact'] = "Kontakt";
 
 // Campaign
+$GLOBALS['strCampaign'] = "Kampanje";
+$GLOBALS['strCampaigns'] = "Kampanjer";
+$GLOBALS['strAddCampaign'] = "Start ny kampanje";
+$GLOBALS['strAddCampaign_Key'] = "Start <u>n</u>y kampanje";
+$GLOBALS['strHiddenCampaign'] = "Kampanje";
+$GLOBALS['strHiddenAd'] = "Annonse";
+$GLOBALS['strHiddenAdvertiser'] = "Annonsør";
+$GLOBALS['strHiddenWebsite'] = "Nettside";
 $GLOBALS['strHiddenZone'] = "Ingen";
 
 // Campaign-zone linking page
+$GLOBALS['strZonesSearch'] = "Søk";
+$GLOBALS['strZonesSearchTitle'] = "Søk i soner og nettsider etter navn";
+$GLOBALS['strAvailable'] = "Tilgjengelig";
+$GLOBALS['strShowing'] = "Viser";
+$GLOBALS['strEditZone'] = "Rediger sone";
+$GLOBALS['strEditWebsite'] = "Rediger nettside";
 
 
 // Campaign properties
+$GLOBALS['strDontExpire'] = "Ikke løp ut";
+$GLOBALS['strActivateNow'] = "Start umiddelbart";
+$GLOBALS['strSetSpecificDate'] = "Sett bestemt dato";
+$GLOBALS['strLow'] = "Lav";
+$GLOBALS['strHigh'] = "Høy";
+$GLOBALS['strExpirationDate'] = "Sluttdato";
 $GLOBALS['strCampaignStatusInactive'] = "aktive";
+$GLOBALS['strCampaignStatusRunning'] = "Kjører";
+$GLOBALS['strCampaignStatusPaused'] = "Pauset";
+$GLOBALS['strCampaignStatusAwaiting'] = "Avventer";
+$GLOBALS['strCampaignStatusExpired'] = "Fullført";
+$GLOBALS['strCampaignStatusApproval'] = "Avventer godkjenning »";
+$GLOBALS['strCampaignStatusRejected'] = "Avvist";
+$GLOBALS['strCampaignStatusAdded'] = "Lagt til";
+$GLOBALS['strCampaignStatusStarted'] = "Startet";
+$GLOBALS['strCampaignStatusRestarted'] = "Startet på nytt";
 $GLOBALS['strCampaignStatusDeleted'] = "Slett";
+$GLOBALS['strCampaignType'] = "Kampanjetype";
+$GLOBALS['strType'] = "Type";
+$GLOBALS['strContract'] = "Kontrakt";
+$GLOBALS['strOverride'] = "Overstyr";
+$GLOBALS['strStandardContract'] = "Kontrakt";
+$GLOBALS['strPricing'] = "Prising";
+$GLOBALS['strPricingModel'] = "Prismodell";
+$GLOBALS['strCampaignBanners'] = "Kampanjens bannere";
 
 // Tracker
 
 // Banners (General)
+$GLOBALS['strBanner'] = "Banner";
+$GLOBALS['strBanners'] = "Bannere";
+$GLOBALS['strAddBanner'] = "Legg til ny banner";
+$GLOBALS['strAddBanner_Key'] = "Add <u>n</u>y banner";
+$GLOBALS['strShowBanner'] = "Vis banner";
+$GLOBALS['strBannerProperties'] = "Banner valg";
+$GLOBALS['strBannerHistory'] = "Banner statistikk";
+$GLOBALS['strShowParentCampaigns'] = "Vis foreldre-kampanjer";
+$GLOBALS['strHideParentCampaigns'] = "Skjul foreldre-kampanjer";
+$GLOBALS['strHideInactiveBanners'] = "Skjul inaktive bannere";
+$GLOBALS['strInactiveBannersHidden'] = "inaktive banner(e) er skjult";
 
 // Banner Preferences
+$GLOBALS['strBannerPreferences'] = "Bannerinnstillinger";
+$GLOBALS['strCampaignPreferences'] = "Kampanje innstillinger";
+$GLOBALS['strDefaultBanners'] = "Standard bannere";
+$GLOBALS['strAllowedBannerTypes'] = "Tillatte banner-typer";
 
 // Banner (Properties)
+$GLOBALS['strChooseBanner'] = "Velg type banner";
 
 // Banner (advanced)
 
 // Banner (swf)
 
 // Display Delviery Rules
+$GLOBALS['strSource'] = "Kilde";
 
 
 if (!isset($GLOBALS['strCappingBanner'])) {
@@ -123,10 +290,13 @@ if (!isset($GLOBALS['strCappingZone'])) {
 }
 
 // Website
+$GLOBALS['strAffiliate'] = "Nettside";
 
 // Website (properties)
+$GLOBALS['strWebsite'] = "Nettside";
 
 // Website (properties - payment information)
+$GLOBALS['strCountry'] = "Land";
 
 // Website (properties - other information)
 
@@ -141,16 +311,25 @@ $GLOBALS['strZones'] = "Ingen";
 
 // Linked banners/campaigns/trackers
 $GLOBALS['strStatusDuplicate'] = "Dupliser";
+$GLOBALS['strConnectionType'] = "Type";
 
 // Statistics
+$GLOBALS['strStats'] = "Statistikk";
+$GLOBALS['strBreakdownByDay'] = "Dag";
+$GLOBALS['strBreakdownByWeek'] = "Uke";
+$GLOBALS['strBreakdownByMonth'] = "Måned";
+$GLOBALS['strBreakdownByDow'] = "Ukedag";
+$GLOBALS['strBreakdownByHour'] = "Time";
 
 // Expiration
 
 // Reports
 
 // Admin_UI_Fields
+$GLOBALS['strAllAdvertisers'] = "Alle annonsører";
 
 // Userlog
+$GLOBALS['strAction'] = "Handling";
 
 // Code generation
 
@@ -160,8 +339,11 @@ $GLOBALS['strStatusDuplicate'] = "Dupliser";
 //Validation
 
 // Email
+$GLOBALS['strMailSubject'] = "Annonsør rapport";
+$GLOBALS['strYourCampaign'] = "Din kampanje";
 
 // Priority
+$GLOBALS['strPriority'] = "Prioritet";
 
 // Preferences
 $GLOBALS['strPreferences'] = "Innstillinger";
@@ -169,6 +351,8 @@ $GLOBALS['strPreferences'] = "Innstillinger";
 // Long names
 
 // Short names
+$GLOBALS['strID_short'] = "ID";
+$GLOBALS['strClicks_short'] = "Klikk";
 
 // Global Settings
 
@@ -179,6 +363,7 @@ $GLOBALS['strPreferences'] = "Innstillinger";
 // Channels
 
 // Tracker Variables
+$GLOBALS['strVariableDescription'] = "Beskrivelse";
 
 // Password recovery
 
@@ -187,6 +372,8 @@ $GLOBALS['strPreferences'] = "Innstillinger";
 // Widget - Audit
 
 // Widget - Campaign
+$GLOBALS['strCampaignGoTo'] = "Gå til kampanjesiden";
+$GLOBALS['strCampaignSetUp'] = "Sett opp en kampanje i dag";
 
 
 
