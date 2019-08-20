eeeb5f913881ae942ca38f8a896444caa2a5585a
Andrew Hill <andrew@fornax.net>
2019-08-20 13:42:31 +0930

    #1051 - Display "Last Login Date" in user list
    Extended the screens for managing user access to accounts to display the most recent login time of users. Includes an update to the RV_Admin_DateTimeFormat class to support formatting of Dates and Times that are already in the user's locale timezone (i.e. not in UTC).

--- lib/RV/Admin/DateTimeFormat.php
+++ lib/RV/Admin/DateTimeFormat.php
@@ -56,7 +56,7 @@ class RV_Admin_DateTimeFormat
      */
     public static function formatUTCDate($sDateTime)
     {
-        // Convert the date string to a PEAR Date object in the user
+        // Convert the date/time string to a PEAR Date object in the user
         // locale's timezone
         $oDate = RV_Admin_DateTimeFormat::_convertUTCtoUserTZ($sDateTime);
         // Obtain the globally set $date_format value from the user's locale
@@ -79,9 +79,69 @@ class RV_Admin_DateTimeFormat
      */
     public static function formatUTCTime($sDateTime)
     {
-        // Convert the time string to a PEAR Date object in the user
+        // Convert the time date/string to a PEAR Date object in the user
         // locale's timezone
         $oTime = RV_Admin_DateTimeFormat::_convertUTCtoUserTZ($sDateTime);
+        // Obtain the globally set $time_format value from the user's locale
+        // which defines how the time output string should be formatted
+        global $time_format;
+        // Return the converted PEAR Date object in the appropriate locale
+        // format for time values
+        return $oTime->format("$time_format");
+    }
+
+    /**
+     * A method that takes a date/time string and formats it using the user's
+     * locale information - i.e. using the appropriate date/time representation.
+     *
+     * @param string dateTime The date/time to be formatted, in a string format
+     * @return string The date/time in the user's locale format
+     */
+    public static function formatDateTime($sDateTime)
+    {
+        // Convert the date/time string to a PEAR Date object
+        $oDateTime = new Date($sDateTime);
+        // Obtain the globally set $date_format and $time_format values
+        // from the user's locale which defines how the date/time output
+        // string should be formatted
+        global $date_format, $time_format;
+        // Return the converted PEAR Date object in the appropriate locale
+        // format for date/time values
+        return $oDateTime->format("$date_format $time_format");
+    }
+
+    /**
+     * A method that takes a date/time string and formats it using the user's
+     * locale information - i.e. using the appropriate date representation.
+     *
+     * @param string dateTime The date/time to be formatted, in a string format
+     * @return string The date/time formatted as a date only, in the user's
+     *                locale format
+     */
+    public static function formatDate($sDateTime)
+    {
+        // Convert the date/time string to a PEAR Date object
+        $oDate = new Date($sDateTime);
+        // Obtain the globally set $date_format value from the user's locale
+        // which defines how the date output string should be formatted
+        global $date_format;
+        // Return the converted PEAR Date object in the appropriate locale
+        // format for date values
+        return $oDate->format("$date_format");
+    }
+
+    /**
+     * A method that takes a date/time string and formats it using the user's
+     * locale information - i.e. using the appropriate time representation.
+     *
+     * @param string dateTime The date/time to be formatted, in a string format
+     * @return string The date/time formatted as a time only, in the user's
+     *                locale format
+     */
+    public static function formatTime($sDateTime)
+    {
+        // Convert the date/time string to a PEAR Date object
+        $oTime = new Date($sDateTime);
         // Obtain the globally set $time_format value from the user's locale
         // which defines how the time output string should be formatted
         global $time_format;
