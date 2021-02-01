c0c94d1440023ea87746fa062ae918b88a0ed492
Matteo Beccati <matteo@beccati.com>
2021-02-01 15:31:03 +0100

    Fix #1231 IP address resolution w/ proxy port number

--- lib/max/Delivery/remotehost.php
+++ lib/max/Delivery/remotehost.php
@@ -86,30 +86,41 @@ function MAX_remotehostProxyLookup()
                 'HTTP_X_FORWARDED_FOR',
                 'HTTP_CLIENT_IP'
             );
+
             foreach ($aHeaders as $header) {
                 if (!empty($_SERVER[$header])) {
                     $ip = $_SERVER[$header];
                     break;
                 }
             }
+
             if (!empty($ip)) {
                 // The "remote IP" may be a list, ensure that
                 // only the FIRST non-private value is used in that case
                 // See http://en.wikipedia.org/wiki/X-Forwarded-For#Format
                 foreach (explode(',', $ip) as $ip) {
-                    $ip = trim($ip);
-                    // If the found address is not unknown or a private network address
-                    if (($ip != 'unknown') && (!MAX_remotehostPrivateAddress($ip))) {
-                        // Set the "real" remote IP address, and unset
-                        // the remote host (as it will be wrong for the
-                        // newly found IP address) and HTTP_VIA header
-                        // (so that we don't accidently do this twice)
-                        $_SERVER['REMOTE_ADDR'] = $ip;
-                        $_SERVER['REMOTE_HOST'] = '';
-                        $_SERVER['HTTP_VIA']    = '';
-                        OX_Delivery_logMessage('real address set to '.$ip, 7);
-                        break;
+                    // Allow IPv4, stripping port number, if any
+                    if (!preg_match('#^(\d+\.\d+\.\d+\.\d+)(?::\d+)?$#D', trim($ip), $m)) {
+                        continue;
                     }
+
+                    $ip = $m[1];
+
+                    // If the found address is not unknown or a private network address
+                    if (MAX_remotehostPrivateAddress($ip)) {
+                        continue;
+                    }
+
+                    // Set the "real" remote IP address, and unset
+                    // the remote host (as it will be wrong for the
+                    // newly found IP address) and HTTP_VIA header
+                    // (so that we don't accidently do this twice)
+                    $_SERVER['REMOTE_ADDR'] = $ip;
+                    $_SERVER['REMOTE_HOST'] = '';
+                    $_SERVER['HTTP_VIA']    = '';
+                    OX_Delivery_logMessage('real address set to '.$ip, 7);
+
+                    return;
                 }
             }
         }
