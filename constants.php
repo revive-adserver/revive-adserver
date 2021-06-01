cbda861aedafdf139eb158843f1c20cd1474b6ca
Matteo Beccati <matteo@beccati.com>
2021-06-01 12:18:19 +0200

    Preparing for 5.2.1

--- constants.php
+++ constants.php
@@ -22,7 +22,7 @@
 function setupConstants()
 {
     // Define this version of Revive Adserver's constants
-    define('VERSION', '5.2.1-dev');
+    define('VERSION', '5.2.1');
     define('PRODUCT_NAME', 'Revive Adserver');
     define('PRODUCT_URL', 'www.revive-adserver.com');
     define('PRODUCT_DOCSURL', 'http://documentation.revive-adserver.com');
