58e22426b2be644819e61fc3d0edcb0b787e23ae
Matteo Beccati <matteo@beccati.com>
2021-07-26 08:00:29 +0000

    Updated translations from Crowdin

---
+++ lib/max/language/ca/invocation.lang.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+  +---------------------------------------------------------------------------+
+  | Revive Adserver                                                           |
+  | http://www.revive-adserver.com                                            |
+  |                                                                           |
+  | Copyright: See the COPYRIGHT.txt file.                                    |
+  | License: GPLv2 or later, see the LICENSE.txt file.                        |
+  +---------------------------------------------------------------------------+
+ */
+
+// Other
+$GLOBALS['strCopyToClipboard'] = "Copia al porta-retalls";
+$GLOBALS['strCopy'] = "copia";
+
+// Measures
+
+// Common Invocation Parameters
+$GLOBALS['strInvocationSource'] = "Font";
+
+// Iframe
+
+// PopUp
+$GLOBALS['strPopUpImmediately'] = "Immediatament";
+$GLOBALS['strPopUpAfterSec'] = "Després de";
+$GLOBALS['strShowStatus'] = "Estat";
+
+// Support for cachebusting code
+
+// IMG invocation selected for tracker with appended code
+$GLOBALS['strWarning'] = "Advertència";
