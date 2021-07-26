eeebda5961b485612a1f979521db92885d3bd67e
Matteo Beccati <matteo@beccati.com>
2021-07-26 08:38:42 +0000

    Updated translations from Crowdin

---
+++ lib/max/language/ca/maintenance.lang.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+  +---------------------------------------------------------------------------+
+  | Revive Adserver                                                           |
+  | http://www.revive-adserver.com                                            |
+  |                                                                           |
+  | Copyright: See the COPYRIGHT.txt file.                                    |
+  | License: GPLv2 or later, see the LICENSE.txt file.                        |
+  +---------------------------------------------------------------------------+
+ */
+
+// Main strings
+
+// Maintenance
+
+
+
+
+
+
+
+
+// Priority
+
+// Banner cache
+$GLOBALS['strBannerCacheRebuildButton'] = "Reconstrueix";
+
+// Cache
+
+// Storage
+$GLOBALS['strStorage'] = "Emmagatzematge";
+
+// Security
+$GLOBALS['strSecurity'] = "Seguretat";
+
+// Encoding
+$GLOBALS['strEncoding'] = "Codificació";
+
+// Product Updates
+$GLOBALS['strAvailableUpdates'] = "Actualitzacions disponibles";
+$GLOBALS['strDownloadZip'] = "Descarrega (.zip)";
+$GLOBALS['strDownloadGZip'] = "Descarrega (.tar.gz)";
+
+
+
+
+
+
+
+
+
+
+
+//  Deliver Limitations
+
+//  Append codes
+
+$GLOBALS['strPlugins'] = "Extensions";
+
+$GLOBALS['strMenus'] = "Menús";
