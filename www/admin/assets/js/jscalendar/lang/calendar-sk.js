b06f319a72ef13cfae4fbc5b6be44c2d1d085e2f
Matteo Beccati <matteo@beccati.com>
2025-04-02 11:56:11 +0200

    [ci skip] Fixed broken translation file

--- www/admin/assets/js/jscalendar/lang/calendar-sk.js
+++ www/admin/assets/js/jscalendar/lang/calendar-sk.js
@@ -2,20 +2,20 @@
 
 // Calendar SK language
 // Author: Peter Valach (pvalach@gmx.net)
-// Encoding: utf-8
+// Encoding: UTF-8
 // Last update: 2003/10/29
 // Distributed under the same terms as the calendar itself.
 
 // full day names
 Calendar._DN = new Array
-("NedeÄľa",
+("Nedeľa",
  "Pondelok",
  "Utorok",
  "Streda",
- "Ĺ tvrtok",
+ "Ĺtvrtok",
  "Piatok",
  "Sobota",
- "NedeÄľa");
+ "Nedeľa");
 
 // short day names
 Calendar._SDN = new Array
@@ -23,20 +23,20 @@ Calendar._SDN = new Array
  "Pon",
  "Uto",
  "Str",
- "Ĺ tv",
+ "Ĺtv",
  "Pia",
  "Sob",
  "Ned");
 
 // full month names
 Calendar._MN = new Array
-("JanuĂˇr",
- "FebruĂˇr",
+("Január",
+ "Február",
  "Marec",
- "AprĂ­l",
- "MĂˇj",
- "JĂşn",
- "JĂşl",
+ "Apríl",
+ "Máj",
+ "Jún",
+ "Júl",
  "August",
  "September",
  "OktĂłber",
@@ -49,9 +49,9 @@ Calendar._SMN = new Array
  "Feb",
  "Mar",
  "Apr",
- "MĂˇj",
- "JĂşn",
- "JĂşl",
+ "Máj",
+ "Jún",
+ "Júl",
  "Aug",
  "Sep",
  "Okt",
@@ -60,40 +60,40 @@ Calendar._SMN = new Array
 
 // tooltips
 Calendar._TT = {};
-Calendar._TT["INFO"] = "O kalendĂˇri";
+Calendar._TT["INFO"] = "O kalendári";
 
 Calendar._TT["ABOUT"] =
 "DHTML Date/Time Selector\n" +
 "(c) dynarch.com 2002-2005 / Author: Mihai Bazon\n" +
-"PoslednĂş verziu nĂˇjdete na: http://www.dynarch.com/projects/calendar/\n" +
-"DistribuovanĂ© pod GNU LGPL.  ViÄŹ http://gnu.org/licenses/lgpl.html pre detaily." +
+"Poslednú verziu nájdete na: http://www.dynarch.com/projects/calendar/\n" +
+"Distribuované pod GNU LGPL.  ViÄŹ http://gnu.org/licenses/lgpl.html pre detaily." +
 "\n\n" +
-"VĂ˝ber dĂˇtumu:\n" +
-"- PouĹľite tlaÄŤidlĂˇ \xab, \xbb pre vĂ˝ber roku\n" +
-"- PouĹľite tlaÄŤidlĂˇ " + String.fromCharCode(0x2039) + ", " + String.fromCharCode(0x203a) + " pre vĂ˝ber mesiaca\n" +
-"- Ak ktorĂ©koÄľvek z tĂ˝chto tlaÄŤidiel podrĹľĂ­te dlhĹˇie, zobrazĂ­ sa rĂ˝chly vĂ˝ber.";
+"Výber dátumu:\n" +
+"- Použite tlačidlá \xab, \xbb pre výber roku\n" +
+"- Použite tlačidlá " + String.fromCharCode(0x2039) + ", " + String.fromCharCode(0x203a) + " pre výber mesiaca\n" +
+"- Ak ktorékoľvek z týchto tlačidiel podržíte dlhšie, zobrazí sa rýchly výber.";
 Calendar._TT["ABOUT_TIME"] = "\n\n" +
-"VĂ˝ber ÄŤasu:\n" +
-"- Kliknutie na niektorĂş poloĹľku ÄŤasu ju zvĂ˝Ĺˇi\n" +
-"- Shift-klik ju znĂ­Ĺľi\n" +
-"- Ak podrĹľĂ­te tlaÄŤĂ­tko stlaÄŤenĂ©, posĂşvanĂ­m menĂ­te hodnotu.";
+"Výber času:\n" +
+"- Kliknutie na niektorú položku času ju zvýši\n" +
+"- Shift-klik ju zníži\n" +
+"- Ak podržíte tlačítko stlačené, posúvaním meníte hodnotu.";
 
-Calendar._TT["PREV_YEAR"] = "PredoĹˇlĂ˝ rok (podrĹľte pre menu)";
-Calendar._TT["PREV_MONTH"] = "PredoĹˇlĂ˝ mesiac (podrĹľte pre menu)";
-Calendar._TT["GO_TODAY"] = "PrejsĹĄ na dneĹˇok";
-Calendar._TT["NEXT_MONTH"] = "Nasl. mesiac (podrĹľte pre menu)";
-Calendar._TT["NEXT_YEAR"] = "Nasl. rok (podrĹľte pre menu)";
-Calendar._TT["SEL_DATE"] = "ZvoÄľte dĂˇtum";
-Calendar._TT["DRAG_TO_MOVE"] = "PodrĹľanĂ­m tlaÄŤĂ­tka zmenĂ­te polohu";
+Calendar._TT["PREV_YEAR"] = "Predošlý rok (podržte pre menu)";
+Calendar._TT["PREV_MONTH"] = "Predošlý mesiac (podržte pre menu)";
+Calendar._TT["GO_TODAY"] = "Prejsť na dnešok";
+Calendar._TT["NEXT_MONTH"] = "Nasl. mesiac (podržte pre menu)";
+Calendar._TT["NEXT_YEAR"] = "Nasl. rok (podržte pre menu)";
+Calendar._TT["SEL_DATE"] = "Zvoľte dátum";
+Calendar._TT["DRAG_TO_MOVE"] = "Podržaním tlačítka zmeníte polohu";
 Calendar._TT["PART_TODAY"] = " (dnes)";
-Calendar._TT["MON_FIRST"] = "ZobraziĹĄ pondelok ako prvĂ˝";
-Calendar._TT["SUN_FIRST"] = "ZobraziĹĄ nedeÄľu ako prvĂş";
-Calendar._TT["CLOSE"] = "ZavrieĹĄ";
+Calendar._TT["MON_FIRST"] = "Zobraziť pondelok ako prvý";
+Calendar._TT["SUN_FIRST"] = "Zobraziť nedeľu ako prvú";
+Calendar._TT["CLOSE"] = "Zavrieť";
 Calendar._TT["TODAY"] = "Dnes";
-Calendar._TT["TIME_PART"] = "(Shift-)klik/ĹĄahanie zmenĂ­ hodnotu";
+Calendar._TT["TIME_PART"] = "(Shift-)klik/ťahanie zmení hodnotu";
 
 // date formats
 Calendar._TT["DEF_DATE_FORMAT"] = "$d. %m. %Y";
 Calendar._TT["TT_DATE_FORMAT"] = "%a, %e. %b";
 
-Calendar._TT["WK"] = "tĂ˝Ĺľ";
+Calendar._TT["WK"] = "týž";
