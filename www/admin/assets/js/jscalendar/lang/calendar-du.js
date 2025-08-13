9558c953d0e0ad20243333dc44fb39df2a0f0616
Erik Geurts <erik.geurts@gmail.com>
2025-08-13 16:12:25 +0200

    Update calendar-du.js - fix localisations

--- www/admin/assets/js/jscalendar/lang/calendar-du.js
+++ www/admin/assets/js/jscalendar/lang/calendar-du.js
@@ -1,40 +1,67 @@
 // ** I18N
 Calendar._DN = new Array
-("Zondag",
- "Maandag",
- "Dinsdag",
- "Woensdag",
- "Donderdag",
- "Vrijdag",
- "Zaterdag",
- "Zondag");
+("zondag",
+ "maandag",
+ "dinsdag",
+ "woensdag",
+ "donderdag",
+ "vrijdag",
+ "zaterdag",
+ "zondag");
+
+// short day names
+Calendar._SDN = new Array
+("zo",
+ "ma",
+ "di",
+ "wo",
+ "do",
+ "vr",
+ "za",
+ "zo");
+
 Calendar._MN = new Array
-("Januari",
- "Februari",
- "Maart",
- "April",
- "Mei",
- "Juni",
- "Juli",
- "Augustus",
- "September",
- "Oktober",
- "November",
- "December");
+("januari",
+ "februari",
+ "maart",
+ "april",
+ "mei",
+ "juni",
+ "juli",
+ "augustus",
+ "september",
+ "oktober",
+ "november",
+ "december");
+
+// short month names
+Calendar._SMN = new Array
+("jan",
+ "feb",
+ "maa",
+ "apr",
+ "mei",
+ "jun",
+ "jul",
+ "aug",
+ "sep",
+ "okt",
+ "nov",
+ "dec");
 
 // tooltips
 Calendar._TT = {};
-Calendar._TT["TOGGLE"] = "Toggle startdag van de week";
+Calendar._TT["TOGGLE"] = "Verander startdag van de week";
 Calendar._TT["PREV_YEAR"] = "Vorig jaar (indrukken voor menu)";
-Calendar._TT["PREV_MONTH"] = "Vorige month (indrukken voor menu)";
-Calendar._TT["GO_TODAY"] = "Naar Vandaag";
-Calendar._TT["NEXT_MONTH"] = "Volgende Maand (indrukken voor menu)";
+Calendar._TT["PREV_MONTH"] = "Vorige maand (indrukken voor menu)";
+Calendar._TT["GO_TODAY"] = "Naar vandaag";
+Calendar._TT["NEXT_MONTH"] = "Volgende maand (indrukken voor menu)";
 Calendar._TT["NEXT_YEAR"] = "Volgend jaar (indrukken voor menu)";
 Calendar._TT["SEL_DATE"] = "Selecteer datum";
 Calendar._TT["DRAG_TO_MOVE"] = "Sleep om te verplaatsen";
 Calendar._TT["PART_TODAY"] = " (vandaag)";
-Calendar._TT["MON_FIRST"] = "Toon Maandag eerst";
-Calendar._TT["SUN_FIRST"] = "Toon Zondag eerst";
+Calendar._TT["MON_FIRST"] = "Toon maandag eerst";
+Calendar._TT["SUN_FIRST"] = "Toon zondag eerst";
 Calendar._TT["CLOSE"] = "Sluiten";
 Calendar._TT["TODAY"] = "Vandaag";
 
