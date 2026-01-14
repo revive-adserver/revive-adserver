revive-adserver/revive-adserver

commit e245a88549c41dd61dfce91db9c1f004a27135a9

Fix h1 report 3470970

www/admin/banner-acl.php
@@ -45,12 +45,12 @@ $pageName = basename($_SERVER['SCRIPT_NAME']);
 $tabindex = 1;
 $aEntities = ['clientid' => $clientid, 'campaignid' => $campaignid, 'bannerid' => $bannerid];
 
+$acl = MAX_AclsRemap($acl ?? []);
+
 if (!empty($action)) {
     $acl = MAX_AclAdjust($acl, $action);
 } elseif (!empty($submit)) {
     OA_Permission::checkSessionToken();
-
-    $acl = MAX_AclsRemap($acl ??= []);
 
     // Only save when inputs are valid
     if (OX_AclCheckInputsFields($acl, $pageName) === true) {

www/admin/channel-acl.php
@@ -50,6 +50,8 @@ if (!empty($affiliateid)) {
     $aOtherChannels = Admin_DA::getChannels(['agency_id' => $agencyId, 'channel_type' => 'agency']);
 }
 
+$acl = MAX_AclsRemap($acl ?? []);
+
 /*-------------------------------------------------------*/
 /* Process submitted form                                */
 /*-------------------------------------------------------*/
@@ -61,8 +63,6 @@ if (!empty($action)) {
     $acl = MAX_AclAdjust($acl, $action);
 } elseif (!empty($submit)) {
     OA_Permission::checkSessionToken();
-
-    $acl = MAX_AclsRemap($acl ??= []);
 
     // Only save when inputs are valid
     if (OX_AclCheckInputsFields($acl, $pageName) === true) {

commit 0ebc96d1968e4f7b68964e2823059b8c828febb2

Fix bug with limitations not being displayed
Introduced in e245a885

www/admin/banner-acl.php
@@ -126,7 +126,7 @@ MAX_displayNavigationBanner($pageName, $aOtherCampaigns, $aOtherBanners, $aEntit
 
 $aBanner = MAX_cacheGetAd($bannerid, false);
 
-if (!isset($acl)) {
+if (empty($acl)) {
     $acl = Admin_DA::getDeliveryLimitations(['ad_id' => $bannerid]);
     // This array needs to be sorted by executionorder, this should ideally be done in SQL
     // When we move to DataObject this should be addressed

www/admin/channel-acl.php
@@ -96,7 +96,8 @@ if (!empty($action)) {
 MAX_displayNavigationChannel($pageName, $aOtherChannels, $aEntities);
 
 $aChannel = Admin_DA::getChannel($channelid);
-if (!isset($acl)) {
+
+if (empty($acl)) {
     $acl = Admin_DA::getChannelLimitations(['channel_id' => $channelid]);
     // This array needs to be sorted by executionorder, this should ideally be done in SQL
     // When we move to DataObject this should be addressed